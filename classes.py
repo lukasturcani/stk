2ba716ca602f21389314588f4cc1620efa812290
Lukas Turcani <lukasturcani93@gmail.com>
2016-07-05 07:05:23 +0100

    Added doc strings to a  bunch of methods in the Population class. A couple of methods still missing them.

--- classes.py
+++ classes.py
@@ -112,17 +112,22 @@ class Population:
 
     This is the central class of MMEA. The GA is invoked by calling the
     ``gen_offspring``, ``gen_mutants`` and ``select`` methods of this 
-    class on a given instance. However, this class is a container for ``Cage`` and other 
+    class on a given instance. However, this class is a container of ``Cage`` and other 
     ``Population`` instances first and foremost. It delegates GA operations to
     its `ga_tools` attribute. Any functionality related to the GA should be 
-    delegated to this attribute, as done with the aforementioned methods. 
-    A comphrehensive account of how this delegation is implemented is provided
-    in the module level docstring.
+    delegated to this attribute. The ``gen_offspring`` and ``gen_mutants``
+    methods can serve as a guide to how this should be done.  
+    A comphrehensive account of how the interaction between these two
+    classes is provided in the developer's guide.
     
     For consistency and maintainability, collections ``Cage`` or ``Population`` instances should always be placed
     in a ``Population`` instance. As a result, any function which should return
     multiple ``Cage`` or ``Population`` instances can be expected
-    to return a single ``Population`` instance holding the desired instances.     
+    to return a single ``Population`` instance holding the desired instances.
+    Some functions will have to return the population organized in a specific way.
+    For example, functions generating the parent pool will generate a population
+    with no direct members but multiple subpopulations of 2 members each. More specific
+    guidelines are provided within the ``Mating`` class.
     
     The only operations directly addressed by this class and definined within
     it are those relevant to its role as a container. It supports
@@ -163,7 +168,7 @@ class Population:
         
         This initializer creates a new population from the 
         ``Population`` and ``Cage`` instances given as arguments.
-        It also accepts a single, optional ``GATools`` instance if the 
+        It also accepts a single, optional, ``GATools`` instance if the 
         population is to have GA operations performed on it.
         
         The arguments can be provided in any order regardless of type.
@@ -185,9 +190,17 @@ class Population:
             
         """    
         
+        # Generate `populations` and `members` attributes.
         self.populations = []
         self.members = []
     
+        # Determine type of supplied arguments and place in the
+        # appropriate attribute.  ``Population`` types added to
+        # `populations` attribute, ``Cage`` into `members` and if
+        # ``GATools`` is supplied it is placed into `ga_tools`.
+        # Raise a ``TypeError`` if more than 1 ``GATools`` argument
+        # was supplied or if an argument was not ``GATools``, ``Cage`` 
+        # or ``Population`` type.
         for arg in args:
             if type(arg) is Population:
                 self.populations.append(arg)
@@ -205,64 +218,383 @@ class Population:
                              " initialized with 'Population',"
                              " 'Cage' and 1 'GATools' type."))
                                     
-    def all_members(self):       
+    def all_members(self):
+        """
+        Yields all members in the population and its subpopulations.
+
+        Yields
+        ------
+        Cage
+            The next ``Cage`` instance held within the population or its
+            subpopulations.
+        
+        """
+        
+        # Go through `members` attribute and yield ``Cage`` instances
+        # held within one by one.
         for ind in self.members:
             yield ind
         
+        # Go thorugh `populations` attribute and for each ``Population``
+        # instance within, yield ``Cage`` instances from its
+        # `all_members()` generator.
         for pop in self.populations:
             yield from pop.all_members()
             
     def add_members(self, *args, duplicates=False):
+        """
+        Adds ``Cage`` or ``Population`` instances into `members`.        
+        
+        Any number of ``Cage`` or ``Population`` instances can be
+        supplied to this method. ``Cage`` instances are added into the
+        `members` attribute directly. In the case of ``Population`` 
+        instances, the ``Cage`` instances held within it are added into 
+        the `members` attribute. The supplied ``Population`` instance 
+        itself is not added. This means that any information the 
+        ``Population`` instance had about subpopulations is lost. This 
+        is because all of its ``Cage`` instances are added into 
+        `members` attribute equally, regardless of which subpopulation 
+        they were originally in.
+
+        The `duplicates` parameter indicates whether multiple instances
+        of the same cage are allowed to be added into the population.
+        Note that the sameness of a cage is judged by the `same_cage`
+        method of the ``Cage`` class, which is invoked by the ``in``
+        operator within this method. See the `__contains__` method of 
+        ``Population`` for details on how the ``in`` operator uses the 
+        `same_cage` method.
+        
+        Parameters
+        ----------
+        *args : Cage, Population
+            ``Cage`` instances to be added to the `members` attribute
+            and/or ``Population`` instances who's members, as generated 
+            by `all_members`, will be added to the `members` attribute.
+        
+        duplicates : bool (default = False)
+            When `False` only cages which are not already held by
+            the population will be added. `True` allows more than one
+            instance of the same cage to be added. Whether two cages
+            are the same is defined by the `same_cage` method of the
+            ``Cage`` class.
+            
+        Raises
+        ------
+        TypeError
+            If a provided argument not of ``Cage`` or ``Population``
+            type.
+        
+        """
+        
+        # Go through supplied arguments and determine their type. If the 
+        # type is ``Population`` add the ``Cage`` instances it contains
+        # into the `members` attribute via a generator. If the type is 
+        # ``Cage``, add it to the `members` attribute directly.
+        # Raise ``TypeError`` if supplied argument is not of ``Cage`` or 
+        # ``Population type.
         for arg in args:
-            if type(arg) is Population:
-                if not duplicates:
-                    self.members.append(cage for cage in arg 
-                                                    if cage not in self)
+            if type(arg) is Population:                
+                # Use the first generator if duplicate ``Cage`` 
+                # instances can be added. Use the second if they cannot.
                 if duplicates:
                     self.members.append(cage for cage in arg)
 
-            if type(arg) is Cage:
+                else:
+                    self.members.append(cage for cage in arg 
+                                                    if cage not in self)
+                continue
+                
+            if type(arg) is Cage:                
+                # Check if the ``Cage`` instance is already present. 
                 is_duplicate = arg in self
+                
+                # Add it to the population only if duplicates are 
+                # allowed or if duplicates are not allowed and it is not 
+                # a duplicate.
                 if duplicates:
                     self.members.append(arg)
-                if not duplicates and not is_duplicate:
-                    self.members.append(arg)                                    
+                elif not duplicates and not is_duplicate:
+                    self.members.append(arg)
+                continue                                    
+            
+            # Raise a ``TypeError`` if a supplied argument is not a 
+            # ``Cage`` or ``Population`` type.
+            raise TypeError(("Supplied arguments must be of ``Cage`` or"
+                             " ``Population`` type."))
     
     def select(self, type_='generational'):
+        """
+        Selects some members to form a new ``Population`` instance.
+        
+        Selection is a GA procedure and as a result this method merely 
+        delegates the selection request to the ``Selection`` instance 
+        held within the `ga_tools` attribute. The ``Selection`` instance
+        then returns the new ``Population`` instance. This 
+        ``Population`` instance is then returned by this method.
+        The selection instance (`self.ga_tools.selection`) performs the
+        selection by being called. See ``Selection`` class documentation
+        for more information.
+        
+        Because selection is required in a number of different ways,
+        such as selecting the parents, ``Cage`` instances for mutation
+        and ``Cage`` instances for the next generation, the type of 
+        selection must be specificed with the `type_` parameter. The
+        valid values for `type_` will correspond to one of the
+        attribute names of the ``Selection`` instance.
+
+        For example, if `type_` is set to 'mating' a selection 
+        algorithm which creates a parent pool will be invoked. If the 
+        `type_` is set to 'generational' an algorithm which selects the 
+        next generation will be invoked. It should be noted that a 
+        ``Population`` instance representing a parent pool will be
+        organized differently to one representing a generation. See
+        ``Selection`` class documentation for more details.
+        
+        The information regarding which generational, parent pool, etc.
+        algorithm is used is held by the ``Selection`` instance. This 
+        method merely requests that the ``Selection`` instance performs 
+        the selection algorithm of the relevant type. The ``Selection`` 
+        instance takes care of all the details to do with selection.
+        
+        Parameters
+        ----------
+        type_ : str (default = 'generational')
+            A string specifying the type of selection to be performed.
+            Valid values will correspond to names of attributes of the 
+            ``Selection`` class. Check ``Selection`` class documentation
+            for details. 
+            
+            Valid values include:              
+                'generational' - selects the next generation
+                'mating' - selects parents
+                'mutation' - selects ``Cage`` instances for mutation
+
+        Returns
+        -------        
+        Population
+            A population generated by using applying a selection
+            algorithm. Can represent a generation, a parent pool, etc.
+        
+        """
+        
         return self.ga_tools.selection(self, type_)
         
     def gen_offspring(self):
+        """
+        Returns a population of offspring ``Cage`` instances.        
+        
+        This is a GA operation and as a result this method merely
+        delegates the request to the ``Mating`` instance held in the 
+        `ga_tools` attribute. The ``Mating`` instance takes care of 
+        selecting parents and combining them to form offspring. The
+        ``Mating`` instace delegates the selection to the ``Selection`` 
+        instance as would be expected. The request to perform mating is 
+        done by calling the ``Mating`` instance with the population as 
+        the argument. Calling of the ``Mating``instance returns a 
+        ``Population`` instance holding the generated offspring. All 
+        details regarding the mating procedure are handled by the 
+        ``Mating`` instance.
+
+        For more details about how mating is implemented see the
+        ``Mating`` class documentation.
+        
+        Returns
+        -------
+        Population
+            A population holding offspring created by mating contained 
+            the ``Cage`` instances.
+        
+        """
+        
         return self.ga_tools.mating(self)
         
     def gen_mutants(self):
+        """
+        Returns a population of mutant ``Cage`` instances.        
+        
+        
+        
+        Returns
+        -------
+        Population
+            A population holding mutants created by mutating contained
+            ``Cage`` instances.
+        
+        """
+
         return self.ga_tools.mutation(self)
         
     def __iter__(self):
-        return iter(self.all_members())
+        """
+        Allows the use of ``for`` loops, ``*`` and ``iter`` function.        
+
+        When ``Population`` instances are iterated through they yield 
+        ``Cage`` instances generated by the `all_members` method. It 
+        also means that a ``Population`` instance can be unpacked with
+        the ``*`` operator. This will produce the ``Cage`` instances
+        yielded by the `all_members` method.
+
+        Returns
+        -------
+        Generator
+            The `all_members` generator. See `all_members` method 
+            documentation for more information.
+        
+        """
+        
+        return self.all_members()
             
     def __getitem__(self, key):
+        """
+        Allows the use of ``[]`` operator.
+
+        Cages held by the ``Population`` instance can be accesed by
+        their index. Slices are also supported. These return a new
+        ``Population`` instance holding the ``Cage`` instances with
+        the requested indices. Using slices will return a flat 
+        ``Population`` instance meaing no subpopulation
+        information is preserved. All of the ``Cage`` instances are
+        placed into the `members` attribute of the returned 
+        ``Population`` instance.
+
+        The index corresponds to the ``Cages`` yielded by the 
+        `all_members` method.
+
+        This can be exploited if one desired to remove all
+        subpopulations and transfer all the ``Cage`` instances into the 
+        members attribute. For example, 
+        
+        >>> pop2 = pop[:]
+        
+        ``pop2`` is a ``Population`` instance with all the same
+        ``Cage`` instances as ``pop``, however all ``Cage`` 
+        instances are held within its `members` attribute and its 
+        `populations` attribute is empty. This may or may not be the 
+        case for the ``pop`` instance.   
+        
+        Parameters
+        ----------
+        key : int, slice
+            An int or slice can be used depending on if a single 
+            ``Cage`` instance needs to be returned or a collection of 
+            ``Cage`` instances.
+        
+        Returns
+        -------
+        Cage
+            If the supplied `key` is an ``int``. Returns the ``Cage`` 
+            instance with the corresponding index from the `all_members` 
+            generator.
+        
+        Population
+            If the supplied `key` is a ``slice``. The returned 
+            ``Population`` instance holds ``Cage`` instances in its 
+            `members` attribute. The ``Cage`` instances correspond to 
+            indices defined by the slice. The slice is implemented 
+            on the `all_members` generator.
+        
+        Raises
+        ------
+        TypeError
+            If the supplied `key` is not an ``int`` or ``slice`` type.        
+        
+        """
+        
+        # Determine if provided key was an ``int`` or a ``slice``.
+        # If ``int``, return the corresponding ``Cage`` instance from
+        # the `all_members` generator.
         if type(key) is int:
             return list(self.all_members())[key]
+        
+        # If ``slice`` return a ``Population`` of the corresponding 
+        # ``Cage`` instances. The returned ``Population`` will have the 
+        # same `ga_tools` attribute as original ``Population`` instance.        
         if type(key) is slice:
             cages = itertools.islice(self.all_members(), 
                                      key.start, key.stop, key.step)
             return Population(*cages, self.ga_tools)
-        
-        raise ValueError("Index must be an integer or slice.")
+
+        # If `key` is not ``int`` or ``slice`` raise ``TypeError``.        
+        raise TypeError("Index must be an integer or slice.")
         
     def __len__(self):
+        """
+        Returns the number of members yielded by `all_members`.
+
+        Returns
+        -------
+        int
+            The number of members held by the population, including
+            those held within its subpopulations.
+        
+        """
+
         return len(list(self.all_members()))
         
-    def __sub__(self, other):        
+    def __sub__(self, other):
+        """
+        Allows use of the ``-`` operator.
+        
+        Subtracting one from another,
+
+            pop3 = pop1 - pop2,        
+        
+        returns a new population, pop3. The returned population contains 
+        all the ``Cage`` instances by in pop1 except those also in pop2.
+        This refers to all of the ``Cage`` instances, including those
+        held within any subpopulations. The returned population is 
+        flat. This means all information about subpopulations in pop1 is 
+        lost as all the ``Cage`` instances are held in the `members` 
+        attribute of pop3.
+
+        Parameters
+        ----------
+        other : Population
+            
+        Returns
+        -------
+        Population
+
+        """
+
         new_pop = Population(self.ga_tools)        
-        new_pop.add_members(*list(cage for cage in self 
-                                                if cage not in other))
+        new_pop.add_members(cage for cage in self 
+                                                if cage not in other)
         return new_pop
         
     def __add__(self, other):
+        """
+        Allows use fo the ``+`` operator.
+        
+        Parameters
+        ----------
+        other : Population
+        
+        Returns
+        -------
+        Population
+
+        
+        """
+        
         return Population(self, other, self.ga_tools)
 
     def __contains__(self, item):
+        """
+        Allows use of the ``in`` operator.
+
+        Parameters
+        ----------
+        item : Cage
+
+        Returns
+        -------
+        bool
+            
+        
+        
+        """
+
         return any(item.same_cage(cage) for cage in self.all_members())
 
     def __str__(self):
