57d2d1c2b5f8c2cbc7da97cd306bc734d474d72b
Lukas Turcani <Lukas Turcani>
2016-07-25 17:22:17 +0100

    added unit test for population initializer

--- tests/test_population.py
+++ tests/test_population.py
@@ -1,6 +1,21 @@
-from ..classes import Population
+from ..classes import Population, Cage, GATools
+import pytest
 
-def test_all_members():
-    a = Population.init_empty()
-    print(len(a))
-    assert len(a) == 24
+def test_init():
+    Population(Cage.__new__(Cage), Cage.__new__(Cage),
+               Population.__new__(Population), Cage.__new__(Cage), 
+               Population.__new__(Population), GATools.__new__(GATools))
+
+    with pytest.raises(TypeError):
+       Population(Cage.__new__(Cage), Cage.__new__(Cage), 
+                  Population.__new__(Population), Cage.__new__(Cage),
+                  Population.__new__(Population), 
+                  GATools.__new__(GATools), GATools.__new__(GATools))
+
+    with pytest.raises(TypeError):        
+        Population(Cage.__new__(Cage), Cage.__new__(Cage), 
+                   Population.__new__(Population), Cage.__new__(Cage),
+                   Population.__new__(Population), 
+                   GATools.__new__(GATools), 12)
+
+    Population(*[None for x in range(0,10)])
