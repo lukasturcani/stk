53bb0fa9a9aef1e10c07fb6e0d68200ef4416625
Lukas Turcani <lukasturcani93@gmail.com>
2016-07-29 00:06:02 +0100

    added shifting to structure class and printing coordinates

--- convenience_functions.py
+++ convenience_functions.py
@@ -6,6 +6,12 @@ def dedupe(iterable, seen=None):
             seen.add(x)
             yield x
             
-            
+def flatten(iterable, excluded_types={str}):
+    for x in iterable:
+        if hasattr(x, '__iter__') and type(x) not in excluded_types:          
+            yield from flatten(x)
+        else:
+            yield x            
             
 
+a = [[1],[1],[1],[[2],[2],['str']]]
