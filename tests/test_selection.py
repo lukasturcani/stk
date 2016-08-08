379c7d3b40a9a1cf24f93b366224502cded43c00
Lukas Turcani <Lukas Turcani>
2016-08-08 17:38:51 +0100

    working. just comments missing from some stuff. maybe also some unit tests.

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 from .test_population import generate_population
 from ..classes import GATools, Selection, FunctionData
 
@@ -16,9 +18,9 @@ def test_selection_population_integration():
         mol.label = labels[index]
 
     # Make a ``GATools`` attribute and give it to the population.
-    fittest = FunctionData('fittest', size=3)
-    fittest2 = FunctionData('fittest', size=4)
-    fittest3 = FunctionData('fittest', size=5)
+    fittest = FunctionData('fittest')
+    fittest2 = FunctionData('fittest')
+    fittest3 = FunctionData('fittest')
     
     selector = Selection(fittest, fittest2, fittest3)
     ga_tools = GATools(selector, 'a', 'b')
@@ -28,15 +30,15 @@ def test_selection_population_integration():
     pop3 = pop1.select('mating')
     pop4 = pop1.select('mutation')
 
-    assert len(pop2) == 3
-    assert len(pop3) == 4
-    assert len(pop4) == 5
+    pop1_members = list(pop1)
+    pop1_members.sort(key=attrgetter('fitness'), reverse=True)
+    assert list(pop2) == pop1_members
+    assert list(pop3) ==  pop1_members
+    assert list(pop4) == pop1_members
     
-    assert all(mol in pop3 and mol in pop4 for mol in pop2)
-    assert all(mol in pop4 for mol in pop3)
     
 def test_fittest():
-    fittest = FunctionData('fittest', size=3)
+    fittest = FunctionData('fittest')
     selector = Selection(fittest, fittest, fittest)
     ga_tools = GATools(selector, 'a', 'b')
     pop1.ga_tools = ga_tools
@@ -49,7 +51,7 @@ def test_fittest():
     pop2 = pop1.select()
 
     
-    assert len(pop2) == 3
+    assert len(list(pop2)) == 22
     for mol in pop2:
         assert mol.label in "abc"
         
@@ -65,10 +67,10 @@ def test_all_combinations():
         mol.label = labels[index]
     
     pop2 = pop1.select('mating')
-    assert not pop2.members
-    for parents in pop2.populations:
-        assert len(parents.members) == 2
-        assert not parents.populations
+    assert list(pop2)
+    for parents in pop2:
+        assert len(parents) == 2
+    
 
 
 
