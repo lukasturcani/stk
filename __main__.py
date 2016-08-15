85169cca7635b9f10b21aa85192523d575f9fe53
Lukas Turcani <Lukas Turcani>
2016-08-15 12:57:48 +0100

    added documentation to parts of optimization module

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import sys
 from itertools import islice
+import time
 
 from .classes import (Population, GATools, Selection, Mutation, Mating, 
                       FunctionData, FourPlusSix, EightPlusTwelve, 
@@ -20,7 +21,15 @@ if 'output' in os.listdir():
     new_dir = os.path.join('old_output', str(num))
     print('Moving old output dir.')
     shutil.copytree('output', new_dir)
-    shutil.rmtree('output')
+
+    # Wait for the copy to complete before removing the old folder.
+    mv_complete = False    
+    while not mv_complete:
+        try:
+            shutil.rmtree('output')
+            mv_complete = True
+        except:
+            pass
     
 # Create a new output directory and move into it. Save its path as the
 # root directory.
@@ -39,11 +48,13 @@ mator = Mating(ga_input.mating_func, ga_input.num_matings)
 mutator = Mutation(ga_input.mutation_func, ga_input.num_mutations)
 ga_tools = GATools(selector, mator, mutator, ga_input.opt_func)
 
-# Generate an initial population.
+# Generate and optimize an initial population.
 pop_init = getattr(Population, ga_input.init_func.name)
 pop = pop_init(**ga_input.init_func.params, 
                size=ga_input.pop_size, 
                ga_tools=ga_tools)
+pop.optimize_population()
+
 # Run the GA.
 for x in range(ga_input.num_generations):
     print(('Generation {0} started. Stop at generation {1}. '
