52e72d36a1f6f42f4dfcb944bd4ee7983318c312
Lukas Turcani <lukasturcani93@gmail.com>
2016-09-21 22:07:47 +0100

    finished working on fitness.py :

--- fitness.py
+++ fitness.py
@@ -98,155 +98,166 @@ def cage(macro_mol, target_size, coeffs=None, exponents=None):
     fitness_value = np.power(fitness_value, exponents)
     fitness_value = np.multiply(fitness_value, coeffs)    
 
-    return 1/np.sum(fitness_value)
-        
-class MolInCage:
+    return 1/np.sum(fitness_value)   
+    
+def cage_target(self, cage, target_mol_file, rotate=False):
     """
-
-    Attributes
+    Calculates the fitness of a cage / target complex.
+    
+    Depending of `rotate` a different number of cage / target 
+    complexes will be generated. When `rotate` is ``True``, the 
+    target molecule is rotated along the x, y and z axes and each
+    rotation forms a new complex. The lowest energy complex is used
+    for the fitness calculation. When `rotate` is ``False`` not 
+    rotation takes place.
+    
+    To see which rotations take place see the documentation of the
+    `generate_complexes` method.
+    
+    Parameters
     ----------
-    target_mol : StructUnit
-        An instance representing the molecule being placed within the 
-        cage. It is created the first time the fitness function is run
-        and returned from this attribute afterwards.
-        
+    cage : Cage
+        The cage which is to have its fitness calculated,
 
-    """    
+    target_mol_file : str
+        The full path of the .mol file hodling the target molecule
+        placed inside the cage.
+
+    rotate : bool (default = False)
+        When ``True`` the target molecule will be rotated inside the
+        cavity of the cage. When ``False`` only the orientation
+        within the .mol file is used.
+        
+    Returns
+    -------
+    float
+        The fitness value of `cage`.
     
+    """
     
-    def __call__(self, cage, target_mol_file, rotate=False):
-        """
-        Calculates the fitness of a cage / target complex.
+    # If the cage already has a fitness value, don't run the
+    # calculation again.
+    if cage.fitness:
+        print('Skipping {0}'.format(cage.prist_mol_file))
+        return cage.fitness            
+    
+    # The first time running the fitness function create an instance
+    # of the target molecule as a ``StructUnit``. Due to caching,
+    # running the initialization again on later attempts will not 
+    # re-initialize. Normally ``StructUnit`` instances do not have
+    # the attribute `optimized`, as these structures do not need to
+    # be optimized. However, in this case it is necessary to know
+    # the energy of the target molecule as part of the fitness 
+    # calculation. Giving ``StructUnit`` the `optimized` attribute
+    # fools the optimization function into running on ``StructUnit``
+    # despite it being designed for ``MacroMolecule`` instances.
+    target = StructUnit(target_mol_file)
+    if not hasattr(target, 'optimized'):
+        target.optimized = False
+        macromodel_opt(target, no_fix=True)
+    else:
+        print('hahahahaha')
+    
+    # This function creates a new molecule holding both the target
+    # and the cage centered at the origin. It then calculates the 
+    # energy of this complex and compares it to the energies of the
+    # molecules when separate. The more stable the complex relative
+    # to the individuals the higher the fitness.
+    
+    # Create rdkit instances of the target in the cage for each
+    # rotation.        
+    rdkit_complexes = list(_generate_complexes(cage, target, rotate))
+    
+    # Place the rdkit complexes into a new .mol file and use that 
+    # to make a new ``StructUnit`` instance of the complex.
+    # ``StructUnit`` is the class of choice here because it can be
+    # initialized from a .mol file. ``MacroMolecule`` requires
+    # things like topology and building blocks for initialization.
+    macromol_complexes = []        
+    for i, complex_ in enumerate(rdkit_complexes):
+        # First the data is loaded into a ``MacroMolecule`` instance
+        # as this is the class which is able to write rdkit
+        # instances to .mol files. Note that this ``MacroMolecule``
+        # instance is just a dummy and only holds the bare minimum
+        # information required to write the complex to a .mol file.
+        mm_complex = MacroMolecule.__new__(MacroMolecule)
+        mm_complex.prist_mol = complex_
+        mm_complex.prist_mol_file = cage.prist_mol_file.replace(
+                            '.mol', '_COMPLEX_{0}.mol'.format(i))
+        mm_complex.write_mol_file('prist')
         
-        Depending of `rotate` a different number of cage / target 
-        complexes will be generated. When `rotate` is ``True``, the 
-        target molecule is rotated along the x, y and z axes and each
-        rotation forms a new complex. The lowest energy complex is used
-        for the fitness calculation. When `rotate` is ``False`` not 
-        rotation takes place.
-        
-        To see which rotations take place see the documentation of the
-        `generate_complexes` method.
-        
-        Parameters
-        ----------
-        cage : Cage
-            The cage which is to have its fitness calculated,
-
-        target_mol_file : str
-            The full path of the .mol file hodling the target molecule
-            placed inside the cage.
-
-        rotate : bool (default = False)
-            When ``True`` the target molecule will be rotated inside the
-            cavity of the cage. When ``False`` only the orientation
-            within the .mol file is used.
-            
-        Returns
-        -------
-        float
-            The fitness value of `cage`.
-        
-        """
-        
-        # If the cage already has a fitness value, don't run the
-        # calculation again.
-        if cage.fitness:
-            print('Skipping {0}'.format(cage.prist_mol_file))
-            return cage.fitness            
-        
-        # The first time running the fitness function create an instance
-        # of the target molecule as a ``StructUnit``. Save it to
-        # `target_mol`. Normally ``StructUnit`` instances do not have
-        # the attribute `optimized`, as these structures do not need to
-        # be optimized. However, in this case it is necessary to know
-        # the energy of the target molecule as part of the fitness 
-        # calculation. Giving ``StructUnit`` the `optimized` attribute
-        # fools the optimization function into running on ``StructUnit``
-        # despite it being designed for ``MacroMolecule`` instances.
-        if not hasattr(self, 'target_mol'):
-            target_mol = StructUnit(target_mol_file)
-            target_mol.optimized = False
-            macromodel_opt(target_mol, no_fix=True)
-            setattr(self, 'target_mol', target_mol)
-        
-        
-        # This function creates a new molecule holding both the target
-        # and the cage centered at the origin. It then calculates the 
-        # energy of this complex and compares it to the energies of the
-        # molecules when separate. The more stable the complex relative
-        # to the individuals the higher the fitness.
-        
-        # Create rdkit instances of the target in the cage for each
-        # rotation.        
-        rdkit_complexes = list(self.generate_complexes(cage, rotate))
-        
-        # Place the rdkit complexes into a new .mol file and use that 
-        # to make a new ``StructUnit`` instance of the complex.
-        # ``StructUnit`` is the class of choice here because it can be
-        # initialized from a .mol file. ``MacroMolecule`` requires
-        # things like topology and building blocks for initialization.
-        macromol_complexes = []        
-        for i, complex_ in enumerate(rdkit_complexes):
-            # First the data is loaded into a ``MacroMolecule`` instance
-            # as this is the class which is able to write rdkit
-            # instances to .mol files. Note that this ``MacroMolecule``
-            # instance is just a dummy and only holds the bare minimum
-            # information required to write the complex to a .mol file.
-            mm_complex = MacroMolecule.__new__(MacroMolecule)
-            mm_complex.prist_mol = complex_
-            mm_complex.prist_mol_file = cage.prist_mol_file.replace(
-                                '.mol', '_COMPLEX_{0}.mol'.format(i))
-            mm_complex.write_mol_file('prist')
-            
-            # Once the .mol file is written load it into a 
-            # ``StructUnit`` instance.
-            macromol_complex = StructUnit(mm_complex.prist_mol_file)
-            # In order to optimize a ``StructUnit`` instance the
-            # `optimized` attribute needs to be added.
-            macromol_complex.optimized = False
-            macromodel_opt(macromol_complex, no_fix=True)
-            macromol_complexes.append(macromol_complex)
-        
-        # Calculate the energy of the complex and compare to the
-        # individual energies. If more than complex was made, use the
-        # most stable version.
-        energy_separate = cage.energy + self.target_mol.energy
-        energy_diff =  min(energy_separate - macromol_complex.energy for 
-                                macromol_complex in macromol_complexes)
-                                
-        return np.exp(energy_diff)
+        # Once the .mol file is written load it into a 
+        # ``StructUnit`` instance.
+        macromol_complex = StructUnit(mm_complex.prist_mol_file)
+        # In order to optimize a ``StructUnit`` instance the
+        # `optimized` attribute needs to be added.
+        macromol_complex.optimized = False
+        macromodel_opt(macromol_complex, no_fix=True)
+        macromol_complexes.append(macromol_complex)
+    
+    # Calculate the energy of the complex and compare to the
+    # individual energies. If more than complex was made, use the
+    # most stable version.
+    energy_separate = cage.energy + target.energy
+    energy_diff =  min(energy_separate - macromol_complex.energy for 
+                            macromol_complex in macromol_complexes)
+                            
+    return np.exp(energy_diff)
    
-    def generate_complexes(self, cage, rotate):
+def _generate_complexes(cage, target, rotate):
+    """
+    Yields rdkit instances of cage / target complexes.
+    
+    Parameters
+    ----------
+    cage : Cage
+        The cage used to form the complex.
         
-        # Define the rotations which are to be used on the target.
-        if rotate:        
-            rotations = [0, np.pi/2, np.pi, 3*np.pi/2]
-        else:
-            rotations = [0]
+    target : StructUnit
+        The target used to form the complex.
+        
+    rotate : bool
+        When ``True`` the target molecule will undergo rotations
+        within the cage cavity and a complex will be yielded for 
+        each configuration.
+        
+    Yields
+    ------
+    rdkit.Chem.rdchem.Mol
+        An rdkit instance holding the cage / target complex. 
+    
+    """
+    
+    # Define the rotations which are to be used on the target.
+    if rotate:        
+        rotations = [0, np.pi/2, np.pi, 3*np.pi/2]
+    else:
+        rotations = [0]
 
-        # First place both the target and cage at the origin.
-        cage.set_position('prist', [0,0,0])
-        self.target_mol.set_position('prist', [0,0,0])
-        og_pos_mat = self.target_mol.position_matrix('prist')
+    # First place both the target and cage at the origin.
+    cage.set_position('prist', [0,0,0])
+    target.set_position('prist', [0,0,0])
+    
+    # Get the position matrix of the target molecule.        
+    og_pos_mat = target.position_matrix('prist')
+    
+    # Carry out every rotation and yield a complex for each case.
+    for rot1, rot2, rot3 in it.combinations_with_replacement(
+                                                    rotations, 3):
+        rot_target = copy.deepcopy(target)
+        rot_mat1 = rotation_matrix_arbitrary_axis(rot1, [1,0,0])
+        rot_mat2 = rotation_matrix_arbitrary_axis(rot2, [0,1,0])
+        rot_mat3 = rotation_matrix_arbitrary_axis(rot3, [0,0,1])
         
+        new_pos_mat = np.dot(rot_mat1, og_pos_mat)
+        new_pos_mat = np.dot(rot_mat2, new_pos_mat)
+        new_pos_mat = np.dot(rot_mat3, new_pos_mat)
+        
+        rot_target.set_position_from_matrix('prist', new_pos_mat)
+        
+        yield chem.CombineMols(cage.prist_mol, rot_target.prist_mol)
+    
 
-        for rot1, rot2, rot3 in it.combinations_with_replacement(
-                                                        rotations, 3):
-            target = copy.deepcopy(self.target_mol)
-            rot_mat1 = rotation_matrix_arbitrary_axis(rot1, [1,0,0])
-            rot_mat2 = rotation_matrix_arbitrary_axis(rot2, [0,1,0])
-            rot_mat3 = rotation_matrix_arbitrary_axis(rot3, [0,0,1])
-            
-            new_pos_mat = np.dot(rot_mat1, og_pos_mat)
-            new_pos_mat = np.dot(rot_mat2, new_pos_mat)
-            new_pos_mat = np.dot(rot_mat3, new_pos_mat)
-            
-            target.set_position_from_matrix('prist', new_pos_mat)
-            
-            yield chem.CombineMols(cage.prist_mol, target.prist_mol)
-        
-molecule_in_cage = MolInCage()
 
     
     
