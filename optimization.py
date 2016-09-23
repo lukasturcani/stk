f415431a0c3a40bd4ce92a1e1ef74f4c30ca94b1
Lukas Turcani <lukasturcani93@gmail.com>
2016-09-23 04:28:19 +0100

    added some shit to optimization

--- optimization.py
+++ optimization.py
@@ -368,6 +368,12 @@ def macromodel_opt(macro_mol, force_field='16',
     except ConversionError as ex:
         print(('Minimization with OPLS3 failed. Trying OPLS_2005. '
                '- {0}'.format(macro_mol.prist_mol_file)))
+        
+        # If OPLS_2005 has been tried already - raise an exception.
+        if force_field=='14':
+            raise MacroMolError(Exception(), macro_mol, 
+                                'Both force fields failed.')
+        # If OPLSE_2005 has not been tried - try it.
         return macromodel_opt(macro_mol, force_field='14', 
                               macromodel_path=macromodel_path)
     
