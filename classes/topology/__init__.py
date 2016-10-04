3b30a45642fd4c7e250c0e57510bc30c3e8e5d52
Lukas Turcani <lukasturcani93@gmail.com>
2016-10-04 22:14:40 +0100

    finished working on topology

---
+++ classes/topology/__init__.py
@@ -0,0 +1,4 @@
+from .cage import (TwoPlusThree, FourPlusSix, SixPlusNine, 
+                   EightPlusTwelve, Dodecahedron, TwoPlusFour, 
+                   ThreePlusSix, FourPlusEight, SixPlusTwelve,
+                   TenPlusTwenty, TwoPlusTwo, SixPlusEight)
