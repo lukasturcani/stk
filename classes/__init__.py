2d1be7a20dd1208dde83433d3508346cdc30f175
Lukas Turcani <Lukas Turcani>
2016-10-04 18:26:24 +0100

    finished topology rewrok almost

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,5 +1,5 @@
 from .molecular import Cage, StructUnit, FGInfo, StructUnit2, StructUnit3, MacroMolecule, Polymer
-from .topology import FourPlusSix, BlockCopolymer, EightPlusTwelve, SixPlusNine, Dodecahedron, TwoPlusThree, TenPlusTwenty, SixPlusEight, TwoPlusTwo, TwoPlusFour, ThreePlusSix, FourPlusEight, SixPlusTwelve
+from .topology import SixPlusEight, TwoPlusThree, TenPlusTwenty, ThreePlusSix, TwoPlusFour, FourPlusSix, EightPlusTwelve, SixPlusNine, Dodecahedron, FourPlusEight, SixPlusTwelve, TwoPlusTwo
 from .ga import GATools, FunctionData, Selection, Mating, Mutation
 from .population import Population
 from .input_parser import GAInput
