cd9b828b31a792e69079690110601618a8b44c81
Lukas Turcani <Lukas Turcani>
2016-10-05 16:24:38 +0100

    reoragnized optimiztion

---
+++ optimization/__init__.py
@@ -0,0 +1,2 @@
+from .optimization import optimize_all, optimize_all_serial, rdkit_optimization, do_not_optimize
+from .macromodel import macromodel_opt, kill_macromodel
---
+++ optimization/macromodel/__init__.py
@@ -0,0 +1,1 @@
+from .macromodel_opt_funcs import macromodel_opt, kill_macromodel
