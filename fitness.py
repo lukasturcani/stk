4a8c1dea1770435c92527877dfbc945a9e270814
Lukas Turcani <lukasturcani93@gmail.com>
2016-10-06 23:14:50 +0100

    for real now

--- fitness.py
+++ fitness.py
@@ -99,6 +99,7 @@ def calc_fitness(func_data, population):
                 var_sum = np.add(unscaled, var_sum)
 
         var_avg = np.divide(var_sum, valid_params)
+        population.var_avg = var_avg
 
     # Apply the function to every member of the population.
     for macro_mol in population:
