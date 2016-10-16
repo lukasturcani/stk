3696bcd5e3e16684b6838b9d6c16989ba5f29369
Lukas Turcani <lukasturcani93@gmail.com>
2016-10-16 20:00:10 +0100

    added optimize folder func

--- optimization/macromodel/__init__.py
+++ optimization/macromodel/__init__.py
@@ -1,1 +1,1 @@
-from .macromodel_opt_funcs import macromodel_opt, kill_macromodel, macromodel_md_opt, macromodel_cage_opt
+from .macromodel_opt_funcs import macromodel_opt, kill_macromodel, macromodel_md_opt, macromodel_cage_opt, optimize_folder
