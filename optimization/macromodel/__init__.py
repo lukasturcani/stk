e0543ad52e5d0b7c7595fa78d45715a09a742659
Lukas Turcani <lukasturcani93@gmail.com>
2016-10-18 23:43:54 +0100

    stuf

--- optimization/macromodel/__init__.py
+++ optimization/macromodel/__init__.py
@@ -1,1 +1,4 @@
-from .macromodel_opt_funcs import macromodel_opt, kill_macromodel, macromodel_md_opt, macromodel_cage_opt, optimize_folder
+from .macromodel_opt_funcs import (macromodel_opt, kill_macromodel, 
+                                   macromodel_md_opt, macromodel_cage_opt, 
+                                   optimize_folder, low_energy_conf,
+                                   extract_conformer)
