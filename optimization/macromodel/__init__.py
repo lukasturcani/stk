a5d6a35f7b4c5aac5b0d345cbb19e99f3caf00f1
Lukas Turcani <lukasturcani@github.com>
2016-10-21 18:17:38 +0100

    should work nicely now

--- optimization/macromodel/__init__.py
+++ optimization/macromodel/__init__.py
@@ -1,4 +1,3 @@
 from .macromodel_opt_funcs import (macromodel_opt, kill_macromodel, 
                                    macromodel_md_opt, macromodel_cage_opt, 
-                                   optimize_folder, low_energy_conf,
-                                   extract_conformer)
+                                   optimize_folder)
