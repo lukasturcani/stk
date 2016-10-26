3d80ab79acaa8c83cf52bf34da70d955956a6357
Lukas Turcani <lukasturcani93@gmail.com>
2016-10-26 23:05:37 +0100

    some init changes, not importnat

--- classes/topology/__init__.py
+++ classes/topology/__init__.py
@@ -1,4 +1,5 @@
 from .cage import (TwoPlusThree, FourPlusSix, SixPlusNine, FourPlusFour,
                    EightPlusTwelve, Dodecahedron, TwoPlusFour, 
                    ThreePlusSix, FourPlusEight, SixPlusTwelve,
-                   TenPlusTwenty, TwoPlusTwo, SixPlusEight, OnePlusOne)
+                   TenPlusTwenty, TwoPlusTwo, SixPlusEight, OnePlusOne,
+                   CageTopology)
