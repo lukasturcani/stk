5eaced5ba971719a960b32ec39eebefb40545961
Lukas Turcani <lukasturcani93@gmail.com>
2016-11-24 09:42:30 +0000

    removed unnecesary functoins

--- convenience_functions/database_prune.py
+++ convenience_functions/database_prune.py
@@ -55,40 +55,7 @@ def fg_prune(input_folder, output_folder, fg, fg_num):
         if len(mol.find_functional_group_atoms()) == fg_num:
             print('Moving {}.'.format(path))
             shutil.copy(path, output_folder)
-            
-def mol2_to_mol(input_folder, output_folder):
-    """
-    Converts all .mol2 in a folder to V3000 .mol files.
-    
-    Parameters
-    ----------
-    input_folder : str
-        The full path of the folder filled with .mol2 files.
-    
-    output_folder : str
-        The full path of the folder where the V3000 .mol files should be
-        placed.
-        
-    Returns
-    -------
-    None : NoneType
-    
-    """
-    
-    input_list = ['babel', '-m', '-imol2', '' , '-omol', '', '-x3']
-    for x in os.listdir(input_folder):
-        input_list[3] = os.path.join(input_folder, x)
-        input_list[5] = os.path.join(output_folder, x).replace('.mol2', '.mol')
-        sp.call(input_list)
 
-def mol2_mol(input_folder, output_folder):
-    for i, x in enumerate(os.listdir(input_folder)):
-        mol = chem.MolFromMol2File(os.path.join(input_folder, x))
-        chem.MolToMolFile(mol, os.path.join(output_folder, x), forceV3000=True)
-        if i == 100:
-            break
-        
-        
 def fg_distance_prune(folder, fg):
     """
     Deletes molecules with functional groups seperated by 1 atom.
