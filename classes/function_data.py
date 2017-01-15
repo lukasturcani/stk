8d9505df34f827844675be3ceaa1e3e7dea4a221
Lukas Turcani <lukasturcani93@gmail.com>
2017-01-15 03:55:24 +0000

    stuff

---
+++ classes/function_data.py
@@ -0,0 +1,41 @@
+
+class FunctionData:
+    """
+    Stores information about functions and their parameters.
+
+    Attributes
+    ----------
+    name : str
+        The name of a function or method.
+
+    params : dict
+        The parameters of the function or method who's name is held by
+        'name' which should be used when that function is called.
+    
+    """
+    
+    __slots__ = ['name', 'params']    
+    
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.params = kwargs
+        
+    def __hash__(self):
+        return 1
+
+    def __eq__(self, other):
+        
+        same_length = len(self) == len(other)
+        same_items = all(x in other.params.items() for x in self.params.items())        
+        same_name = self.name == other.name        
+        
+        return same_length and same_items and same_name
+        
+    def __len__(self):
+        return len(self.params.items())        
+        
+    def __str__(self):
+        s = ", ".join("{}={}".format(key, repr(value)) for key, value in self.params.items())
+        return "FunctionData({}, ".format(self.name) + s + ")"
+    def __repr__(self):
+        return str(self)
