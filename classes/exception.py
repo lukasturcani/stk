21f4582b1e34d4627642cd07320c1d3440b9b761
Lukas Turcani <lukasturcani93@gmail.com>
2017-01-16 04:24:47 +0000

    updated exception documentaiton

--- classes/exception.py
+++ classes/exception.py
@@ -1,9 +1,9 @@
 import os
 import traceback
 
-class MacroMolError(Exception):
+class MolError(Exception):
     """
-    A class for raising errors when using ``MacroMolecule`` instances.
+    A class for raising errors when using ``Molecule`` instances.
     
     There are a lot of reason why MMEA might receive an error. The 
     error can originate when rkdit is trying to assembly a molucue, 
@@ -11,47 +11,32 @@ class MacroMolError(Exception):
     optimizations may go wrong for one reason or another and raise an
     error. This is good, as problems should be identified, not ignored.
     
-    However, errors raised just by rdkit are not as useful as one would
-    hope. The error get raised but the user is not told which
-    ``MacroMolecule`` instance was being manipulated. This makes
-    replication of the error difficult as the GA randomly creates 
-    molecules. For it to randomly replicate the error on the same 
-    molecule is highly unlikely.
+    However, errors raised by rdkit are not as useful as one would hope. 
+    The error gets raised but the user is not told which ``Molecule`` 
+    instance was being manipulated. This makes replication of the error 
+    difficult.    
     
-    In order to address this, whenever an error is raised by MMEA it
-    should be placed into an error of this type. Error's of this class
-    are initialized with the ``MacroMolecule`` instance that caused the
-    error. Furthermore the ``MacroMolecule`` and rdkit/optimization/etc. 
-    error is written to a file ``failures.txt``. This file is located in
-    the same directory as the ``output`` direcotry. Writing the 
-    ``MacroMolecule`` instance to file means it can be easily rebuilt as
-    its building blocks, topology and any other parameters required for 
-    initialization are written to this file. The writing happens during
-    initialization of the error. This means that even if the error is
-    ignored during runtime such as
-        
-        try:
-            raise MacroMolError(...)
-        
-        except MacroMolError as ex:
-            pass
-        
-    it will still be written to ``failures.txt`` and will not go
-    undetected.
+    In order to address this, all errors should be caught and placed
+    into a ``MolError`` along with the Molecule instance on which the 
+    error occured. On initialization of a MolError an entry is made in 
+    the file ``failures.txt``. This file is located in the ``output`` 
+    directory. Writing the entry to the file means that a Molecule can 
+    be easily rebuilt as its building blocks, topology and any other 
+    parameters are written to this file.
     
-    Because of this, MMEA should always create error of this type when
-    handling exception. This means every try/except statement in  MMEA
-    should be something like:
+    Every try/except statement in  MMEA should be something like:
     
         try:
             raise SomeExceptionByRdkit(...)
         
         except Exception as ex:
-            MacroMolError(ex, macro_mol, 'error in init')
+            MolError(ex, mol, 'error in init')
             
             # Do some stuff here. Reraise if you want or pass.
     
-    This means that no error will go undected or ignored by accident.       
+    Notice that though an error is not raised, it is recorded in the
+    ``failures.txt`` file. This is because a MolError instance was
+    initialized during error handling.         
     
     Attributes
     ----------
@@ -59,8 +44,8 @@ class MacroMolError(Exception):
         The exception raised by some other part of MMEA such as rdkit,
         or MacroModel.
         
-    macro_mol : MacroMolecule
-        The macromolecule on which the error was raised.
+    mol : Molecule
+        The Molecule on which the error was raised.
         
     notes : str
         Any additional comments about the exception. For example, where
@@ -68,23 +53,23 @@ class MacroMolError(Exception):
     
     """
     
-    def __init__(self, ex, macro_mol, notes):
-        # If a macro_mol caused a ``MacroMolError`` it should not be
-        # used by the GA. To ensure it is skipped by the optimization
-        # and fitness functions, set these attributes of `macro_mol`.
-        macro_mol.optimized = True
-        macro_mol.fitness = 1e-4
-        macro_mol.unscaled_fitness = 1e-4
-        macro_mol.fitness_fail = True
+    def __init__(self, ex, mol, notes):
+        # If a mol caused a ``MolError`` it should not be used by the 
+        # GA. To ensure it is skipped by the optimization and fitness 
+        # functions, set these attributes for `mol`.
+        mol.optimized = True
+        mol.fitness = 1e-4
+        mol.unscaled_fitness = 1e-4
+        mol.fitness_fail = True
         
         self.ex = ex
         self.notes = notes
-        self.write_to_file(macro_mol)
-        print('\n\nMacroMolError written to ``failures.txt``.\n\n')
+        self.write_to_file(mol)
+        print('\n\nMolError written to ``failures.txt``.\n\n')
         
-    def write_to_file(self, macro_mol):
+    def write_to_file(self, mol):
         """
-        Writes the exception and macromolecule to ``failures.txt``.
+        Writes the exception and Molecule to ``failures.txt``.
         
         This method is run during initialization. This means that even
         if an exception is ignored during runtime it will be still
@@ -103,23 +88,23 @@ class MacroMolError(Exception):
             name = os.path.join(cwd, 'failures.txt')
         
         with open(name, 'a') as f:
-            f.write("{} - {}\n\n".format(type(self.ex).__name__, self.ex))
+            f.write("{} - {}\n\n".format(type(self.ex).__name__, 
+                                            self.ex))
             
             traceback.print_exc(file=f)
             
             f.write('\nnote = {}\n'.format(self.notes))
 
-            f.write('prist_mol_file = {}\n'.format(
-                                              macro_mol.prist_mol_file))
+            f.write('prist_mol_file = {}\n'.format(mol.prist_mol_file))
             
-            if hasattr(macro_mol, 'building_blocks'):       
+            if hasattr(mol, 'building_blocks'):       
                 f.write('building blocks = {}\n'.format(
-                                             macro_mol.building_blocks))
+                                                   mol.building_blocks))
     
-                f.write('topology = {}\n'.format(macro_mol.topology)) 
+                f.write('topology = {}\n'.format(mol.topology)) 
                
                 f.write('topology_args = {}\n'.format(
-                                              macro_mol.topology_args))    
+                                                     mol.topology_args))    
                              
             f.write('\n\n\n')                             
                              
