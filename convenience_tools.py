bc9e06f6f3da47fe77497c12efc458438094f671
Lukas Turcani <lukasturcani93@gmail.com>
2017-01-29 00:49:32 +0000

    updated doc a bit

--- convenience_tools.py
+++ convenience_tools.py
@@ -1,6 +1,9 @@
 """
 This module defines general-purpose objects, functions and classes.
 
+Functions, classes, etc. defined here should not depending on any other
+part of MMEA. They must be completely self-sufficient.
+
 """
 
 import rdkit.Chem as chem
