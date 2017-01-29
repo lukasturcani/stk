da3eccce4bd9758414e15016be8293bb74548bf9
Lukas Turcani <lukasturcani93@gmail.com>
2017-01-29 00:24:11 +0000

    reaarrnegled direoctyr sturcture

--- classes/ga/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ga_tools import GATools
-from .selection import Selection
-from .crossover import Crossover
-from .mutation import Mutation
-from .normalization import Normalization
-from .input import *
-from .progress import *
