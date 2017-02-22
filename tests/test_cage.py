dbbbc1f21d849ea29f8b0297006ad2f9753890ba
Lukas Turcani <lukasturcani93@gmail.com>
2017-02-22 14:50:37 +0000

    finished macromolecule tests

---
+++ tests/test_cage.py
@@ -0,0 +1,7 @@
+from ..molecular import Cage
+
+def test_window_difference():
+    ...
+
+def test_windows():
+    ...
--- tests/test_macromolecule.py
+++ tests/test_macromolecule.py
@@ -1,5 +1,8 @@
 from types import SimpleNamespace
-from ..molecular import MacroMolecule
+from os.path import join
+from ..molecular import (MacroMolecule, Molecule, FourPlusSix,
+                         StructUnit,
+                         StructUnit2, StructUnit3, MacroMolKey)
 
 def test_same():
     """
@@ -42,3 +45,55 @@ def test_comparison():
     assert a == b
     assert c > b
     assert c >= a
+
+def test_caching():
+    og_c = dict(MacroMolecule.cache)
+    try:
+        # Empty the cache.
+        MacroMolecule.cache = {}
+        # Make a MacroMolecule the regular way.
+        bb1 = StructUnit2(join('data', 'struct_unit2', 'amine.mol2'))
+        bb2 = StructUnit3(join('data', 'struct_unit3', 'amine.mol2'))
+        mol1 = MacroMolecule({bb1, bb2}, FourPlusSix())
+
+        # Make a MacroMolecule using JSON.
+        with open(join('data', 'macromolecule', 'mm.json'), 'r') as f:
+            mol2 = Molecule.load(f.read())
+
+        assert mol1 is not mol2
+
+        # Remake the MacroMolecules.
+        mol3 = Molecule.load(mol1.json())
+        mol4 = MacroMolecule(mol2.building_blocks,
+                             mol2.topology.__class__())
+
+        # Confirm they are cached.
+        assert mol1 is mol3
+        assert mol1 is not mol4
+        assert mol2 is mol4
+        assert mol2 is not mol3
+
+    finally:
+        MacroMolecule.cache = og_c
+
+def test_json_init():
+    # Make a MacroMolecule using JSON.
+    with open(join('data', 'macromolecule', 'mm.json'), 'r') as f:
+        mol = Molecule.load(f.read())
+
+    assert mol.fitness == None
+    assert all(isinstance(x, StructUnit) for x in mol.building_blocks)
+    assert isinstance(mol.key, MacroMolKey)
+    assert mol.optimized == True
+    assert mol.unscaled_fitness == {}
+    assert mol.bonder_ids == [6, 15, 24, 59, 68, 77, 112, 121, 130,
+                              165, 174, 183, 219, 222, 252, 255, 285,
+                              288, 318, 321, 351, 354, 384, 387]
+    assert mol.failed == False
+    assert mol.energy.__class__.__name__ == 'Energy'
+    assert mol.topology.__class__.__name__ == 'FourPlusSix'
+    assert len(mol.mol.GetAtoms()) == 410
+    assert mol.bonds_made == 12
+    assert set(mol.bb_counter.values()) == {4, 6}
+    assert mol.progress_params == None
+    
