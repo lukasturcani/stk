25a389cbecf5d9eebf3be5584c1fc231340be0b5
Lukas Turcani <lukasturcani93@gmail.com>
2017-02-22 23:22:24 +0000

    stuff

--- tests/test_energy.py
+++ tests/test_energy.py
@@ -1,12 +1,14 @@
 from os.path import join
 from ..molecular import Molecule
 import numpy as np
+import json
 
-path = join('data', 'cage', 'cage.json')
+path = join('data', 'macromolecule', 'mm.json')
 with open(path, 'r') as f:
-    mol = Molecule.load(f.read())
+    mol = Molecule.load(json.load(f))
 
 def test_rdkit():
+    print(mol)
     assert np.isclose(mol.energy.rdkit('uff'), -0.84, atol=0.01)
     assert np.isclose(mol.energy.rdkit('mmff'), 3476.86, atol=0.02)
 
@@ -17,4 +19,5 @@ def test_pseudoformation():
     ...
 
 def test_logging():
+    print(mol)
     assert len(mol.energy.values.keys()) == 2
