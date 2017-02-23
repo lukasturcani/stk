dc20e206f23d7b5dd31ddc49a9ed51aa8248dff9
Lukas Turcani <lukasturcani93@gmail.com>
2017-02-23 14:30:10 +0000

    stuff

--- tests/test_macromolecule.py
+++ tests/test_macromolecule.py
@@ -4,6 +4,9 @@ import json
 from ..molecular import (MacroMolecule, Molecule, FourPlusSix,
                          StructUnit,
                          StructUnit2, StructUnit3, MacroMolKey)
+from ..ga import Population
+
+Population.load(join('data', 'macromolecule', 'mm.json'))
 
 def test_same():
     """
@@ -48,52 +51,52 @@ def test_comparison():
     assert c >= a
 
 def test_caching():
-    og_c = dict(MacroMolecule.cache)
-    try:
-        # Empty the cache.
-        MacroMolecule.cache = {}
-        # Make a MacroMolecule the regular way.
-        bb1 = StructUnit2(join('data', 'struct_unit2', 'amine.mol2'))
-        bb2 = StructUnit3(join('data', 'struct_unit3', 'amine.mol2'))
-        mol1 = MacroMolecule({bb1, bb2}, FourPlusSix())
 
-        # Make a MacroMolecule using JSON.
-        with open(join('data', 'macromolecule', 'mm.json'), 'r') as f:
-            mol2 = Molecule.load(json.load(f))
+    # Make a MacroMolecule the regular way.
+    bb1 = StructUnit2(join('data', 'struct_unit2', 'amine.mol2'))
+    bb2 = StructUnit3(join('data', 'struct_unit3', 'amine.mol2'))
+    mol1 = MacroMolecule({bb1, bb2}, FourPlusSix())
 
-        assert mol1 is not mol2
-
-        # Remake the MacroMolecules.
-        mol3 = Molecule.load(mol1.json())
-        mol4 = MacroMolecule(mol2.building_blocks,
-                             mol2.topology.__class__())
-
-        # Confirm they are cached.
-        assert mol1 is mol3
-        assert mol1 is not mol4
-        assert mol2 is mol4
-        assert mol2 is not mol3
-
-    finally:
-        MacroMolecule.cache = og_c
-
-def test_json_init():
     # Make a MacroMolecule using JSON.
     with open(join('data', 'macromolecule', 'mm.json'), 'r') as f:
-        mol = Molecule.load(json.load(f))
+        mol2 = Molecule.load(json.load(f)[0])
 
-    assert mol.fitness == None
-    assert all(isinstance(x, StructUnit) for x in mol.building_blocks)
-    assert isinstance(mol.key, MacroMolKey)
-    assert mol.optimized == True
-    assert mol.unscaled_fitness == {}
-    assert mol.bonder_ids == [6, 15, 24, 59, 68, 77, 112, 121, 130,
+    assert mol1 is not mol2
+
+    # Remake the MacroMolecules.
+    mol3 = Molecule.load(mol1.json())
+    mol4 = MacroMolecule(mol2.building_blocks,
+                         mol2.topology.__class__())
+
+    # Confirm they are cached.
+    assert mol1 is mol3
+    assert mol1 is not mol4
+    assert mol2 is mol4
+    assert mol2 is not mol3
+
+def test_json_init():
+    og_c = dict(MacroMolecule.cache)
+    try:
+        # Make a MacroMolecule using JSON.
+        MacroMolecule.cache = {}
+        with open(join('data', 'macromolecule', 'mm.json'), 'r') as f:
+            mol = Molecule.load(json.load(f)[0])
+
+        assert mol.fitness == None
+        assert all(isinstance(x, StructUnit) for x in
+                                        mol.building_blocks)
+        assert isinstance(mol.key, MacroMolKey)
+        assert mol.optimized == True
+        assert mol.unscaled_fitness == {}
+        assert mol.bonder_ids == [6, 15, 24, 59, 68, 77, 112, 121, 130,
                               165, 174, 183, 219, 222, 252, 255, 285,
                               288, 318, 321, 351, 354, 384, 387]
-    assert mol.failed == False
-    assert mol.energy.__class__.__name__ == 'Energy'
-    assert mol.topology.__class__.__name__ == 'FourPlusSix'
-    assert len(mol.mol.GetAtoms()) == 410
-    assert mol.bonds_made == 12
-    assert set(mol.bb_counter.values()) == {4, 6}
-    assert mol.progress_params == None
+        assert mol.failed == False
+        assert mol.energy.__class__.__name__ == 'Energy'
+        assert mol.topology.__class__.__name__ == 'FourPlusSix'
+        assert len(mol.mol.GetAtoms()) == 410
+        assert mol.bonds_made == 12
+        assert set(mol.bb_counter.values()) == {4, 6}
+        assert mol.progress_params == None
+    finally:
+        MacroMolecule.cache = og_c
