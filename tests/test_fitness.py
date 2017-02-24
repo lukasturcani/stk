4ed8976d7855dd6aafba7be9be4f263fc6a62e1e
Lukas Turcani <lukasturcani93@gmail.com>
2017-02-24 15:48:36 +0000

    broke a lot of stuff

--- tests/test_fitness.py
+++ tests/test_fitness.py
@@ -1,4 +1,13 @@
+from ..ga import cage, cage_target, cage_c60
+from ..molecular import Molecule
+from os.path import join
 
+cagemol = Molecule.load(join('data', 'fitness', 'cage.json'))
+
+def test_cage():
+    cage(cagemol, 8)
+
+    assert cagemol.unscaled_fitness == [1,1,1,1]
 
 def test_cage_target():
     ...
