ba5c5ec8914b4fd97b5e49a1d3b926ff4208ea8b
Lukas Turcani <lukasturcani93@gmail.com>
2017-03-21 14:35:00 +0000

    made sure tests write .pdb files instead of .mol, made sure that floating point errors in vector_theta function are fixed.

--- tests/test_cage_topologies.py
+++ tests/test_cage_topologies.py
@@ -13,7 +13,7 @@ def test_SixPlusEight():
     bb1 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     bb2 = StructUnit3(join(data_dir, 'amine4.mol'))
     c = Cage([bb1, bb2], SixPlusEight())
-    c.write(join(test_dir, 'SixPlusEight.mol'))
+    c.write(join(test_dir, 'SixPlusEight.pdb'))
 
 
 # 2 + 4 topolgy tests.
@@ -21,35 +21,35 @@ def test_TwoPlusFour():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'amine4.mol'))
     c = Cage([bb1, bb2], TwoPlusFour())
-    c.write(join(test_dir, 'TwoPlusFour.mol'))
+    c.write(join(test_dir, 'TwoPlusFour.pdb'))
 
 
 def test_ThreePlusSix():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'amine4.mol'))
     c = Cage([bb1, bb2], ThreePlusSix())
-    c.write(join(test_dir, 'ThreePlusSix.mol'))
+    c.write(join(test_dir, 'ThreePlusSix.pdb'))
 
 
 def test_FourPlusEight():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'amine4.mol'))
     c = Cage([bb1, bb2], FourPlusEight())
-    c.write(join(test_dir, 'FourPlusEight.mol'))
+    c.write(join(test_dir, 'FourPlusEight.pdb'))
 
 
 def test_SixPlusTwelve():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'amine4.mol'))
     c = Cage([bb1, bb2], SixPlusTwelve())
-    c.write(join(test_dir, 'SixPlusTwelve.mol'))
+    c.write(join(test_dir, 'SixPlusTwelve.pdb'))
 
 
 def test_TenPlusTwenty():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'amine4.mol'))
     c = Cage([bb1, bb2], TenPlusTwenty())
-    c.write(join(test_dir, 'TenPlusTwenty.mol'))
+    c.write(join(test_dir, 'TenPlusTwenty.pdb'))
 
 
 # 3 + 3 cage topologies.
@@ -57,21 +57,21 @@ def test_OnePlusOne():
     bb1 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], OnePlusOne())
-    c.write(join(test_dir, 'OnePlusOne.mol'))
+    c.write(join(test_dir, 'OnePlusOne.pdb'))
 
 
 def test_TwoPlusTwo():
     bb1 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], TwoPlusTwo())
-    c.write(join(test_dir, 'TwoPlusTwo.mol'))
+    c.write(join(test_dir, 'TwoPlusTwo.pdb'))
 
 
 def test_FourPlusFour():
     bb1 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], FourPlusFour())
-    c.write(join(test_dir, 'FourPlusFour.mol'))
+    c.write(join(test_dir, 'FourPlusFour.pdb'))
 
 
 # 2 + 3 cage topologies.
@@ -79,39 +79,39 @@ def test_TwoPlusThree():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], TwoPlusThree())
-    c.write(join(test_dir, 'TwoPlusThree.mol'))
+    c.write(join(test_dir, 'TwoPlusThree.pdb'))
 
 
 def test_FourPlusSix():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], FourPlusSix())
-    c.write(join(test_dir, 'FourPlusSix.mol'))
+    c.write(join(test_dir, 'FourPlusSix.pdb'))
 
 
 def test_FourPlusSix2():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], FourPlusSix2())
-    c.write(join(test_dir, 'FourPlusSix2.mol'))
+    c.write(join(test_dir, 'FourPlusSix2.pdb'))
 
 
 def test_SixPlusNine():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], SixPlusNine())
-    c.write(join(test_dir, 'SixPlusNine.mol'))
+    c.write(join(test_dir, 'SixPlusNine.pdb'))
 
 
 def test_EightPlusTwelve():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], EightPlusTwelve())
-    c.write(join(test_dir, 'EightPlusTwelve.mol'))
+    c.write(join(test_dir, 'EightPlusTwelve.pdb'))
 
 
 def test_Dodecahedron():
     bb1 = StructUnit2(join(data_dir, 'amine2.mol'))
     bb2 = StructUnit3(join(data_dir, 'aldehyde3.mol'))
     c = Cage([bb1, bb2], Dodecahedron())
-    c.write(join(test_dir, 'Dodecahedron.mol'))
+    c.write(join(test_dir, 'Dodecahedron.pdb'))
