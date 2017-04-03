7ee537237d02d695d51668f5439226f8d53d119d
Lukas Turcani <lukasturcani93@gmail.com>
2017-04-03 13:53:28 +0100

    stuff

--- ga/plotting.py
+++ ga/plotting.py
@@ -88,7 +88,7 @@ def parameter_epp(pop, plot_name, xlabel='Generation'):
 
     """
 
-    func_name = pop.ga_tools.input.fitnessor().name
+    func_name = pop.ga_tools.fitness.name
     fitness_func = globals()[func_name]
 
     # Exit if a function which does not have param_labels was used.
