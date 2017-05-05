2c119287e07a75a4165cced9931ac69d090a9d65
eberardo <berardo.enrico@gmail.com>
2017-05-05 10:44:16 +0100

    modified mopac

--- molecular/optimization/mopac.py
+++ molecular/optimization/mopac.py
@@ -74,7 +74,7 @@ def mopac_opt(macro_mol, mopac_path, settings={}):
                 precision work, 0.01 is recommended, as these results are
                 easily good enough for all high precision work.
 
-            'eps' : float (detault = 0)
+            'eps' : float (detault = 80.1)
                 Sets the dielectric constant for the solvent. Presence of this
                 keyword will cause the COSMO (Conductor-like Screening Model)
                 method to be used to approximate the effect of a solvent model
@@ -85,13 +85,11 @@ def mopac_opt(macro_mol, mopac_path, settings={}):
                 80.1 can be used to model a water environment at room
                 temperature.
 
-            'charge' : list of floats (default = [0])
+            'charge' : list of floats (default = 0)
                 When the system being studied is an ion, the charge, n, on the
                 ion must be supplied as an integer. For cations n can be 1, 2,
                 3, etc.; for anions -1, -2, -3, etc.
-                If more than one value is provided, then multiple runs will be
-                executed, but the final geometry will be taken from the neutral
-                run (charge = 0) if present.
+
 
             'fileout' : string (default = 'PDBOUT')
                 This generates the pdb file with the optimized structure.
@@ -117,8 +115,8 @@ def mopac_opt(macro_mol, mopac_path, settings={}):
             'hamiltonian': 'PM7',
             'method': 'OPT',
             'gradient': 0.01,
-            'eps': 0,
-            'charge': [0],
+            'eps': 80.1,
+            'charge': 0,
             'fileout': 'PDBOUT',
             'timeout': 172800,
             }
@@ -132,28 +130,146 @@ def mopac_opt(macro_mol, mopac_path, settings={}):
         macro_mol.write(macro_mol._file)
         # MOPAC requires a ``.mop`` file as input. This creates a ``.mop``
         # file holding the molecule.
-        # Make sure that an int was provided - otherwise Execpts
-        try:
-            for number in vals['charge']:
-                vals['charge'] = number
-                charge_file = _create_mop(macro_mol, vals)
-            # Run the optimization
-            _run_mopac(charge_file, mopac_path)
-            # Update the rdkit mol info with the ``.pdb`` file generated from
-            # the MOPAC run
-            _convert_mopout_to_mol(macro_mol)
-        except:
-            print(" You did not provide an integer value for the charges.")
+        _create_mop(macro_mol, vals)
+        # Run the optimization
+        _run_mopac(macro_mol, mopac_path, settings)
+        # Update the rdkit mol info with the ``.pdb`` file generated from
+        # the MOPAC run
+        _convert_mopout_to_mol(macro_mol)
+
+    except:
+        print(" There is an issue with the molecule provided")
 
 
-def _run_mopac(charge_file, mopac_path, timeout=600):
+def mopac_out_noupdate(macro_mol, mopac_path, settings={}):
+    """
+    Optimizes the molecule using MOPAC.
+
+    This function runs an optimization, but does not update the mol instance.
+    It is possible to provide different options, which correspond to the input
+    keywords from MOPAC: http://openmopac.net/Manual/index.html
+
+
+    Parameters
+    ----------
+    macro_mol : MacroMolecule
+        The macromolecule who's structure must be optimized.
+
+    mopac_path : str
+        The full path of the ``MOPAC`` suite within the user's
+        machine. For example, in a default MacOS installation the
+        folder will probably be something like
+        ``/opt/mopac/MOPAC2016.exe``.
+
+    settings: dict (default = {})
+        A dictionary which maps the names of the optimization parameters to
+        their values. Valid values are:
+
+            'hamiltonian' : string(default = 'PM7')
+                A series of different methods can be selected:
+                PM7, PM6, AM1, CIS (CISD, CISDT), MNDO, RM1, etc..
+
+                PM7 is the latest version of the reparametrization of the NDDO
+                theory, where all the atomic and diatomic parameters were
+                re-optimized - update compared to PM6.
+                http://openmopac.net/PM7_accuracy/PM7_accuracy.html
+
+            'method' : string (default = 'OPT')
+                The default calculation consists in a geometry optimization.
+                You can run single point calculations (SCF) or transition
+                search algorithms (TS). Refer to the MOPAC website for specific
+                keywords.
+
+            'gradient' : float (default = 0.01)
+                The gradient at which the geometry optimization reaches the
+                convergence criteria (kcal/mol/Angstrom). For small system high
+                precision work, 0.01 is recommended, as these results are
+                easily good enough for all high precision work.
+
+            'eps' : float (detault = 80.1)
+                Sets the dielectric constant for the solvent. Presence of this
+                keyword will cause the COSMO (Conductor-like Screening Model)
+                method to be used to approximate the effect of a solvent model
+                surrounding the molecule. Solvents with low dielectric constant
+                are not likely to work well with this model.
+                0 means that the dielectric constant is not included in the
+                calculation.
+                80.1 can be used to model a water environment at room
+                temperature.
+
+            'charge' : list of floats (default = 0)
+                When the system being studied is an ion, the charge, n, on the
+                ion must be supplied as an integer. For cations n can be 1, 2,
+                3, etc.; for anions -1, -2, -3, etc.
+
+
+            'fileout' : string (default = 'PDBOUT')
+                This generates the pdb file with the optimized structure.
+
+            'timeout' : float (default = 172800)
+                The amount in seconds the optimization is allowed to run before
+                being terminated. The default value is 2 days =
+                172,800 seconds.
+
+    Modifies
+    --------
+    None: NoneType
+
+    Returns
+    -------
+    None : NoneType
+
+    """
+
+    vals = {
+            'hamiltonian': 'PM7',
+            'method': 'OPT',
+            'gradient': 0.01,
+            'eps': 80.1,
+            'charge': 0,
+            'fileout': 'PDBOUT',
+            'timeout': 172800,
+            }
+    vals.update(settings)
+
+    try:
+        macro_mol._file = getattr(macro_mol, '_file',
+                                  '{}.mol'.format(uuid4().int))
+
+        # First write a .mol file of the molecule.
+        macro_mol.write(macro_mol._file)
+        # MOPAC requires a ``.mop`` file as input. This creates a ``.mop``
+        # file holding the molecule.
+        _create_mop(macro_mol, vals)
+        # Run the optimization
+        _run_mopac(macro_mol, mopac_path, settings)
+
+    except:
+        print(" There is an issue with the molecule provided")
+
+
+def _run_mopac(macro_mol, mopac_path, settings, timeout=600):
+
+    # Check the charge
+    charge = int(settings['charge'])
+    if charge < 0:
+        name_charge = '_an' + str(abs(charge))
+    elif charge > 0:
+        name_charge = '_cat' + str(abs(charge))
+    else:
+        name_charge = '_neut'
+
+    name, ext = os.path.splitext(macro_mol._file)
+    mop_file = name + name_charge + '.mop'
+
     print("", time.ctime(time.time()),
-          'Running MOPAC - {}.'.format(charge_file, sep='\n'))
+          'Running MOPAC - {}.'.format(mop_file, sep='\n'))
 
     # To run MOPAC a command is issued to the console via
     # ``subprocess.Popen``. The command is the full path of the
     # ``mopac`` program.
-    file_root, ext = os.path.splitext(charge_file)
+    file_root, ext = os.path.splitext(mop_file)
+    print("FILE NAME {}".format(file_root))
     opt_cmd = [mopac_path, file_root]
     opt_proc = psutil.Popen(opt_cmd, stdout=sp.PIPE,
                             stderr=sp.STDOUT,
@@ -166,60 +282,60 @@ def _run_mopac(charge_file, mopac_path, timeout=600):
             proc_out, _ = opt_proc.communicate()
     except sp.TimeoutExpired:
         print(('\nMinimization took too long and was terminated '
-               'by force - {}\n').format(charge_file))
-        _kill_mopac(charge_file)
+               'by force - {}\n').format(macro_mol))
+        _kill_mopac(macro_mol)
         proc_out = ""
 
 
-def _kill_mopac(charge_file):
+def _kill_mopac(macro_mol):
     """
     To kill a MOPAC run for a specific structure it is enough to generate
     a non empty file with the molecule's name with the `.end` extension.
     """
-    name, ext = os.path.splitext(charge_file)
+    name, ext = os.path.splitext(macro_mol)
     end_file = name + '.end'
 
     with open(end_file, 'w') as end:
         end.write('SHUT')
 
 
-def _license_found(output):
-    """
-    Checks to see if minimization failed due to a missing license.
-
-    The user can be notified of this in one of two ways. Sometimes the
-    output of the submission contains the message informing that the
-    license was not found and in other cases it will be the log file.
-    This function checks both of these sources for this message.
-
-    Parameters
-    ----------
-    output : str
-        The outout from submitting the minimization of the structure
-        to the MOPAC program.
-
-    Returns
-    -------
-    bool
-        ``True`` if the license was found. ``False`` if the
-        minimization did not occur due to a missing license.
-
-    """
-
-    # To check if the log file mentions a missing license file open the
-    # the log file and scan for the apporpriate string.
-
-    # Check if the file exists first. If not, this is often means the
-    # calculation must be redone so return False anyway.
-    output_file_path = macro_mol._file.replace('mol', 'out')
-    with open(output_file_path, 'r') as output_file:
-        output_file_content = output_file.read()
-
-    fail_string = 'is currently inaccessible.   Correct this fault before continuing (FATAL)'
-    if fail_string in output_file_content:
-        return False
-
-    return True
+# def _license_found(output):
+#     """
+#     Checks to see if minimization failed due to a missing license.
+#
+#     The user can be notified of this in one of two ways. Sometimes the
+#     output of the submission contains the message informing that the
+#     license was not found and in other cases it will be the log file.
+#     This function checks both of these sources for this message.
+#
+#     Parameters
+#     ----------
+#     output : str
+#         The outout from submitting the minimization of the structure
+#         to the MOPAC program.
+#
+#     Returns
+#     -------
+#     bool
+#         ``True`` if the license was found. ``False`` if the
+#         minimization did not occur due to a missing license.
+#
+#     """
+#
+#     # To check if the log file mentions a missing license file open the
+#     # the log file and scan for the apporpriate string.
+#
+#     # Check if the file exists first. If not, this is often means the
+#     # calculation must be redone so return False anyway.
+#     output_file_path = macro_mol._file.replace('mol', 'out')
+#     with open(output_file_path, 'r') as output_file:
+#         output_file_content = output_file.read()
+#
+#     fail_string = 'is currently inaccessible.   Correct this fault before continuing (FATAL)'
+#     if fail_string in output_file_content:
+#         return False
+#
+#     return True
 
 
 def _mop_line(settings):
@@ -247,14 +363,16 @@ def _mop_line(settings):
     mopac_run_str = mopac_run_str + settings['hamiltonian']
     # Add method and GNORM if 'OPT' otherwise just print the method
     if settings['method'] == 'OPT':
-        gnorm_info = ' GNORM = {} '.format(settings['gradient'])
-        mopac_run_str = mopac_run_str + settings['method'] + gnorm_info
+        gnorm_info = ' GNORM={} '.format(settings['gradient'])
+        mopac_run_str = mopac_run_str + " " + settings['method'] + gnorm_info
     else:
         mopac_run_str = mopac_run_str + settings['method']
     # Add EPS info
-    eps_info = ' EPS = {} '.format(settings['eps'])
+    eps_info = ' EPS={} '.format(settings['eps'])
+    mopac_run_str = mopac_run_str + eps_info
     # Add Charge info
-    charge_info = ' CHARGE = {} '.format(settings['charge'])
+    charge_info = ' CHARGE={} '.format(settings['charge'])
+    mopac_run_str = mopac_run_str + charge_info
     # Add fileout info
     mopac_run_str = mopac_run_str + " " + settings['fileout']
 
@@ -300,16 +418,18 @@ def _create_mop(macro_mol, settings):
     else:
         name_charge = '_neut'
 
-    print('Creating .mop file - {}.'.format(macro_mol.name + name_charge))
-
-    name, ext = os.path.splitext(macro_mol.file)
+    name, ext = os.path.splitext(macro_mol._file)
     mop_file = name + name_charge + '.mop'
     mol = macro_mol.mol
+
+    print(" REACHING 2 {}".format(macro_mol._file))
+    print('Creating .mop file - {}.'.format(name + name_charge))
+    print('mop file {}'.format(mop_file))
 
     # Generate the mop file containing the MOPAC run info
     with open(mop_file, 'w') as mop:
         # line for the run info
-        mop.write(_mop_line(setting) + "\n")
+        mop.write(_mop_line(settings) + "\n")
         # line with the name of the molecule
         mop.write(name + name_charge + "\n\n")
 
@@ -317,9 +437,11 @@ def _create_mop(macro_mol, settings):
         for atom in mol.GetAtoms():
             atom_id = atom.GetIdx()
             atom_symbol = atom.GetSymbol()
-            x, y, z = mol.atom_coords(atom_id)
-            atom_info = "{}   {}   +1  {}   +1  {}   +1 \n".format(atom_id,
+            x, y, z = mol.GetConformer().GetAtomPosition(atom_id)
+            print(atom_id, atom_symbol, x, y, z)
+            atom_info = "{}   {}   +1  {}   +1  {}   +1 \n".format(atom_symbol,
                                                                    x, y, z)
+            mop.write(atom_info)
 
     return mop_file
 
@@ -353,10 +475,10 @@ def _convert_mopout_to_mol(macro_mol):
 
     """
 
-    print("Updating molecule with MOPAC optimized one - {}.".format(macro_mol.name))
+    print("Updating molecule with MOPAC optimized one - {}.".format(macro_mol._file))
 
     name, ext = os.path.splitext(macro_mol._file)
-    pdb_file = name '_neut' + ".pdb"
+    pdb_file = name + '_neut' + ".pdb"
 
     try:
         new_mol = rdkit.MolFromPDBFile(pdb_file, sanitize=False,
@@ -364,6 +486,6 @@ def _convert_mopout_to_mol(macro_mol):
         # Updating the macro_mol.mol infos with the new mol
         macro_mol.mol = new_mol
     except FileNotFoundError:
-        print("File not found, {} is not updated.".format(macro_mol.name))
+        print("File not found, {} is not updated.".format(macro_mol._file))
 
     return
