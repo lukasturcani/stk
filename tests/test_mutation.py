a9364c853b8fa653e935bddd684c813c186b20f1
Lukas Turcani <lukasturcani93@gmail.com>
2017-05-07 00:26:45 +0100

    tests passing

--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -8,6 +8,7 @@ path = join('data', 'mutation', 'mutants.json')
 pop = Population.load(path)
 mol = pop[0]
 
+
 def test_cage_random_bb():
     mutant = Mutation(None, None).cage_random_bb(mol,
                 join('data', 'mutation', 'cage', 'bb'),
@@ -15,12 +16,14 @@ def test_cage_random_bb():
 
     assert mutant.topology.__class__ == mol.topology.__class__
 
+
 def test_cage_random_lk():
     mutant = Mutation(None, None).cage_random_lk(mol,
                 join('data', 'mutation', 'cage', 'lk'),
                      'amine')
 
     assert mutant.topology.__class__ == mol.topology.__class__
+
 
 def test_cage_similar_bb():
     db = join('data', 'mutation', 'cage', 'bb')
@@ -41,6 +44,7 @@ def test_cage_similar_bb():
     assert StructUnit3(most_sim, 'aldehyde') is mbb1
     assert mutant.topology.__class__ == mol.topology.__class__
 
+
 def test_cage_similar_lk():
     db = join('data', 'mutation', 'cage', 'lk')
     mutant = Mutation(None, None).cage_similar_lk(mol, db, 'amine')
@@ -59,6 +63,7 @@ def test_cage_similar_lk():
     most_sim = bb1.similar_molecules(db)[0][1]
     assert StructUnit2(most_sim, 'amine') is mlk1
     assert mutant.topology.__class__ == mol.topology.__class__
+
 
 def test_random_topology():
     mutant = Mutation.random_topology(None, mol, [EightPlusTwelve(),
