28dc927e7bb7d0c781654a3530f1e6962c6e9273
Lukas Turcani <lukasturcani93@gmail.com>
2017-06-14 02:33:27 +0100

    made progress on periodic systems

---
+++ molecular/topologies/periodic.py
@@ -0,0 +1,96 @@
+import rdkit.Chem.AllChem as rdkit
+from functools import partial
+from scipy.spatial.distance import euclidean
+
+from .base import Topology
+
+
+class Connection:
+    def __init__(self, atom1, direction1, atom2, direction2):
+        self.atom1 = atom1
+        self.direction1 = direction1
+        self.atom2 = atom2
+        self.direction2 = direction2
+
+
+def is_bonder(macro_mol, atom_id):
+    atom = macro_mol.mol.GetAtomWithIdx(atom_id)
+    if atom.HasProp('bonder'):
+        return True
+    return False
+
+
+class Hexagonal(Topology):
+    vertices = [[0, 0, 0],
+                [10, 10, 0]]
+    connections = []
+
+    def place_mols(self, macro_mol):
+        # Make the rdkit molecule.
+        macro_mol.mol = rdkit.Mol()
+        # Get the building blocks.
+        bb1, bb2 = macro_mol.building_blocks
+        # Place and set orientation of the first building block.
+        bb1.set_position(self.vertices[0])
+        bb1.set_orientation2([0, 0, 1])
+        bb1.minimize_theta(bb1.bonder_ids[0], [0, -1, 0], [0, 0, 1])
+        # Add to the macromolecule.
+        macro_mol.mol = rdkit.CombineMols(macro_mol.mol, bb1.mol)
+        # Place and set orientation of the second building block.
+        bb2.set_position(self.vertices[1])
+        bb2.set_orientation2([0, 0, 1])
+        bb2.minimize_theta(bb2.bonder_ids[0], [0, 1, 0], [0, 0, 1])
+        # Add to the macromolecule.
+        macro_mol.mol = rdkit.CombineMols(macro_mol.mol, bb2.mol)
+
+    def join_mols(self, macro_mol):
+        # Get the fragments.
+        frag1, frag2 = rdkit.GetMolFrags(macro_mol.mol,
+                                         sanitizeFrags=False)
+        # Get rid of any non bonder atoms.
+        frag1 = [x for x in frag1 if is_bonder(macro_mol, x)]
+        frag2 = [x for x in frag2 if is_bonder(macro_mol, x)]
+
+        # The fragment with the larger bonder ids has higher x and y
+        # values - due to place_mols() implmentation. It is the "top"
+        # fragment.
+        top = frag1 if frag1[0] > frag2[0] else frag2
+        bottom = frag2 if top is frag1 else frag1
+
+        # In the top fragment find the bonder atom with the
+        # largest y value and connect it to the bonder atom in the
+        # bottom fragment with the lowest y value. Note that the
+        # connection must be registered as perdiodic, hence the
+        # directions are 1/-1.
+        top_atom = max(top, key=lambda x: macro_mol.atom_coords(x)[1])
+        bottom_atom = min(bottom,
+                          key=lambda x: macro_mol.atom_coords(x)[1])
+        self.connections.append(Connection(top_atom, [0, 1, 0],
+                                           bottom_atom, [0, -1, 0]))
+        # Do the same for the x-axis periodic bonds.
+        right_atom = max(top,
+                         key=lambda x: macro_mol.atom_coords(x)[0])
+        left_atom = min(bottom,
+                        key=lambda x: macro_mol.atom_coords(x)[0])
+
+        self.connections.append(Connection(right_atom, [1, 0, 0],
+                                           left_atom, [-1, 0, 0]))
+
+        # For the bond which gets created directly, find the bonder
+        # atom in the bottom fragment closest to the position of the
+        # top fragment. Create a bond between it and the bonder atom
+        # in the top fragment closest to the position of the bottom
+        # fragment.
+        bottom_bonder = min(bottom, key=lambda x:
+                            euclidean(self.vertices[1],
+                                      macro_mol.atom_coords(x)))
+        top_bonder = min(top, key=lambda x:
+                         euclidean(self.vertices[0],
+                                   macro_mol.atom_coords(x)))
+
+        emol = rdkit.EditableMol(macro_mol.mol)
+        bond_type = self.determine_bond_type(macro_mol,
+                                             top_bonder,
+                                             bottom_bonder)
+        emol.AddBond(top_bonder, bottom_bonder, bond_type)
+        macro_mol.mol = emol.GetMol()
