7771ef1950f21be1857cb380de6221cd58d0dbea
Lukas Turcani <lukasturcani93@gmail.com>
2017-06-15 17:30:07 +0100

    periodic almost done

--- addons/pyWINDOW/pywindow/__init__.py
+++ addons/pyWINDOW/pywindow/__init__.py
@@ -4,5 +4,5 @@ from .io_tools import *
 from .utilities import *
 from .molecular import *
 from .trajectory import *
-from .postprocess import *
+# from .postprocess import *
 # from .shape import *
--- molecular/topologies/periodic.py
+++ molecular/topologies/periodic.py
@@ -8,9 +8,9 @@ from .base import Topology
 class Connection:
     def __init__(self, atom1, direction1, atom2, direction2):
         self.atom1 = atom1
-        self.direction1 = direction1
+        self.direction1 = np.array(direction1)
         self.atom2 = atom2
-        self.direction2 = direction2
+        self.direction2 = np.array(direction2)
 
 
 def is_bonder(macro_mol, atom_id):
@@ -20,10 +20,25 @@ def is_bonder(macro_mol, atom_id):
     return False
 
 
-class Hexagonal(Topology):
-    x = 1
-    cell_dimensions = a, b, c = [x*np.array([1, 0, 0]),
-                                 x*np.array([0.5, 0.866, 0]),
+class PeriodicPattern(Topology):
+    def del_atoms(self, macro_mol):
+        macro_mol.terminator_coords = {}
+        for atom in macro_mol.GetAtoms():
+            if not atom.HasProp('bonder'):
+                continue
+            for neighbor in atom.GetNeighbors():
+                if not neighbor.HasProp('del'):
+                    continue
+                bi = macro_mol.bonder_ids.index(atom.GetIdx())
+                macro_mol.terminator_coords[bi] = (
+                                       macro_mol.atom_coords(neighbor))
+
+        super().del_atoms(macro_mol)
+
+
+class Hexagonal(PeriodicPattern):
+    cell_dimensions = a, b, c = [np.array([1, 0, 0]),
+                                 np.array([0.5, 0.866, 0]),
                                  np.array([0, 0, 10.0000/1.7321])]
 
     vertices = [(a/3 + b/3),
@@ -36,20 +51,24 @@ class Hexagonal(Topology):
         # Get the building blocks.
         bb1, bb2 = macro_mol.building_blocks
         cell_size = bb1.max_diameter()[0] + bb2.max_diameter()[0]
-        self.cell_dimensions = [cell_size*x for x in self.cell_dimensions]
+        self.cell_dimensions = [cell_size*x for x in
+                                self.cell_dimensions]
         self.vertices = [cell_size*x for x in self.vertices]
         # Place and set orientation of the first building block.
-        bb1.set_position(self.vertices[0])
+        bb1.set_bonder_centroid(self.vertices[0])
         bb1.set_orientation2([0, 0, 1])
         bb1.minimize_theta(bb1.bonder_ids[0], [0, -1, 0], [0, 0, 1])
         # Add to the macromolecule.
         macro_mol.mol = rdkit.CombineMols(macro_mol.mol, bb1.mol)
         # Place and set orientation of the second building block.
-        bb2.set_position(self.vertices[1])
+        bb2.set_bonder_centroid(self.vertices[1])
         bb2.set_orientation2([0, 0, 1])
         bb2.minimize_theta(bb2.bonder_ids[0], [0, 1, 0], [0, 0, 1])
         # Add to the macromolecule.
         macro_mol.mol = rdkit.CombineMols(macro_mol.mol, bb2.mol)
+        # Add the bonder_ids prematurely for this topology. Needed for
+        # making supercells - see join_mols().
+        macro_mol.save_ids()
 
     def join_mols(self, macro_mol):
         # Get the fragments.
@@ -73,6 +92,11 @@ class Hexagonal(Topology):
         top_atom = max(top, key=lambda x: macro_mol.atom_coords(x)[1])
         bottom_atom = min(bottom,
                           key=lambda x: macro_mol.atom_coords(x)[1])
+        # The bonder atoms are registered by their index within
+        # `bonder_ids`. This is because del_atoms will change the
+        # atom ids.
+        top_atom = macro_mol.bonder_ids.index(top_atom)
+        bottom_atom = macro_mol.bonder_ids.index(bottom_atom)
         self.connections.append(Connection(top_atom, [0, 1, 0],
                                            bottom_atom, [0, -1, 0]))
         # Do the same for the x-axis periodic bonds.
@@ -80,7 +104,11 @@ class Hexagonal(Topology):
                          key=lambda x: macro_mol.atom_coords(x)[0])
         left_atom = min(bottom,
                         key=lambda x: macro_mol.atom_coords(x)[0])
-
+        # The bonder atoms are registered by their index within
+        # `bonder_ids`. This is because del_atoms will change the
+        # atom ids.
+        right_atom = macro_mol.bonder_ids.index(right_atom)
+        left_atom = macro_mol.bonder_ids.index(left_atom)
         self.connections.append(Connection(right_atom, [1, 0, 0],
                                            left_atom, [-1, 0, 0]))
 
