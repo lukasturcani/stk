17796ca0efcf9ab7008b3255dabbabe2851d1523
Lukas Turcani <lukasturcani93@gmail.com>
2017-06-19 16:20:52 +0100

    updated cage_target fitness functions to have more fitness parameters

--- tests/test_fitness.py
+++ tests/test_fitness.py
@@ -2,6 +2,7 @@ from .. import FunctionData, Molecule
 from ..ga.fitness import cage, cage_target, cage_c60
 from os.path import join
 import numpy as np
+import pytest
 
 cagemol = Molecule.load(join('data', 'fitness', 'cage.json'))
 target = join('data', 'fitness', 'target.pdb')
@@ -12,13 +13,23 @@ def test_cage():
 
 
 def test_cage_target():
-    assert isinstance(cage_target(cagemol, target,
-                      FunctionData('rdkit', forcefield='mmff'),
-                      FunctionData('do_not_optimize')), np.ndarray)
+    # Because no optimization function is used this function will fail.
+    with pytest.raises(ValueError) as ex:
+        isinstance(cage_target(cagemol, target,
+                   FunctionData('rdkit', forcefield='mmff'),
+                   FunctionData('do_not_optimize')), np.ndarray)
+        # The 3rd fitness parameter should be the only one that failed.
+        ex.args[1].pop(2)
+        assert all(x is not None for x in ex.args[1])
 
 
 def test_cage_c60():
-    assert isinstance(cage_c60(cagemol, target,
-                      FunctionData('rdkit', forcefield='mmff'),
-                      FunctionData('do_not_optimize'), 1, 1),
-                      np.ndarray)
+    # Because no optimization function is used this function will fail.
+    with pytest.raises(ValueError) as ex:
+        isinstance(cage_c60(cagemol, target,
+                   FunctionData('rdkit', forcefield='mmff'),
+                   FunctionData('do_not_optimize'), 1, 1),
+                   np.ndarray)
+        # The 3rd fitness parameter should be the only one that failed.
+        ex.args[1].pop(2)
+        assert all(x is not None for x in ex.args[1])
