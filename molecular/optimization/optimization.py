65fe9da20c9764eb0f7e8bf1f63605100347b7e0
Enrico <eberardo@ic.ac.uk>
2017-08-16 10:29:14 +0100

    added dihedral strain and implemented Lukas comments

--- molecular/optimization/optimization.py
+++ molecular/optimization/optimization.py
@@ -44,7 +44,9 @@ import numpy as np
 import logging
 from threading import Thread
 from traceback import format_exc
+
 from .macromodel import macromodel_opt, macromodel_cage_opt
+
 from ...convenience_tools import FakeLogger, mplogger, StopLogging
 
 
