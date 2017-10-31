e3683f8dc78d54103d6cff203d9eb009777cd4d8
Enrico Berardo <eberardo@ic.ac.uk>
2017-10-31 11:36:45 +0000

    fixed issue with empty population when plotting

--- ga/plotting.py
+++ ga/plotting.py
@@ -63,8 +63,13 @@ def fitness_epp(pop, plot_name=False,
             mins.append(0)
         else:
             maxs.append(max(x.fitness for x in subpop))
-            means.append(np.mean([x.fitness for x in subpop if x.fitness != 0.0001]))
-            mins.append(min(x.fitness for x in subpop if x.fitness != 0.0001))
+            clean_pop = [x.fitness for x in subpop if x.fitness != 0.0001]
+            if clean_pop:
+                means.append(np.mean(clean_pop))
+                mins.append(min(clean_pop))
+            else:
+                means.append(0.0001)
+                mins.append(0.0001)
 
     # Save the plot data.
     if plot_name and dump_name is None:
