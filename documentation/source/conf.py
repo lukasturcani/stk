d4a7130a4e0ce648f28a80ff82556f53d58ec7b6
Lukas Turcani <lukasturcani93@gmail.com>
2017-11-01 17:24:12 +0000

    finished fitness.py documentation

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -32,10 +32,10 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon']
+              'sphinx.ext.coverage',
+              'sphinx.ext.imgmath',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon']
 
 # autodoc settings
 autodoc_default_flags = ['private-members', 'special-members']
@@ -159,6 +159,3 @@ texinfo_documents = [
      author, 'mtk', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
