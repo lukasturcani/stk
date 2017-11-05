cebcb49b02ec204748474647ddc2997fad4eed23
Lukas Turcani <lukasturcani93@gmail.com>
2017-11-05 19:33:51 +0000

    updated tests

--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -1,9 +1,12 @@
 from ..ga import Crossover, Population
+from ..molecular import Molecule
 from os.path import join
 
-pop = Population.load(join('data', 'crossover', 'molecules.json'))
+pop = Population.load(join('data', 'crossover', 'molecules.json'),
+                      Molecule.fromdict)
 p1 = pop[0]
 p2 = pop[1]
+
 
 def test_bb_lk_exchange():
     offspring = Crossover.bb_lk_exchange(None, p1, p2)
