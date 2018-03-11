4b360491a5361f563063694fb7fbd330ded1fc4e
Lukas Turcani <lukasturcani93@gmail.com>
2018-03-11 13:18:37 +0000

    placement seems to be working

---
+++ tests/test_cof_topologies.py
@@ -0,0 +1,42 @@
+from ..molecular import (StructUnit3, StructUnit2, Periodic,
+                         Honeycomb, Hexagonal, Square, Kagome)
+import os
+
+test_dir = 'cof_topology_tests'
+if not os.path.exists(test_dir):
+    os.mkdir(test_dir)
+
+
+def test_honeycomb():
+    bb1 = StructUnit2(
+        '/home/lukas/databases/liverpool_refined/amines_2f/0.mol')
+    bb2 = StructUnit3(
+        '/home/lukas/databases/liverpool_refined/aldehydes_3f/0.mol')
+    cof = Periodic([bb1, bb2], Honeycomb())
+    cof.write(os.path.join(test_dir, 'honeycomb.mol'))
+
+
+def test_hexagonal():
+    bb1 = StructUnit2(
+        '/home/lukas/databases/liverpool_refined/amines_2f/0.mol')
+    bb2 = StructUnit3('/home/lukas/Dropbox/workspace/6_aldehyde.mol')
+    cof = Periodic([bb1, bb2], Hexagonal())
+    cof.write(os.path.join(test_dir, 'hexagonal.mol'))
+
+
+def test_square():
+    bb1 = StructUnit2(
+        '/home/lukas/databases/liverpool_refined/amines_2f/0.mol')
+    bb2 = StructUnit3(
+        '/home/lukas/databases/liverpool_refined/aldehydes_4f/59.mol')
+    cof = Periodic([bb1, bb2], Square())
+    cof.write(os.path.join(test_dir, 'square.mol'))
+
+
+def test_kagome():
+    bb1 = StructUnit2(
+        '/home/lukas/databases/liverpool_refined/amines_2f/0.mol')
+    bb2 = StructUnit3(
+        '/home/lukas/databases/liverpool_refined/aldehydes_4f/59.mol')
+    cof = Periodic([bb1, bb2], Kagome())
+    cof.write(os.path.join(test_dir, 'kagome.mol'))
