2074ec4780169f904eee80abdc42d3bdc7409e26
Lukas Turcani <lukasturcani93@gmail.com>
2018-04-10 11:54:39 +0100

    made compatible with distutils

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='stk',
+      author='Lukas Turcani',
+      author_email='lukasturcani93@gmail.com',
+      url='www.github.com/lukasturcani/stk',
+      packages=['stk',
+                'stk.convenience_tools',
+                'stk.molecular',
+                'stk.molecular.topologies',
+                'stk.molecular.topologies.cage',
+                'stk.optimization',
+                'stk.ga'])
