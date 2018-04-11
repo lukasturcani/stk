2029dacf62474c73b8d2d4cdc2adf5ca55bd2f86
Lukas Turcani <lukasturcani93@gmail.com>
2018-04-11 18:07:21 +0100

    typo

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='stk',
       requires=['networkx',
                 'scipy',
                 'matplotlib',
-                'scikit-learn',
+                'sklearn',
                 'psutil',
                 'pywindowx'])
