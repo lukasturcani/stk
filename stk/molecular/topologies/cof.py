b2bf05e858377d2779301aecb2096e9dd9a9cfc3
Lukas Turcani <lukasturcani93@gmail.com>
2018-05-04 07:04:56 +0100

    cofs done

--- stk/molecular/topologies/cof.py
+++ stk/molecular/topologies/cof.py
@@ -472,27 +472,6 @@ class Edge:
         self.fg_map = {0: fgs[0], 1: fgs[1]}
 
 
-def is_bonder(macro_mol, atom_id):
-    """
-    ``True`` if atom has ``'bonder'`` property.
-
-    Parameters
-    ----------
-    macro_mol : :class:`.MacroMolecule`
-        The macromolecule to which the atom belongs.
-
-    Returns
-    -------
-    :class:`bool`
-        ``True`` if atom has ``'bonder'`` property else ``False``.
-
-    """
-
-    return (True if
-            macro_mol.mol.GetAtomWithIdx(atom_id).HasProp('bonder')
-            else False)
-
-
 def bb_size(macro_mol):
     """
     Sums the diameters of the building blocks.
@@ -557,6 +536,18 @@ class COFLattice(Topology):
     def __init__(self, scale_func=bb_size):
         self.scale_func = scale_func
         super().__init__()
+
+    def _update_fg_id(self, macro_mol, mol):
+        """
+
+        """
+
+        max_id = max((a.GetIntProp('fg_id') for
+                      a in macro_mol.mol.GetAtoms() if
+                      a.HasProp('fg_id')), default=-1) + 1
+        for a in mol.GetAtoms():
+            if a.HasProp('fg_id'):
+                a.SetIntProp('fg_id', a.GetIntProp('fg_id')+max_id)
 
 
 class LinkerCOFLattice(COFLattice):
@@ -691,67 +682,9 @@ class LinkerCOFLattice(COFLattice):
 
             e.create_fg_map(macro_mol, cell_params)
 
-        # super(macro_mol.__class__, macro_mol).save_ids()
-
-    def _update_fg_id(self, macro_mol, mol):
-        """
-
-        """
-
-        max_id = max((a.GetIntProp('fg_id') for
-                      a in macro_mol.mol.GetAtoms() if
-                      a.HasProp('fg_id')), default=-1) + 1
-        for a in mol.GetAtoms():
-            if a.HasProp('fg_id'):
-                a.SetIntProp('fg_id', a.GetIntProp('fg_id')+max_id)
-
 
 class NoLinkerCOFLattice(COFLattice):
-    def del_atoms(self, macro_mol):
-        """
-        Deletes the atoms which are lost during assembly.
-
-        Notes
-        -----
-        The parameter `macro_mol` has two attributes changed.
-        :attr:`~.MacroMolecule.mol` has deleter atoms removed, while
-        :attr:`~.Periodic.deleters` is updated with the
-        coordinates, element type and bond type of every removed atom.
-
-        Parameters
-        ----------
-        macro_mol : :class:`.Periodic`
-            The periodic macromolecule being assembled.
-
-        Returns
-        -------
-        None : :class:`NoneType`
-
-        """
-
-        # This loop checks if an atom is an bonder and if its is finds
-        # a neighboring deleter atom. The deleter atom has its
-        # coordinates relative to the bonder found and saved in
-        # `terminator_coords`.
-        macro_mol.deleters = defaultdict(list)
-        for atom in macro_mol.mol.GetAtoms():
-            if not atom.HasProp('bonder'):
-                continue
-            for neighbor in atom.GetNeighbors():
-                if not neighbor.HasProp('del'):
-                    continue
-                bi = macro_mol.bonder_ids.index(atom.GetIdx())
-                nid = neighbor.GetIdx()
-                tcoords = (macro_mol.atom_coords(nid) -
-                           macro_mol.atom_coords(atom.GetIdx()))
-
-                bond = macro_mol.mol.GetBondBetweenAtoms(nid, atom.GetIdx())
-                macro_mol.deleters[bi].append([tcoords,
-                                               neighbor.GetAtomicNum(),
-                                               bond.GetBondType()])
-
-        super().del_atoms(macro_mol)
-        macro_mol._ids_updated = False
+    ...
 
 
 class NoLinkerHoneycomb(NoLinkerCOFLattice):
@@ -767,87 +700,63 @@ class NoLinkerHoneycomb(NoLinkerCOFLattice):
     vertices = [(a/3 + b/3 + c/2),
                 (2*a/3 + 2*b/3 + c/2)]
 
-    def join_mols(self, macro_mol):
+    def bonded_fgs(self, macro_mol):
         """
-        Joins the building blocks in the unit cell.
+        Yields functional groups to be bonded.
 
         Notes
         -----
-        The rdkit instance in the :attr:`~.MacroMolecule.mol` attribute
-        has bonds added to it. The
-        :attr:`~.MacroMolecule.periodic_bonds` attribute is also filled
-        with :class:`PeriodicBond` instances.
+        The :attr:`~.Periodic.periodic_bonds` attribute is filled
+        with :class:`.PeriodicBond` instances.
 
         Parameters
         ----------
         macro_mol : :class:`.MacroMolecule`
             The unit cell being constructed.
 
-        Returns
-        -------
-        None : :class:`NoneType`
+        Yields
+        ------
+        :class:`tuple` of :class:`int`
+            The ids of the functional groups to be bonded.
 
         """
 
-        # Get the fragments.
-        frag1, frag2 = rdkit.GetMolFrags(macro_mol.mol,
-                                         sanitizeFrags=False)
-        # Get rid of any non bonder atoms.
-        frag1 = [x for x in frag1 if is_bonder(macro_mol, x)]
-        frag2 = [x for x in frag2 if is_bonder(macro_mol, x)]
-
-        # The fragment with the larger bonder ids has higher x and y
+        # The fragment with the larger fg ids has higher x and y
         # values - due to place_mols() implmentation. It is the "top"
         # fragment.
-        top = frag1 if frag1[0] > frag2[0] else frag2
-        bottom = frag2 if top is frag1 else frag1
+        top = [3, 4, 5]
+        bottom = [0, 1, 2]
 
-        # In the top fragment find the bonder atom with the
-        # largest y value and connect it to the bonder atom in the
+        # In the top fragment find the fg with the
+        # largest y value and connect it to the fg in the
         # bottom fragment with the lowest y value. Note that the
         # connection must be registered as periodic, hence the
         # directions are 1/-1.
-        top_atom = max(top, key=lambda x: macro_mol.atom_coords(x)[1])
-        bottom_atom = min(bottom,
-                          key=lambda x: macro_mol.atom_coords(x)[1])
-        # The bonder atoms are registered by their index within
-        # `bonder_ids`. This is because del_atoms will change the
-        # atom ids but not the ordering of this list.
-        top_atom = macro_mol.bonder_ids.index(top_atom)
-        bottom_atom = macro_mol.bonder_ids.index(bottom_atom)
+        top_fg = max(top, key=lambda x: macro_mol.fg_centroid(x)[1])
+        bottom_fg = min(bottom,
+                        key=lambda x: macro_mol.fg_centroid(x)[1])
         macro_mol.periodic_bonds.append(
-                    PeriodicBond(top_atom, bottom_atom, [0, 1, 0]))
+                            PeriodicBond(top_fg, bottom_fg, [0, 1, 0]))
         # Do the same for the x-axis periodic bonds.
-        right_atom = max(top,
-                         key=lambda x: macro_mol.atom_coords(x)[0])
-        left_atom = min(bottom,
-                        key=lambda x: macro_mol.atom_coords(x)[0])
-        # The bonder atoms are registered by their index within
-        # `bonder_ids`. This is because del_atoms will change the
-        # atom ids.
-        right_atom = macro_mol.bonder_ids.index(right_atom)
-        left_atom = macro_mol.bonder_ids.index(left_atom)
+        right_fg = max(top,
+                       key=lambda x: macro_mol.fg_centroid(x)[0])
+        left_fg = min(bottom,
+                      key=lambda x: macro_mol.fg_centroid(x)[0])
         macro_mol.periodic_bonds.append(
-                        PeriodicBond(right_atom, left_atom, [1, 0, 0]))
+                            PeriodicBond(right_fg, left_fg, [1, 0, 0]))
 
         # For the bond which gets created directly, find the bonder
         # atom in the bottom fragment closest to the position of the
-        # top fragment. Create a bond between it and the bonder atom
-        # in the top fragment closest to the position of the bottom
-        # fragment.
-        bottom_bonder = min(bottom, key=lambda x:
-                            euclidean(self.vertices[1],
-                                      macro_mol.atom_coords(x)))
-        top_bonder = min(top, key=lambda x:
-                         euclidean(self.vertices[0],
-                                   macro_mol.atom_coords(x)))
-
-        emol = rdkit.EditableMol(macro_mol.mol)
-        bond_type = self.determine_bond_type(macro_mol,
-                                             top_bonder,
-                                             bottom_bonder)
-        emol.AddBond(top_bonder, bottom_bonder, bond_type)
-        macro_mol.mol = emol.GetMol()
+        # top fragment.
+        bottom_fg2 = min(bottom,
+                         key=lambda x: euclidean(
+                                            self.vertices[1],
+                                            macro_mol.fg_centroid(x)))
+        top_fg2 = min(top,
+                      key=lambda x: euclidean(
+                                            self.vertices[0],
+                                            macro_mol.fg_centroid(x)))
+        yield top_fg2, bottom_fg2
 
     def place_mols(self, macro_mol):
         """
@@ -855,7 +764,7 @@ class NoLinkerHoneycomb(NoLinkerCOFLattice):
 
         Notes
         -----
-        This method modifies `macro_mol`. An rdkit molecule of the
+        This method modifies `macro_mol`. An ``rdkit`` molecule of the
         unit cell with the building blocks not joined up is placed in
         the :attr:`~.MacroMolecule.mol` attribute.
 
@@ -881,7 +790,7 @@ class NoLinkerHoneycomb(NoLinkerCOFLattice):
         # Place and set orientation of the first building block.
         bb1.set_bonder_centroid(self.vertices[0])
         bb1.set_orientation2([0, 0, 1])
-        bb1.minimize_theta2(bb1.bonder_ids[0], [0, -1, 0], [0, 0, 1])
+        bb1.minimize_theta2(0, [0, -1, 0], [0, 0, 1])
         # Add to the macromolecule.
         add_fragment_props(bb1.mol,
                            macro_mol.building_blocks.index(bb1),
@@ -890,19 +799,15 @@ class NoLinkerHoneycomb(NoLinkerCOFLattice):
         # Place and set orientation of the second building block.
         bb2.set_bonder_centroid(self.vertices[1])
         bb2.set_orientation2([0, 0, 1])
-        bb2.minimize_theta2(bb2.bonder_ids[0], [0, 1, 0], [0, 0, 1])
+        bb2.minimize_theta2(0, [0, 1, 0], [0, 0, 1])
         # Add to the macromolecule.
         add_fragment_props(bb2.mol,
                            macro_mol.building_blocks.index(bb2),
                            0)
-        macro_mol.mol = rdkit.CombineMols(macro_mol.mol, bb2.mol)
-        # Add the bonder_ids prematurely for this topology. Needed for
-        # making supercells - see join_mols(). Using the ``super``
-        # version here because Periodic.save_ids() tries to update the
-        # atom ids in periodic bonds, which is needed later in the
-        # assembly process. Here only saving of the atom ids
-        # is needed, which is done by the ``super`` version.
-        super(macro_mol.__class__, macro_mol).save_ids()
+
+        mol = rdkit.Mol(bb2.mol)
+        self._update_fg_id(macro_mol, mol)
+        macro_mol.mol = rdkit.CombineMols(macro_mol.mol, mol)
 
 
 class Honeycomb(LinkerCOFLattice):
