10059e0adc00961bf1f602f373a4b984018eb412
Lukas Turcani <lukasturcani93@gmail.com>
2019-01-28 11:38:54 +0000

    added pandas to requirements

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ setup(name='stk',
                         'scikit-learn',
                         'psutil',
                         'pywindowx==0.0.1',
+                        'pandas',
                         'seaborn'],
       python_requires='>=3.6')
