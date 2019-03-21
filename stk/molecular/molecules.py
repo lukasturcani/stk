6b4d2b1ca55ad0aa443c15289ac5cdd5818ce28c
Lukas Turcani <lukasturcani93@gmail.com>
2019-03-21 11:44:32 +0000

    Added a position matrix method to Molecule.

--- stk/molecular/molecules.py
+++ stk/molecular/molecules.py
@@ -972,6 +972,26 @@ class Molecule:
                 "\n"
                 "$$$$\n")
 
+    def position_matrix(self, conformer=-1):
+        """
+        Returns a matrix holding the atomic positions of a conformer.
+
+        Parameters
+        ----------
+        conformer : :class:`int`, optional
+            The conformer to use.
+
+        Returns
+        -------
+        :class:`numpy.ndarray`
+            The array has the shape ``[3, n]``. Each column holds the
+            x, y and z coordinates of a bonder centroid. The index of
+            the column corresponds to the atom id.
+
+        """
+
+        return self.mol.GetConformer(conformer).GetPositions().T
+
     def same(self, other):
         """
         Check if `other` has the same molecular structure.
