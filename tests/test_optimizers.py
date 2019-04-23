e76456ca6a008418be4809f8f7bc366f0b526665
Lukas Turcani <lukasturcani93@gmail.com>
2019-04-24 00:13:07 +0300

    Polished up tests.

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -12,12 +12,12 @@ if not os.path.exists(odir):
 
 def test_raising_optimizer(tmp_polymer):
     etkdg = stk.RDKitEmbedder(rdkit.ETKDGv2())
-    always_raiser = stk.RaisingOptimizer(fn=etkdg.optimize,
+    always_raiser = stk.RaisingOptimizer(optimizer=etkdg.optimize,
                                          fail_chance=1)
     with pytest.raises(stk.RaisingOptimizerError):
         always_raiser.optimize(tmp_polymer)
 
-    never_raiser = stk.RaisingOptimizer(fn=etkdg.optimize,
+    never_raiser = stk.RaisingOptimizer(optimizer=etkdg.optimize,
                                         fail_chance=0)
     tmp_polymer.write(join(odir, 'raising_optimizer_before.mol'))
     never_raiser.optimize(tmp_polymer)
@@ -25,17 +25,17 @@ def test_raising_optimizer(tmp_polymer):
 
 
 def test_mmff(tmp_polymer):
-    tmp_polymer.write(join(odir, 'rdkit_force_field_before.mol'))
-    mmff = stk.RDKitForceField(rdkit.MMFFOptimizeMolecule)
+    tmp_polymer.write(join(odir, 'mmff_before.mol'))
+    mmff = stk.MMFF()
     mmff.optimize(tmp_polymer)
-    tmp_polymer.write(join(odir, 'rdkit_force_field_after.mol'))
+    tmp_polymer.write(join(odir, 'mmff_after.mol'))
 
 
 def test_uff(tmp_polymer):
-    tmp_polymer.write(join(odir, 'rdkit_force_field_before.mol'))
-    mmff = stk.RDKitForceField(rdkit.MMFFOptimizeMolecule)
+    tmp_polymer.write(join(odir, 'uff_before.mol'))
+    mmff = stk.UFF()
     mmff.optimize(tmp_polymer)
-    tmp_polymer.write(join(odir, 'rdkit_force_field_after.mol'))
+    tmp_polymer.write(join(odir, 'uff_after.mol'))
 
 
 def test_rdkit_embedder(tmp_polymer):
@@ -46,12 +46,12 @@ def test_rdkit_embedder(tmp_polymer):
 
 
 def test_optimize_sequence(tmp_polymer):
-    tmp_polymer.write(join(odir, 'pipeline_before.mol'))
+    tmp_polymer.write(join(odir, 'optimize_sequence_before.mol'))
     etkdg = stk.RDKitEmbedder(rdkit.ETKDG())
-    mmff = stk.RDKitForceField(rdkit.MMFFOptimizeMolecule)
-    pipeline = stk.OptimizerPipeline(etkdg, mmff)
+    mmff = stk.MMFF()
+    pipeline = stk.Sequence(etkdg, mmff)
     pipeline.optimize(tmp_polymer)
-    tmp_polymer.write(join(odir, 'pipeline_after.mol'))
+    tmp_polymer.write(join(odir, 'optimize_sequence_after.mol'))
 
 
 def test_cache_use(tmp_polymer):
@@ -64,8 +64,21 @@ def test_cache_use(tmp_polymer):
 
 
 def test_cage_optimizer_sequence(tmp_cc3, tmp_cage):
-    mmff = stk.RDKitForceField(rdkit.MMFFOptimizeMolecule)
+    mmff = stk.MMFF()
     etkdg = stk.RDKitEmbedder(rdkit.ETKDG())
     pipeline = stk.CageOptimizerPipeline(etkdg, mmff)
     pipeline.optimize(tmp_cage)
     pipeline.optimize(tmp_cc3)
+
+
+def test_try_catch_optimizer(tmp_amine2):
+    etkdg = stk.RDKitEmbedder(rdkit.ETKDGv2())
+    always_raiser = stk.RaisingOptimizer(optimizer=etkdg,
+                                         fail_chance=1)
+    success = stk.TryCatchOptimizer(try_optimizer=etkdg,
+                                    catch_optimizer=None)
+    try_catch = stk.TryCatchOptimizer(try_optimizer=always_raiser,
+                                      catch_optimizer=success)
+    tmp_amine2.write('try_catch_optimizer_before.mol', 1)
+    try_catch.optimize(tmp_amine2, 1)
+    tmp_amine2.write('try_catch_optimizer_after.mol', 1)
