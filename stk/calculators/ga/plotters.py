80628bba9795338958053afd5de3b27dc49952d1
Lukas Turcani <lukasturcani93@gmail.com>
2019-04-24 14:06:31 +0300

    Updated plotter docs.

--- stk/calculators/ga/plotters.py
+++ stk/calculators/ga/plotters.py
@@ -1,4 +1,23 @@
 """
+Defines plotters.
+
+Plotters are calculators which produce graphs. They generally do this
+by decorating other calculators to hook into them, collect data from
+them and then plot the results. To see examples of how to use plotters
+look at the documentation of the individual plotters. For example
+:class:`SelectionPlotter` and :class:`ProgressPlotter`.
+
+.. _`adding plotters`:
+
+Extending stk: Making new plotters.
+-----------------------------------
+
+New plotters should inherit :class:`Plotter` and define a
+:meth:`~Plotter.plot` method. Note that plotters do no necessarily have
+to make a plot when :meth:`~Plotter.plot` is made. For example,
+:class:`SelectionPlotter` plots every time the
+:class:`.Selector` it is used with becomes exhausted and it's
+:meth:`~SelectionPlotter.plot` does nothing..
 
 """
 
@@ -14,20 +33,100 @@ plt.switch_backend('agg')
 
 class Plotter:
     """
+    A base class for plotters.
 
     """
 
     def plot(self, progress):
+        """
+        Plots a graph.
+
+        Parameters
+        ----------
+        progress : :class:`.Population`
+            A :class:`.Population` where each generation of the GA is
+            a subpopulation.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
         raise NotImplementedError()
 
 
 class ProgressPlotter(Plotter):
     """
+    Plots how an attribute changes during a GA.
+
+    The produced plot will show the GA generations on the x axis and
+    the min, mean and max values of an attribute on the y axis.
+
+    Attributes
+    ----------
+    attr : :class:`str`
+        The name of the attribute which is plotted. It must be an
+        attribute on the :class:`.Molecule` objects made by the GA.
+
+    y_label : :class:`str`
+        The y label for the produced graph.
+
+    default : :class:`float`
+        When a molecule in the population does not have attribute
+        :attr:`attr`, this is the default value to use for it.
+
+    Examples
+    --------
+    Plot how fitness value changes with GA generations.
+
+    .. code-block:: python
+
+        # progress has subpopulations where each subpopulation is a
+        # generation of a GA.
+        progress = Population(...)
+
+        # Make the plotter which plots the fitness change across
+        # generations.
+        plotter = ProgressPlotter(attr='fitness',
+                                  y_label='Fitness',
+                                  default=1e-4)
+        plotter.plot(progress)
+
+    Plot how the number of atoms changes with GA generations.
+
+    .. code-block:: python
+
+        # progress has subpopulations where each subpopulation is a
+        # generation of a GA.
+        progress = Population(...)
+
+        # Make the plotter which plots the number of atoms across
+        # generations. Note that the GA must create the num_atoms
+        # attribute on the molecules. This can be done in the fitness
+        # function, for example.
+        plotter = ProgressPlotter(attr='num_atoms',
+                                  y_label='Number of Atoms',
+                                  default=0)
+        plotter.plot(progress)
 
     """
 
     def __init__(self, attr, y_label, default):
         """
+        Initializes a :class:`ProgressPlotter` instance.
+
+        Parameters
+        ----------
+        attr : :class:`str`
+            The name of the attribute which is plotted. It must be an
+            attribute on the :class:`.Molecule` objects made by the GA.
+
+        y_label : :class:`str`
+            The y label for the produced graph.
+
+        default : :class:`float`
+            When a molecule in the population does not have attribute
+            :attr:`attr`, this is the default value to use for it.
 
         """
 
@@ -37,6 +136,17 @@ class ProgressPlotter(Plotter):
 
     def plot(self, progress):
         """
+        Plots a progress plot.
+
+        Parameters
+        ----------
+        progress : :class:`.Population`
+            A :class:`.Population` where each generation of the GA is
+            a subpopulation.
+
+        Returns
+        -------
+        None : :class:`NoneType`
 
         """
 
@@ -86,32 +196,93 @@ class ProgressPlotter(Plotter):
 
 class SelectionPlotter(Plotter):
     """
-    Saves a ``.png`` file holding a plot of `counter`.
+    Plots which molecules a :class:`.Selector` selects.
 
-    The counter should hold the number of times a certain population
-    member was selected.
-
-    Parameters
+    Attributes
     ----------
-    counter : :class:`collections.Counter`
-        A counter of which members of a population were selected.
+    filename : :class:`str`
+        The basename of the files. This means it should exclude
+        file extensions.
 
-    plot_name : :class:`str`
-        The full path of the ``.png`` where the plot is to be saved.
+    selector : :class:`.Selector`
+        The :class:`.Selector` whose selection of molecules is
+        plotted.
 
-    Returns
-    -------
-    None : :class:`NoneType`
+    Examples
+    --------
+    .. code-block:: python
+
+        # Make a population of molecules.
+        pop = Population(...)
+
+        # Make a selector.
+        roulette = Roulette(num=10)
+
+        # Make a plotter.
+        SelectionPlotter('roulette_counter', roulette)
+
+        # Select the molecules.
+        selected = list(roulette.select(pop))
+
+        # There should now be a file called "roulette_counter_1.png"
+        # which shows a graph of all the selected molecules.
+
+        # Make another population.
+        pop2 = Population(...)
+
+        # Select molecules from this other population.
+        selected2 = list(roulette.select(pop2))
+
+        # There should now be a file called "roulette_counter_2.png"
+        # which shows a graph of all the selected molecules.
+
+        # Select from the original population again.
+        selected3 = list(roulette.select(pop))
+
+        # There should now be a file called "roulette_counter_3.png"
+        # which shows a graph of all the selected molecules.
+
+        # And so on every time you use "roulette.select()".
 
     """
 
     def __init__(self, filename, selector):
+        """
+        Initializes a :class:`SelectionPlotter` instance.
+
+        Parameters
+        ----------
+        filename : :class:`str`
+            The basename of the files. This means it should exclude
+            file extensions.
+
+        selector : :class:`.Selector`
+            The :class:`.Selector` whose selection of molecules is
+            plotted.
+
+        """
+
         self.plots = 0
         self.filename = filename
         selector.select = self.update_counter(selector.select)
 
     def update_counter(self, select):
         """
+        Decorates :meth:`.Selector.select`.
+
+        This is a decorator which makes sure that every time
+        :meth:`.Selector.select` selects a :class:`.Molecule` a
+        counter keeping track of selected molecules is updated.
+
+        Parameters
+        ----------
+        select : :class:`function`
+            The :meth:`Selector.select` method to decorate.
+
+        Returns
+        -------
+        :class:`function`
+            The decorated :meth:`.Selector.select` method.
 
         """
 
@@ -133,6 +304,21 @@ class SelectionPlotter(Plotter):
         return inner
 
     def _plot(self, counter):
+        """
+        Plots a selection counter.
+
+        Parameters
+        ----------
+        counter : :class:`Counter`
+            A counter specifying which molecules were selected and how
+            many times.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
+
         self.plots += 1
         sns.set(style='darkgrid')
         fig = plt.figure()
@@ -169,6 +355,20 @@ class SelectionPlotter(Plotter):
 
     def plot(self, progress):
         """
+        Does nothing.
+
+        This :class:`Plotter` creates a plot each time :attr:`selector`
+        is finished selecting molecules.
+
+        Parameters
+        ----------
+        progress : :class:`.Population`
+            A :class:`.Population` where each generation of the GA is
+            a subpopulation.
+
+        Returns
+        -------
+        None : :class:`NoneType`
 
         """
 
