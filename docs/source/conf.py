906369697a42f91540def0fa084e2a31e0fbf3b8
Lukas Turcani <lukasturcani93@gmail.com>
2019-04-30 21:31:00 +0300

    flags to options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,12 +35,12 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon']
 
-autodoc_default_flags = [
-    'private-members',
-    'special_members',
-    'inherited-members',
-    'show-inheritance'
-]
+autodoc_default_options = {
+    'private-members': True,
+    'special_members': True,
+    'inherited-members': True,
+    'show-inheritance': True
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
