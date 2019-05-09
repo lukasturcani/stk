7db1cd39b0bce162572825456055e0a78301a540
Filip Sczypiński <47000594+fiszczyp@users.noreply.github.com>
2019-05-09 17:15:24 +0100

    Added macrocycle classes.

--- stk/molecular/molecules.py
+++ stk/molecular/molecules.py
@@ -3393,6 +3393,87 @@ class Cage(MacroMolecule):
         return valid_windows[:self.topology.n_windows]
 
 
+class MacrocycleBase:
+    """
+    Used to represent macrocycles.
+
+    Macrocycles are molecules that contain a large cycle. A simple
+    example is a polymer with two ends connected together. This base
+    class allows for the macrocyles to be initialised as either
+    :class:`StructUnit` or :class:`MacroMolecule` but to be equally
+    identified as macrocycles.
+
+    """
+
+    def macro_atoms(self, xyz=None, conformer=-1):
+        """
+        Find the macrocyclic atoms in the molecule.
+
+        Coordinates of the atoms comprising the largest ring in the
+        macrocycle are returned and the xyz coordinates file containing
+        only those atoms can be saved. The method uses
+        :meth:`rdkit.GetSymSSSR()` to identify the Smallest Set of
+        Symmetric Rings, so the results are not unique. This should not
+        be a problem in most applications.
+
+        Parameters
+        ----------
+        xyz : :class:`str`
+            A path where the xyz file should be saved. If None then
+            no file is produced. InChKey is used as the filename.
+
+        Returns
+        -------
+        :class:`list` of :class:`list` of :class:`float`
+            Coordinates of the atoms in the largest ring in the format
+            [atomic_number, *xyz].
+
+        :class:`list` of :class:`int`
+            Atom ids of the atoms comprising the largest ring.
+
+        """
+        ssr = rdkit.GetSymmSSSR(self.mol)
+        conf = self.mol.GetConformer(conformer)
+        ring_atom_ids = list(max(ssr, key=len))
+        macrocycle = (self.mol.GetAtomWithIdx(i)
+                      for i in ring_atom_ids)
+        macro_coords = [[atom.GetAtomicNum(),
+                         *conf.GetAtomPosition(atom.GetIdx())]
+                        for atom in macrocycle]
+
+        if xyz is not None:
+            name = rdkit.MolToInchiKey(self.mol)
+            xyz_file = f'{len(macro_coords)}\n\n'
+
+            for anum, *coords in macro_coords:
+                xyz_file += f'{anum} {coords[0]} {coords[1]} '
+                xyz_file += f'{coords[2]}\n'
+
+            if not os.path.exists(xyz):
+                os.makedirs(xyz)
+
+            with open(f'{xyz}/{name}.xyz', 'w') as f:
+                f.write(xyz_file)
+
+        return macro_coords, ring_atom_ids
+
+
+class MacrocycleStructUnit(MacrocycleBase, StructUnit):
+    """
+    Used to represent macrocyles loaded as :class:`StructUnit`s.
+
+    """
+    pass
+
+
+class Macrocycle(MacrocycleBase, MacroMolecule):
+    """
+    Used to represent macrocycles constructed by ``stk``.
+
+    """
+    pass
+
+
 class Polymer(MacroMolecule):
     """
     Used to represent polymers.
