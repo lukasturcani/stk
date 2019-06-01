24c61c9ffab14c82daf2048681ccb42061b84d16
Filip Szczypiński <47000594+fiszczyp@users.noreply.github.com>
2019-06-01 10:25:50 +0100

    Test macrocycle using atom index.

--- tests/test_macrocycle.py
+++ tests/test_macrocycle.py
@@ -17,7 +17,7 @@ def test_cycle_coords(tmp_cycle):
     tmp_cycle.set_position_from_matrix(new_pos_mat.T, 0)
 
     for i, coords in enumerate(tmp_cycle.cycle_coords(conformer=0), 1):
-        assert type(coords[0]) == int
+        assert coords[0] in catoms
         assert np.allclose(coords[1], np.array([0, 0, 0]), atol=1e-5)
         assert np.allclose(coords[2], np.array([0, 0, 0]), atol=1e-5)
         assert np.allclose(coords[3], np.array([0, 0, 0]), atol=1e-5)
