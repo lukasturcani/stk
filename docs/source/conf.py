df30e3f4d5a9fb44efa4e5b7e36c523a390cce44
Lukas Turcani <lukasturcani93@gmail.com>
2019-06-05 15:43:19 +0100

    __init__ methods should be documented now.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,18 @@ autodoc_default_options = {
     'show-inheritance': True
 }
 
+
+# Document __init__.
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
