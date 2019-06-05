51734a97c9ed1dc09542dc2176df80b30e6f07e8
Lukas Turcani <lukasturcani93@gmail.com>
2019-06-05 21:21:51 +0100

    MacroModel tests check for energy decrease.

--- tests/test_macromodel.py
+++ tests/test_macromodel.py
@@ -25,33 +25,52 @@ if not os.path.exists(outdir):
 
 @macromodel
 def test_restricted_force_field(tmp_cc3, macromodel_path):
+
+    mm_energy = stk.MacroModelEnergy(macromodel_path, force_field=16)
+    init_energy = mm_energy.energy(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'rmm_ff_before.mol'), conformer=0)
 
-    mm = stk.MacroModelForceField(macromodel_path=macromodel_path,
-                                  output_dir='rmm_ff',
-                                  restricted=True,
-                                  minimum_gradient=1)
+    mm = stk.MacroModelForceField(
+        macromodel_path=macromodel_path,
+        output_dir='rmm_ff',
+        restricted=True,
+        minimum_gradient=1,
+        force_field=16
+    )
     mm.optimize(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'rmm_ff_after.mol'), conformer=0)
+
+    assert mm_energy.energy(tmp_cc3, conformer=0) < init_energy
 
 
 @macromodel
 def test_unrestricted_force_field(tmp_cc3, macromodel_path):
+
+    mm_energy = stk.MacroModelEnergy(macromodel_path, force_field=16)
+    init_energy = mm_energy.energy(tmp_cc3, conformer=0)
     tmp_cc3.write(
         path=join(outdir, 'umm_ff_before.mol'),
         conformer=0
     )
 
-    mm = stk.MacroModelForceField(macromodel_path=macromodel_path,
-                                  output_dir='umm_ff',
-                                  restricted=False,
-                                  minimum_gradient=1)
+    mm = stk.MacroModelForceField(
+        macromodel_path=macromodel_path,
+        output_dir='umm_ff',
+        restricted=False,
+        minimum_gradient=1,
+        force_field=16
+    )
     mm.optimize(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'umm_ff_after.mol'), conformer=0)
+
+    assert mm_energy.energy(tmp_cc3, conformer=0) < init_energy
 
 
 @macromodel
 def test_restricted_md(tmp_cc3, macromodel_path):
+
+    mm_energy = stk.MacroModelEnergy(macromodel_path, force_field=16)
+    init_energy = mm_energy.energy(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'rmm_md_before.mol'), conformer=0)
 
     # Freeze one of the bonders.
@@ -60,37 +79,48 @@ def test_restricted_md(tmp_cc3, macromodel_path):
     for neighbor in tmp_cc3.mol.GetAtomWithIdx(bonder).GetNeighbors():
         restricted_bonds.append(frozenset((bonder, neighbor.GetIdx())))
 
-    mm = stk.MacroModelMD(macromodel_path=macromodel_path,
-                          output_dir='rmm_md',
-                          minimum_gradient=1,
-                          simulation_time=20,
-                          eq_time=2,
-                          conformers=2,
-                          time_step=0.1,
-                          restricted_bonds=restricted_bonds)
+    mm = stk.MacroModelMD(
+        macromodel_path=macromodel_path,
+        output_dir='rmm_md',
+        minimum_gradient=1,
+        simulation_time=20,
+        eq_time=2,
+        conformers=2,
+        time_step=0.1,
+        force_field=16,
+        restricted_bonds=restricted_bonds
+    )
     mm.optimize(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'rmm_md_after.mol'), conformer=0)
+
+    assert mm_energy.energy(tmp_cc3, conformer=0) < init_energy
 
 
 @macromodel
 def test_unrestricted_md(tmp_cc3, macromodel_path):
+
+    mm_energy = stk.MacroModelEnergy(macromodel_path, force_field=16)
+    init_energy = mm_energy.energy(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'umm_md_before.mol'), conformer=0)
 
-    mm = stk.MacroModelMD(macromodel_path=macromodel_path,
-                          output_dir='umm_md',
-                          minimum_gradient=1,
-                          simulation_time=20,
-                          eq_time=2,
-                          conformers=2,
-                          time_step=0.1)
+    mm = stk.MacroModelMD(
+        macromodel_path=macromodel_path,
+        output_dir='umm_md',
+        minimum_gradient=1,
+        simulation_time=20,
+        eq_time=2,
+        conformers=2,
+        time_step=0.1,
+        force_field=16
+    )
     mm.optimize(tmp_cc3, conformer=0)
     tmp_cc3.write(join(outdir, 'umm_md_after.mol'), conformer=0)
+
+    assert mm_energy.energy(tmp_cc3, conformer=0) < init_energy
 
 
 @macromodel
 def test_energy(amine2, macromodel_path):
     mm = stk.MacroModelEnergy(macromodel_path, 'energy_calc')
     a = mm.energy(amine2)
-    assert np.allclose(a=a,
-                       b=49.0655,
-                       atol=1e-2)
+    assert np.allclose(a=a, b=49.0655, atol=1e-2)
