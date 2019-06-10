64e351a69b31b424036225eae9a656fc82c00ed4
Lukas Turcani <lukasturcani93@gmail.com>
2019-06-10 11:02:31 +0100

    Updated docs.

--- stk/calculators/optimization/__init__.py
+++ stk/calculators/optimization/__init__.py
@@ -1,7 +1,9 @@
 """
+Defines optimizers.
+
 Optimizers are objects used to optimize molecules. Each optimizer is
 initialized with some settings and used to optimize a molecule
-with :meth:`~Optimizer.optimize`.
+with :meth:`~.Optimizer.optimize`.
 
 .. code-block:: python
 
@@ -21,7 +23,7 @@ with :meth:`~Optimizer.optimize`.
 Sometimes it is desirable to chain multiple optimizations, one after
 another. For example, before running an optimization, it may be
 desirable to embed a molecule first, to generate an initial structure.
-:class:`OptimizerSequence` may be used for this.
+:class:`.OptimizerSequence` may be used for this.
 
 .. code-block:: python
 
@@ -32,10 +34,10 @@ desirable to embed a molecule first, to generate an initial structure.
     # Run each optimizer in sequence.
     optimizer_sequence.optimize(polymer)
 
-By default, running :meth:`.Optimizer.optimize` twice in a row will
-perform an optimization a second time on a molecule. If we want to
-skip optimizations on molecules which have already been optimized
-we can use the :attr:`~.Optimizer.use_cache` flag.
+By default, running :meth:`.Optimizer.optimize` twice on the same
+molecule will perform an optimization a second time on a molecule. If
+we want to skip optimizations on molecules which have already been
+optimized we can use the :attr:`~.Optimizer.use_cache` flag.
 
 .. code-block:: python
 
@@ -47,7 +49,7 @@ we can use the :attr:`~.Optimizer.use_cache` flag.
 
 Caching is done on a per :class:`.Optimizer` basis. Just because the
 molecule has been cached by one :class:`.Optimizer` instance does not
-mean that a different :class:`Optimizer` instance will no longer
+mean that a different :class:`.Optimizer` instance will no longer
 optimize the molecule.
 
 Available optimizers.
@@ -61,7 +63,7 @@ Available optimizers.
 #. :class:`.MacroModelForceField`
 #. :class:`.MacroModelMD`
 #. :class:`.MOPAC`
-#. :class:.`OptimizerSequence`
+#. :class:`.OptimizerSequence`
 #. :class:`.CageOptimizerSequence`
 #. :class:`.TryCatchOptimizer`
 #. :class:`.RaisingOptimizer`
@@ -73,12 +75,12 @@ Extending stk: Making new optimizers.
 
 New optimizers can be made by simply making a class which inherits the
 :class:`.Optimizer` class. In addition to this, the new class must
-define a :meth:`~Optimize.optimize` method. The method must take 2
+define a :meth:`~.Optimizer.optimize` method. The method must take 2
 arguments a mandatory `mol` argument and an optional `conformer`
 argument. :meth:`~.Optimizer.optimize` will take a molecule and change
 its structure in whatever way it likes. Beyond this there are no
 requirements. New optimizers can be added into the :mod:`.optimizers`
-submodule or in a new submodule.
+submodule or into a new submodule.
 
 """
 
