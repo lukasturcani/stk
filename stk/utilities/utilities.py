Fixed normalization of vectors.

stk/utilities/utilities.py
@@ -1015,9 +1015,12 @@ def vector_theta(vector1, vector2):
     denominator = (np.linalg.norm(vector1) * np.linalg.norm(vector2))
     # This if statement prevents returns of NaN due to floating point
     # incurracy.
-    if np.isclose(numerator, denominator, atol=1e-8):
+    term = numerator/denominator
+    if np.isclose(term, 1, atol=1e-8):
         return 0.0
-    return np.arccos(numerator/denominator)
+    if np.isclose(term, -1, atol=1e-8):
+        return 2*np.pi
+    return np.arccos(term)
 
 
 class XTBInvalidSolventError(Exception):