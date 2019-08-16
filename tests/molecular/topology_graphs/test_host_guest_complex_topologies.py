316d02b7f856943b52457e83b10bd530b56a909e
Lukas Turcani <lukasturcani93@gmail.com>
2019-08-16 07:14:44 +0200

    wip

--- tests/molecular/topology_graphs/test_host_guest_complex_topologies.py
+++ tests/molecular/topology_graphs/test_host_guest_complex_topologies.py
@@ -1,17 +1,62 @@
-def test_cage_complex(amine2, amine2_alt1, aldehyde3, chained_c60):
-    c = stk.Cage([amine2, amine2_alt1, aldehyde3],
-                 stk.FourPlusSix(bb_positions={
-                     amine2: [5],
-                     amine2_alt1: [0, 1, 2, 3, 4],
-                     aldehyde3: [0, 1, 2, 3]
-                 }))
+import stk
+import os
+from os.path import join
+import numpy as np
 
+
+from ..._test_utilities import _test_dump_and_load
+
+
+test_dir = 'host_guest_complex_topology_tests_output'
+if not os.path.exists(test_dir):
+    os.mkdir(test_dir)
+
+
+def test_vertex():
+    complex1 = stk.host_guest_complex.Complex()
+
+    complex2 = stk.host_guest_complex.Complex(
+        axis=[1, 0, 0],
+        angle=np.pi/4,
+        displacement=23
+    )
+
+
+def _create_host(amine2, amine2_alt1, aldehyde3):
+    four_plus_six = stk.cage.FourPlusSix()
+    return stk.ConstructedMolecule(
+        building_blocks=[amine2, amine2_alt1, aldehyde3],
+        topology_graph=four_plus_six,
+        building_block_vertices={
+            amine2: four_plus_six.vertices[5:],
+            amine2_alt1: four_plus_six.vertices[4:5],
+            aldehyde3: four_plus_six.vertices[:4]
+        }
+    )
+
+
+def _test_construction(complex_, i, n):
+    complex_.write(join(test_dir, f'complex_{i}.mol'))
+
+
+def test_host_guest_complex(
+    amine2,
+    amine2_alt1,
+    aldehyde3,
+    chained_c60
+):
+
+    host = _create_host(amine2, amine2_alt1, aldehyde3)
     n = 3
     for i in range(n):
-        cage_complex = stk.CageComplex(
-            [c, chained_c60],
-            stk.CageWithGuest(axis=[1, 0, 0],
-                              angle=2*np.pi*i/n,
-                              displacement=[2*i, 0, 0])
+        complex_ = stk.ConstructedMolecule(
+            building_blocks=[host, chained_c60],
+            topology_graph=stk.host_guest_complex.Complex(
+                axis=[1, 0, 0],
+                angle=2*np.pi*i/n,
+                displacement=[2**i, 0, 0]
+            )
         )
-        cage_complex.write(join(test_dir, f'cage_with_guest_{i}.mol'))
+
+        _test_construction(complex_, i, n)
+        _test_dump_and_load(test_dir, complex_)
--- tests/molecular/topology_graphs/test_linear_topology.py
+++ tests/molecular/topology_graphs/test_linear_topology.py
@@ -1,107 +1,26 @@
 import os
 import stk
 import numpy as np
-from os.path import join
-import itertools as it
+
+
+from ..._test_utilities import _test_dump_and_load
+
 
 test_dir = 'linear_topology_tests_output'
 if not os.path.exists(test_dir):
     os.mkdir(test_dir)
 
 
-def test_dump_and_load(tmp_polymer):
-
-    path = join(test_dir, 'mol.dump')
-
-    tmp_polymer.test_attr1 = 'something'
-    tmp_polymer.test_attr2 = 12
-    tmp_polymer.test_attr3 = ['12', 'something', 21]
-    tmp_polymer.test_attr4 = 'skip'
-
-    bb1, bb2 = tmp_polymer.building_block_vertices
-    bb1.test_attr1 = 1232
-    bb2.test_attr5 = 'alpha'
-
-    include_attrs = [
-        'test_attr1',
-        'test_attr2',
-        'test_attr3',
-        'test_attr5'
-    ]
-
-    # Add some custom atom properties.
-    tmp_polymer.atoms[0].some_prop = 'custom atom prop'
-    # Add some custom bond properties
-    tmp_polymer.bonds[2].other_prop = 1999
-
-    tmp_polymer.dump(
-        path=path,
-        include_attrs=include_attrs,
-        ignore_missing_attrs=True
-    )
-    mol2 = stk.Molecule.load(path)
-
-    assert tmp_polymer.__class__ is mol2.__class__
-    assert tmp_polymer is not mol2
-    fgs = it.zip_longest(tmp_polymer.func_groups, mol2.func_groups)
-    for fg1, fg2 in fgs:
-        atoms = it.zip_longest(fg1.atoms, fg2.atoms)
-        bonders = it.zip_longest(fg1.bonders, fg2.bonders)
-        deleters = it.zip_longest(fg1.deleters, fg2.deleters)
-        for a1, a2 in it.chain(atoms, bonders, deleters):
-            assert a1.__class__ is a2.__class__
-            assert a1.id is a1.id
-
-    for a1, a2 in zip(tmp_polymer.atoms, mol2.atoms):
-        assert a1.__class__ is a2.__class__
-        d1, d2 = vars(a1), vars(a2)
-        bb1, bb2 = d1.pop('building_block'), d2.pop('building_block')
-        assert d1 == d2
-        assert bb1.is_identical(bb2)
-
-    for b1, b2 in zip(tmp_polymer.bonds, mol2.bonds):
-        assert b1.__class__ is b2.__class__
-        d1, d2 = vars(b1), vars(b2)
-        assert repr(d1.pop('atom1')) == repr(d2.pop('atom1'))
-        assert repr(d1.pop('atom2')) == repr(d2.pop('atom2'))
-        assert d1 == d2
-
-    assert (
-        repr(tmp_polymer.topology_graph) == repr(mol2.topology_graph)
-    )
-    assert (
-        len(tmp_polymer.construction_bonds) ==
-        len(mol2.construction_bonds)
-    )
-    assert tmp_polymer.test_attr1 == mol2.test_attr1
-    assert tmp_polymer.test_attr2 == mol2.test_attr2
-    assert tmp_polymer.test_attr3 == mol2.test_attr3
-    assert not hasattr(mol2, 'test_attr4')
-
-    bbs1 = list(tmp_polymer.building_block_vertices.keys())
-    bbs2 = list(mol2.building_block_vertices.keys())
-    for bb1, bb2 in it.zip_longest(bbs1, bbs2):
-        assert bb1.is_identical(bb2)
-        assert bb1 is not bb2
-        bb1_count = tmp_polymer.building_block_counter[bb1]
-        bb2_count = mol2.building_block_counter[bb2]
-        assert bb1_count == bb2_count
-
-    assert bbs2[0].test_attr1 == 1232
-    assert bbs2[1].test_attr5 == 'alpha'
-
-    mol3 = stk.Molecule.load(path, use_cache=True)
-    mol4 = stk.Molecule.load(path, use_cache=True)
-    assert mol3 is mol4
-
-
-def test_linear_vertex(tmp_amine2):
-    t = stk.polymer.Linear(
+def test_vertex(tmp_amine2):
+    chain = stk.polymer.Linear(
         repeating_unit='AB',
         orientations=[0, 0],
         n=3
     )
-    v1, v2 = t.vertices[2:4]
+
+    for vertex in chain.vertices:
+        _test_placement(vertex, tmp_amine2)
+        _test_assignment(vertex, tmp-amine2)
 
     new_coords = v1.place_building_block(tmp_amine2)
     assert np.allclose(
@@ -132,23 +51,32 @@ def test_construction(amine2, aldehyde2, boronic_acid2, diol2):
     repeat_units = 3
     monomer_joins = 2*repeat_units - 1
 
-    p1 = stk.ConstructedMolecule(
-        building_blocks=[amine2, aldehyde2],
-        topology_graph=stk.polymer.Linear(
-            repeating_unit='AB',
-            orientations=[1, 1],
-            n=repeat_units
+    polymers = (
+        stk.ConstructedMolecule(
+            building_blocks=[amine2, aldehyde2],
+            topology_graph=stk.polymer.Linear(
+                repeating_unit='AB',
+                orientations=[1, 1],
+                n=repeat_units
+            )
+        ),
+        stk.ConstructedMolecule(
+            building_blocks=[boronic_acid2, diol2],
+            topology_graph=stk.polymer.Linear(
+                repeating_unit='AB',
+                orientations=[0, 0],
+                n=repeat_units
+            )
         )
+
     )
 
-    p2 = stk.ConstructedMolecule(
-        building_blocks=[boronic_acid2, diol2],
-        topology_graph=stk.polymer.Linear(
-            repeating_unit='AB',
-            orientations=[0, 0],
-            n=repeat_units
-        )
-    )
+    for polymer in polymers:
+        _test_construction(polymer)
+
+    _test_dump_and_load(test_dir, p1)
+
+    _test_dump_and_load(test_dir, p2)
 
     path = os.path.join(test_dir, 'p1.mol')
     p1.write(path)
