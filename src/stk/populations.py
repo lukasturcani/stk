5dd5009b945a958f0968887468730377884b686a
Lukas Turcani <lukasturcani93@gmail.com>
2019-08-18 01:36:19 +0200

    all tests passing

--- src/stk/populations.py
+++ src/stk/populations.py
@@ -943,7 +943,7 @@ class Population:
         sorted_opt = sorted(optimized, key=lambda m: repr(m))
         sorted_pop = sorted(self, key=lambda m: repr(m))
         for old, new in zip(sorted_pop, sorted_opt):
-            assert old.identity_key() == new.identity_key()
+            assert old.get_identity_key() == new.get_identity_key()
             old.__dict__ = dict(vars(new))
             if optimizer.is_caching():
                 optimizer.add_to_cache(old)
