9e048f0ea7c3355ad2008a56d31b695cb5a32a4e
Lukas Turcani <lukasturcani93@gmail.com>
2019-08-19 18:06:53 +0200

    renames

--- tests/calculators/ea/test_terminators.py
+++ tests/calculators/ea/test_terminators.py
@@ -1,32 +1,32 @@
 import stk
 
 
-def test_any_exiter():
+def test_any_terminator():
     pop1 = stk.Population(*(stk.Population() for i in range(5)))
     pop2 = stk.Population(*(stk.Population() for i in range(10)))
     pop3 = stk.Population(*(stk.Population() for i in range(20)))
 
-    exiter = stk.AnyExiter(
+    terminator = stk.AnyTerminator(
         stk.NumGenerations(7),
         stk.NumGenerations(15)
     )
-    assert not exiter.exit(pop1)
-    assert exiter.exit(pop2)
-    assert exiter.exit(pop3)
+    assert not terminator.terminate(pop1)
+    assert terminator.terminate(pop2)
+    assert terminator.terminate(pop3)
 
 
-def test_all_exiters():
+def test_all_terminators():
     pop1 = stk.Population(*(stk.Population() for i in range(5)))
     pop2 = stk.Population(*(stk.Population() for i in range(10)))
     pop3 = stk.Population(*(stk.Population() for i in range(20)))
 
-    exiter = stk.AllExiters(
+    terminator = stk.AllTerminators(
         stk.NumGenerations(7),
         stk.NumGenerations(15)
     )
-    assert not exiter.exit(pop1)
-    assert not exiter.exit(pop2)
-    assert exiter.exit(pop3)
+    assert not terminator.terminate(pop1)
+    assert not terminator.terminate(pop2)
+    assert terminator.terminate(pop3)
 
 
 def test_num_generations():
@@ -34,21 +34,21 @@ def test_num_generations():
     pop2 = stk.Population(*(stk.Population() for i in range(10)))
     pop3 = stk.Population(*(stk.Population() for i in range(15)))
 
-    exiter = stk.NumGenerations(10)
+    terminator = stk.NumGenerations(10)
 
-    assert not exiter.exit(pop1)
-    assert exiter.exit(pop2)
-    assert exiter.exit(pop3)
+    assert not terminator.terminate(pop1)
+    assert terminator.terminate(pop2)
+    assert terminator.terminate(pop3)
 
 
 def test_molecule_present(amine2):
     pop1 = stk.Population(stk.Population())
     pop2 = stk.Population(stk.Population(amine2))
 
-    exiter = stk.MoleculePresent(amine2)
+    terminator = stk.MoleculePresent(amine2)
 
-    assert not exiter.exit(pop1)
-    assert exiter.exit(pop2)
+    assert not terminator.terminate(pop1)
+    assert terminator.terminate(pop2)
 
 
 def test_fitness_plateau():
@@ -65,14 +65,14 @@ def test_fitness_plateau():
     for i, mol in enumerate(pop):
         mol.fitness = i
 
-    exiter = stk.FitnessPlateau(2)
+    terminator = stk.FitnessPlateau(2)
 
-    assert not exiter.exit(pop)
+    assert not terminator.terminate(pop)
 
     for mol in pop:
         mol.fitness = 2
-    assert not exiter.exit(pop)
+    assert not terminator.terminate(pop)
 
     pop2 = stk.Population(*pop)
     pop3 = stk.Population(pop2, pop2)
-    assert exiter.exit(pop3)
+    assert terminator.terminate(pop3)
