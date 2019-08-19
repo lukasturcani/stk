926776d74dd9480c7e99c8dc0e6202d3be5c779f
Lukas Turcani <lukasturcani93@gmail.com>
2019-08-19 19:21:54 +0200

    api

--- tests/data/ea_input_files/parallel.py
+++ tests/data/ea_input_files/parallel.py
@@ -107,7 +107,7 @@ fitness_normalizer = stk.NormalizerSequence(
 # Exit condition.
 # #####################################################################
 
-exiter = stk.NumGenerations(25)
+terminator = stk.NumGenerations(25)
 
 # #####################################################################
 # Make plotters.
