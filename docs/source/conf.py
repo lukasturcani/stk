2a17335eb73475a97108d4847f66027d705b0fe3
Lukas Turcani <lukasturcani93@gmail.com>
2019-08-19 14:59:49 +0200

    Updated docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ autodoc_default_options = {
 }
 
 add_module_names = False
+numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
