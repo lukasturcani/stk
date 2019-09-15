a879640b9050232db2f252c9d86587154ab4c19f
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-09-15 21:38:35 +0100

    tets mono and bidentate metal complexes

--- tests/molecular/test_script.py
+++ tests/molecular/test_script.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # Distributed under the terms of the MIT License.
 import stk
-import numpy as np
 from rdkit.Chem import AllChem as Chem
 
 
@@ -42,27 +41,75 @@ def main():
         coordination_info=metal_coord_info
     )
     ligand = stk.BuildingBlock(
-        'c1cc(-c2ccc(-c3ccncc3)cc2)ccn1',
-        functional_groups=['pyridine_N_metal']
+        'NCCN',
+        functional_groups=['amine_metal']
     )
-    print(metal)
-    print(ligand)
 
-    import sys
-    sys.exit()
-    sqpl = stk.metal_complex.SquarePlanar()
+    print(metal)
+    print(metal.func_groups)
+    print(ligand)
+    print(ligand.func_groups)
+    ligand.write('lig.pdb')
+    input()
+
+    # v0 = stk.metal_complex.SquarePlanarBidentate.vertices[0]
+    # v1 = stk.metal_complex.SquarePlanarBidentate.vertices[1]
+    # v2 = stk.metal_complex.SquarePlanarBidentate.vertices[2]
+    sqpl = stk.metal_complex.SquarePlanarBidentate(
+        unsatured_vertices=[],
+        # vertex_alignments={
+        #
+        # }
+    )
+    print(sqpl)
+    print('--------------------------------------------------------')
     pdl2_sqpl_complex = stk.ConstructedMolecule(
         building_blocks=[metal, ligand],
         topology_graph=sqpl,
         building_block_vertices={
-            metal: sqpl.vertices[0],
-            ligand: sqpl.vertices[1:2]
+            metal: tuple([sqpl.vertices[0]]),
+            ligand: sqpl.vertices[1:]
         }
     )
+    print('--------------------------------------------------------')
     print(pdl2_sqpl_complex)
+    print(pdl2_sqpl_complex.get_position_matrix()[:5])
     pdl2_sqpl_complex.write('metal_complex.mol')
     pdl2_sqpl_complex.write('metal_complex.pdb')
 
+    ligand = stk.BuildingBlock(
+        'c1cc(-c2ccc(-c3ccncc3)cc2)ccn1',
+        functional_groups=['pyridine_N_metal']
+    )
+    # Handle multiple functional groups.
+    ligand.func_groups = tuple(i for i in [ligand.func_groups[0]])
+    print(metal)
+    print(metal.func_groups)
+    print(ligand)
+    print(ligand.func_groups)
+    ligand.write('lig1.pdb')
+    input()
+
+    sqpl = stk.metal_complex.SquarePlanarMonodentate(
+        # unsatured_vertices=[3, 4]
+    )
+    print(sqpl)
+    print('--------------------------------------------------------')
+    pdl2_sqpl_complex = stk.ConstructedMolecule(
+        building_blocks=[metal, ligand],
+        topology_graph=sqpl,
+        building_block_vertices={
+            metal: tuple([sqpl.vertices[0]]),
+            ligand: sqpl.vertices[1:]
+        }
+    )
+    print('--------------------------------------------------------')
+    print(pdl2_sqpl_complex)
+    print(pdl2_sqpl_complex.get_position_matrix()[:5])
+    pdl2_sqpl_complex.write('metal_complex_1.mol')
+    pdl2_sqpl_complex.write('metal_complex_1.pdb')
+    import sys
+    sys.exit()
     print('testing script done')
 
 
