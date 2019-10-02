5e3bacac2d4bc28e4bb476b5a6ca5256b8c36140
Lukas Turcani <lukasturcani93@gmail.com>
2019-10-02 12:48:19 +0200

    updated test for filter

--- tests/calculators/ea/test_plotters.py
+++ tests/calculators/ea/test_plotters.py
@@ -23,6 +23,25 @@ def test_progress_plotter(progress):
     )
     plotter2.plot(progress)
 
+    # Make a plot where all fitness values are filtered out.
+    plotter3 = stk.ProgressPlotter(
+        filename=join(odir, 'no_fitness'),
+        property_fn=lambda mol: mol.fitness,
+        y_label='No Fitness',
+        filter=lambda mol: False,
+    )
+    plotter3.plot(progress)
+
+    # Make a plot where some fitness values are filtered out.
+    # Ie by plotting only odd fitness values.
+    plotter4 = stk.ProgressPlotter(
+        filename=join(odir, 'odd_fitness'),
+        property_fn=lambda mol: mol.fitness,
+        y_label='Odd Fitness',
+        filter=lambda mol: mol.fitness % 1 == 0,
+    )
+    plotter4.plot(progress)
+
 
 def test_selection_plotter(progress):
     roulette = stk.Roulette(num_batches=10)
