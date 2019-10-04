9c1b76379db53a040400239f972df7defd57aef3
Lukas Turcani <lukasturcani93@gmail.com>
2019-10-04 11:20:50 +0200

    added TryCatch

--- src/stk/calculators/logical.py
+++ src/stk/calculators/logical.py
@@ -4,9 +4,13 @@ Logical Calculators
 
 """
 
+import logging
 
 from .optimization import Optimizer
 from .energy import EnergyCalculator
+
+
+logger = logging.getLogger(__name__)
 
 
 class If(Optimizer, EnergyCalculator):
@@ -39,3 +43,45 @@ class If(Optimizer, EnergyCalculator):
         if self._condition(mol):
             return self._true_calculator.get_energy(mol)
         return self._false_calculator.get_energy(mol)
+
+
+class TryCatch(Optimizer, EnergyCalculator):
+    """
+
+    """
+
+    def __init__(
+        self,
+        try_calculator,
+        catch_calculator,
+        use_cache=False,
+    ):
+        """
+
+        """
+
+        self._try_calculator = try_calculator
+        self._catch_calculator = catch_calculator
+        super().__init__(use_cache=use_cache)
+
+    def _optimize(self, mol):
+        try:
+            return self._try_calculator.optimize(mol)
+        except Exception:
+            self._log_failure()
+            return self._catch_calculator.optimize(mol)
+
+    def _get_energy(self, mol):
+        try:
+            return self._try_calculator.get_energy(mol)
+        except Exception:
+            self._log_failure()
+            return self._catch_calculator.get_energy(mol)
+
+    def _log_failure(self):
+        try_name = self._try_calculator.__class__.__name__
+        catch_name = self._catch_calculator.__class__.__name__
+        logger.error(
+            f'{try_name} failed, trying {catch_name}.',
+            exc_info=True
+        )
