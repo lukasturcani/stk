597845b5a5644df1c91e84331d9efbe8fcb51949
Lukas Turcani <lukasturcani93@gmail.com>
2019-10-07 18:40:33 +0200

    wip

--- src/stk/calculators/ea/fitness_normalizers.py
+++ src/stk/calculators/ea/fitness_normalizers.py
@@ -576,7 +576,31 @@ class ShiftUp(_FilteringNormalizer, FitnessNormalizer):
             yield mol, fitness_values[mol] + shift
 
 
-class ReplaceFitness(_FitnessNormalizer, FitnessNormalizer):
+class ReplaceFitness(FitnessNormalizer):
+    """
+    Replaces fitness values of a certain value with a new value.
+
+    Examples
+    --------
+    Replace all fitness values which are ``None`` with the half the
+    minimum fitness value in the population
+
+    .. code-block:: python
+
+        import stk
+
+        replacer = stk.ReplaceFitness(
+            replacement_fn=lambda population:
+                min(
+                    f for _, f in population.get_fitness_values()
+                    if f is not None
+                ) / 2,
+            filter=lambda population, mol:
+                population.get_fitness_values()[mol] is None,
+        )
+
+    """
+
     def __init__(self, replacement_fn, filter=lambda mol: True):
         """
         Initialize a :class:`.ReplaceFitness` instance.
@@ -598,20 +622,18 @@ class ReplaceFitness(_FitnessNormalizer, FitnessNormalizer):
 
         """
 
-        super().__init__(filter=filter)
+        self._replacement_fn = replacement_fn
+        self._filter = filter
 
-    def normalize(self, population, fitness_values):
+    def _normalize(self, population):
         """
         Normalize the fitness values in `population`.
 
         Parameters
         ----------
-        population : :class:`.Population`
+        population : :class:`.EAPopulation`
             The molecules which need to have their fitness values
             normalized.
-
-        fitness_values : :class:`dict`
-            Maps every molecule in `population` to its fitness value.
 
         Returns
         -------
@@ -621,8 +643,11 @@ class ReplaceFitness(_FitnessNormalizer, FitnessNormalizer):
 
         """
 
+        def filter_fn(mol):
+            return self._filter(population, mol)
+
         replacement_value = self._replacement_fn(population)
-        normalized = dict(fitness_values)
-        for mol in filter(self._filter, population):
+        normalized = population.get_fitness_values()
+        for mol in filter(filter_fn, population):
             normalized[mol] = replacement_value
         return normalized
