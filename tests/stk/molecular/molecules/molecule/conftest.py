cf076298dfb827cdc25bf714b183b81482d74ead
Lukas Turcani <lukasturcani93@gmail.com>
2019-11-29 22:36:28 +0100

    changed formatting

--- tests/stk/molecular/molecules/molecule/conftest.py
+++ tests/stk/molecular/molecules/molecule/conftest.py
@@ -13,54 +13,68 @@ valid_molecules = [
 ]
 
 
-@pytest.fixture(params=[
-    *valid_molecules,
-    stk.BuildingBlock('NCCN').set_position_matrix(np.zeros((12, 3))),
-])
+@pytest.fixture(
+    params=[
+        *valid_molecules,
+        stk.BuildingBlock('NCCN').set_position_matrix(
+            position_matrix=np.zeros((12, 3))
+        ),
+    ],
+)
 def molecule(request):
-    return request.param
+    return request.param.clone()
 
 
-@pytest.fixture(params=[
-    *valid_molecules
-])
+@pytest.fixture(
+    params=[
+        *valid_molecules
+    ],
+)
 def valid_molecule(request):
-    return request.param
+    return request.param.clone()
 
 
-@pytest.fixture(params=[
-    [0, 0, 0],
-    [10, 20, 30],
-    [-10, 20, -30],
-    [0.5, 10, -0.921],
-])
+@pytest.fixture(
+    params=[
+        [0, 0, 0],
+        [10, 20, 30],
+        [-10, 20, -30],
+        [0.5, 10, -0.921],
+    ],
+)
 def displacement(request):
-    return request.param
+    return list(request.param)
 
 
-@pytest.fixture(params=[
-    -np.pi/2,
-    np.pi/2,
-])
+@pytest.fixture(
+    params=[
+        -np.pi/2,
+        np.pi/2,
+    ],
+)
 def angle(request):
     return request.param
 
 
-@pytest.fixture(params=[
-    np.array([0, 1, 0]),
-    np.array([1, 0, 0]),
-    np.array([1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)])
-])
+@pytest.fixture(
+    params=[
+        np.array([0, 1, 0]),
+        np.array([1, 0, 0]),
+        np.array([1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)])
+    ],
+)
 def axis(request):
-    return request.param
+    return np.array(request.param)
 
 
-@pytest.fixture(params=[
-    np.array([0, 0, 0]),
-    np.array([-1.2, 10.12, 3]),
-])
+@pytest.fixture(
+    params=[
+        np.array([0, 0, 0]),
+        np.array([-1.2, 10.12, 3]),
+    ],
+)
 def origin(request):
-    return request.param
+    return np.array(request.param)
 
 
 # Values for get_atom_ids should never cause any dependent test
@@ -80,22 +94,26 @@ never_fail_get_atom_ids = [
 
 # Values for get_atom_ids which might cause some functions to fail.
 # Functions which expect to fail depend on this fixture.
-@pytest.fixture(params=[
-    *never_fail_get_atom_ids,
-    pytest.param(
-        lambda molecule: (),
-        marks=pytest.mark.xfail(strict=True, raises=ValueError),
-    ),
-])
+@pytest.fixture(
+    params=[
+        *never_fail_get_atom_ids,
+        pytest.param(
+            lambda molecule: (),
+            marks=pytest.mark.xfail(strict=True, raises=ValueError),
+        ),
+    ],
+)
 def get_atom_ids(request):
     return request.param
 
 
 # Values for get_atom_ids which might cause some functions to fail.
 # Functions which do not expect to fail depend on this fixture.
-@pytest.fixture(params=[
-    *never_fail_get_atom_ids,
-    lambda molecule: (),
-])
+@pytest.fixture(
+    params=[
+        *never_fail_get_atom_ids,
+        lambda molecule: (),
+    ],
+)
 def get_atom_ids_no_fail(request):
     return request.param
