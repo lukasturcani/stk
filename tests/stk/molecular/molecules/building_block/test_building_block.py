f27a7e03fa1bb0fcd31ad8d01659cd7b4faffb91
Lukas Turcani <lukasturcani93@gmail.com>
2019-12-03 15:21:06 +0100

    added test

--- tests/stk/molecular/molecules/building_block/test_building_block.py
+++ tests/stk/molecular/molecules/building_block/test_building_block.py
@@ -474,24 +474,47 @@ def test_get_bonder_ids(building_block, get_fg_ids):
         assert bonder_id == expected_bonder_id
 
 
-def test_get_bonder_centroids(
-    building_block,
-    get_fg_ids,
-    expected_bonder_centroids,
-):
-    bonder_centroids = building_block.get_bonder_centroids(
-        fg_ids=get_fg_ids(building_block),
-    )
-    bonder_centroids = it.zip_longest(
-        bonder_centroids,
-        expected_bonder_centroids,
-    )
-    for bonder_centroid, expected_bonder_centroid in bonder_centroids:
-        assert np.allclose(
-            a=bonder_centroid,
-            b=expected_bonder_centroid,
-            atol=1e-32,
+class TestGetBonderCentroids:
+    def case1():
+        building_block = stk.BuildingBlock('BrCCBr', ['bromine'])
+        position_matrix = np.zeros((len(building_block.atoms), 3))
+        bonder1 = [1.0, 0., 0.]
+        bonder2 = [10., 0., 0.]
+        position_matrix[1, :] = bonder1
+        position_matrix[2, :] = bonder2
+        building_block.set_position_matrix(position_matrix)
+        fg_ids = (0, 1)
+        return building_block, fg_ids, [bonder1, bonder2]
+
+    @pytest.mark.parametrize(
+        argnames=(
+            'building_block',
+            'fg_ids',
+            'expected_bonder_centroids',
+        ),
+        argvalues=(
+            case1(),
         )
+    )
+    def test(
+        self,
+        building_block,
+        fg_ids,
+        expected_bonder_centroids,
+    ):
+        bonder_centroids = building_block.get_bonder_centroids(
+            fg_ids=fg_ids,
+        )
+        bonder_centroids = it.zip_longest(
+            bonder_centroids,
+            expected_bonder_centroids,
+        )
+        for centroid, expected_bonder_centroid in bonder_centroids:
+            assert np.allclose(
+                a=centroid,
+                b=expected_bonder_centroid,
+                atol=1e-32,
+            )
 
 
 def test_get_bonder_plane(
