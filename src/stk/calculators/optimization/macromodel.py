283860194dd722ae7654964d554c657599192f84
Steven Bennett <stevenbennettonline@gmail.com>
2019-12-09 14:25:06 +0000

    Added empty log checking for Schrodinger jobs

--- src/stk/calculators/optimization/macromodel.py
+++ src/stk/calculators/optimization/macromodel.py
@@ -320,8 +320,15 @@ class _MacroModel(_MoleculeCalculator, Optimizer):
         if mol is None:
             return True
 
+        # Check if the log file is empty.
+        # If it is empty, the calculation has most likely failed and
+        # will need to be re-run.
+
+        if os.path.getsize(run_name):
+            return False
+
         # To check if the log file mentions a missing license file open
-        # the log file and scan for the apporpriate string.
+        # the log file and scan for the appropriate string.
 
         # Check if the file exists first. If not, this is often means
         # the calculation must be redone so return False anyway.
@@ -465,7 +472,7 @@ class _MacroModel(_MoleculeCalculator, Optimizer):
         """
         Fix bond distances and angles in ``.com`` file.
 
-        For each bond distance, bond angle and torisional angle that
+        For each bond distance, bond angle and torsional angle that
         does not involve a bond created by
         :meth:`~.Topology.construct`, a "FX" command is added to the
         body of the ``.com`` file.
@@ -752,7 +759,7 @@ class MacroModelForceField(_MacroModel):
 
         # Go through all the bonds in the rdkit molecule. If the bond
         # is not between bonder atoms add a fix line to the
-        # ``fix_block``. If the bond does invovle two bonder atoms go
+        # ``fix_block``. If the bond does involve two bonder atoms go
         # to the next bond. This is because a bond between 2 bonder
         # atoms was added during construction and should therefore not
         # be fixed.
@@ -908,7 +915,7 @@ class MacroModelMD(_MacroModel):
             Cannot be more than ``99999.99``.
 
         eq_time : :class:`float`, optional
-            The equilibriation time in ``ps`` before the MD is run.
+            The equilibration time in ``ps`` before the MD is run.
             Cannot be more than ``999999.99``.
 
         maximum_iterations : :class:`int`, optional
@@ -1049,7 +1056,7 @@ class MacroModelMD(_MacroModel):
             Cannot be more than ``99999.99``.
 
         eq_time : :class:`float`
-            The equilibriation time in ``ps`` before the MD is run.
+            The equilibration time in ``ps`` before the MD is run.
             Cannot be more than ``999999.99``.
 
         minimum_gradient : :class:`float`
@@ -1240,7 +1247,7 @@ class MacroModelMD(_MacroModel):
 
         # Go through all the bonds in the rdkit molecule. If the bond
         # is not between bonder atoms add a fix line to the
-        # ``fix_block``. If the bond does invovle two bonder atoms go
+        # ``fix_block``. If the bond does involve two bonder atoms go
         # to the next bond. This is because a bond between 2 bonder
         # atoms was added during construction and should therefore not
         # be fixed.
