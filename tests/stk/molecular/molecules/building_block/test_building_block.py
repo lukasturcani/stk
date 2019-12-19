7741612fb15983d7dce4c10a66b20661a321c9a6
Lukas Turcani <lukasturcani93@gmail.com>
2019-12-19 16:08:01 +0100

    wip

--- tests/stk/molecular/molecules/building_block/test_building_block.py
+++ tests/stk/molecular/molecules/building_block/test_building_block.py
@@ -703,19 +703,44 @@ class TestGetBonderDirectionVectors:
             )
 
 
-def test_get_centroid_centroid_direction_vector(
-    building_block,
-    get_fg_ids,
-    expected_bonder_centroid_direction_vector,
-):
-    direction = building_block.get_centroid_centroid_direction_vector(
-        fg_ids=get_fg_ids(building_block),
+class TestGetCentroidCentroidDirectionVector:
+    def case1():
+        building_block = stk.BuildingBlock(
+            smiles='BrCC(CBr)(CBr)CBr',
+            functional_groups=['bromine'],
+        )
+        coords = np.zeros((len(building_block.atoms), 3))
+        for bonder_id in building_block.get_bonder_ids():
+            coords[bonder_id] = [1, 0, 0]
+        building_block.set_position_matrix(coords)
+        return building_block, None, [-1, 0, 0]
+
+    @pytest.mark.parametrize(
+        argnames=(
+            'building_block',
+            'fg_ids',
+            'expected_vector',
+        ),
+        argvalues=(
+            case1(),
+        )
     )
-    assert np.allclose(
-        a=direction,
-        b=expected_bonder_centroid_direction_vector,
-        atol=1e-32,
-    )
+    def test(
+        self,
+        building_block,
+        fg_ids,
+        expected_vector,
+    ):
+        direction = (
+            building_block.get_centroid_centroid_direction_vector(
+                fg_ids=fg_ids,
+            )
+        )
+        assert np.allclose(
+            a=stk.normalize_vector(direction),
+            b=expected_vector,
+            atol=1e-32,
+        )
 
 
 def test_dump_and_load(tmpdir, building_block):
