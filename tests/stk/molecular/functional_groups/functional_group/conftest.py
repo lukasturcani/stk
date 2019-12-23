f4e709ca4944d133b8fbfb63fe3dc8e93e0cce9a
Lukas Turcani <lukasturcani93@gmail.com>
2019-12-23 19:23:42 +0100

    wip

--- tests/stk/molecular/functional_groups/functional_group/conftest.py
+++ tests/stk/molecular/functional_groups/functional_group/conftest.py
@@ -1,18 +1,66 @@
 import pytest
 import stk
 
-atoms = (stk.H(0), stk.F(1), stk.P(2), stk.H(3), stk.C(4), stk.K(5))
+
+@pytest.fixture(
+    params=[
+
+    ],
+)
+def make_atom_map(request):
+    return request.param
+
+
+class FunctionalGroupAtoms:
+    def __init__(self, atoms, bonders, deleters):
+        self.atoms = atoms
+        self.bonders = bonders
+        self.deleters = deleters
+
+    def clone(self):
+        return self.__class__(
+            atoms=tuple(a.clone() for a in self.atoms),
+            bonders=tuple(a.clone() for a in self.bonders),
+            deleters=tuple(a.clone() for a in self.deleters),
+        )
 
 
 @pytest.fixture(
     params=[
-        stk.FunctionalGroup(
-            atoms=atoms,
-            bonders=atoms[:2],
-            deleters=atoms[-2:],
-            fg_type=stk.functional_groups.fg_types['bromine'],
+        FunctionalGroupAtoms(
+            atoms=(),
+            bonders=(),
+            deleters=(),
+        ),
+        FunctionalGroupAtoms(
+            atoms=(),
+            bonders=(),
+            deleters=(),
+        ),
+        FunctionalGroupAtoms(
+            atoms=(),
+            bonders=(),
+            deleters=(),
         ),
     ],
 )
-def functional_group(request):
+def functional_group_atoms(request):
     return request.param.clone()
+
+
+@pytest.fixture(
+    params=[
+
+    ],
+)
+def make_functional_group(request):
+    return request.param
+
+
+@pytest.fixture
+def functional_group(make_functional_group):
+    return make_functional_group(
+        atoms=(stk.C(0), stk.H(2), stk.N(4), stk.P(32)),
+        bonders=(stk.C(0), stk.P(32)),
+        deleters=(stk.H(2), ),
+    )
--- tests/stk/molecular/functional_groups/functional_group/test_functional_group.py
+++ tests/stk/molecular/functional_groups/functional_group/test_functional_group.py
@@ -1,55 +1,126 @@
 import itertools as it
 
 
-def test_clone(functional_group):
-    clone = functional_group.clone()
+def test_clone(functional_group, make_atom_map):
+    functional_group.attr = 1
+    functional_group._attr = 2
+    atom_map = make_atom_map(functional_group)
+    clone = functional_group.clone(atom_map)
+    assert clone.attr == functional_group.attr
+    assert not hasattr(clone, '_attr')
+
     atoms = it.zip_longest(
-        functional_group.atoms,
-        clone.atoms,
+        functional_group.get_atoms(),
+        clone.get_atoms(),
     )
     for a1, a2 in atoms:
-        assert a1 is not a2
-        assert a1.id == a2.id
+        assert is_equivalent_atom(a1, a2)
 
     bonders = it.zip_longest(
-        functional_group.bonders,
-        clone.bonders,
+        functional_group.get_bonders(),
+        clone.get_bonders(),
     )
     for b1, b2 in bonders:
-        assert b1 is not b2
-        assert b1.id == b2.id
+        assert is_equivalent_atom(b1, b2)
 
     deleters = it.zip_longest(
-        functional_group.deleters,
-        clone.deleters,
+        functional_group.get_deleters(),
+        clone.get_deleters(),
     )
     for d1, d2 in deleters:
-        assert d1 is not d2
-        assert d1.id == d2.id
+        assert is_equivalent_atom(d1, d2)
 
 
-def test_get_atom_ids(functional_group):
+def is_equivalent_atom(atom1, atom2):
+    return (
+        atom1 is not atom2
+        and atom1.id == atom2.id
+        and atom1.__class__ is atom2.__class__
+    )
+
+
+def test_get_atoms(make_functional_group, functional_group_atoms):
+    functional_group = make_functional_group(
+        atoms=functional_group_atoms.atoms,
+        bonders=functional_group_atoms.bonders,
+        deleters=functional_group_atoms.deleters,
+    )
     atoms = it.zip_longest(
-        functional_group.atoms,
+        functional_group.get_atoms(),
+        functional_group_atoms.atoms,
+    )
+    for atom1, atom2 in atoms:
+        assert is_equivalent_atom(atom1, atom2)
+
+
+def test_get_atom_ids(make_functional_group, functional_group_atoms):
+    functional_group = make_functional_group(
+        atoms=functional_group_atoms.atoms,
+        bonders=functional_group_atoms.bonders,
+        deleters=functional_group_atoms.deleters,
+    )
+    atoms = it.zip_longest(
         functional_group.get_atom_ids(),
+        functional_group_atoms.atoms,
     )
-    for atom, id_ in atoms:
-        assert atom.id == id_
+    for id_, atom in atoms:
+        assert id_ == atom.id
 
 
-def test_get_bonder_ids(functional_group):
+def test_get_bonders(make_functional_group, functional_group_atoms):
+    functional_group = make_functional_group(
+        atoms=functional_group_atoms.atoms,
+        bonders=functional_group_atoms.bonders,
+        deleters=functional_group_atoms.deleters,
+    )
     atoms = it.zip_longest(
-        functional_group.bonders,
+        functional_group.get_bonders(),
+        functional_group_atoms.bonders,
+    )
+    for atom1, atom2 in atoms:
+        assert is_equivalent_atom(atom1, atom2)
+
+
+def test_get_bonder_ids(make_functional_group, functional_group_atoms):
+    functional_group = make_functional_group(
+        atoms=functional_group_atoms.atoms,
+        bonders=functional_group_atoms.bonders,
+        deleters=functional_group_atoms.deleters,
+    )
+    atoms = it.zip_longest(
         functional_group.get_bonder_ids(),
+        functional_group_atoms.bonders,
     )
-    for atom, id_ in atoms:
-        assert atom.id == id_
+    for id_, atom in atoms:
+        assert id_ == atom.id
 
 
-def test_get_deleter_ids(functional_group):
+def test_get_deleters(make_functional_group, functional_group_atoms):
+    functional_group = make_functional_group(
+        atoms=functional_group_atoms.atoms,
+        bonders=functional_group_atoms.bonders,
+        deleters=functional_group_atoms.deleters,
+    )
     atoms = it.zip_longest(
-        functional_group.deleters,
-        functional_group.get_deleter_ids(),
+        functional_group.get_deleters(),
+        functional_group_atoms.deleters,
     )
-    for atom, id_ in atoms:
-        assert atom.id == id_
+    for atom1, atom2 in atoms:
+        assert is_equivalent_atom(atom1, atom2)
+
+
+def test_get_deleter_ids(
+    make_functional_group,
+    functional_group_atoms,
+):
+    functional_group = make_functional_group(
+        atoms=functional_group_atoms.atoms,
+        bonders=functional_group_atoms.bonders,
+        deleters=functional_group_atoms.deleters,
+    )
+    atoms = it.zip_longest(
+        functional_group.get_deleter_ids(),
+        functional_group_atoms.deleters,
+    )
+    for id_, atom in atoms:
+        assert id_ == atom.id
