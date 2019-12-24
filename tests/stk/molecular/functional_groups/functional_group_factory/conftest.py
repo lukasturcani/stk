4035b1b11bfb26840d372c93ee9e021775a9f0d3
Lukas Turcani <lukasturcani93@gmail.com>
2019-12-24 15:14:31 +0100

    wip

--- tests/stk/molecular/functional_groups/functional_group_factory/conftest.py
+++ tests/stk/molecular/functional_groups/functional_group_factory/conftest.py
@@ -3,15 +3,17 @@ import stk
 
 
 class GetFunctionalGroupsTestCase:
-    def __init__(self, functional_groups, molecule, factory):
-        self.functional_groups = functional_groups
+    def __init__(self, molecule, factory, functional_groups):
         self.molecule = molecule
         self.factory = factory
+        self.functional_groups = functional_groups
 
 
 @pytest.fixture(
     params=[
         GetFunctionalGroupsTestCase(
+            molecule=stk.BuildingBlock('NCCN'),
+            factory=stk.AmineFactory(),
             functional_groups=(
                 stk.Amine(
                     atoms=(stk.N(0), stk.H(4), stk.H(5)),
@@ -24,9 +26,42 @@ class GetFunctionalGroupsTestCase:
                     deleters=(stk.H(10), stk.H(11)),
                 ),
             ),
-            molecule=stk.BuildingBlock('NCCN'),
-            factory=stk.AmineFactory(),
         ),
+
+        GetFunctionalGroupsTestCase(
+            molecule=stk.BuildingBlock('NCCN'),
+            factory=stk.AmineFactory(num_deleters=1),
+            functional_groups=(
+                stk.Amine(
+                    atoms=(stk.N(0), stk.H(4), stk.H(5)),
+                    bonders=(stk.N(0), ),
+                    deleters=(stk.H(4), ),
+                ),
+                stk.Amine(
+                    atoms=(stk.N(3), stk.H(10), stk.H(11)),
+                    bonders=(stk.N(3), ),
+                    deleters=(stk.H(10), ),
+                ),
+            ),
+        ),
+
+        GetFunctionalGroupsTestCase(
+            molecule=stk.BuildingBlock('NCCN'),
+            factory=stk.AmineFactory(num_deleters=0),
+            functional_groups=(
+                stk.Amine(
+                    atoms=(stk.N(0), stk.H(4), stk.H(5)),
+                    bonders=(stk.N(0), ),
+                    deleters=(),
+                ),
+                stk.Amine(
+                    atoms=(stk.N(3), stk.H(10), stk.H(11)),
+                    bonders=(stk.N(3), ),
+                    deleters=(),
+                ),
+            ),
+        ),
+
     ],
 )
 def get_functional_groups_test_case(request):
