36c2936fabe2cab68e15d03ba5941bb4fe3247b2
Lukas Turcani <lukasturcani93@gmail.com>
2020-01-02 13:22:54 +0100

    encapsulated bonds adn atoms

--- src/stk/molecular/atoms.py
+++ src/stk/molecular/atoms.py
@@ -16,21 +16,6 @@ class Atom:
     Atoms of a particular element can be made with this
     class or with the subclass representing that element.
 
-    Attributes
-    ----------
-    atomic_number : :class:`int`
-        A class attribute. Specifies the atomic number.
-
-    mass : :class:`float`
-        A class attribute. Specifies the standard atomic weight.
-
-    id : :class:`int`
-        The id of the atom. Should be equal to its index in
-        :attr:`.Molecule.atoms`.
-
-    charge : :class:`int`
-        The formal charge of the atom.
-
     Examples
     --------
     *Initialization.*
@@ -44,7 +29,7 @@ class Atom:
         import stk
 
         # h0 is an instance of the H class.
-        h0 = stk.Atom(id=0, atomic_number=1)
+        h0 = stk.Atom(id=0,_atomic_number=1)
 
         # h1 is also an instance of the H class.
         h1 = stk.H(id=1)
@@ -55,12 +40,12 @@ class Atom:
     .. code-block:: python
 
         # Both he0 and he1 are instances of the He class.
-        he0 = stk.Atom(id=2, atomic_number=2)
+        he0 = stk.Atom(id=2,_atomic_number=2)
         he1 = stk.He(id=3)
 
         # Both c0 and c1 are instances of the
         # C class.
-        c0 = stk.Atom(id=4, atomic_number=6)
+        c0 = stk.Atom(id=4,_atomic_number=6)
         c1 = stk.C(id=5)
 
     """
@@ -72,7 +57,7 @@ class Atom:
         # The init method for subclasses takes a slightly different
         # form.
         cls.__init__ = cls._subclass_init
-        cls._elements[cls.atomic_number] = cls
+        cls._elements[cls._atomic_number] = cls
 
     @staticmethod
     def _subclass_init(self, id, charge=0):
@@ -89,7 +74,7 @@ class Atom:
 
         """
 
-        Atom.__init__(self, id, self.atomic_number, charge)
+        Atom.__init__(self, id, self._atomic_number, charge)
 
     def __init__(self, id, atomic_number, charge=0):
         """
@@ -100,24 +85,75 @@ class Atom:
         id : :class:`int`
             The id of the atom.
 
-        atomic_number : :class:`int`
+        _atomic_number : :class:`int`
             The atomic number.
 
         charge : :class:`int`
             The formal charge.
 
-        **kwargs : :class:`object`
-            Additional attributes to be added to the atom.
-
         """
 
         self.__class__ = self._elements[atomic_number]
-        self.id = id
-        self.charge = charge
+        self._id = id
+        self._charge = charge
+
+    def get_id(self):
+        """
+        Get the id of the atom.
+
+        Returns
+        -------
+        :class:`int`
+            The id.
+
+        """
+
+        return self._id
+
+    def get_atomic_number(self):
+        """
+        Get the atomic number of the atom.
+
+        Returns
+        -------
+        :class:`int`
+            The atomic number.
+
+        """
+
+        return self._atomic_number
+
+    def get_charge(self):
+        """
+        Get the charge of the atom.
+
+        Returns
+        -------
+        :class:`int`
+            The charge.
+
+        """
+
+        return self._charge
+
+    def get_mass(self):
+        """
+        Get the _mass of the atom.
+
+        Returns
+        -------
+        :class:`float`
+            The _mass.
+
+        """
+
+        return self._mass
 
     def __repr__(self):
-        charge = f', charge={self.charge}' if self.charge != 0 else ''
-        return f'{self.__class__.__name__}({self.id}{charge})'
+        charge = (
+            f', charge={self._charge}' if self._charge != 0 else ''
+        )
+        return f'{self.__class__.__name__}({self._id}{charge})'
 
     def __str__(self):
         return repr(self)
@@ -143,590 +179,590 @@ class Atom:
 
 
 class H(Atom):
-    atomic_number = 1
-    mass = 1.008
+    _atomic_number = 1
+    _mass = 1.008
 
 
 class He(Atom):
-    atomic_number = 2
-    mass = 4.003
+    _atomic_number = 2
+    _mass = 4.003
 
 
 class Li(Atom):
-    atomic_number = 3
-    mass = 6.941
+    _atomic_number = 3
+    _mass = 6.941
 
 
 class Be(Atom):
-    atomic_number = 4
-    mass = 9.012
+    _atomic_number = 4
+    _mass = 9.012
 
 
 class B(Atom):
-    atomic_number = 5
-    mass = 10.811
+    _atomic_number = 5
+    _mass = 10.811
 
 
 class C(Atom):
-    atomic_number = 6
-    mass = 12.011
+    _atomic_number = 6
+    _mass = 12.011
 
 
 class N(Atom):
-    atomic_number = 7
-    mass = 14.007
+    _atomic_number = 7
+    _mass = 14.007
 
 
 class O(Atom):
-    atomic_number = 8
-    mass = 15.999
+    _atomic_number = 8
+    _mass = 15.999
 
 
 class F(Atom):
-    atomic_number = 9
-    mass = 18.998
+    _atomic_number = 9
+    _mass = 18.998
 
 
 class Ne(Atom):
-    atomic_number = 10
-    mass = 20.180
+    _atomic_number = 10
+    _mass = 20.180
 
 
 class Na(Atom):
-    atomic_number = 11
-    mass = 22.990
+    _atomic_number = 11
+    _mass = 22.990
 
 
 class Mg(Atom):
-    atomic_number = 12
-    mass = 24.305
+    _atomic_number = 12
+    _mass = 24.305
 
 
 class Al(Atom):
-    atomic_number = 13
-    mass = 26.982
+    _atomic_number = 13
+    _mass = 26.982
 
 
 class Si(Atom):
-    atomic_number = 14
-    mass = 28.086
+    _atomic_number = 14
+    _mass = 28.086
 
 
 class P(Atom):
-    atomic_number = 15
-    mass = 30.974
+    _atomic_number = 15
+    _mass = 30.974
 
 
 class S(Atom):
-    atomic_number = 16
-    mass = 32.066
+    _atomic_number = 16
+    _mass = 32.066
 
 
 class Cl(Atom):
-    atomic_number = 17
-    mass = 35.453
+    _atomic_number = 17
+    _mass = 35.453
 
 
 class Ar(Atom):
-    atomic_number = 18
-    mass = 39.948
+    _atomic_number = 18
+    _mass = 39.948
 
 
 class K(Atom):
-    atomic_number = 19
-    mass = 39.098
+    _atomic_number = 19
+    _mass = 39.098
 
 
 class Ca(Atom):
-    atomic_number = 20
-    mass = 40.078
+    _atomic_number = 20
+    _mass = 40.078
 
 
 class Sc(Atom):
-    atomic_number = 21
-    mass = 44.956
+    _atomic_number = 21
+    _mass = 44.956
 
 
 class Ti(Atom):
-    atomic_number = 22
-    mass = 47.867
+    _atomic_number = 22
+    _mass = 47.867
 
 
 class V(Atom):
-    atomic_number = 23
-    mass = 50.942
+    _atomic_number = 23
+    _mass = 50.942
 
 
 class Cr(Atom):
-    atomic_number = 24
-    mass = 51.996
+    _atomic_number = 24
+    _mass = 51.996
 
 
 class Mn(Atom):
-    atomic_number = 25
-    mass = 54.938
+    _atomic_number = 25
+    _mass = 54.938
 
 
 class Fe(Atom):
-    atomic_number = 26
-    mass = 55.845
+    _atomic_number = 26
+    _mass = 55.845
 
 
 class Co(Atom):
-    atomic_number = 27
-    mass = 58.933
+    _atomic_number = 27
+    _mass = 58.933
 
 
 class Ni(Atom):
-    atomic_number = 28
-    mass = 58.693
+    _atomic_number = 28
+    _mass = 58.693
 
 
 class Cu(Atom):
-    atomic_number = 29
-    mass = 63.546
+    _atomic_number = 29
+    _mass = 63.546
 
 
 class Zn(Atom):
-    atomic_number = 30
-    mass = 65.38
+    _atomic_number = 30
+    _mass = 65.38
 
 
 class Ga(Atom):
-    atomic_number = 31
-    mass = 69.723
+    _atomic_number = 31
+    _mass = 69.723
 
 
 class Ge(Atom):
-    atomic_number = 32
-    mass = 72.631
+    _atomic_number = 32
+    _mass = 72.631
 
 
 class As(Atom):
-    atomic_number = 33
-    mass = 74.922
+    _atomic_number = 33
+    _mass = 74.922
 
 
 class Se(Atom):
-    atomic_number = 34
-    mass = 78.971
+    _atomic_number = 34
+    _mass = 78.971
 
 
 class Br(Atom):
-    atomic_number = 35
-    mass = 79.904
+    _atomic_number = 35
+    _mass = 79.904
 
 
 class Kr(Atom):
-    atomic_number = 36
-    mass = 83.798
+    _atomic_number = 36
+    _mass = 83.798
 
 
 class Rb(Atom):
-    atomic_number = 37
-    mass = 85.468
+    _atomic_number = 37
+    _mass = 85.468
 
 
 class Sr(Atom):
-    atomic_number = 38
-    mass = 87.62
+    _atomic_number = 38
+    _mass = 87.62
 
 
 class Y(Atom):
-    atomic_number = 39
-    mass = 88.906
+    _atomic_number = 39
+    _mass = 88.906
 
 
 class Zr(Atom):
-    atomic_number = 40
-    mass = 91.224
+    _atomic_number = 40
+    _mass = 91.224
 
 
 class Nb(Atom):
-    atomic_number = 41
-    mass = 92.906
+    _atomic_number = 41
+    _mass = 92.906
 
 
 class Mo(Atom):
-    atomic_number = 42
-    mass = 95.95
+    _atomic_number = 42
+    _mass = 95.95
 
 
 class Tc(Atom):
-    atomic_number = 43
-    mass = 98.907
+    _atomic_number = 43
+    _mass = 98.907
 
 
 class Ru(Atom):
-    atomic_number = 44
-    mass = 101.07
+    _atomic_number = 44
+    _mass = 101.07
 
 
 class Rh(Atom):
-    atomic_number = 45
-    mass = 102.906
+    _atomic_number = 45
+    _mass = 102.906
 
 
 class Pd(Atom):
-    atomic_number = 46
-    mass = 106.42
+    _atomic_number = 46
+    _mass = 106.42
 
 
 class Ag(Atom):
-    atomic_number = 47
-    mass = 107.868
+    _atomic_number = 47
+    _mass = 107.868
 
 
 class Cd(Atom):
-    atomic_number = 48
-    mass = 112.414
+    _atomic_number = 48
+    _mass = 112.414
 
 
 class In(Atom):
-    atomic_number = 49
-    mass = 114.818
+    _atomic_number = 49
+    _mass = 114.818
 
 
 class Sn(Atom):
-    atomic_number = 50
-    mass = 118.711
+    _atomic_number = 50
+    _mass = 118.711
 
 
 class Sb(Atom):
-    atomic_number = 51
-    mass = 121.760
+    _atomic_number = 51
+    _mass = 121.760
 
 
 class Te(Atom):
-    atomic_number = 52
-    mass = 127.6
+    _atomic_number = 52
+    _mass = 127.6
 
 
 class I(Atom):
-    atomic_number = 53
-    mass = 126.904
+    _atomic_number = 53
+    _mass = 126.904
 
 
 class Xe(Atom):
-    atomic_number = 54
-    mass = 131.293
+    _atomic_number = 54
+    _mass = 131.293
 
 
 class Cs(Atom):
-    atomic_number = 55
-    mass = 132.905
+    _atomic_number = 55
+    _mass = 132.905
 
 
 class Ba(Atom):
-    atomic_number = 56
-    mass = 137.328
+    _atomic_number = 56
+    _mass = 137.328
 
 
 class La(Atom):
-    atomic_number = 57
-    mass = 138.905
+    _atomic_number = 57
+    _mass = 138.905
 
 
 class Ce(Atom):
-    atomic_number = 58
-    mass = 140.116
+    _atomic_number = 58
+    _mass = 140.116
 
 
 class Pr(Atom):
-    atomic_number = 59
-    mass = 140.908
+    _atomic_number = 59
+    _mass = 140.908
 
 
 class Nd(Atom):
-    atomic_number = 60
-    mass = 144.243
+    _atomic_number = 60
+    _mass = 144.243
 
 
 class Pm(Atom):
-    atomic_number = 61
-    mass = 144.913
+    _atomic_number = 61
+    _mass = 144.913
 
 
 class Sm(Atom):
-    atomic_number = 62
-    mass = 150.36
+    _atomic_number = 62
+    _mass = 150.36
 
 
 class Eu(Atom):
-    atomic_number = 63
-    mass = 151.964
+    _atomic_number = 63
+    _mass = 151.964
 
 
 class Gd(Atom):
-    atomic_number = 64
-    mass = 157.25
+    _atomic_number = 64
+    _mass = 157.25
 
 
 class Tb(Atom):
-    atomic_number = 65
-    mass = 158.925
+    _atomic_number = 65
+    _mass = 158.925
 
 
 class Dy(Atom):
-    atomic_number = 66
-    mass = 162.500
+    _atomic_number = 66
+    _mass = 162.500
 
 
 class Ho(Atom):
-    atomic_number = 67
-    mass = 164.930
+    _atomic_number = 67
+    _mass = 164.930
 
 
 class Er(Atom):
-    atomic_number = 68
-    mass = 167.259
+    _atomic_number = 68
+    _mass = 167.259
 
 
 class Tm(Atom):
-    atomic_number = 69
-    mass = 168.934
+    _atomic_number = 69
+    _mass = 168.934
 
 
 class Yb(Atom):
-    atomic_number = 70
-    mass = 173.055
+    _atomic_number = 70
+    _mass = 173.055
 
 
 class Lu(Atom):
-    atomic_number = 71
-    mass = 174.967
+    _atomic_number = 71
+    _mass = 174.967
 
 
 class Hf(Atom):
-    atomic_number = 72
-    mass = 178.49
+    _atomic_number = 72
+    _mass = 178.49
 
 
 class Ta(Atom):
-    atomic_number = 73
-    mass = 180.948
+    _atomic_number = 73
+    _mass = 180.948
 
 
 class W(Atom):
-    atomic_number = 74
-    mass = 183.84
+    _atomic_number = 74
+    _mass = 183.84
 
 
 class Re(Atom):
-    atomic_number = 75
-    mass = 186.207
+    _atomic_number = 75
+    _mass = 186.207
 
 
 class Os(Atom):
-    atomic_number = 76
-    mass = 190.23
+    _atomic_number = 76
+    _mass = 190.23
 
 
 class Ir(Atom):
-    atomic_number = 77
-    mass = 192.217
+    _atomic_number = 77
+    _mass = 192.217
 
 
 class Pt(Atom):
-    atomic_number = 78
-    mass = 195.085
+    _atomic_number = 78
+    _mass = 195.085
 
 
 class Au(Atom):
-    atomic_number = 79
-    mass = 196.967
+    _atomic_number = 79
+    _mass = 196.967
 
 
 class Hg(Atom):
-    atomic_number = 80
-    mass = 200.592
+    _atomic_number = 80
+    _mass = 200.592
 
 
 class Tl(Atom):
-    atomic_number = 81
-    mass = 204.383
+    _atomic_number = 81
+    _mass = 204.383
 
 
 class Pb(Atom):
-    atomic_number = 82
-    mass = 207.2
+    _atomic_number = 82
+    _mass = 207.2
 
 
 class Bi(Atom):
-    atomic_number = 83
-    mass = 208.980
+    _atomic_number = 83
+    _mass = 208.980
 
 
 class Po(Atom):
-    atomic_number = 84
-    mass = 208.982
+    _atomic_number = 84
+    _mass = 208.982
 
 
 class At(Atom):
-    atomic_number = 85
-    mass = 209.987
+    _atomic_number = 85
+    _mass = 209.987
 
 
 class Rn(Atom):
-    atomic_number = 86
-    mass = 222.018
+    _atomic_number = 86
+    _mass = 222.018
 
 
 class Fr(Atom):
-    atomic_number = 87
-    mass = 223.020
+    _atomic_number = 87
+    _mass = 223.020
 
 
 class Ra(Atom):
-    atomic_number = 88
-    mass = 226.025
+    _atomic_number = 88
+    _mass = 226.025
 
 
 class Ac(Atom):
-    atomic_number = 89
-    mass = 227.028
+    _atomic_number = 89
+    _mass = 227.028
 
 
 class Th(Atom):
-    atomic_number = 90
-    mass = 232.038
+    _atomic_number = 90
+    _mass = 232.038
 
 
 class Pa(Atom):
-    atomic_number = 91
-    mass = 231.036
+    _atomic_number = 91
+    _mass = 231.036
 
 
 class U(Atom):
-    atomic_number = 92
-    mass = 238.029
+    _atomic_number = 92
+    _mass = 238.029
 
 
 class Np(Atom):
-    atomic_number = 93
-    mass = 237.048
+    _atomic_number = 93
+    _mass = 237.048
 
 
 class Pu(Atom):
-    atomic_number = 94
-    mass = 244.064
+    _atomic_number = 94
+    _mass = 244.064
 
 
 class Am(Atom):
-    atomic_number = 95
-    mass = 243.061
+    _atomic_number = 95
+    _mass = 243.061
 
 
 class Cm(Atom):
-    atomic_number = 96
-    mass = 247.070
+    _atomic_number = 96
+    _mass = 247.070
 
 
 class Bk(Atom):
-    atomic_number = 97
-    mass = 247.070
+    _atomic_number = 97
+    _mass = 247.070
 
 
 class Cf(Atom):
-    atomic_number = 98
-    mass = 251.080
+    _atomic_number = 98
+    _mass = 251.080
 
 
 class Es(Atom):
-    atomic_number = 99
-    mass = 254
+    _atomic_number = 99
+    _mass = 254
 
 
 class Fm(Atom):
-    atomic_number = 100
-    mass = 257.095
+    _atomic_number = 100
+    _mass = 257.095
 
 
 class Md(Atom):
-    atomic_number = 101
-    mass = 258.1
+    _atomic_number = 101
+    _mass = 258.1
 
 
 class No(Atom):
-    atomic_number = 102
-    mass = 259.101
+    _atomic_number = 102
+    _mass = 259.101
 
 
 class Lr(Atom):
-    atomic_number = 103
-    mass = 262
+    _atomic_number = 103
+    _mass = 262
 
 
 class Rf(Atom):
-    atomic_number = 104
-    mass = 261
+    _atomic_number = 104
+    _mass = 261
 
 
 class Db(Atom):
-    atomic_number = 105
-    mass = 262
+    _atomic_number = 105
+    _mass = 262
 
 
 class Sg(Atom):
-    atomic_number = 106
-    mass = 266
+    _atomic_number = 106
+    _mass = 266
 
 
 class Bh(Atom):
-    atomic_number = 107
-    mass = 264
+    _atomic_number = 107
+    _mass = 264
 
 
 class Hs(Atom):
-    atomic_number = 108
-    mass = 269
+    _atomic_number = 108
+    _mass = 269
 
 
 class Mt(Atom):
-    atomic_number = 109
-    mass = 278
+    _atomic_number = 109
+    _mass = 278
 
 
 class Ds(Atom):
-    atomic_number = 110
-    mass = 281
+    _atomic_number = 110
+    _mass = 281
 
 
 class Rg(Atom):
-    atomic_number = 111
-    mass = 280
+    _atomic_number = 111
+    _mass = 280
 
 
 class Cn(Atom):
-    atomic_number = 112
-    mass = 285
+    _atomic_number = 112
+    _mass = 285
 
 
 class Nh(Atom):
-    atomic_number = 113
-    mass = 286
+    _atomic_number = 113
+    _mass = 286
 
 
 class Fl(Atom):
-    atomic_number = 114
-    mass = 289
+    _atomic_number = 114
+    _mass = 289
 
 
 class Mc(Atom):
-    atomic_number = 115
-    mass = 289
+    _atomic_number = 115
+    _mass = 289
 
 
 class Lv(Atom):
-    atomic_number = 116
-    mass = 293
+    _atomic_number = 116
+    _mass = 293
 
 
 class Ts(Atom):
-    atomic_number = 117
-    mass = 294
+    _atomic_number = 117
+    _mass = 294
 
 
 class Og(Atom):
-    atomic_number = 118
-    mass = 294
+    _atomic_number = 118
+    _mass = 294
--- src/stk/molecular/bond.py
+++ src/stk/molecular/bond.py
@@ -8,32 +8,13 @@ class Bond:
     """
     Represents an atomic bond.
 
-    Attributes
-    ----------
-    atom1 : :class:`.Atom`
-        The first atom in the bond.
-
-    atom2 : :class:`.Atom`
-        The second atom in the bond.
-
-    order : :class:`int`
-        The bond order.
-
-    periodicity : :class:`tuple` of :class:`int`
-        The directions across which the bond is periodic. For example,
-        ``(1, 0, -1)`` means that when going from
-        :attr:`~.Bond.atom1` to :attr:`~.Bond.atom2` the bond is
-        periodic across the x axis in the positive direction, is not
-        periodic across the y axis and is periodic across the z axis
-        in the negative direction.
-
     """
 
     def __init__(self, atom1, atom2, order, periodicity=(0, 0, 0)):
         """
         Initialize a :class:`Bond`.
 
-        Attributes
+        Parameters
         ----------
         atom1 : :class:`.Atom`
             The first atom in the bond.
@@ -47,17 +28,123 @@ class Bond:
         periodicity : :class:`tuple` of :class:`int`, optional
             The directions across which the bond is periodic. For
             example, ``(1, 0, -1)`` means that when going from
-            :attr:`~.Bond.atom1` to :attr:`~.Bond.atom2` the bond is
+            `atom1` to `atom2` the bond is
             periodic across the x axis in the positive direction, is
             not periodic across the y axis and is periodic across the z
             axis in the negative direction.
 
         """
 
-        self.atom1 = atom1
-        self.atom2 = atom2
-        self.order = order
-        self.periodicity = periodicity
+        self.dangerous_init(
+            atom1=atom1.clone(),
+            atom2=atom2.clone(),
+            order=order,
+            periodicity=periodicity,
+        )
+
+    @classmethod
+    def dangerous_init(
+        self,
+        atom1,
+        atom2,
+        order,
+        periodicity=(0, 0, 0),
+    ):
+        """
+        Dangerously initialize a :class:`.Bond`.
+
+        Notes
+        -----
+        This method behaves like :meth:`__init__`, except that
+        the bond does not create copies of `atom1` and `atom2` for
+        its internal state, but uses them directly.
+        This means the bond initialized with this method is not
+        fully encapsulated, and is therefore dangerous. However, the
+        bond also takes up less memory, because it does not create
+        an extra copy of the atoms.
+
+        Parameters
+        ----------
+        atom1 : :class:`.Atom`
+            The first atom in the bond.
+
+        atom2 : :class:`.Atom`
+            The second atom in the bond.
+
+        order : :class:`int`
+            The bond order.
+
+        periodicity : :class:`tuple` of :class:`int`, optional
+            The directions across which the bond is periodic. For
+            example, ``(1, 0, -1)`` means that when going from
+            `atom1` to `atom2` the bond is
+            periodic across the x axis in the positive direction, is
+            not periodic across the y axis and is periodic across the z
+            axis in the negative direction.
+
+        """
+
+        self._atom1 = atom1
+        self._atom2 = atom2
+        self._order = order
+        self._periodicity = periodicity
+
+    def get_atom1(self):
+        """
+        Get the first atom of the bond.
+
+        Returns
+        -------
+        :class:`.Atom`
+            The first atom of the bond.
+
+        """
+
+        return self._atom1.clone()
+
+    def get_atom2(self):
+        """
+        Get the second atom of the bond.
+
+        Returns
+        -------
+        :class:`.Atom`
+            The second atom of the bond.
+
+        """
+
+        return self._atom2.clone()
+
+    def get_order(self):
+        """
+        Get the bond order of the bond.
+
+        Returns
+        -------
+        :class:`int`
+            THe bond order.
+
+        """
+
+        return self._order
+
+    def get_periodicity(self):
+        """
+        Get the periodicity of the bond.
+
+        Returns
+        -------
+        :class:`tuple` of :class:`int`
+            The directions across which the bond is periodic. For
+            example, ``(1, 0, -1)`` means that when going from
+            `atom1` to `atom2` the bond is
+            periodic across the x axis in the positive direction, is
+            not periodic across the y axis and is periodic across the z
+            axis in the negative direction.
+
+        """
+
+        return self._periodicity
 
     def clone(self, atom_map=None):
         """
@@ -88,11 +175,10 @@ class Bond:
             c5 = stk.C(5)
             bond = stk.Bond(c0, c5, 1)
 
-            # bond_clone holds clones of c0 and c5 in its atom1 and
-            # atom2 attributes, respectively.
+            # bond_clone holds clones of c0 and c5.
             bond_clone = bond.clone()
 
-        It is possible to make sure that the clone holds different
+        It is possible to make sure that the clone holds specific
         atoms
 
         .. code-block:: python
@@ -101,17 +187,16 @@ class Bond:
             n3 = stk.N(3)
 
             # clone2 is also a clone, except that it holds
-            # li2 in the atom2 attribute. Its atom1 attribute holds a
-            # clone of c0.
+            # li2 internally. It also holds a clone of c0.
             clone2 = bond.clone(atom_map={
-                c5.id: li2,
+                c5.get_id(): li2,
             })
 
             # clone3 is also a clone, except that it holds n3 and
-            # li2 in its atom1 and atom2 attributes, respectively.
+            # li2 internally.
             clone3 = bond.clone(atom_map={
-                c0.id: n3,
-                c5.id: li2,
+                c0.get_id(): n3,
+                c5.get_id(): li2,
             })
 
         """
@@ -123,8 +208,14 @@ class Bond:
         for attr, val in vars(self).items():
             if not attr.startswith('_'):
                 setattr(clone, attr, val)
-        clone.atom1 = atom_map.get(self.atom1.id, self.atom1.clone())
-        clone.atom2 = atom_map.get(self.atom2.id, self.atom2.clone())
+        clone.atom1 = atom_map.get(
+            self._atom1.get_id(),
+            self._atom1.clone(),
+        )
+        clone.atom2 = atom_map.get(
+            self._atom2.get_id(),
+            self._atom2.clone(),
+        )
         return clone
 
     def is_periodic(self):
@@ -138,22 +229,22 @@ class Bond:
 
         """
 
-        return any(direction != 0 for direction in self.periodicity)
+        return any(direction != 0 for direction in self._periodicity)
 
     def __repr__(self):
-        if isinstance(self.order, float) and self.order.is_integer():
-            self.order = int(self.order)
+        if isinstance(self._order, float) and self._order.is_integer():
+            self._order = int(self._order)
 
         periodicity = (
-            f', periodicity={self.periodicity}'
+            f', periodicity={self._periodicity}'
             if self.is_periodic()
             else ''
         )
 
         cls_name = self.__class__.__name__
         return (
-            f'{cls_name}({self.atom1!r}, {self.atom2!r}, '
-            f'{self.order}{periodicity})'
+            f'{cls_name}({self._atom1!r}, {self._atom2!r}, '
+            f'{self._order}{periodicity})'
         )
 
     def __str__(self):
