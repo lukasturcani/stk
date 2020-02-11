45d51d261640932df556ae083448fa7cc44f76b4
Lukas Turcani <lukasturcani93@gmail.com>
2020-02-11 19:38:14 +0100

    Removed old code.

--- src/stk/molecular/__init__.py
+++ src/stk/molecular/__init__.py
@@ -1,11 +1,3 @@
-from .topology_graphs import (
-    cof,
-    macrocycle,
-    polymer,
-    rotaxane,
-    cage,
-    host_guest
-)
 from .topology_graphs.topology_graph import *
 from .functional_groups import *
 from .molecules import *
