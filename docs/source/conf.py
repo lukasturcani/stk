03729c93c9fea28ca7a4044a50844c198709e779
Lukas Turcani <lukasturcani93@gmail.com>
2020-02-17 12:31:43 +0100

    Updated mock modules.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
+
 MOCK_MODULES = [
     'rdkit',
     'numpy',
@@ -38,6 +39,7 @@ MOCK_MODULES = [
     'pathos',
     'seaborn',
 ]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
