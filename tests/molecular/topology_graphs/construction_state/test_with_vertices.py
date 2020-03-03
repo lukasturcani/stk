197643a1b56b0da9f37a4e89fc0bb2a6feef7bd4
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-03 07:24:26 +0100

    Added test.

---
+++ tests/molecular/topology_graphs/construction_state/test_with_vertices.py
@@ -0,0 +1,32 @@
+import pytest
+import stk
+
+
+from .utilities import is_clone
+
+
+def test_with_vertices(construction_state, vertices):
+    clone = construction_state.clone()
+    _test_with_vertices(construction_state, vertices)
+    # Test immutability.
+    is_clone(construction_state, clone)
+
+
+def _test_with_vertices(construction_state, vertices):
+    clone = construction_state.with_vertices(vertices)
+    assert clone.get_num_vertices() == len(vertices)
+    for vertex_id in range(len(vertices)):
+        assert clone.get_vertex(vertex_id) is vertices[vertex_id]
+
+
+@pytest.fixture(
+    params=(
+        (
+            stk.Vertex(0, [0, 0, 0]),
+            stk.Vertex(1, [10, 0, 0]),
+            stk.Vertex(2, [20, 0, 0]),
+        ),
+    ),
+)
+def vertices(request):
+    return request.param
