382bf846608c4281cf09b1dd76ea006e473c280e
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-07 23:14:38 +0100

    Done cage topologies!

--- tests/molecular/molecules/molecule/conftest.py
+++ tests/molecular/molecules/molecule/conftest.py
@@ -933,6 +933,67 @@ def get_atom_ids(request):
                 '112)C41[C+]=N[C+]51'
             ),
         ),
+        CaseData(
+            molecule=stk.ConstructedMolecule(
+                building_blocks=(
+                    stk.BuildingBlock(
+                        smiles=(
+                            'Br[C+]1[C+2][C+](Br)[C+]2[C+](F)[C+2]C2(B'
+                            'r)[C+2]1'
+                        ),
+                        functional_groups=[stk.BromoFactory()],
+                    ),
+                ),
+                topology_graph=stk.cage.OnePlusOne(),
+            ),
+            smiles=(
+                'F[C+]1[C+2]C23[C+2][C+]4[C+2][C+]([C+]12)C12[C+2][C+]'
+                '(F)[C+]1[C+]3[C+2][C+]4[C+2]2'
+            ),
+        ),
+        CaseData(
+            molecule=stk.ConstructedMolecule(
+                building_blocks=(
+                    stk.BuildingBlock(
+                        smiles=(
+                            'Br[C+]1[C+2][C+](Br)[C+]2[C+](F)[C+2]C2(B'
+                            'r)[C+2]1'
+                        ),
+                        functional_groups=[stk.BromoFactory()],
+                    ),
+                ),
+                topology_graph=stk.cage.TwoPlusTwo(),
+            ),
+            smiles=(
+                'F[C+]1[C+2]C23[C+2][C+]4[C+2][C+]([C+]5[C+2][C+]6[C+]'
+                '7[C+](F)[C+2]C7([C+2]5)[C+]5[C+2][C+]2[C+2]C2([C+2][C'
+                '+](F)[C+]52)[C+]2[C+2][C+]4[C+2]C64[C+2][C+](F)[C+]24'
+                ')[C+]13'
+            ),
+        ),
+        CaseData(
+            molecule=stk.ConstructedMolecule(
+                building_blocks=(
+                    stk.BuildingBlock(
+                        smiles=(
+                            'Br[C+]1[C+2][C+](Br)[C+]2[C+](F)[C+2]C2(B'
+                            'r)[C+2]1'
+                        ),
+                        functional_groups=[stk.BromoFactory()],
+                    ),
+                ),
+                topology_graph=stk.cage.FourPlusFour(),
+            ),
+            smiles=(
+                'F[C+]1[C+2]C23[C+2][C+]4[C+2][C+]([C+]5[C+2][C+]6[C+]'
+                '7[C+](F)[C+2]C7([C+2]5)[C+]5[C+2][C+]7[C+2]C8([C+2][C'
+                '+](F)[C+]58)[C+]5[C+2][C+]8[C+2]C9([C+2][C+](F)[C+]59'
+                ')[C+]5[C+2][C+]([C+2]C69[C+2][C+](F)[C+]59)C56[C+2][C'
+                '+](F)[C+]5[C+]([C+2][C+]4[C+2]6)[C+]4[C+2][C+]([C+]5['
+                'C+](F)[C+2]C85[C+2]4)C45[C+2][C+](F)[C+]4[C+]7[C+2][C'
+                '+]2[C+2]5)[C+]13'
+            ),
+        ),
     ),
 )
 def case_data(request):
