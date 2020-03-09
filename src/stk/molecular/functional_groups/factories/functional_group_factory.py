f4bdcfa49d29533c7062de5d0f3d6b664fc8d533
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-09 13:37:20 +0100

    improved docs

--- src/stk/molecular/functional_groups/factories/functional_group_factory.py
+++ src/stk/molecular/functional_groups/factories/functional_group_factory.py
@@ -1,6 +1,77 @@
+"""
+Functional Group Factory
+========================
+
+#. :class:`.AlcoholFactory`
+#. :class:`.AldehydeFactory`
+#. :class:`.AmideFactory`
+#. :class:`.BoronicAcidFactory`
+#. :class:`.BromoFactory`
+#. :class:`.CarboxylicAcidFactory`
+#. :class:`.DibromoFactory`
+#. :class:`.DifluoroFactory`
+#. :class:`.DiolFactory`
+#. :class:`.FluoroFactory`
+#. :class:`.IodoFactory`
+#. :class:`.PrimaryAminoFactory`
+#. :class:`.RingAmineFactory`
+#. :class:`.SecondaryAminoFactory`
+#. :class:`.TerminalAlkeneFactory`
+#. :class:`.TerminalAlkyneFactory`
+#. :class:`.ThioacidFactory`
+#. :class:`.ThiolFactory`
+
+"""
+
+
 class FunctionalGroupFactory:
     """
-    Creates :class:`.FunctionalGroup` instances.
+    An abstract base class for functional group factories.
+
+    The purpose of a functional group factory is to create
+    :class:`.FunctionalGroup` instances. It allows the user to avoid
+    creating :class:`.FunctionalGroup` instances manually. The user
+    will use :meth:`.get_functional_groups` to achieve this. Subclasses
+    of this class are made to customize the automatic creation
+    process.
+
+    Examples
+    --------
+    You have a building block with two functional groups, but you want
+    to use just one.
+
+    .. code-block:: python
+
+        import stk
+
+        amino_factory = stk.PrimaryAminoFactory()
+        building_block = stk.BuildingBlock('NCCN')
+        amino_group = next(
+            amino_factory.get_functional_groups(building_block)
+        )
+        building_block = building_block.with_functional_groups(
+            functional_groups=((amino_group, ),
+        )
+
+    You have multiple functional groups, but you want the building
+    block to use a specific subset.
+
+    .. code-block:: python
+
+        import stk
+
+        bromo_factory = stk.BromoFactory()
+        building_block = stk.BuildingBlock('BrCC(Br)CC(Br)CC(Br)CCBr')
+        bromo_groups = tuple(
+            bromo_factory.get_functional_groups(molecule)
+        )
+        building_block = building_block.with_functional_groups(
+            functional_groups=(
+                bromo_groups[0],
+                bromo_groups[3],
+                bromo_groups[4],
+            ),
+        )
 
     """
 
@@ -10,12 +81,16 @@ class FunctionalGroupFactory:
 
         Parameters
         ----------
-        :class:`.Molecule`
+        molecule : :class:`.Molecule`
 
         Yields
         ------
         :class:`.FunctionalGroup`
             A functional group in `molecule`.
+
+        Examples
+        --------
+        See :class:`.FunctionalGroupFactory`.
 
         """
 
