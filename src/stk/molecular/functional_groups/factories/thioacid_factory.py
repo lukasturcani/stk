c04125e96a797f58fbfdf457c56f6802367ebb3d
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-09 19:19:35 +0100

    docs

--- src/stk/molecular/functional_groups/factories/thioacid_factory.py
+++ src/stk/molecular/functional_groups/factories/thioacid_factory.py
@@ -16,6 +16,49 @@ class ThioacidFactory(FunctionalGroupFactory):
     Creates functional groups from substructures, which match the
     ``[*][C](=[O])[S][H]`` functional group string.
 
+    Examples
+    --------
+    You want to create a building block which has :class:`.Thioacid`
+    functional groups. You want the carbon atom in those functional
+    groups to be the *bonder* atom, and SH group to be the *deleter*
+    atoms.
+
+    .. code-block:: python
+
+        import stk
+
+        building_block = stk.BuildingBlock(
+            smiles='SC(=O)CC(=O)S',
+            functional_groups=(stk.ThioacidFactory(), ),
+        )
+
+    You want to create a building block which has :class:`.Thioacid`
+    functional groups. You want the carbon atom to be the *bonder*
+    atom and the oxygen atom to be the *deleter* atom.
+
+    .. code-block:: python
+
+        import stk
+
+        thioacid_factory = stk.ThioacidFactory(
+            # The index of the carbon atom in the functional
+            # group string (see docstring) is 1.
+            bonders=(1, ),
+            # The index of the oxygen atom in the functional
+            # group string (see docstring) is 2.
+            deleters=(2, ),
+        )
+        building_block = stk.BuildingBlock(
+            smiles='SC(=O)CC(=O)S',
+            functional_groups=(thioacid_factory, ),
+        )
+
+
+    See Also
+    --------
+    :class:`.GenericFunctionalGroup`
+        Defines *bonders* and  *deleters*.
+
     """
 
     def __init__(self, bonders=(1, ), deleters=(3, 4)):
@@ -26,11 +69,11 @@ class ThioacidFactory(FunctionalGroupFactory):
         ----------
         bonders : :class:`tuple` of :class:`int`
             The indices of atoms in the functional group string, which
-            are bonder atoms.
+            are *bonder* atoms.
 
         deleters : :class:`tuple` of :class:`int`
             The indices of atoms in the functional group string, which
-            are deleter atoms.
+            are *deleter* atoms.
 
         """
 
