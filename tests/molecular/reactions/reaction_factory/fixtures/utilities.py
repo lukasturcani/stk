5fe313cb4f66f9bdf696bb32ae91e96537c6bec3
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-13 17:27:28 +0100

    wip

--- tests/molecular/reactions/reaction_factory/fixtures/utilities.py
+++ tests/molecular/reactions/reaction_factory/fixtures/utilities.py
@@ -1,4 +1,12 @@
 import stk
+construction_state = stk.construction_state
+_GraphState = construction_state.graph_state._GraphState
+_MoleculeState = construction_state.molecule_state._MoleculeState
+
+
+class MockGraphState(_GraphState):
+    def __init__(self, edges):
+        self._edges = edges
 
 
 class MockConstructionState(stk.ConstructionState):
@@ -8,9 +16,12 @@ class MockConstructionState(stk.ConstructionState):
         edge_functional_groups,
         position_matrix=None,
     ):
-        self._edges = edges
-        self._edge_functional_groups = edge_functional_groups
-        self._position_matrix = position_matrix
+        self._molecule_state = _MoleculeState()
+        self._molecule_state._position_matrix = position_matrix
+        self._molecule_state._edge_functional_groups = (
+            edge_functional_groups
+        )
+        self._graph_state = MockGraphState(edges)
 
 
 class MockEdge(stk.Edge):
