a78ac93d026a0b62f0f86274d8160b086608ee60
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-17 19:56:19 +0100

    wip

--- tests/molecular/functional_groups/functional_group/generic_functional_group/test_get_bonder_ids.py
+++ tests/molecular/functional_groups/functional_group/generic_functional_group/test_get_bonder_ids.py
@@ -2,6 +2,21 @@ import itertools as it
 
 
 def test_get_bonder_ids(generic_case_data):
+    """
+    Test :meth:`.GenericFunctionalGroup.get_bonder_ids`.
+
+    Parameters
+    ----------
+    generic_case_data : :class:`.GenericCaseData`
+        The test case. Holds the functional group to test and the
+        atoms holding the correct bonder ids.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     _test_get_bonder_ids(
         functional_group=generic_case_data.functional_group,
         bonders=generic_case_data.bonders,
@@ -9,6 +24,23 @@ def test_get_bonder_ids(generic_case_data):
 
 
 def _test_get_bonder_ids(functional_group, bonders):
+    """
+    Test :meth:`.GenericFunctionalGroup.get_bonder_ids`.
+
+    Parameters
+    ----------
+    functional_group : :class:`.GenericFunctionalGroup`
+        The functional group to test.
+
+    bonders : :class:`tuple` of :class:`.Atom`
+        The atoms holding the correct bonder ids.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     fg_atoms = it.zip_longest(
         functional_group.get_bonder_ids(),
         bonders,
