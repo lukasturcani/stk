289bdb3c8b2885afa56a05fca602a5a6b239ee5e
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-17 20:08:33 +0100

    wip

--- tests/molecular/functional_groups/functional_group/generic_functional_group/test_get_num_bonders.py
+++ tests/molecular/functional_groups/functional_group/generic_functional_group/test_get_num_bonders.py
@@ -1,4 +1,19 @@
 def test_get_num_bonders(generic_case_data):
+    """
+    Test :meth:`.GenericFunctionalGroup.get_num_bonders`.
+
+    Parameters
+    ----------
+    generic_case_data : :class:`.GenericCaseData`
+        Test case. Holds the functional group to test and the
+        correct number of bonder atoms.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     _test_get_num_bonders(
         functional_group=generic_case_data.functional_group,
         num_bonders=len(generic_case_data.bonders),
@@ -6,4 +21,17 @@ def test_get_num_bonders(generic_case_data):
 
 
 def _test_get_num_bonders(functional_group, num_bonders):
+    """
+    Test :meth:`.GenericFunctionalGroup.get_num_bonders`.
+
+    Parameters
+    ----------
+    functional_group : :class:`.GenericFunctionalGroup`
+        The functional group to test.
+
+    num_bonders : :class:`int`
+        The correct number of bonder atoms.
+
+    """
+
     assert functional_group.get_num_bonders() == num_bonders
