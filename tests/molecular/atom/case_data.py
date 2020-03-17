5019c9684be091b24d04f30cddf8cdc0ff2ef265
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-17 12:55:01 +0100

    Added case data

---
+++ tests/molecular/atom/case_data.py
@@ -0,0 +1,7 @@
+class CaseData:
+    def __init__(self, atom, id, charge, atomic_number, mass):
+        self.atom = atom
+        self.id = id
+        self.charge = charge
+        self.atomic_number = atomic_number
+        self.mass = mass
