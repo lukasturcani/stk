f345b67ae1ce24cda130d6dcdbeefe5ed34ee60b
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-17 18:59:23 +0100

    wip

--- tests/molecular/functional_groups/functional_group/case_data.py
+++ tests/molecular/functional_groups/functional_group/case_data.py
@@ -2,9 +2,42 @@ class CaseData:
     """
     A test case for :class:`.FunctionalGroup` instances.
 
+    Attributes
+    ----------
+    functional_group : :class:`.FunctionalGroup`
+        The functional group to test.
+
+    atoms : :class:`tuple` of :class:`.Atom`
+        The correct atoms held by the functional group.
+
+    placers : :class:`tuple` of :class:`.Atom`
+        The correct placer atoms held by the functional group.
+
+    core_atoms : :class:`tuple` of :class:`.Atom`
+        The correct core atoms held by the functional group.
+
     """
 
     def __init__(self, functional_group, atoms, placers, core_atoms):
+        """
+        Initialize a :class:`.CaseData` instance.
+
+        Parameters
+        ----------
+        functional_group : :class:`.FunctionalGroup`
+            The functional group to test.
+
+        atoms : :class:`tuple` of :class:`.Atom`
+            The correct atoms held by the functional group.
+
+        placers : :class:`tuple` of :class:`.Atom`
+            The correct placer atoms held by the functional group.
+
+        core_atoms : :class:`tuple` of :class:`.Atom`
+            The correct core atoms held by the functional group.
+
+        """
+
         self.functional_group = functional_group
         self.atoms = atoms
         self.placers = placers
@@ -15,17 +48,57 @@ class GenericCaseData(CaseData):
     """
     A test case for :class:`.GenericFunctionalGroup` instances.
 
+    Attributes
+    ----------
+    functional_group : :class:`.FunctionalGroup`
+        The functional group to test.
+
+    atoms : :class:`tuple` of :class:`.Atom`
+        The correct atoms held by the functional group.
+
+    placers : :class:`tuple` of :class:`.Atom`
+        The correct placer atoms held by the functional group.
+
+    core_atoms : :class:`tuple` of :class:`.Atom`
+        The correct core atoms held by the functional group.
+
+    bonders : :class:`tuple` of :class:`.Atom`
+        The correct bonder atoms held by the functional group.
+
+    deleters : :class:`tuple` of :class:`.Atom`
+        The correct deleter atoms held by the functional group.
+
     """
 
     def __init__(self, functional_group, atoms, bonders, deleters):
+        """
+        Initialize a :class:`.GenericCaseData` instance.
+
+        Parameters
+        ----------
+        functional_group : :class:`.FunctionalGroup`
+            The functional group to test.
+
+        atoms : :class:`tuple` of :class:`.Atom`
+            The correct atoms held by `functional_group`.
+
+        bonders : :class:`tuple` of :class:`.Atom`
+            The correct bonder atoms of `functional_group`.
+
+        deleters : :class:`tuple` of :class:`.Atom`
+            The correct deleter atoms of `functional_group`.
+
+        """
+
         self.bonders = bonders
         self.deleters = deleters
-        deleter_set = set(deleters)
+        deleter_ids = set(atom.get_id() for atom in deleters)
         super().__init__(
             functional_group=functional_group,
             atoms=atoms,
             placers=bonders,
             core_atoms=tuple(
-                atom for atom in atoms if atom not in deleter_set
+                atom for atom in atoms
+                if atom.get_id() not in deleter_ids
             ),
         )
