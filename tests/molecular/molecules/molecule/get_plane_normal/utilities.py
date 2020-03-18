7ab9d54851d82a5b20d5d7b52cb4495404766d19
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-18 15:58:52 +0100

    wip

--- tests/molecular/molecules/molecule/get_plane_normal/utilities.py
+++ tests/molecular/molecules/molecule/get_plane_normal/utilities.py
@@ -3,6 +3,12 @@ from ..utilities import get_centroid
 
 
 def get_plane_normal(position_matrix, atom_ids):
+    # In this case, Molecule.get_plane_normal should raise the error,
+    # so this should should just fail silently, and the explicit
+    # failure of Molecule.get_plane_normal will be caught.
+    if len(atom_ids) == 0:
+        return
+
     atomic_positions = position_matrix[atom_ids, :]
     centroid = get_centroid(position_matrix, atom_ids)
     return np.linalg.svd(atomic_positions - centroid)[-1][2, :]
--- tests/molecular/molecules/molecule/utilities.py
+++ tests/molecular/molecules/molecule/utilities.py
@@ -2,10 +2,22 @@ import numpy as np
 
 
 def get_centroid(position_matrix, atom_ids):
+    # In this case, Molecule.get_centroid should raise the error, so
+    # this should should just fail silently, and the explicit failure
+    # of Molecule.get_centroid will be caught.
+    if len(atom_ids) == 0:
+        return
+
     return np.sum(position_matrix[atom_ids, :], axis=0) / len(atom_ids)
 
 
 def get_direction(position_matrix, atom_ids):
+    # In this case, Molecule.get_direction should raise the error, so
+    # this should should just fail silently, and the explicit failure
+    # of Molecule.get_direction will be caught.
+    if len(atom_ids) == 0:
+        return
+
     atom_positions = position_matrix[atom_ids, :]
     centered_positions = atom_positions - atom_positions.mean(axis=0)
     return np.linalg.svd(centered_positions)[-1][0]
