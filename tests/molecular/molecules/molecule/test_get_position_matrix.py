0d667a3111bf96aa07f2d870f9510d7f2c7a7937
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-18 13:15:29 +0100

    wip

--- tests/molecular/molecules/molecule/test_get_position_matrix.py
+++ tests/molecular/molecules/molecule/test_get_position_matrix.py
@@ -1,10 +1,23 @@
 import numpy as np
 
 
-def test_get_position_matrix(molecule, get_position_matrix):
-    position_matrix = get_position_matrix(molecule)
-    molecule = molecule.with_position_matrix(position_matrix)
+def test_get_position_matrix(case_data):
+    """
+    Test :meth:`.Molecule.get_position_matrix`.
+
+    Parameters
+    ----------
+    case_data : :class:`.CaseData
+        A test case. Holds the molecule to test and the correct
+        position matrix.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     assert np.all(np.equal(
-        position_matrix,
-        molecule.get_position_matrix(),
+        case_data.position_matrix,
+        case_data.molecule.get_position_matrix(),
     ))
