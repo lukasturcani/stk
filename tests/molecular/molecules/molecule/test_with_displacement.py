09cdc849af95c802cbb8bf7561fbaf7ded17e253
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-18 13:58:58 +0100

    wip

--- tests/molecular/molecules/molecule/test_with_displacement.py
+++ tests/molecular/molecules/molecule/test_with_displacement.py
@@ -8,6 +8,23 @@ def displacement(origin):
 
 
 def test_with_displacement(molecule, displacement):
+    """
+    Test :meth:`.Molecule.with_displacement`.
+
+    Parameters
+    ----------
+    molecule : :class:`.Molecule`
+        The moleclue to test.
+
+    displacement : :class:`numpy.ndarray`
+        The displacement the clone should have.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     new = molecule.with_displacement(displacement)
     assert np.allclose(
         a=molecule.get_position_matrix()+displacement,
