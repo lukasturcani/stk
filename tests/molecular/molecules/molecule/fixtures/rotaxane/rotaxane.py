945e3c0e953fbd6e0736297e04d6ea16f8771cf2
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-18 19:11:33 +0100

    wip

--- tests/molecular/molecules/molecule/fixtures/rotaxane/rotaxane.py
+++ tests/molecular/molecules/molecule/fixtures/rotaxane/rotaxane.py
@@ -1,6 +1,9 @@
 from pytest_lazyfixture import lazy_fixture
 import pytest
 
+# Fixtures must be visible for lazy_fixture() calls.
+from .nrotaxane import *  # noqa
+
 
 @pytest.fixture(
     params=(
