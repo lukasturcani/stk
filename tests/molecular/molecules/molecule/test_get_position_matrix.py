099ee528dc0edebe0ffb5a2b71fcb540bc17b411
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-18 23:01:50 +0100

    Revert "weaker position checking"
    This reverts commit 2c5347e41abe146802b027801991ad0232e234db.

--- tests/molecular/molecules/molecule/test_get_position_matrix.py
+++ tests/molecular/molecules/molecule/test_get_position_matrix.py
@@ -17,8 +17,7 @@ def test_get_position_matrix(case_data):
 
     """
 
-    assert np.allclose(
-        a=case_data.position_matrix,
-        b=case_data.molecule.get_position_matrix(),
-        atol=1e-13,
+    assert np.all(np.equal(
+        case_data.position_matrix,
+        case_data.molecule.get_position_matrix(),
     ))
