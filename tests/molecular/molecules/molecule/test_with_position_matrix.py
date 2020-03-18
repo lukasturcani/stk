4366ca04644bb9b79e5cdf89f5a15a0d1bf16550
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-18 14:03:40 +0100

    wip

--- tests/molecular/molecules/molecule/test_with_position_matrix.py
+++ tests/molecular/molecules/molecule/test_with_position_matrix.py
@@ -2,9 +2,28 @@ import numpy as np
 
 
 def test_with_position_matrix(molecule, get_position_matrix):
+    """
+    Test :meth:`.Molecule.with_position_matrix`.
+
+    Parameters
+    ----------
+    molecule : :class:`.Molecule`
+        The molecule to test.
+
+    get_position_matrix : :class:`callable`
+        Takes a single parameter, `molecule`, and returns a valid
+        position matrix to use for this test.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    # Save a copy of the position matrix, to ensure the original is
+    # not modified by the test.
     position_matrix = molecule.get_position_matrix()
     _test_with_position_matrix(molecule, get_position_matrix)
-    # Test immutability.
     assert np.all(np.equal(
         position_matrix,
         molecule.get_position_matrix(),
@@ -12,6 +31,24 @@ def test_with_position_matrix(molecule, get_position_matrix):
 
 
 def _test_with_position_matrix(molecule, get_position_matrix):
+    """
+    Test :meth:`.Molecule.with_position_matrix`.
+
+    Parameters
+    ----------
+    molecule : :class:`.Molecule`
+        The molecule to test.
+
+    get_position_matrix : :class:`callable`
+        Takes a single parameter, `molecule`, and returns a valid
+        position matrix to use for this test.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     position_matrix = get_position_matrix(molecule)
     molecule = molecule.with_position_matrix(position_matrix)
     assert np.all(np.equal(
