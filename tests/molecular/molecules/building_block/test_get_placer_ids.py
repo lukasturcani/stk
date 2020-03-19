c70c3ef1cc446b89f3732de72066b6c84019f007
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-20 00:50:32 +0100

    wip

--- tests/molecular/molecules/building_block/test_get_placer_ids.py
+++ tests/molecular/molecules/building_block/test_get_placer_ids.py
@@ -1,192 +1,22 @@
-import pytest
-import rdkit.Chem.AllChem as rdkit
-import numpy as np
-import stk
 import itertools as it
 
 
-class CaseData:
-    def __init__(self, building_block, placer_ids):
-        self.building_block = building_block
-        self.placer_ids = placer_ids
-
-
-@pytest.fixture(
-    params=(
-        CaseData(
-            building_block=stk.BuildingBlock('Br[C+2][C+2]Br'),
-            placer_ids=(0, 1, 2, 3),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock(
-                smiles='BrCCCBr',
-                functional_groups=[stk.BromoFactory()],
-                placer_ids=(),
-            ),
-            placer_ids=(),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock(
-                smiles='BrCCCBr',
-                functional_groups=[stk.BromoFactory()],
-                placer_ids=(2, ),
-            ),
-            placer_ids=(2, ),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock(
-                smiles='Br[C+2][C+2]Br',
-                functional_groups=[stk.IodoFactory()],
-            ),
-            placer_ids=(0, 1, 2, 3),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock(
-                smiles='Br[C+2][C+2]Br',
-                functional_groups=[stk.BromoFactory()],
-            ),
-            placer_ids=(1, 2),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init(
-                atoms=(stk.Br(0), stk.C(1), stk.C(2), stk.Br(3)),
-                bonds=(
-                    stk.Bond(stk.Br(0), stk.C(1), 1),
-                    stk.Bond(stk.C(1), stk.C(2), 1),
-                    stk.Bond(stk.C(2), stk.Br(3), 1),
-                ),
-                position_matrix=np.array([
-                    [0., 0., 0.],
-                    [2., 0., 0.],
-                    [3., 0., 0.],
-                    [4., 0., 0.],
-                ]),
-                functional_groups=(
-                    stk.Bromo(stk.Br(0), stk.C(1), (stk.C(1), ), ()),
-                    stk.Bromo(stk.Br(3), stk.C(2), (stk.C(2), ), ()),
-                ),
-                placer_ids=(),
-            ),
-            placer_ids=(),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init(
-                atoms=(stk.Br(0), stk.C(1), stk.C(2), stk.Br(3)),
-                bonds=(
-                    stk.Bond(stk.Br(0), stk.C(1), 1),
-                    stk.Bond(stk.C(1), stk.C(2), 1),
-                    stk.Bond(stk.C(2), stk.Br(3), 1),
-                ),
-                position_matrix=np.array([
-                    [0., 0., 0.],
-                    [2., 0., 0.],
-                    [3., 0., 0.],
-                    [4., 0., 0.],
-                ]),
-                functional_groups=(
-                    stk.Bromo(stk.Br(0), stk.C(1), (stk.C(1), ), ()),
-                    stk.Bromo(stk.Br(3), stk.C(2), (stk.C(2), ), ()),
-                ),
-                placer_ids=(3, ),
-            ),
-            placer_ids=(3, ),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init(
-                atoms=(stk.Br(0), stk.C(1), stk.C(2), stk.Br(3)),
-                bonds=(
-                    stk.Bond(stk.Br(0), stk.C(1), 1),
-                    stk.Bond(stk.C(1), stk.C(2), 1),
-                    stk.Bond(stk.C(2), stk.Br(3), 1),
-                ),
-                position_matrix=np.array([
-                    [0., 0., 0.],
-                    [2., 0., 0.],
-                    [3., 0., 0.],
-                    [4., 0., 0.],
-                ]),
-                functional_groups=[stk.IodoFactory()],
-            ),
-            placer_ids=(0, 1, 2, 3),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init(
-                atoms=(stk.Br(0), stk.C(1), stk.C(2), stk.Br(3)),
-                bonds=(
-                    stk.Bond(stk.Br(0), stk.C(1), 1),
-                    stk.Bond(stk.C(1), stk.C(2), 1),
-                    stk.Bond(stk.C(2), stk.Br(3), 1),
-                ),
-                position_matrix=np.array([
-                    [0., 0., 0.],
-                    [2., 0., 0.],
-                    [3., 0., 0.],
-                    [4., 0., 0.],
-                ]),
-                functional_groups=(
-                    stk.Bromo(stk.Br(0), stk.C(1), (stk.C(1), ), ()),
-                    stk.Bromo(stk.Br(3), stk.C(2), (stk.C(2), ), ()),
-                ),
-            ),
-            placer_ids=(1, 2),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init_from_molecule(
-                molecule=stk.BuildingBlock('Br[C+2][C+2]Br'),
-                functional_groups=[stk.IodoFactory()],
-            ),
-            placer_ids=(0, 1, 2, 3),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init_from_molecule(
-                molecule=stk.BuildingBlock('Br[C+2][C+2]Br'),
-                functional_groups=[stk.BromoFactory()],
-            ),
-            placer_ids=(1, 2),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init_from_molecule(
-                molecule=stk.BuildingBlock('Br[C+2][C+2]Br'),
-                functional_groups=[stk.BromoFactory()],
-                placer_ids=(3, ),
-            ),
-            placer_ids=(3, ),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init_from_rdkit_mol(
-                molecule=(
-                    stk.BuildingBlock('Br[C+2][C+2]Br').to_rdkit_mol()
-                ),
-                functional_groups=[stk.IodoFactory()],
-            ),
-            placer_ids=(0, 1, 2, 3),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init_from_rdkit_mol(
-                molecule=(
-                    stk.BuildingBlock('Br[C+2][C+2]Br').to_rdkit_mol()
-                ),
-                functional_groups=[stk.BromoFactory()],
-            ),
-            placer_ids=(1, 2),
-        ),
-        CaseData(
-            building_block=stk.BuildingBlock.init_from_rdkit_mol(
-                molecule=(
-                    stk.BuildingBlock('Br[C+2][C+2]Br').to_rdkit_mol()
-                ),
-                functional_groups=[stk.BromoFactory()],
-                placer_ids=(3, ),
-            ),
-            placer_ids=(3, ),
-        ),
-    ),
-)
-def case_data(request):
-    return request.param
-
-
 def test_get_placer_ids(case_data):
+    """
+    Test :meth:`.BuildingBlock.get_placer_ids`.
+
+    Parameters
+    ----------
+    case_data : :class:`.CaseData`
+        A test case. Holds the building block to test and the
+        correct placer ids.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     _test_get_placer_ids(
         building_block=case_data.building_block,
         placer_ids=case_data.placer_ids,
@@ -194,6 +24,23 @@ def test_get_placer_ids(case_data):
 
 
 def _test_get_placer_ids(building_block, placer_ids):
+    """
+    Test :meth:`.BuildingBlock.get_placer_ids`.
+
+    Parameters
+    ----------
+    building_block : :class:`.BuildingBlock`
+        The building block to test.
+
+    placer_ids : :class:`tuple` of :class:`int`
+        The correct placer ids.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     for placer1, placer2 in it.zip_longest(
         building_block.get_placer_ids(),
         placer_ids,
--- tests/molecular/molecules/building_block/test_with_functional_groups.py
+++ tests/molecular/molecules/building_block/test_with_functional_groups.py
@@ -1,6 +1,3 @@
-import stk
-import pytest
-
 from ..utilities import has_same_structure, is_equivalent_molecule
 from .utilities import (
     is_equivalent_building_block,
@@ -8,60 +5,53 @@ from .utilities import (
 )
 
 
-class _TestCase:
-    def __init__(self, building_block, functional_groups):
-        self.building_block = building_block
-        self.functional_groups = functional_groups
+def test_with_functional_groups(building_block, get_functional_groups):
+    """
+    Test :meth:`.BuildingBlock.with_functional_groups`.
 
+    Parameters
+    ----------
+    building_block : :class:`.BuildingBlock`
+        The building block to test.
 
-@pytest.mark.parametrize(
-    argnames='test_case',
-    argvalues=(
-        _TestCase(
-            building_block=stk.BuildingBlock(
-                smiles='NCC(Br)CN',
-                functional_groups=[stk.BromoFactory()],
-            ),
-            functional_groups=(
-                stk.PrimaryAmino(
-                    nitrogen=stk.N(0),
-                    hydrogen1=stk.H(6),
-                    hydrogen2=stk.H(7),
-                    atom=stk.C(1),
-                    bonders=(stk.N(0), ),
-                    deleters=(stk.H(6), stk.H(7)),
-                ),
-                stk.PrimaryAmino(
-                    nitrogen=stk.N(5),
-                    hydrogen1=stk.H(13),
-                    hydrogen2=stk.H(14),
-                    atom=stk.C(4),
-                    bonders=(stk.N(5), ),
-                    deleters=(stk.H(13), stk.H(14)),
-                ),
-            ),
-        ),
-    ),
-)
-def test_with_functional_groups(test_case):
-    _test_with_functional_groups(
-        building_block=test_case.building_block,
-        functional_groups=test_case.functional_groups,
-    )
+    get_functional_groups : :class:`callable`
+        Takes a single parameter, `building_block` and returns the
+        `functional_groups` parameter to use for this test.
 
+    Returns
+    -------
+    None : :class:`NoneType`
 
-def _test_with_functional_groups(building_block, functional_groups):
+    """
+
+    # Save clone to check immutability.
     clone = building_block.clone()
-    _test_with_functional_groups_0(
+    _test_with_functional_groups(
         building_block=building_block,
-        functional_groups=functional_groups,
+        functional_groups=tuple(get_functional_groups(building_block)),
     )
-    # Test immutability.
     is_equivalent_building_block(building_block, clone)
     has_same_structure(building_block, clone)
 
 
-def _test_with_functional_groups_0(building_block, functional_groups):
+def _test_with_functional_groups(building_block, functional_groups):
+    """
+    Test :meth:`.BuildingBlock.with_functional_groups`.
+
+    Parameters
+    ----------
+    building_block : :class:`.BuildingBlock`
+        The building block to test.
+
+    functional_groups : :class:`tuple` of :class:`.FunctionalGroup`
+        The functional groups the new building block should hold.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
     new = building_block.with_functional_groups(functional_groups)
     are_equivalent_functional_groups(
         new.get_functional_groups(),
