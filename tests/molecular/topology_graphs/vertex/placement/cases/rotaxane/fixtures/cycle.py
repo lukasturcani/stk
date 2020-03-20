405e9cef8945e2c07274fec8c6481039feb79161
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-20 21:01:59 +0100

    wip

--- tests/molecular/topology_graphs/vertex/placement/cases/rotaxane/fixtures/cycle.py
+++ tests/molecular/topology_graphs/vertex/placement/cases/rotaxane/fixtures/cycle.py
@@ -17,10 +17,10 @@ macrocycle = stk.ConstructedMolecule(
         num_repeating_units=5,
     ),
 )
-macrocycle_ids = max(
+macrocycle_ids = tuple(max(
     rdkit.GetSymmSSSR(macrocycle.to_rdkit_mol()),
     key=len,
-)
+))
 
 
 def get_plane_normal(building_block):
@@ -32,27 +32,13 @@ def get_plane_normal(building_block):
     if np.allclose(normal, [1, 0, 0], atol=1e-13):
         return np.array([1, 0, 0], dtype=np.float64)
     elif np.allclose(normal, [-1, 0, 0], atol=1e-13):
-        return np.array([-1, 0, 0], dtype=np.float64)
+        return np.array([1, 0, 0], dtype=np.float64)
 
     return normal
 
 
 @pytest.fixture(
     params=(
-        CaseData(
-            vertex=vertices._CycleVertex(0, (1, 2, 3), True),
-            edges=(),
-            building_block=stk.BuildingBlock.init_from_molecule(
-                molecule=macrocycle,
-            ),
-            position=np.array([1, 2, 3], dtype=np.float64),
-            alignment_tests={},
-            functional_group_edges={
-                get_plane_normal:
-                    np.array([-1, 0, 0], dtype=np.float64),
-            },
-            position_ids=macrocycle_ids,
-        ),
         CaseData(
             vertex=vertices._CycleVertex(0, (1, 2, 3), False),
             edges=(),
@@ -61,6 +47,22 @@ def get_plane_normal(building_block):
             ),
             position=np.array([1, 2, 3], dtype=np.float64),
             alignment_tests={
+                get_plane_normal:
+                    np.array([1, 0, 0], dtype=np.float64),
+            },
+            functional_group_edges={},
+            position_ids=macrocycle_ids,
+        ),
+        CaseData(
+            vertex=vertices._CycleVertex(0, (1, 2, 3), True),
+            edges=(),
+            building_block=stk.BuildingBlock.init_from_molecule(
+                molecule=macrocycle,
+            ),
+            position=np.array([1, 2, 3], dtype=np.float64),
+            alignment_tests={
+                # If correctly aligned, get_plane_normal returns the
+                # same vector, regardless of flip status.
                 get_plane_normal:
                     np.array([1, 0, 0], dtype=np.float64),
             },
