75bfcad620eb30392a4088bc809f23bb1aec0d6d
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-20 15:08:30 +0100

    wip

---
+++ tests/molecular/topology_graphs/vertex/placement/cases/host_guest/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from pytest_lazyfixture import lazy_fixture
+
+# Fixtures need to be visible for lazy_fixture() calls.
+from .fixtures import *  # noqa
+
+
+@pytest.fixture(
+    params=(
+        lazy_fixture('host'),
+        lazy_fixture('guest'),
+    ),
+)
+def case_data(request):
+    return request.param
