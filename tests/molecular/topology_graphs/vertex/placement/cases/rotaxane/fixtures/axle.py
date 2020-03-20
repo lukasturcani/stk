99a07b17c3181e451febcbf57eda26e5ab58d7fc
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-20 19:08:27 +0100

    wip

--- tests/molecular/topology_graphs/vertex/placement/cases/rotaxane/fixtures/axle.py
+++ tests/molecular/topology_graphs/vertex/placement/cases/rotaxane/fixtures/axle.py
@@ -7,12 +7,25 @@ from ....case_data import CaseData
 vertices = stk.molecular.topology_graphs.rotaxane.vertices
 
 
+macrocycle = stk.ConstructedMolecule(
+    topology_graph=stk.macrocycle.Macrocycle(
+        building_blocks=(
+            stk.BuildingBlock('BrCCCBr', [stk.BromoFactory()]),
+        ),
+        repeating_unit='A',
+        num_repeating_units=5,
+    ),
+)
+
+
 @pytest.fixture(
     params=(
         CaseData(
             vertex=vertices._CycleVertex(0, (1, 2, 3), True),
             edges=(),
-            building_block=stk.BuildingBlock('BrCCBr'),
+            building_block=stk.BuildingBlock.init_from_molecule(
+                molecule=macrocycle,
+            ),
             position=np.array([1, 2, 3], dtype=np.float64),
             alignment_tests={},
             functional_group_edges={},
@@ -20,7 +33,9 @@ vertices = stk.molecular.topology_graphs.rotaxane.vertices
         CaseData(
             vertex=vertices._CycleVertex(0, (1, 2, 3), False),
             edges=(),
-            building_block=stk.BuildingBlock('BrCCBr'),
+            building_block=stk.BuildingBlock.init_from_molecule(
+                molecule=macrocycle,
+            ),
             position=np.array([1, 2, 3], dtype=np.float64),
             alignment_tests={},
             functional_group_edges={},
