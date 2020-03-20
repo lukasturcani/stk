5228f1d33194f07a6d481753d87c76d2a02e851f
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-20 16:23:46 +0100

    wip

--- src/stk/molecular/topology_graphs/macrocycle/vertices.py
+++ src/stk/molecular/topology_graphs/macrocycle/vertices.py
@@ -99,7 +99,7 @@ class _CycleVertex(Vertex):
 
     def __str__(self):
         return (
-            f'Vertex(id={self.id}, '
+            f'Vertex(id={self._id}, '
             f'position={self._position.tolist()}, '
             f'flip={self._flip}, '
             f'angle={self._angle})'
