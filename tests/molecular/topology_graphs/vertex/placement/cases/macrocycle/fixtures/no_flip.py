92dcf5f9974036bcd2b8675b17e39aa3227f3e6a
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-20 18:43:37 +0100

    wip

--- tests/molecular/topology_graphs/vertex/placement/cases/macrocycle/fixtures/no_flip.py
+++ tests/molecular/topology_graphs/vertex/placement/cases/macrocycle/fixtures/no_flip.py
@@ -1,5 +1,4 @@
 import pytest
-from scipy.spatial.distance import euclidean
 import numpy as np
 import stk
 
@@ -33,25 +32,14 @@ def no_flip(position, building_block_2, angle):
         )
 
     vertex = vertices._CycleVertex(0, position, False, angle+np.pi/2)
-    edge1, edge2 = edges = tuple(get_edges(vertex, angle))
-
-    fg0, = building_block_2.get_functional_groups(0)
-    fg0_position = building_block_2.get_centroid(fg0.get_placer_ids())
-
-    def fg0_distance(edge):
-        return euclidean(fg0_position, edge.get_position())
-
-    edge0 = min(edges, key=fg0_distance)
-    edge1 = edges[1] if edge0 is edges[0] else edges[0]
-
     return CaseData(
         vertex=vertex,
-        edges=edges,
+        edges=tuple(get_edges(vertex, angle)),
         building_block=building_block_2,
         position=position,
         alignment_tests={
             get_fg0_point: point1,
             get_fg1_point: point2,
         },
-        functional_group_edges={0: edge0.get_id(), 1: edge1.get_id()},
+        functional_group_edges={0: 0, 1: 1},
     )
