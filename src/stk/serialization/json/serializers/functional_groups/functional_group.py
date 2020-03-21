2ad81acc6699e46130e35aa1879cb2fe4c4d10ae
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-21 15:43:19 +0100

    wip

---
+++ src/stk/serialization/json/serializers/functional_groups/functional_group.py
@@ -0,0 +1,26 @@
+"""
+Functional Group Serializer
+===========================
+
+"""
+
+
+class FunctionalGroupSerializer:
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+
+        return super().__init__()
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return (
+            f'{self.__class__.__name__}()'
+        )
---
+++ src/stk/serialization/json/serializers/molecules/utilities.py
@@ -0,0 +1,67 @@
+def molecule_to_json(molecule):
+    """
+    Return a JSON representation of `molecule`.
+
+    Parameters
+    ----------
+    molecule : :class:`.Molecule`
+        The molecule to serialize.
+
+    Returns
+    -------
+    :class:`dict`
+        A JSON representation of `molecule`.
+
+    """
+
+    return {
+        'atoms': tuple(map(atom_to_json, molecule.get_atoms())),
+        'bonds': tuple(map(bond_to_json, molecule.get_bonds())),
+    }
+
+
+def atom_to_json(atom):
+    """
+    Return a JSON representation of `atom`.
+
+    Parameters
+    ----------
+    atom : :class:`.Atom`
+        The atom to serialize.
+
+    Returns
+    -------
+    :class:`dict`
+        A JSON representation of `atom`.
+
+    """
+
+    return {
+        'id': atom.get_id(),
+        'atomic_number': atom.get_atomic_number(),
+        'charge': atom.get_charge(),
+    }
+
+
+def bond_to_json(bond):
+    """
+    Return a JSON representation of `bond`.
+
+    Parameters
+    ----------
+    bond : :class:`.Bond`
+        The bond to serialize.
+
+    Returns
+    -------
+    :class:`dict`
+        A JSON representation of `bond`.
+
+    """
+
+    return {
+        'atom1': bond.get_atom1().get_id(),
+        'atom2': bond.get_atom2().get_id(),
+        'order': bond.get_order(),
+        'periodicity': bond.get_periodicity(),
+    }
