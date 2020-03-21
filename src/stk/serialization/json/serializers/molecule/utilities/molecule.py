cc3958ce78c2f7917e1932a53ae2467f9bef2041
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-21 18:09:25 +0100

    wip

--- src/stk/serialization/json/serializers/molecule/utilities/molecule.py
+++ src/stk/serialization/json/serializers/molecule/utilities/molecule.py
@@ -43,3 +43,47 @@ class _MoleculeSerializer:
             'atoms': tuple(map(atom_to_json, molecule.get_atoms())),
             'bonds': tuple(map(bond_to_json, molecule.get_bonds())),
         }
+def atom_to_json(atom):
+    """
+    Return a JSON representation of `atom`.
+
+    Parameters
+    ----------
+    atom : :class:`.Atom`
+        The atom to serialize.
+
+    Returns
+    -------
+    :class:`dict`
+        A JSON representation of `atom`.
+
+    """
+
+    return {
+        'atomic_number': atom.get_atomic_number(),
+        'charge': atom.get_charge(),
+    }
+
+
+def bond_to_json(bond):
+    """
+    Return a JSON representation of `bond`.
+
+    Parameters
+    ----------
+    bond : :class:`.Bond`
+        The bond to serialize.
+
+    Returns
+    -------
+    :class:`dict`
+        A JSON representation of `bond`.
+
+    """
+
+    return {
+        'atom1': bond.get_atom1().get_id(),
+        'atom2': bond.get_atom2().get_id(),
+        'order': bond.get_order(),
+        'periodicity': bond.get_periodicity(),
+    }
