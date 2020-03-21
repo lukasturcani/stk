6a66f9345625071a27a9925c66857e667d62453d
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-21 20:09:30 +0100

    wp

--- src/stk/serialization/json/serializers/molecule/molecule.py
+++ src/stk/serialization/json/serializers/molecule/molecule.py
@@ -1,23 +1,35 @@
-def molecule_to_json(molecule):
+from .utilities import atom_to_json, bond_to_json
+
+
+class MoleculeJsonizer:
     """
-    Return a JSON representation of `molecule`.
-
-    Parameters
-    ----------
-    molecule : :class:`.Molecule`
-        The molecule to serialize.
-
-    Returns
-    -------
-    :class:`dict`
-        A JSON representation of `molecule`.
 
     """
 
-    return {
-        'atoms': tuple(map(atom_to_json, molecule.get_atoms())),
-        'bonds': tuple(map(bond_to_json, molecule.get_bonds())),
-    }
+    def __init__(self, key_name, molecule_key):
+        self._molecule_key = molecule_key
+
+    def to_json(self, molecule):
+        """
+        Return a JSON representation of `molecule`.
+
+        Parameters
+        ----------
+        molecule : :class:`.Molecule`
+            The molecule to serialize.
+
+        Returns
+        -------
+        :class:`dict`
+            A JSON representation of `molecule`.
+
+        """
+
+        return {
+            self._key_name: self._molecule_key(molecule),
+            'atoms': tuple(map(atom_to_json, molecule.get_atoms())),
+            'bonds': tuple(map(bond_to_json, molecule.get_bonds())),
+        }
 
 
 def atom_to_json(atom):
