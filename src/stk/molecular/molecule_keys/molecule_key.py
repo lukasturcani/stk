a7285ef473dab69e0d928b0b283046575dcdfeca
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-21 20:51:34 +0100

    wip

--- src/stk/molecular/molecule_keys/molecule_key.py
+++ src/stk/molecular/molecule_keys/molecule_key.py
@@ -2,6 +2,9 @@
 Molecule Key
 ============
 
+#. :class:`.Inchi`
+#. :class:`.InchiKey`
+
 """
 
 
@@ -17,16 +20,58 @@ class MoleculeKey:
     simply default implementations, which can be safely ignored or
     overridden, when implementing subclasses.
 
-
+    Examples
+    --------
+    *Subclass Implementation*
 
     """
 
     def __init__(self, name, key):
+        """
+        Initialize a :class:`.MoleculeKey` instance.
+
+        Parameters
+        ----------
+        name : :class:`str`
+            The name of the key.
+
+        key : :class:`callable`
+            Takes a single parameter, `molecule`, and returns the
+            key to use for that molecule. The value passed to the
+            parameter must be a :class:`.Molecule` instance.
+
+        """
+
         self._name = name
         self._key = key
 
     def get_name(self):
+        """
+        Get the name of the key.
+
+        Returns
+        -------
+        :class:`str`
+            The name of the key.
+
+        """
+
         return self._name
 
     def get_key(self, molecule):
+        """
+        Get the key of `molecule`.
+
+        Parameters
+        ----------
+        molecule : :class:`.Molecule`
+            The molecule for which a key is needed.
+
+        Returns
+        -------
+        :class:`object`
+            The key of `molecule`.
+
+        """
+
         return self._key(molecule)
