8f0e5fab1717f109846ed18bd9fd50ef0e7479e9
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-22 10:30:28 +0100

    wip

--- src/stk/molecular/key_makers/constructed_molecule/topology_graph/cage.py
+++ src/stk/molecular/key_makers/constructed_molecule/topology_graph/cage.py
@@ -4,6 +4,8 @@ Cage Key Maker
 
 """
 
+from ...building_block import BuildingBlockKeyMaker
+
 
 class CageKeyMaker:
     """
@@ -11,8 +13,18 @@ class CageKeyMaker:
 
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        building_block_key_maker=BuildingBlockKeyMaker(),
+    ):
         """
+        Initialize a :class:`.CageKeyMaker` instance.
+
+        Parameters
+        ----------
+        building_block_key_maker : \
+                :class:`.BuildingBlockKeyMaker`, optional
+            Used to create the keys of the cage's building blocks.
 
         """
 
@@ -20,7 +32,29 @@ class CageKeyMaker:
 
     def get_key(self, cage):
         """
+        Get the key of `cage`.
+
+        Parameters
+        ----------
+        cage : :class:`.Cage`
+            The cage for which a key is wanted.
+
+        Returns
+        -------
+        :class:`object`
+            The key.
 
         """
 
-        ...
+        building_blocks = (
+            building_block.with_canonical_atom_ordering()
+            for building_block in cage.get_building_blocks()
+        )
+        key = (
+            cage.__class__.__name__,
+            tuple(sorted(map(
+                self._building_block_key_maker.get_key,
+                building_blocks,
+            ))),
+        )
+        return str(key)
