10d2581b4e13e7703c131da33bd6b08f36efb197
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-22 02:41:30 +0100

    wip

--- src/stk/molecular/molecular_keys/building_block_keys/functional_group_keys/alkene_key.py
+++ src/stk/molecular/molecular_keys/building_block_keys/functional_group_keys/alkene_key.py
@@ -1,2 +1,48 @@
+"""
+Alkene Key
+==========
+
+"""
+
+
 class AlkeneKey:
-    pass
+    """
+    Generates keys for :class:`.Alkene` instances.
+
+    """
+
+    # Keep the empty __init__() method for the docstring.
+    def __init__(self):
+        """
+        Initialize an :class:`.AlkeneKey` instance.
+
+        """
+
+        return
+
+    def get_key(self, alkene):
+        """
+        Get the key of `alkene`.
+
+        Parameters
+        ----------
+        alkene : :class:`.Alkene`
+            The alkene for which a key is needed.
+
+        Returns
+        -------
+        :class:`object`
+            The key.
+
+        """
+
+        return (
+            alkene.get_carbon1().get_id(),
+            alkene.get_atom1().get_id(),
+            alkene.get_atom2().get_id(),
+            alkene.get_carbon2().get_id(),
+            alkene.get_atom3().get_id(),
+            alkene.get_atom4().get_id(),
+            tuple(sorted(alkene.get_bonder_ids())),
+            tuple(sorted(alkene.get_deleter_ids())),
+        )
