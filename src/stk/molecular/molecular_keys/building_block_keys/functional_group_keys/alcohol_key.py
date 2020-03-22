0aa28995a86a6aeecb74e23a64701f26027228ac
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-22 02:27:32 +0100

    wip

--- src/stk/molecular/molecular_keys/building_block_keys/functional_group_keys/alcohol_key.py
+++ src/stk/molecular/molecular_keys/building_block_keys/functional_group_keys/alcohol_key.py
@@ -1,2 +1,44 @@
+"""
+Alcohol Key
+===========
+
+"""
+
+
 class AlcoholKey:
-    pass
+    """
+    Generates keys for :class:`.Alcohol` instances.
+
+    """
+
+    def __init__(self):
+        """
+        Initialize an :class:`.AlcoholKey` instance.
+
+        """
+
+        return
+
+    def get_key(self, alcohol):
+        """
+        Get the key of `alcohol`.
+
+        Parameters
+        ----------
+        alcohol : :class:`.Alcohol`
+            The alcohol for which a key is needed.
+
+        Returns
+        -------
+        :class:`object`
+            The key.
+
+        """
+
+        return (
+            alcohol.get_oxygen().get_id(),
+            alcohol.get_hydrogen().get_id(),
+            alcohol.get_atom().get_id(),
+            tuple(sorted(alcohol.get_bonder_ids())),
+            tuple(sorted(alcohol.get_deleter_ids())),
+        )
