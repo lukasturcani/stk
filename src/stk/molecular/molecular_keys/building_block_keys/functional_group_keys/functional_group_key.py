6bc361a2ce23b7bef51e7fc4d31da4d0065b35b4
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-22 03:16:37 +0100

    wip

--- src/stk/molecular/molecular_keys/building_block_keys/functional_group_keys/functional_group_key.py
+++ src/stk/molecular/molecular_keys/building_block_keys/functional_group_keys/functional_group_key.py
@@ -1,2 +1,44 @@
+"""
+Functional Group Key
+====================
+
+"""
+
+
 class FunctionalGroupKey:
-    pass
+    """
+    Generates keys for :class:`.FunctionalGroup` instances.
+
+    """
+
+    # Keep the empty __init__() method for the docstring.
+    def __init__(self):
+        """
+        Initialize a :class:`.FunctionalGroupKey` instance.
+
+        """
+
+        return
+
+    def get_key(self, functional_group):
+        """
+        Get the key of `functional_group`.
+
+        Parameters
+        ----------
+        functional_group : :class:`.FunctionalGroup`
+            The functionalGroup for which a key is needed.
+
+        Returns
+        -------
+        :class:`object`
+            The key.
+
+        """
+
+        return (
+            'functional_group',
+            tuple(sorted(functional_group.get_atom_ids())),
+            tuple(sorted(functional_group.get_placer_ids())),
+            tuple(sorted(functional_group.get_core_atom_ids())),
+        )
