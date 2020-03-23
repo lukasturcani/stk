4900e61867f5dc6d44d00d5826c8d96073af7ce9
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-23 14:47:54 +0100

    wip

--- tests/molecular/molecules/molecule/with_canonical_atom_ordering/test_with_canonical_atom_ordering.py
+++ tests/molecular/molecules/molecule/with_canonical_atom_ordering/test_with_canonical_atom_ordering.py
@@ -1,3 +1,6 @@
+import rdkit.Chem.AllChem as rdkit
+import numpy as np
+
 from ...utilities import is_clone
 
 
@@ -9,4 +12,13 @@ def test_with_canonical_atom_ordering(case_data):
 
 
 def _test_with_canonical_atom_ordering(molecule, result):
-    is_clone(molecule.with_canonical_atom_ordering(), result)
+    ordered = molecule.with_canonical_atom_ordering()
+    is_clone(ordered, result)
+    order = rdkit.CanonicalRankAtoms(molecule.to_rdkit_mol())
+    old_position_matrix = molecule.get_position_matrix()
+    new_position_matrix = ordered.get_position_matrix()
+    for old_id, new_id in enumerate(order):
+        assert np.all(np.equal(
+            old_position_matrix[old_id],
+            new_position_matrix[new_id],
+        ))
