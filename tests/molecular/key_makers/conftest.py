3ea9f6c8df51a190b4e4b58be0fa573c2c127f21
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-23 11:02:55 +0100

    wip

--- tests/molecular/key_makers/conftest.py
+++ tests/molecular/key_makers/conftest.py
@@ -50,6 +50,32 @@ from .case_data import CaseData
                 (2, ),
             )),
         ),
+        CaseData(
+            key_maker=stk.ConstructedMoleculeKeyMaker(
+                key_name='MyKey',
+                molecule_key_maker=stk.Inchi(),
+            ),
+            molecule=stk.ConstructedMolecule(
+                topology_graph=stk.polymer.Linear(
+                    building_blocks=(
+                        stk.BuildingBlock(
+                            smiles='NCCN',
+                            functional_groups=[
+                                stk.PrimaryAminoFactory()
+                            ],
+                        ),
+                    ),
+                    repeating_unit='A',
+                    num_repeating_units=2,
+                ),
+            ),
+            key_name='MyKey',
+            key=str((
+                'InChI=1S/C4H14N4/c5-1-3-7-8-4-2-6/h7-8H,1-6H2',
+                ('InChI=1S/C2H8N2/c3-1-2-4/h1-4H2', ),
+                (2, ),
+            )),
+        ),
     ),
 )
 def case_data(request):
