06e18da7d5583fd367a8ffd76b120f7ec1b1c270
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-23 07:45:52 +0100

    wip

--- src/stk/molecular/key_makers/__init__.py
+++ src/stk/molecular/key_makers/__init__.py
@@ -2,23 +2,23 @@
 Molecular Key Makers
 ====================
 
-#. :class:`.BuildingBlockKeyMaker`
 #. :class:`.ConstructedMoleculeKeyMaker`
 #. :class:`.MoleculeKeyMaker`
 
 Molecular key makers are used by :mod:`stk` to provide some identity
 metric for different molecules. Each molecular key maker provides a
-different metric for identity. There are 3 abstract base classes from
+different metric for identity. There are 2 abstract base classes from
 which a molecular key maker can be subclassed. However, each molecular
 key will maker subclass at most 1 of them.
 
 A very important thing to note, is that the abstract base classes of
 molecular key makers are not related by inheritance. For example,
-a :class:`.BuildingBlockKeyMaker` IS NOT A :class:`.MoleculeKeyMaker`,
-despite the fact that a :class:`.BuildingBlock` IS A
-:class:`.Molecule`. This means you cannot use :
-class:`.BuildingBlockKeyMaker` where a :class:`.MoleculeKeyMaker` is
-required.
+a :class:`.ConstructedMoleculeKeyMaker` IS NOT A
+:class:`.MoleculeKeyMaker`, despite the fact that a
+:class:`.ConstructedMolecule` IS A
+:class:`.Molecule`. This means you cannot use
+:class:`.ConstructedMoleculeKeyMaker` where a
+:class:`.MoleculeKeyMaker` is required.
 
 More information about each type of molecular key maker can be seen in
 the documentation of each of the abstract base classes.
@@ -42,50 +42,54 @@ methods, it is clear that they are not interchangeable.
 
 For example, :class:`.MoleculeKeyMaker.get_key` takes a
 :class:`.Molecule` object and returns a key. However,
-:class:`.BuildingBlockKeyMaker` takes a :class:`.BuildingBlock` object
-and returns a key. This alone makes them completely unrelated
-methods. The easiest way to explain why this is the case is through
-an example.
+:class:`.ConstructedMoleculeKeyMaker` takes a
+:class:`.ConstructedMolecule` object and returns a key. This alone
+makes them completely unrelated methods. The easiest way to explain
+why this is the case is through an example.
 
 For the sake of the example, lets define a really simple
-:class:`.BuildingBlockKeyMaker` subclass. Note that because
-were are defining a :class:`.BuildingBlockKeyMaker`, we can use the
-entire interface of :class:`.BuildingBlock`. This subclass simply
-looks at the number of functional groups the building block
+:class:`.ConstructedMoleculeKeyMaker` subclass. Note that because
+were are defining a :class:`.ConstructedMoleculeKeyMaker`, we can use
+the entire interface of :class:`.ConstructedMolecule`. This subclass
+simply looks at the number of building blocks the constructed molecule
 has, and uses that as a key
 
 .. code-block:: python
 
     import stk
 
-    class MyBuildingBlockKeyMaker(stk.BuildingBlockKeyMaker):
-        def get_name(self):
-            return 'MyBuildingBlockKeyMaker'
+    class MyKeyMaker(stk.ConstructedMoleculeKeyMaker):
+        def get_key_name(self):
+            return 'MyConstructedMoleculeKeyMaker'
 
-        def get_key(self, building_block):
-            return building_block.get_num_functional_groups()
+        def get_key(self, constructed_molecule):
+            building_blocks = tuple(
+                constructed_molecule.get_building_blocks()
+            )
+            return len(building_blocks)
 
-Already, it should be obvious why a :class:`.BuildingBlockKeyMaker` IS
-NOT A :class:`.MoleculeKeyMaker`. If we give
-:class:`MyBuildingBlockKeyMaker` to a function which expects a
-:class:`.MoleculeKeyMaker`, at some point,
+Already, it should be obvious why a
+:class:`.ConstructedMoleculeKeyMaker` IS NOT A
+:class:`.MoleculeKeyMaker`. If we give :class:`MyKeyMaker` to a
+function, which expects a :class:`.MoleculeKeyMaker`, at some point,
 a :class:`.Molecule` will get passed to the :meth:`get_key` method
-of :class:`MyBuildingBlockKeyMaker`. Once this happens,
-:meth:`.~BuildingBlock.get_num_functional_groups` will be called.
+of :class:`MyKeyMaker`. Once this happens,
+:meth:`~.ConstructedMolecule.get_building_blocks` will be called.
 However, a :class:`.Molecule` instance is not guaranteed to have this
 method. If a plain :class:`.Molecule` instance was used, or a
-:class:`.ConstructedMolecule` was used, a runtime error will occur,
-because such as method does not exist.
+was used, a runtime error will occur, because such as method does not
+exist on plain :class:`.Molecule` objects.
 
-Therefore, you cannot give a :class:`.BuildingBlockKeyMaker` where a
-:class:`.MoleculeKeyMaker` is expected, because eventually you will
-pass a :class:`.Molecule`, which is not also a :class:`.BuildingBlock`
-to the :class:`.BuildingBlockKeyMaker` and that will cause a runtime
-error if the :class:`.BuildingBlockKeyMaker` makes use of methods found
-on a :class:`.BuildingBlock` and not a plain :class:`.Molecule`.
+Therefore, you cannot give a :class:`.ConstructedMoleculeKeyMaker`
+where a :class:`.MoleculeKeyMaker` is expected, because eventually you
+will pass a :class:`.Molecule`, which is not also a
+:class:`.ConstructedMolecule` to the
+:class:`.ConstructedMoleculeKeyMaker` and that will cause a runtime
+error if the :class:`.ConstructedMoleculeKeyMaker` makes use of methods
+found on a :class:`.ConstructedMolecule` and not a plain
+:class:`.Molecule`.
 
 """
 
 from .molecule import *
-from .building_block import *
 from .constructed_molecule import *
