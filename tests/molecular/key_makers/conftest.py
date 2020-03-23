2dafa9dc3fad59ef2ca4e09b138138208f1dab92
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-23 10:52:46 +0100

    wip

--- tests/molecular/key_makers/conftest.py
+++ tests/molecular/key_makers/conftest.py
@@ -12,6 +12,21 @@ from .case_data import CaseData
             key_name='InChI',
             key='InChI=1S/C2H8N2/c3-1-2-4/h1-4H2',
         ),
+        CaseData(
+            key_maker=stk.InchiKey(),
+            molecule=stk.BuildingBlock('NCCN'),
+            key_name='InChIKey',
+            key='PIICEJLVQHRZGT-UHFFFAOYSA-N',
+        ),
+        CaseData(
+            key_maker=stk.MoleculeKeyMaker(
+                key_name='NumAtoms',
+                get_key=lambda molecule: molecule.get_num_atoms(),
+            ),
+            molecule=stk.BuildingBlock('NCCN'),
+            key_name='NumAtoms',
+            key=12,
+        ),
     ),
 )
 def case_data(request):
