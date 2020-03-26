811024893ba4ed075be3a80fbdb9ca063e0fb63e
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-26 05:48:15 +0100

    wip

--- tests/caches/molecule/case_data.py
+++ tests/caches/molecule/case_data.py
@@ -5,6 +5,33 @@ class CaseData:
     Attributes
     ----------
     cache : :class:`.MoleculeCache
+        The cache to test.
+
+    molecule : :class:`.Molecule`
+        The molecule to put and get from the :attr:`.cache`.
+
+    key : :class:`object`
+        The key used to retrieve :attr:`.molecule` from the cache.
 
     """
 
+    def __init__(self, cache, molecule, key):
+        """
+        Initialize a :class:`.CaseData` instance.
+
+        Parameters
+        ----------
+        cache : :class:`.MoleculeCache
+            The cache to test.
+
+        molecule : :class:`.Molecule`
+            The molecule to put and get from the `cache`.
+
+        key : :class:`object`
+            The key used to retrieve `molecule` from the cache.
+
+        """
+
+        self.cache = cache
+        self.molecule = molecule
+        self.key = key
---
+++ tests/caches/molecule/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from pytest_lazyfixture import lazy_fixture
+
+# Fixtures need to be visible for lazy_fixture() calls.
+from .fixtures import *  # noqa
+
+
+@pytest.fixture(
+    lazy_fixture('mongo_db_molecule_cache'),
+    lazy_fixture('mongo_db_constructed_molecule_cache'),
+)
+def case_data(request):
+    return request.param
