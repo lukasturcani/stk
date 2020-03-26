0dc841cb959ca4fd8d635a644b043c0fd8f51c46
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-26 11:22:13 +0100

    wip

---
+++ src/stk/caches/value/constructed_molecule/__init__.py
@@ -0,0 +1,3 @@
+from .constructed_moleucle import *
+from .mongo_db import MongoDbConstructedMoleculeValueCache
+from .ram import *
---
+++ src/stk/caches/value/constructed_molecule/constructed_molecule.py
@@ -0,0 +1,67 @@
+"""
+Constructed Molecule Value Cache
+================================
+
+#. :class:`.MongoDbMoleculeValueCache`
+#. :class:`.MongoDbConstructedMoleculeValueCache`
+#. :class:`.MoleculeValueCache`
+#. :class:`.RamConstructedMoleculeValueCache`
+#. :class:`.RamMoleculeValueCache`
+
+"""
+
+
+class ConstructedMoleculeValueCache:
+    """
+    Abstract base class for caching of molecular property values.
+
+    Note that a :class:`.MoleculeValueCache` can be used anywhere a
+    :class:`.ConstructedMoleculeValueCache` is required. However, the
+    opposite is not true. If something requires a
+    :class:`.MoleculeValueCache` you cannot use a
+    :class:`.ConstructedMoleculeValueCache` in its place.
+
+    """
+
+    def put(self, molecule, value):
+        """
+        Put a value into the cache.
+
+        Parameters
+        ----------
+        molecule : :class:`.ConstructedMolecule`
+            The molecule which is associated with the `value`.
+
+        value : :class:`object`
+            Some value associated with `molecule`.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
+
+        raise NotImplementedError()
+
+    def get(self, molecule):
+        """
+        Get the cached value for `molecule`.
+
+        Parameters
+        ----------
+        molecule : :class:`.ConstructedMolecule`
+            The molecule whose value is to be retrieved from the cache.
+
+        Returns
+        -------
+        :class:`object`
+            The value associated with `molecule`.
+
+        Raises
+        ------
+        :class:`KeyError`
+            If `molecule` is not found in the cache.
+
+        """
+
+        raise NotImplementedError()
