a6aee1810d6b871d9aa2cadfcb889d7a8296acab
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-26 15:07:56 +0100

    wip

--- tests/caches/value/fixtures/mongo_db_constructed_molecule_value_cache.py
+++ tests/caches/value/fixtures/mongo_db_constructed_molecule_value_cache.py
@@ -11,6 +11,27 @@ from ...utilities import MockMongoClient
             cache=stk.MongoDbConstructedMoleculeValueCache(
                 mongo_client=MockMongoClient(),
                 collection='values',
+                lru_cache_size=128,
+            ),
+            molecule=stk.ConstructedMolecule(
+                topology_graph=stk.polymer.Linear(
+                    building_blocks=(
+                        stk.BuildingBlock(
+                            smiles='BrCCBr',
+                            functional_groups=[stk.BromoFactory()],
+                        ),
+                    ),
+                    repeating_unit='A',
+                    num_repeating_units=2,
+                ),
+            ),
+            value=12,
+        ),
+        CaseData(
+            cache=stk.MongoDbConstructedMoleculeValueCache(
+                mongo_client=MockMongoClient(),
+                collection='values',
+                lru_cache_size=0,
             ),
             molecule=stk.ConstructedMolecule(
                 topology_graph=stk.polymer.Linear(
