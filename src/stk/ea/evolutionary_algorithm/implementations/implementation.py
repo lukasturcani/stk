b747fd19eff6ef48137ca4b1fdf948c02c8fb0d8
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-27 10:48:02 +0100

    wip

--- src/stk/ea/evolutionary_algorithm/implementations/implementation.py
+++ src/stk/ea/evolutionary_algorithm/implementations/implementation.py
@@ -108,7 +108,7 @@ class Implementation:
             )
 
     def _with_fitness_values(self, map_, population):
-        no_fitness = map(
+        no_fitness = (
             record for record in population
             if record.get_fitness_value() is None
         )
