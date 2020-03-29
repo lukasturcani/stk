3048bb936a424302f8bac736a86373c44100598a
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-29 17:31:53 +0200

    wip

---
+++ tests/ea/crossover/crossers/test_cross.py
@@ -0,0 +1,63 @@
+from tests.utilities import is_equivalent
+
+
+def test_cross(case_data):
+    """
+    Test :meth:`cross`.
+
+    Parameters
+    ----------
+    case_data : :class:`.CaseData`
+        A test case. Holds the crosser to test and the correct
+        crossover records.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    _test_cross(
+        crosser=case_data.crosser,
+        records=case_data.records,
+        crossover_records=case_data.crossover_records,
+    )
+
+
+def _test_cross(crosser, records, crossover_records):
+    """
+    Test :meth:`cross`.
+
+    Parameters
+    ----------
+    crosser : :class:`.MoleculeCrosser` or \
+            :class:`.ConstructedMoleculeCrosser`
+        The crosser to test.
+
+    records : :class:`tuple` of :class:`.MoleculeRecord` or \
+            :class:`tuple` of :class:`.ConstructedMoleculeRecord`
+        The molecules to cross.
+
+    crossover_records : :class:`tuple` of \
+            :class:`MoleculeCrossoverRecord` or \
+            :class:`tuple` of \
+            :class:`.ConstructedMoleculeCrossoverRecord`
+        The correct offspring.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    for record1, record2 in zip(
+        crosser.cross(records),
+        crossover_records,
+    ):
+        assert (
+            record1.get_crosser_name() == record2.get_crosser_name()
+        )
+        assert is_equivalent(
+            record1.get_molecule_record.get_molecule(),
+            record2.get_molecule_record.get_molecule(),
+        )
