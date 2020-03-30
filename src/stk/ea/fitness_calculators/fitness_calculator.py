d27b9c0b005938a6f00cc754363552936c12cce3
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-30 13:48:30 +0200

    wip

--- src/stk/ea/fitness_calculators/fitness_calculator.py
+++ src/stk/ea/fitness_calculators/fitness_calculator.py
@@ -1,3 +1,10 @@
+"""
+Fitness Calculator
+==================
+
+"""
+
+
 class FitnessCalculator:
     """
     Calculates fitness values of molecules.
@@ -5,24 +12,6 @@ class FitnessCalculator:
     """
 
     def get_fitness_value(self, molecule):
-        """
-        Return the fitness value of `molecule`.
-
-        Parameters
-        ----------
-        molecule : :class:`.Molecule`
-            The molecule whose fitness value should be calculated.
-
-        Returns
-        -------
-        :class:`object`
-            The fitness value of `molecule`.
-
-        """
-
-        return self._cache_result(self._get_fitness, mol)
-
-    def _get_fitness(self, mol):
         """
         Return the fitness value of `molecule`.
 
