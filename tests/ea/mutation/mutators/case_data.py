0be5bf1290ec9ff9221fb7c39e53f842a8e18a7a
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-30 07:06:10 +0200

    wip

---
+++ tests/ea/mutation/mutators/case_data.py
@@ -0,0 +1,41 @@
+class CaseData:
+    """
+    A test case.
+
+    Attributes
+    ----------
+    mutator : :class:`.MoleculeMutator` or \
+            :class:`.ConstructedMoleculeMutator`
+
+    record : :class:`.MoleculeRecord` or \
+            :class:`.ConstructedMoleculeRecord`
+        The molecule to mutate.
+
+    mutation_record : :class:`.MoleculeMutationRecord` or \
+            :class:`.ConstructedMoleculeMutationRecord`
+        The correct mutation record.
+
+    """
+
+    def __init__(self, mutator, record, mutation_record):
+        """
+        Initialize a :class:`.CaseData` instance.
+
+        Parameters
+        ----------
+        mutator : :class:`.MoleculeMutator` or \
+                :class:`.ConstructedMoleculeMutator`
+
+        record : :class:`.MoleculeRecord` or \
+                :class:`.ConstructedMoleculeRecord`
+            The molecule to mutate.
+
+        mutation_record : :class:`.MoleculeMutationRecord` or \
+                :class:`.ConstructedMoleculeMutationRecord`
+            The correct mutation record.
+
+        """
+
+        self._mutator = mutator
+        self._record = record
+        self._mutation_record = mutation_record
