f4c45992ee9bf62e54f8f36eb0d6d6f800d2a2e0
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-30 09:25:53 +0200

    wip

--- tests/ea/mutation/mutator/fixtures/random_topology_graph.py
+++ tests/ea/mutation/mutator/fixtures/random_topology_graph.py
@@ -4,10 +4,15 @@ import stk
 from ...case_data import CaseData
 
 
+def get_eight_plus_twelve(graph):
+    return stk.EightPlusTwelve(graph.get_building_blocks())
+
+
 @pytest.fixture(
     params=(
         CaseData(
             mutator=stk.RandomTopologyGraph(
+                replacement_funcs=(get_eight_plus_twelve, ),
             ),
             record=stk.ConstructedMoleculeRecord(
             ),
