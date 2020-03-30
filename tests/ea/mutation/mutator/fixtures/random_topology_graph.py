6108f60a4d54ebfbd20ed30afcc16114d71f1e5b
Lukas Turcani <lukasturcani93@gmail.com>
2020-03-30 09:30:33 +0200

    wip

--- tests/ea/mutation/mutator/fixtures/random_topology_graph.py
+++ tests/ea/mutation/mutator/fixtures/random_topology_graph.py
@@ -5,7 +5,7 @@ from ..case_data import CaseData
 
 
 def get_eight_plus_twelve(graph):
-    return stk.EightPlusTwelve(graph.get_building_blocks())
+    return stk.cage.EightPlusTwelve(graph.get_building_blocks())
 
 
 @pytest.fixture(
