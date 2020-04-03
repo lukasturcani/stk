15852a6b33926de92542f54b0650ab4e9bc997f0
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-03 12:51:49 +0200

    wip

--- tests/ea/fitness_calculator/fixtures/property_vector.py
+++ tests/ea/fitness_calculator/fixtures/property_vector.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import stk
 
 from ..case_data import CaseData
@@ -6,6 +7,26 @@ from ..case_data import CaseData
 
 @pytest.fixture(
     params=(
+        CaseData(
+            fitness_calculator=stk.PropertyVector(
+                stk.Molecule.get_num_atoms,
+                stk.Molecule.get_num_bonds,
+                stk.Molecule.get_maximum_diameter,
+            ),
+            molecule=stk.BuildingBlock('BrCCBr').with_position_matrix(
+                position_matrix=np.array([
+                    [0, 0, 0],
+                    [10, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                ], dtype=np.float64),
+            ),
+            fitness_value=(8, 7, 10),
+        ),
     ),
 )
 def property_vector(request):
