dac1b7f5cb96f3d3c46c120977ab824f1ca45aac
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-03 21:31:19 +0200

    wip

--- src/stk/ea/fitness_normalizers/multiply.py
+++ src/stk/ea/fitness_normalizers/multiply.py
@@ -16,49 +16,59 @@ class Multiply(FitnessNormalizer):
 
     Examples
     --------
-    Multiplying a fitness value by a coefficient.
+    *Multiplying Fitness Values by a set of Coefficients*
+
+    In this example, assume that each fitness value consists of a
+    :class:`tuple` of numbers, each representing a different property
+    of the molecule, and each contributing to the final fitness value.
+    The properties can be anything, such as  energy, number of atoms
+    or diameter.
+
+    If your final fitness value depends on the combination of these
+    properties, you will probably first want to scale them with
+    :class:`.DivideByMean`. Once this is done, you may want to
+    multiply each property by some coefficient, which reflects its
+    relative importance to the final fitness value. For example
+    if you multiply the value of one property by ``1`` and another
+    by ``2``, the second will contribute twice as much to the
+    final fitness value, assuming that you get the final fitness value
+    by using the :class:`.Sum` normalizer after :class:`.Multiply`.
+
+    Giving a concrete example
 
     .. code-block:: python
 
         import stk
 
-        # Create the normalizer.
-        multiply = stk.Multiply(2)
+        normalizer = stk.Coefficient((1, 2, 3))
+        # Assuming that population holds molecule record instances
+        # with the following fitness values
+        # (1, 1, 1), (2, 2, 2), (3, 3, 3)
+        # normalized will hold fitness values of
+        # (1, 2 ,3), (2, 4, 6), (3, 6, 9)
+        normalized = tuple(normalizer.normalize(population))
 
-        # Normalize the fitness values. Assume the fitness values are
-        # {mol1: 1, mol2: 2, mol3: 3}.
-        normalized = multiply.normalize(pop)
+    *Selectively Normalizing Fitness Values*
 
-        # normalized is
-        # {mol1: 2, mol2: 4, mol3: 6}.
-
-
-    Multiplying a vector of fitness values by some coefficient
-
-    .. code-block:: python
-
-        multiply = stk.Multiply(2)
-
-        # Normalize the fitness values. Assume the fitness values are
-        # {mol1: [1, 2, 3], mol2: [4, 5, 6], mol3: [7, 8, 9]}.
-        normalized = multiply.normalize(pop)
-
-        # normalized is
-        # {mol1: [2, 4, 6], mol2: [8, 10, 12], mol3: [14, 16, 18]}.
-
-
-    Multiplying a vector of fitness values by different coefficients
+    Sometimes, you only want to normalize some members of a population,
+    for example if some do not have an assigned fitness value,
+    because the fitness calculation failed for whatever reason.
+    You can use the `filter` parameter to exclude records from the
+    normalization
 
     .. code-block:: python
 
-        multiple = stk.Multiply([1, 2, 3])
+        import stk
 
-        # Normalize the fitness values. Assume the fitness values are
-        # {mol1: [1, 2, 3], mol2: [4, 5, 6], mol3: [7, 8, 9]}.
-        normalized = multiply.normalize(pop)
+        normalizer = stk.Coefficient(
+            coefficient=(1, 2, 3),
+            # Only normalize values which are not None.
+            filter=lambda population, record:
+                record.get_fitness_value() is not None,
+        )
+        normalized = tuple(normalizer.normalize(population))
 
-        # normalized is
-        # {mol1: [1, 4, 9], mol2: [4, 10, 18], mol3: [7, 16, 27]}.
+
 
     """
 
