99b6317599092324e2058958b3cea3323dab3194
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-04 18:26:41 +0200

    wip

--- src/stk/ea/terminators/__init__.py
+++
@@ -1,6 +0,0 @@
-from .all_terminators import *
-from .any_terminator import *
-from .fitness_plateau import *
-from .molecule_present import *
-from .num_generations import *
-from .terminator import *
