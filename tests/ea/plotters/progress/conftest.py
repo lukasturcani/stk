44af6ca5cf730581c639bab53e3b532874450166
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-04 17:07:31 +0200

    wip

--- tests/ea/plotters/progress/conftest.py
+++ tests/ea/plotters/progress/conftest.py
@@ -5,97 +5,78 @@ import pandas as pd
 from .case_data import CaseData
 
 
-@pytest.fixture(
-    CaseData(
-        plotter=stk.ProgressPlotter(
-            generations=(
-                stk.Generation(
-                    id=0,
-                    molecule_records=(
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrBr'),
-                        ).with_fitness_value(0),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCBr'),
-                        ).with_fitness_value(1),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCBr'),
-                        ).with_fitness_value(2),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCCBr'),
-                        ),
-                    ),
-                ),
-                stk.Generation(
-                    id=1,
-                    molecule_records=(
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrBr'),
-                        ).with_fitness_value(10),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCBr'),
-                        ).with_fitness_value(20),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCBr'),
-                        ).with_fitness_value(30),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCCBr'),
-                        ),
-                    ),
-                ),
-                stk.Generation(
-                    id=2,
-                    molecule_records=(
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrBr'),
-                        ).with_fitness_value(10),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCBr'),
-                        ).with_fitness_value(20),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCBr'),
-                        ).with_fitness_value(30),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCCBr'),
-                        ),
-                    ),
-                ),
-                stk.Generation(
-                    id=3,
-                    molecule_records=(
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrBr'),
-                        ).with_fitness_value(40),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCBr'),
-                        ).with_fitness_value(50),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCBr'),
-                        ).with_fitness_value(60),
-                    ),
-                ),
-                stk.Generation(
-                    id=4,
-                    molecule_records=(
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrBr'),
-                        ).with_fitness_value(70),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCBr'),
-                        ).with_fitness_value(80),
-                        stk.MoleculeRecord(
-                            molecule=stk.BuildingBlock('BrCCBr'),
-                        ).with_fitness_value(90),
-                    ),
-                )
-            ),
-            get_property=lambda record: record.get_fitness_value(),
-            y_label='Fitness Value',
-            filter=lambda record:
-                record.get_fitness_value() is not None,
-        ),
-        plot_data=pd.DataFrame
+def get_generation(*fitness_values):
+    v1, v2, v3, *_ = fitness_values
+    return (
+        stk.MoleculeRecord(
+            molecule=stk.BuildingBlock('BrBr'),
+        ).with_fitness_value(v1),
+        stk.MoleculeRecord(
+            molecule=stk.BuildingBlock('BrCBr'),
+        ).with_fitness_value(v2),
+        stk.MoleculeRecord(
+            molecule=stk.BuildingBlock('BrCCBr'),
+        ).with_fitness_value(v3),
+        stk.MoleculeRecord(
+            molecule=stk.BuildingBlock('BrCCCBr'),
+        )
     )
+
+
+@pytest.fixture(
+    params=(
+        CaseData(
+            plotter=stk.ProgressPlotter(
+                generations=(
+                    stk.Generation(
+                        id=0,
+                        molecule_records=get_generation(0, 1, 2),
+                        mutation_records=(),
+                        crossover_records=(),
+                    ),
+                    stk.Generation(
+                        id=1,
+                        molecule_records=get_generation(10, 20, 30),
+                        mutation_records=(),
+                        crossover_records=(),
+                    ),
+                    stk.Generation(
+                        id=2,
+                        molecule_records=get_generation(40, 50, 60),
+                        mutation_records=(),
+                        crossover_records=(),
+                    ),
+                    stk.Generation(
+                        id=3,
+                        molecule_records=get_generation(40, 50, 60),
+                        mutation_records=(),
+                        crossover_records=(),
+                    ),
+                    stk.Generation(
+                        id=4,
+                        molecule_records=get_generation(70, 80, 90),
+                        mutation_records=(),
+                        crossover_records=(),
+                    ),
+                ),
+                get_property=lambda record: record.get_fitness_value(),
+                y_label='Fitness Value',
+                filter=lambda record:
+                    record.get_fitness_value() is not None,
+            ),
+            plot_data=pd.DataFrame({
+                'Generation': [0, 1, 2, 3, 4],
+                'Fitness Value': [
+                    2, 1, 0,
+                    30, 20, 10,
+                    60, 50, 40,
+                    60, 50, 40,
+                    90, 80, 70,
+                ],
+                'Type': ['Max', 'Mean', 'Min']*5
+            }),
+        )
+    ),
 )
 def case_data(request):
     return request.param
