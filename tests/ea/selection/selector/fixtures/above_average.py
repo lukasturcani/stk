db68d4e9f8992fa3ef9ff5d204dcdccba15e80b7
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-05 14:59:18 +0200

    wip

--- tests/ea/selection/selector/fixtures/above_average.py
+++ tests/ea/selection/selector/fixtures/above_average.py
@@ -4,26 +4,41 @@ import stk
 from ..case_data import CaseData
 
 
+population1 = (
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCBr'),
+    ).with_fitness_value(5),
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCCBr'),
+    ).with_fitness_value(1),
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCCCBr'),
+    ).with_fitness_value(1),
+)
+
+
 @pytest.fixture(
     params=(
         CaseData(
             selector=stk.AboveAverage(),
-            population=(
-                stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCBr'),
-                ).with_fitness_value(5),
-                stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCBr'),
-                ).with_fitness_value(1),
-                stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCCBr'),
-                ).with_fitness_value(1),
-            ),
+            population=population1,
             selected=(
                 stk.Batch(
                     records=(
+                        population1[0],
                     ),
-                    fitness_values=get_fitness_values(,
+                    fitness_values={
+                        population1[0]: 5,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(
+                        population1[0]
+                    ),
+                    fitness_values={
+                        population1[0]: 5,
+                    },
                     key_maker=stk.Inchi(),
                 )
             ),
