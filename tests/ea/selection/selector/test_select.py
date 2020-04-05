f89f05cdf8f7ecda83446cde5cf59ecf59ca372d
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-05 14:34:29 +0200

    wip

---
+++ tests/ea/selection/selector/test_select.py
@@ -0,0 +1,45 @@
+def test_select(case_data):
+    """
+    Test :meth:`.Selector.select`.
+
+    Parameters
+    ----------
+    case_data : :class:`.CaseData`
+        A test case. Holds the selector to test and the batches which
+        should be selected.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    _test_select(
+        selector=case_data.selector,
+        population=case_data.population,
+        selected=case_data.selected,
+    )
+
+
+def _test_select(selector, population, selected):
+    """
+    Test :meth:`.Selector.select`.
+
+    Parameters
+    ----------
+    selector : :class:`.Selector`
+        The selector to test.
+
+    population : :class:`tuple` of :class:`.MoleculeRecord`
+        The population from which batches are selected.
+
+    selected : :class:`tuple` of :class:`.Batch`
+            The batches which should be selected.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    result = selector.select(population)
