652bd422d1d6c4444349ad03c0e208b220cffa11
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-05 15:09:32 +0200

    wip

--- tests/ea/selection/selector/fixtures/above_average.py
+++ tests/ea/selection/selector/fixtures/above_average.py
@@ -9,6 +9,9 @@ population1 = (
         molecule=stk.BuildingBlock('BrCBr'),
     ).with_fitness_value(5),
     stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCNCBr'),
+    ).with_fitness_value(3),
+    stk.MoleculeRecord(
         molecule=stk.BuildingBlock('BrCCBr'),
     ).with_fitness_value(1),
     stk.MoleculeRecord(
@@ -31,6 +34,11 @@ population1 = (
                 stk.Batch(
                     records=(population1[0], ),
                     fitness_values={population1[0]: 5},
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[1], ),
+                    fitness_values={population1[1]: 3},
                     key_maker=stk.Inchi(),
                 ),
             ),
