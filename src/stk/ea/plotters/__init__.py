043931b9a8b15be556729a71345191cbd795405c
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-05 10:13:57 +0200

    wip

--- src/stk/ea/plotters/__init__.py
+++ src/stk/ea/plotters/__init__.py
@@ -5,6 +5,11 @@ Plotters
 #. :class:`.ProgressPlotter`
 #. :class:`.SelectionPlotter`
 
+
+The are multiple different plotters, but they are not related by an
+inheritance hierachy. Each plotter has its own distinct API, according
+to its needs.
+
 """
 
 from .progress import *
