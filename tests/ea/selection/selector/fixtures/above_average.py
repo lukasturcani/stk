c39b1f83ba4780d53ab9341b637b489058f5322a
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-06 11:41:39 +0200

    wip

--- tests/ea/selection/selector/fixtures/above_average.py
+++ tests/ea/selection/selector/fixtures/above_average.py
@@ -220,6 +220,71 @@ population1 = (
                 ),
             ),
         ),
+        CaseData(
+            selector=stk.AboveAverage(
+                batch_size=2,
+                duplicate_batches=False,
+            ),
+            population=population1,
+            selected=(
+                stk.Batch(
+                    records=(population1[0], population1[1]),
+                    fitness_values={
+                        population1[0]: 10,
+                        population1[1]: 9,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[0], population1[2], ),
+                    fitness_values={
+                        population1[0]: 10,
+                        population1[2]: 2,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[0], population1[3], ),
+                    fitness_values={
+                        population1[0]: 10,
+                        population1[3]: 1,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[0], population1[4], ),
+                    fitness_values={
+                        population1[0]: 10,
+                        population1[4]: 1,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[1], population1[2], ),
+                    fitness_values={
+                        population1[1]: 9,
+                        population1[2]: 2,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[1], population1[3], ),
+                    fitness_values={
+                        population1[1]: 9,
+                        population1[3]: 1,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+                stk.Batch(
+                    records=(population1[1], population1[4], ),
+                    fitness_values={
+                        population1[1]: 9,
+                        population1[4]: 1,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+            ),
+        ),
     ),
 )
 def above_average(request):
