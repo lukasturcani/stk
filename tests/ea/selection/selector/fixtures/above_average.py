fcf0917c1ac33b77a5ff20df7eb068fadc12ce36
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-06 10:10:28 +0200

    wip

--- tests/ea/selection/selector/fixtures/above_average.py
+++ tests/ea/selection/selector/fixtures/above_average.py
@@ -152,6 +152,23 @@ population1 = (
                 ),
             ),
         ),
+        CaseData(
+            selector=stk.AboveAverage(
+                batch_size=2,
+                duplicate_molecules=False,
+            ),
+            population=population1,
+            selected=(
+                stk.Batch(
+                    records=(population1[0], population1[1]),
+                    fitness_values={
+                        population1[0]: 5,
+                        population1[1]: 3,
+                    },
+                    key_maker=stk.Inchi(),
+                ),
+            ),
+        ),
     ),
 )
 def above_average(request):
