908a90590685bcc818d3a34c2443deaef2628ce6
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-06 17:55:56 +0200

    wip

---
+++ tests/ea/selection/selector/fixtures/remove_batches.py
@@ -0,0 +1,45 @@
+import pytest
+import stk
+
+from ..case_data import CaseData
+
+
+population1 = (
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCBr'),
+    ).with_fitness_value(10),
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCNCBr'),
+    ).with_fitness_value(9),
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCNNCBr'),
+    ).with_fitness_value(2),
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCCBr'),
+    ).with_fitness_value(1),
+    stk.MoleculeRecord(
+        molecule=stk.BuildingBlock('BrCCCBr'),
+    ).with_fitness_value(1),
+)
+
+
+@pytest.fixture(
+    params=(
+        CaseData(
+            selector=stk.RemoveBatches(
+                filter=stk.Worst(4),
+                selector=stk.Best(),
+            ),
+            population=population1,
+            selected=(
+                stk.Batch(
+                    records=(population1[0], ),
+                    fitness_values={population1[0]: 10},
+                    key_maker=stk.Inchi(),
+                ),
+            ),
+        ),
+    ),
+)
+def filter_batches(request):
+    return request.param
