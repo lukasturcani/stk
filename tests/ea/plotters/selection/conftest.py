dd9ebf44a2d10fabf047807af7dd655e1e63a9b6
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-07 16:53:07 +0200

    wip

--- tests/ea/plotters/selection/conftest.py
+++ tests/ea/plotters/selection/conftest.py
@@ -10,64 +10,64 @@ from .case_data import CaseData
             selector=stk.Roulette(num_batches=50),
             population=(
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCBr'),
+                    molecule=stk.BuildingBlock('C'),
                 ).with_fitness_value(10),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCNCBr'),
+                    molecule=stk.BuildingBlock('C'*2),
                 ).with_fitness_value(9),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCNNCBr'),
+                    molecule=stk.BuildingBlock('C'*3),
                 ).with_fitness_value(2),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCBr'),
+                    molecule=stk.BuildingBlock('C'*4),
                 ).with_fitness_value(1),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCCBr'),
+                    molecule=stk.BuildingBlock('C'*5),
                 ).with_fitness_value(1),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCBr'),
+                    molecule=stk.BuildingBlock('C'*6),
                 ).with_fitness_value(100),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCNCBr'),
+                    molecule=stk.BuildingBlock('C'*7),
                 ).with_fitness_value(9),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCNNCBr'),
+                    molecule=stk.BuildingBlock('C'*8),
                 ).with_fitness_value(5),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCCBr'),
+                    molecule=stk.BuildingBlock('C'*9),
                 ).with_fitness_value(3),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCCCBr'),
+                    molecule=stk.BuildingBlock('C'*10),
                 ).with_fitness_value(1),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCl'),
+                    molecule=stk.BuildingBlock('C'*11),
                 ).with_fitness_value(50),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCNCCl'),
+                    molecule=stk.BuildingBlock('C'*12),
                 ).with_fitness_value(9),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCNNCCl'),
+                    molecule=stk.BuildingBlock('C'*13),
                 ).with_fitness_value(2),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCCl'),
+                    molecule=stk.BuildingBlock('C'*14),
                 ).with_fitness_value(1),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('BrCCCCl'),
+                    molecule=stk.BuildingBlock('C'*15),
                 ).with_fitness_value(1),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCCl'),
+                    molecule=stk.BuildingBlock('C'*16),
                 ).with_fitness_value(100),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCNCCl'),
+                    molecule=stk.BuildingBlock('C'*17),
                 ).with_fitness_value(9),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCNNCCl'),
+                    molecule=stk.BuildingBlock('C'*18),
                 ).with_fitness_value(5),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCCCl'),
+                    molecule=stk.BuildingBlock('C'*19),
                 ).with_fitness_value(3),
                 stk.MoleculeRecord(
-                    molecule=stk.BuildingBlock('ClCCCCl'),
+                    molecule=stk.BuildingBlock('C'*20),
                 ).with_fitness_value(1),
             ),
         ),
