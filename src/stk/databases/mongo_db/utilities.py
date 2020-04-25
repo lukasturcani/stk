f5143276e9f46c4e2f7ee7d87b6d55a3baef90fb
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-25 17:53:57 +0200

    Added missing eq method. (#100)

--- src/stk/databases/mongo_db/utilities.py
+++ src/stk/databases/mongo_db/utilities.py
@@ -1,3 +1,6 @@
 class HashableDict(dict):
     def __hash__(self):
         return hash((frozenset(self), frozenset(self.values())))
+
+    def __eq__(self, other):
+        return super().__eq__(other)
