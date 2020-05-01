9cd8d911bd0502a34b5aaa3f0763c06d49be9744
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-05-01 10:18:35 +0100

    remove some exceptions

--- src/stk/molecular/key_makers/utilities.py
+++ src/stk/molecular/key_makers/utilities.py
@@ -15,23 +15,12 @@ def get_inchi(molecule):
     :class:`str`
         The InChI.
 
-    Raises
-    ------
-    :class:`ValueError`
-        If InChI of `molecule` is an empty string.
-
     """
 
-    key = rdkit.MolToInchi(
+    return rdkit.MolToInchi(
         mol=molecule.to_rdkit_mol(),
         treatWarningAsError=True,
     )
-    if not key:
-        raise ValueError(
-            f'InChI of {molecule} is empty string'
-        )
-
-    return key
 
 
 def get_inchi_key(molecule):
@@ -78,25 +67,15 @@ def get_smiles(molecule):
     :class:`str`
         The SMILES.
 
-    Raises
-    ------
-    :class:`ValueError`
-        If SMILES of `molecule` is an empty string.
-
     """
 
     rdkit_mol = molecule.to_rdkit_mol()
     rdkit.AssignStereochemistryFrom3D(rdkit_mol)
     rdkit.SanitizeMol(rdkit_mol)
     rdkit_mol = rdkit.RemoveHs(rdkit_mol)
-    key = rdkit.MolToSmiles(
+
+    return rdkit.MolToSmiles(
         mol=rdkit_mol,
         isomericSmiles=True,
         canonical=True,
     )
-    if not key:
-        raise ValueError(
-            f'SMILES of {molecule} is empty string'
-        )
-
-    return key
