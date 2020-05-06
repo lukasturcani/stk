70cb3db53888abf77886413538bc225d0a81a70b
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-06 09:10:50 +0200

    Update dative_reaction_factory.py

--- src/stk/molecular/reactions/factories/dative_reaction_factory.py
+++ src/stk/molecular/reactions/factories/dative_reaction_factory.py
@@ -10,34 +10,30 @@ from ..reactions import DativeReaction
 
 class DativeReactionFactory(ReactionFactory):
     """
-    Create reactions for :class:`.GenericFunctionalGroup` instances.
+    Create :class:`.DativeReaction` instances.
 
     This reaction factory assumes that the functional groups, which
     belong to the :class:`.EdgeGroup` passed to it, are
     :class:`.GenericFunctionalGroup` instances. It returns a
     :class:`.Reaction` suitable for two such instances.
 
-    This class uses a :class:`.GenericReactionFactory` instance to
-    define the :class:`Reaction`s that are performed, which are treated
-    as :class:`DativeReaction`s to handle the directionality of dative
-    bonds.
-
-    Dative bonds are defined with a `bond_order` of 9.
+    Dative bonds are defined with a `bond_order` of 9, 
+    running from the non-metal atom to the metal atom.
+    
     """
 
-    def __init__(self, generic_reaction_factory):
+    def __init__(self, reaction_factory):
         """
         Initialize a :class:`.DativeReactionFactory`.
 
         Parameters
         ----------
-        generic_reaction_factory : :class:`.GenericReactionFactory`
-            :class:`.GenericReactionFactory` that defines the
-            reactions.
+        reaction_factory : :class:`.GenericReactionFactory`
+            Used to create reactions.
 
         """
 
-        self._reaction_factory = generic_reaction_factory
+        self._reaction_factory = reaction_factory
 
     def get_reaction(self, construction_state, edge_group):
         reaction = self._reaction_factory.get_reaction(
@@ -50,8 +46,7 @@ class DativeReactionFactory(ReactionFactory):
         return repr(self)
 
     def __repr__(self):
-        bond_orders = (
-            '' if self._default_bond_orders
-            else f'{self._reaction_factory._bond_orders}'
+        return (
+            f'{self.__class__.__name__}'
+            f'({self._reaction_factory})'
         )
-        return f'{self.__class__.__name__}({bond_orders})'
