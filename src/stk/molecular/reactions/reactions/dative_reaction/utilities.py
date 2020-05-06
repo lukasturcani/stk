963c6eb070b7a1a3f9325e3678ad2c2b3a5d5bbb
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-06 11:13:52 +0200

    Moved dative reaction.

---
+++ src/stk/molecular/reactions/reactions/dative_reaction/utilities.py
@@ -0,0 +1,25 @@
+from itertools import chain
+
+
+def is_metal(atom):
+    """
+    Check if `atom` is a metal atom.
+
+    Parameters
+    ----------
+    atom : :class:`.Atom`
+        An atom.
+
+    Returns
+    -------
+    :class:`bool`
+        ``True`` if `atom` is a metal atom.
+
+    """
+
+    metal_atomic_numbers = chain(
+        range(21, 31),
+        range(39, 49),
+        range(72, 81),
+    )
+    return atom in metal_atomic_numbers
