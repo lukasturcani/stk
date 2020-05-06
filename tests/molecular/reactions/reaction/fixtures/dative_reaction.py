7b2197128da1528039183decaaf9c11fd9443452
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-06 11:42:36 +0200

    Update dative_reaction.py

--- tests/molecular/reactions/reaction/fixtures/dative_reaction.py
+++ tests/molecular/reactions/reaction/fixtures/dative_reaction.py
@@ -7,8 +7,9 @@ from ..case_data import CaseData
 
 @pytest.fixture
 def dative_reaction(
-    dative_functional_groups,
-    dative_bond_order,
+    functional_group1,
+    functional_group2,
+    bond_order,
     periodicity,
 ):
     """
@@ -16,23 +17,21 @@ def dative_reaction(
 
     """
 
-    functional_group1, functional_group2 = dative_functional_groups
-
     return CaseData(
         reaction=stk.DativeReaction(
-            stk.OneOneReaction(
+            reaction=stk.OneOneReaction(
                 functional_group1=functional_group1,
                 functional_group2=functional_group2,
                 bond_order=dative_bond_order,
                 periodicity=periodicity,
-            )
+            ),
         ),
         new_atoms=(),
         new_bonds=(
             get_bond(
                 functional_group1=functional_group1,
                 functional_group2=functional_group2,
-                bond_order=dative_bond_order,
+                bond_order=bond_order,
                 periodicity=periodicity,
             ),
         ),
@@ -50,30 +49,29 @@ def get_bond(
     periodicity,
 ):
 
-    bondera = next(functional_group1.get_bonders())
-    bonderb = next(functional_group2.get_bonders())
+    bonder1, = functional_group1.get_bonders()
+    bonder2, = functional_group2.get_bonders()
 
-    if is_metal_atom(bondera):
-        bonder2 = bondera
-        bonder1 = bonderb
-    elif is_metal_atom(bonderb):
-        bonder2 = bonderb
-        bonder1 = bondera
+    if is_metal(bonder1):
+        return stk.Bond(
+            atom1=bonder2,
+            atom2=bonder1,
+            order=bond_order,
+            periodicity=periodicity,
+        )
+    else:
+        return stk.Bond(
+            atom1=bonder1,
+            atom2=bonder2,
+            order=bond_order,
+            periodicity=periodicity,
+        )
 
-    return stk.Bond(
-        atom1=bonder1,
-        atom2=bonder2,
-        order=bond_order,
-        periodicity=periodicity,
-    )
 
-
-def is_metal_atom(atom):
-    # Metal atomic numbers.
+def is_metal(atom):
     metal_atomic_numbers = set(it.chain(
-        list(range(21, 31)),
-        list(range(39, 49)),
-        list(range(72, 81))
+        range(21, 31),
+        range(39, 49),
+        range(72, 81),
     ))
-
     return atom.get_atomic_number() in metal_atomic_numbers
