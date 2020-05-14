304c5fb97b0ba155d521a65a8cfba02537919290
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-14 09:07:43 +0200

    Update porphyrin.py

--- src/stk/molecular/topology_graphs/metal_complex/porphyrin/porphyrin.py
+++ src/stk/molecular/topology_graphs/metal_complex/porphyrin/porphyrin.py
@@ -17,13 +17,14 @@ class Porphyrin(MetalComplex):
     required for this topology.
 
     Ligand building blocks with two functional groups are required for
-    this topology.
+    this topology graph.
 
-    .. code-block:: python
+    When using a :class:`dict` for initialization, a
+    :class:`.BuildingBlock` needs to be assigned to each of the
+    following numbers:
 
-        # :class:`.BuildingBlock` placements:
-        metals: (0, )
-        ligands: (0, )
+        | metals: (0, )
+        | ligands: (0, )
 
     See :class:`.MetalComplex` for more details and examples.
 
