e1491755d05a020a217fce81757fb65979834da5
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-14 09:18:26 +0200

    Update paddlewheel.py

--- src/stk/molecular/topology_graphs/metal_complex/paddlewheel/paddlewheel.py
+++ src/stk/molecular/topology_graphs/metal_complex/paddlewheel/paddlewheel.py
@@ -17,13 +17,14 @@ class Paddlewheel(MetalComplex):
     required for this topology.
 
     Ligand building blocks with two functional groups are required for
-    this topology.
+    this topology graph.
 
-    .. code-block:: python
+    When using a :class:`dict` for initialization, a
+    :class:`.BuildingBlock` needs to be assigned to each of the
+    following numbers:
 
-        # :class:`.BuildingBlock` placements:
-        metals: (0, 1)
-        ligands: (0, 1, 2, 3)
+        | metals: (0, 1)
+        | ligands: (0, 1, 2, 3)
 
     See :class:`.MetalComplex` for more details and examples.
 
