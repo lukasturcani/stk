c121f172ac1e647f1c275b2f1d7cdf1464f0e527
Lukas Turcani <lukasturcani93@gmail.com>
2020-05-14 09:13:12 +0200

    Update octahedral.py

--- src/stk/molecular/topology_graphs/metal_complex/octahedral/octahedral.py
+++ src/stk/molecular/topology_graphs/metal_complex/octahedral/octahedral.py
@@ -17,13 +17,14 @@ class Octahedral(MetalComplex):
     required for this topology.
 
     Ligand building blocks with one functional group are required for
-    this topology.
+    this topology graph.
 
-    .. code-block:: python
+    When using a :class:`dict` for initialization, a
+    :class:`.BuildingBlock` needs to be assigned to each of the
+    following numbers:
 
-        # :class:`.BuildingBlock` placements:
-        metals: (0, )
-        ligands: (0, 1, 2, 3, 4, 5)
+        | metals: (0, )
+        | ligands: (0, 1, 2, 3, 4, 5)
 
     See :class:`.MetalComplex` for more details and examples.
 
