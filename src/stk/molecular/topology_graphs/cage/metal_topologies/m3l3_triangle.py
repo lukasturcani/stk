f40eccc17ef21efbf2cc7e8cff786c18cb230745
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-05-18 15:31:49 +0100

    Add M3l3Triangle (#158)
    * Add M3L3Triangle topology.
    
    * Add test.
    
    * Remove example

---
+++ src/stk/molecular/topology_graphs/cage/metal_topologies/m3l3_triangle.py
@@ -0,0 +1,160 @@
+"""
+M3L3 Triangle
+=============
+
+"""
+
+import numpy as np
+
+from ..cage import Cage
+from ..vertices import _LinearCageVertex
+from ...topology_graph import Edge
+from ...reactions import GenericReactionFactory
+
+
+class M3L3Triangle(Cage):
+    """
+    Represents a cage topology graph.
+
+    Both `corner` and `linker` vertices require building blocks with
+    two functional groups for this topology.
+
+    When using a :class:`dict` for initialization, a
+    :class:`.BuildingBlock` needs to be assigned to each of the
+    following numbers:
+
+        | corners: (0, 1, 2)
+        | linkers: (3, 4, 5)
+
+    See :class:`.Cage` for more details and examples.
+
+    """
+
+    def __init__(
+        self,
+        corners,
+        linkers,
+        vertex_alignments=None,
+        reaction_factory=GenericReactionFactory(),
+        num_processes=1,
+    ):
+        """
+        Initialize a :class:`.M3L3Triangle`.
+
+        Parameters
+        ----------
+        corners : :class:`dict` or :class:`.BuildingBlock`
+            Can be a :class:`dict` which maps the
+            :class:`.BuildingBlock` instances to the ids of the
+            vertices it should be placed on.
+
+            Can also be a :class:`.BuildingBlock` instance, which
+            should be placed on all corner vertices on the topology
+            graph.
+
+        linkers : :class:`dict` or :class:`.BuildingBlock`
+            Can be a :class:`dict` which maps the
+            :class:`.BuildingBlock` instances to the ids of the
+            vertices it should be placed on.
+
+            Can also be a :class:`.BuildingBlock` instance, which
+            should be placed on all linker vertices on the topology
+            graph.
+
+        vertex_alignments : :class:`dict`, optional
+            A mapping from the id of a :class:`.Vertex`
+            to an :class:`.Edge` connected to it.
+            The :class:`.Edge` is used to align the first
+            :class:`.FunctionalGroup` of a :class:`.BuildingBlock`
+            placed on that vertex. Only vertices which need to have
+            their default edge changed need to be present in the
+            :class:`dict`. If ``None`` then the default edge is used
+            for each vertex. Changing which :class:`.Edge` is used will
+            mean that the topology graph represents different
+            structural isomers. The edge is referred to by a number
+            between ``0`` (inclusive) and the number of edges the
+            vertex is connected to (exclusive).
+
+        reaction_factory : :class:`.ReactionFactory`, optional
+            The reaction factory to use for creating bonds between
+            building blocks.
+
+        num_processes : :class:`int`, optional
+            The number of parallel processes to create during
+            :meth:`construct`.
+
+
+        """
+
+        if isinstance(corners, dict):
+            building_blocks = {
+                corner: tuple(self._get_vertices(ids))
+                for corner, ids in corners.items()
+            }
+        else:
+            ids = (0, 1, 2)
+            building_blocks = {
+                corners: tuple(self._get_vertices(ids))
+            }
+
+        if isinstance(linkers, dict):
+            linkers_dict = {
+                linker: tuple(self._get_vertices(ids))
+                for linker, ids in corners.items()
+            }
+        else:
+            ids = (3, 4, 5)
+            linkers_dict = {
+                linkers: tuple(self._get_vertices(ids))
+            }
+
+        building_blocks.update(
+            (building_block, vertices)
+            for building_block, vertices in linkers_dict.items()
+        )
+
+        super().__init__(
+            building_blocks,
+            vertex_alignments=vertex_alignments,
+            reaction_factory=reaction_factory,
+            num_processes=num_processes,
+        )
+
+    _x = 2*np.sqrt(3)/4
+    _y = 2
+    _vertex_prototypes = (
+        _LinearCageVertex(0, [0, _x, 0]),
+        _LinearCageVertex(1, [_y/2, -_x, 0]),
+        _LinearCageVertex(2, [-_y/2, -_x, 0]),
+    )
+
+    _vertex_prototypes = (
+        *_vertex_prototypes,
+
+        _LinearCageVertex.init_at_center(
+            id=3,
+            vertices=(_vertex_prototypes[0], _vertex_prototypes[1]),
+        ),
+        _LinearCageVertex.init_at_center(
+            id=4,
+            vertices=(_vertex_prototypes[1], _vertex_prototypes[2]),
+        ),
+        _LinearCageVertex.init_at_center(
+            id=5,
+            vertices=(_vertex_prototypes[2], _vertex_prototypes[0]),
+        ),
+    )
+
+    _edge_prototypes = (
+        Edge(0, _vertex_prototypes[0], _vertex_prototypes[3]),
+        Edge(1, _vertex_prototypes[1], _vertex_prototypes[3]),
+
+        Edge(2, _vertex_prototypes[1], _vertex_prototypes[4]),
+        Edge(3, _vertex_prototypes[2], _vertex_prototypes[4]),
+
+        Edge(4, _vertex_prototypes[2], _vertex_prototypes[5]),
+        Edge(5, _vertex_prototypes[0], _vertex_prototypes[5]),
+    )
+
+    _num_windows = 1
+    _num_window_types = 1
