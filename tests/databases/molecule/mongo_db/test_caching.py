cb3575d788916d783655816c56cd3dfd1a223b3d
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-03 00:35:14 +0200

    Added a test to make sure caching is working.

---
+++ tests/databases/molecule/mongo_db/test_caching.py
@@ -0,0 +1,55 @@
+import stk
+import numpy as np
+import pymongo
+
+
+def test_put_caching():
+    database_name = '_test_put_caching'
+    client = pymongo.MongoClient()
+    client.drop_database(database_name)
+
+    database = stk.MoleculeMongoDb(
+        mongo_client=client,
+        database=database_name,
+    )
+    molecule = stk.BuildingBlock('CCC')
+    database.put(molecule)
+    database.put(molecule)
+
+    cache_info = database._put.cache_info()
+    assert cache_info.hits == 1
+    assert cache_info.misses == 1
+
+    database.put(
+        molecule=molecule.with_position_matrix(
+            position_matrix=np.zeros((molecule.get_num_atoms(), 3)),
+        ),
+    )
+    cache_info = database._put.cache_info()
+    assert cache_info.hits == 1
+    assert cache_info.misses == 2
+
+
+def test_get_caching():
+    database_name = '_test_get_caching'
+    client = pymongo.MongoClient()
+    client.drop_database(database_name)
+
+    database = stk.MoleculeMongoDb(
+        mongo_client=client,
+        database=database_name,
+    )
+    molecule = stk.BuildingBlock('CCC')
+    database.put(molecule)
+    database.get({
+        stk.InchiKey().get_key_name():
+            stk.InchiKey().get_key(molecule),
+    })
+    database.get({
+        stk.InchiKey().get_key_name():
+            stk.InchiKey().get_key(molecule),
+    })
+
+    cache_info = database._get.cache_info()
+    assert cache_info.hits == 1
+    assert cache_info.misses == 1
