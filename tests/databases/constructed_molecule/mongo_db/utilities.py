29049c2ed38d7eff44164d542fdd3e096e5ddac4
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-06 15:31:11 +0200

    Added a test to make sure database is updating. (#202)

---
+++ tests/databases/constructed_molecule/mongo_db/utilities.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+from ...utilities import DatabaseState, get_entry
+
+
+def get_database_state(database):
+    """
+    Get the state of a :class:`.ValueMongoDb`.
+
+    Parameters
+    ----------
+    database : :class:`.ValueMongoDb`
+        The database whose state is wanted.
+
+    Returns
+    -------
+    :class:`.DatabaseState`
+        The current state of `database`.
+
+    """
+
+    entries = Counter()
+    entries.update(map(get_entry, database._molecules.find({})))
+    entries.update(map(
+        get_entry,
+        database._position_matrices.find({})
+    ))
+    entries.update(map(
+        get_entry,
+        database._constructed_molecules.find({}),
+    ))
+    entries.update(map(
+        get_entry,
+        database._building_block_position_matrices.find({}),
+    ))
+    return DatabaseState(entries)
