ee8e5df856bac7ea63ef77d44c23b6f8926b7713
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-06 16:07:36 +0200

    Make index on constructed flag.

--- src/stk/databases/mongo_db/constructed_molecule.py
+++ src/stk/databases/mongo_db/constructed_molecule.py
@@ -295,6 +295,9 @@ class ConstructedMoleculeMongoDb(ConstructedMoleculeDatabase):
         self._get = lru_cache(maxsize=get_lru_cache_size)(self._get)
         self._put = lru_cache(maxsize=put_lru_cache_size)(self._put)
 
+        if 'c_1' not in self._molecules.index_information():
+            self._molecules.create_index('c')
+
         for index in indices:
             # Do not create the same index twice.
             if f'{index}_1' not in self._molecules.index_information():
