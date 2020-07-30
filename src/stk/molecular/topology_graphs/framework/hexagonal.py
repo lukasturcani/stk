16455dbe60abd039532b3ab92672b06ded149cfc
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-07-30 17:08:15 +0100

    Cof -> Framework

--- src/stk/molecular/topology_graphs/framework/hexagonal.py
+++ src/stk/molecular/topology_graphs/framework/hexagonal.py
@@ -7,19 +7,19 @@ Hexagonal
 import numpy as np
 
 
-from .cof import Cof
-from .vertices import _LinearCofVertex, _NonLinearCofVertex
+from .framework import Framework
+from .vertices import _LinearFrameworkVertex, _NonLinearFrameworkVertex
 from ..topology_graph import Edge
 
 
-class Hexagonal(Cof):
+class Hexagonal(Framework):
     """
-    Represents a hexagonal COF topology graph.
+    Represents a hexagonal framework topology graph.
 
     Building blocks with six and two functional groups are required
     for this topology graph.
 
-    See :class:`.Cof` for more details and examples.
+    See :class:`.Framework` for more details and examples.
 
     """
 
@@ -30,71 +30,71 @@ class Hexagonal(Cof):
     )
 
     _vertex_prototypes = (
-        _NonLinearCofVertex(0, (1/4)*_a + (1/4)*_b + (1/2)*_c),
-        _NonLinearCofVertex(1, (1/4)*_a + (3/4)*_b + (1/2)*_c),
-        _NonLinearCofVertex(2, (3/4)*_a + (1/4)*_b + (1/2)*_c),
-        _NonLinearCofVertex(3, (3/4)*_a + (3/4)*_b + (1/2)*_c),
+        _NonLinearFrameworkVertex(0, (1/4)*_a + (1/4)*_b + (1/2)*_c),
+        _NonLinearFrameworkVertex(1, (1/4)*_a + (3/4)*_b + (1/2)*_c),
+        _NonLinearFrameworkVertex(2, (3/4)*_a + (1/4)*_b + (1/2)*_c),
+        _NonLinearFrameworkVertex(3, (3/4)*_a + (3/4)*_b + (1/2)*_c),
     )
 
     _vertex_prototypes = (
         *_vertex_prototypes,
-        _LinearCofVertex.init_at_center(
+        _LinearFrameworkVertex.init_at_center(
             id=4,
             vertices=(_vertex_prototypes[0], _vertex_prototypes[1]),
         ),
-        _LinearCofVertex.init_at_center(
+        _LinearFrameworkVertex.init_at_center(
             id=5,
             vertices=(_vertex_prototypes[0], _vertex_prototypes[2]),
         ),
-        _LinearCofVertex.init_at_center(
+        _LinearFrameworkVertex.init_at_center(
             id=6,
             vertices=(_vertex_prototypes[1], _vertex_prototypes[2]),
         ),
-        _LinearCofVertex.init_at_center(
+        _LinearFrameworkVertex.init_at_center(
             id=7,
             vertices=(_vertex_prototypes[1], _vertex_prototypes[3]),
         ),
-        _LinearCofVertex.init_at_center(
+        _LinearFrameworkVertex.init_at_center(
             id=8,
             vertices=(_vertex_prototypes[2], _vertex_prototypes[3]),
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=9,
             vertices=(_vertex_prototypes[0], _vertex_prototypes[2]),
             cell_shifts=((0, 0, 0), (-1, 0, 0)),
             lattice_constants=_lattice_constants,
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=10,
             vertices=(_vertex_prototypes[0], _vertex_prototypes[1]),
             cell_shifts=((0, 0, 0), (0, -1, 0)),
             lattice_constants=_lattice_constants,
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=11,
             vertices=(_vertex_prototypes[0], _vertex_prototypes[3]),
             cell_shifts=((0, 0, 0), (0, -1, 0)),
             lattice_constants=_lattice_constants,
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=12,
             vertices=(_vertex_prototypes[2], _vertex_prototypes[1]),
             cell_shifts=((0, 0, 0), (1, -1, 0)),
             lattice_constants=_lattice_constants,
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=13,
             vertices=(_vertex_prototypes[2], _vertex_prototypes[3]),
             cell_shifts=((0, 0, 0), (0, -1, 0)),
             lattice_constants=_lattice_constants,
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=14,
             vertices=(_vertex_prototypes[1], _vertex_prototypes[3]),
             cell_shifts=((0, 0, 0), (-1, 0, 0)),
             lattice_constants=_lattice_constants,
         ),
-        _LinearCofVertex.init_at_shifted_center(
+        _LinearFrameworkVertex.init_at_shifted_center(
             id=15,
             vertices=(_vertex_prototypes[3], _vertex_prototypes[0]),
             cell_shifts=((0, 0, 0), (1, 0, 0)),
