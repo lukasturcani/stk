3178d772a2319f45376f3e4268e8cca5c601c259
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-08-17 19:49:34 +0100

    Bug fix.

--- src/stk/molecular/molecules/utilities.py
+++ src/stk/molecular/molecules/utilities.py
@@ -7,6 +7,9 @@ This module defines utilities for molecules.
 def sort_bond_atoms_by_id(bond):
     if bond.get_atom1().get_id() < bond.get_atom2().get_id():
         return bond
+    elif bond.get_order() == 9:
+        # Bond ordering in Dative bonds cannot change.
+        return bond
     else:
         return bond.__class__(
             atom1=bond.get_atom2(),
@@ -18,7 +21,7 @@ def sort_bond_atoms_by_id(bond):
 
 def get_bond_atom_ids(bond):
     return sorted((
-        bond.get_atom1().get_id(), 
+        bond.get_atom1().get_id(),
         bond.get_atom2().get_id(),
     ))
 
