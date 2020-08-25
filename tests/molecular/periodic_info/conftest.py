e8aa7e2c50a8eaa9d65c212a0e9d13da55f41c72
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-08-25 13:26:55 +0100

    Update test.

--- tests/molecular/periodic_info/conftest.py
+++ tests/molecular/periodic_info/conftest.py
@@ -4,18 +4,15 @@ import stk
 
 from .case_data import CaseData
 
-bb1 = stk.BuildingBlock('BrCCBr', [stk.BromoFactory()])
-bb2 = stk.BuildingBlock('BrCC(CBr)CBr', [stk.BromoFactory()])
-topology_graph = stk.cof.PeriodicHoneycomb(
-    building_blocks=(bb1, bb2),
-    lattice_size=(3, 1, 2),
-)
-
 
 @pytest.fixture(
     params=(
         CaseData(
-            periodic_info=topology_graph.get_periodic_info(),
+            periodic_info=stk.PeriodicInfo(
+                x_vector=np.array([109.29499828, 0., 0.]),
+                y_vector=np.array([18.21583305, 31.54982284, 0.]),
+                z_vector=np.array([0., 0., 210.33234855]),
+            ),
             x_vector=np.array([109.29499828, 0., 0.]),
             y_vector=np.array([18.21583305, 31.54982284, 0.]),
             z_vector=np.array([0., 0., 210.33234855]),
