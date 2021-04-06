7dafc3776a0463b4e1c2a904ea4179eed5174973
Steven Bennett <stevenbennett@stevens-air.home>
2021-04-06 11:18:47 +0100

    Updated

--- src/stk/ea/mutation/mutators/molecule/__init__.py
+++ src/stk/ea/mutation/mutators/molecule/__init__.py
@@ -2,3 +2,4 @@ from .mutator import *  # noqa
 from .random_building_block import *  # noqa
 from .random_topology_graph import *  # noqa
 from .similar_building_block import *  # noqa
+from .random_smarts import * # noqa
