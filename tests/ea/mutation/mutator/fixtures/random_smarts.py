eb369d06a6d426b0abfe324f618a23599e9ac973
Steven Bennett <steven_bennett_1996@icloud.com>
2021-04-19 17:11:50 +0100

    Add file

---
+++ tests/ea/mutation/mutator/fixtures/random_smarts.py
@@ -0,0 +1,36 @@
+import pytest
+import stk
+from ..case_data import CaseData
+
+
+def has_bromo(building_block):
+    fg, = building_block.get_functional_groups(0)
+    return fg.__class__ is stk.Bromo
+
+
+bb1 = stk.BuildingBlock('BrCC(Cl)CBr', [stk.BromoFactory()])
+graph1 = stk.polymer.Linear((bb1, ), 'A', 2)
+
+bb2 = stk.BuildingBlock('BrCC(F)CBr', [stk.BromoFactory()])
+graph2 = stk.polymer.Linear((bb2, ), 'A', 2)
+
+
+@pytest.fixture(
+    params=(
+        CaseData(
+                mutator=stk.RandomSmarts(
+                    query_smarts='Cl',
+                    replacement_smarts='F',
+                    is_replaceable=has_bromo,
+                    replacement_fgs=[stk.BromoFactory()],
+                ),
+                record=stk.MoleculeRecord(graph1),
+                mutation_record=stk.MutationRecord(
+                    molecule_record=stk.MoleculeRecord(graph2),
+                    mutator_name='RandomSmarts'
+                ),
+        ),
+    ),
+)
+def random_smarts(request):
+    return request.param
