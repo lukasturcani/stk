a2cce25b5d546003a17632aa91883a4a70a2cf3a
Steven Bennett <steven_bennett_1996@icloud.com>
2021-04-19 16:58:44 +0100

    Fixed linting

--- src/stk/ea/mutation/mutators/molecule/random_smarts.py
+++ src/stk/ea/mutation/mutators/molecule/random_smarts.py
@@ -34,7 +34,7 @@ class RandomSmarts(MoleculeMutator):
         query_smarts,
         replacement_smarts,
         is_replaceable,
-        replacement_functional_groups,
+        replacement_fgs,
         replacement_specifier='all',
         name='RandomSmarts',
         random_seed=None,
@@ -63,8 +63,10 @@ class RandomSmarts(MoleculeMutator):
             If `all`, all matching SMARTS Swill be replacemed.
             If `one`, a single random SMARTS replacement will occur.
 
-        replacement_functional_groups : :class:`list` of :class:`.FunctionalGroupFactory`
-            The functional group factories used to assign functional groups within the generated molecule.
+        replacement_fgs : :class:`list` of
+         :class:`.FunctionalGroupFactory`
+            The functional group factories used to assign functional
+            groups within the generated molecule.
 
         name : :class:`str`, optional
             A name to help identify the mutator instance.
@@ -79,7 +81,7 @@ class RandomSmarts(MoleculeMutator):
         self._name = name
         self._replacement_specifier = replacement_specifier
         self._generator = np.random.RandomState(random_seed)
-        self._replacement_functional_groups = replacement_functional_groups
+        self._replacement_fgs = replacement_fgs
 
     def mutate(self, record):
         replaceable_building_blocks = tuple(filter(
@@ -93,17 +95,25 @@ class RandomSmarts(MoleculeMutator):
         query = Chem.MolFromSmarts(self._query_smarts)
         replacer_smarts = Chem.MolFromSmarts(self._replacement_smarts)
         if self._replacement_specifier == 'all':
-            new_rdmol = Chem.rdmolops.ReplaceSubstructs(rdmol, query, replacer_smarts, replaceAll=True)[0]
+            new_rdmol = Chem.rdmolops.ReplaceSubstructs(
+                rdmol, query, replacer_smarts, replaceAll=True
+            )[0]
 
         elif self._replacement_specifier == 'one':
-            new_rdmols = Chem.rdmolops.ReplaceSubstructs(rdmol, query, replacer_smarts, replaceAll=True)
+            new_rdmols = Chem.rdmolops.ReplaceSubstructs(
+                rdmol, query, replacer_smarts, replaceAll=True
+            )
             new_rdmol = self._generator.choice(
                 new_rdmols
             )
         else:
-            raise RuntimeError('Invalid argument for replacement_specifier received')
+            raise RuntimeError(
+                'Invalid argument for replacement_specifier received'
+            )
         # Create new BuildingBlock
-        replacement = BuildingBlock.init_from_rdkit_mol(new_rdmol, functional_groups=self._replacement_functional_groups)
+        replacement = BuildingBlock.init_from_rdkit_mol(
+            new_rdmol, functional_groups=self._replacement_fgs
+        )
         graph = record.get_topology_graph().with_building_blocks({
             replaced_building_block: replacement,
         })
