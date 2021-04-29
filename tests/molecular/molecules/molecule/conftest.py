18f43264789b70e1f7916a9760d148f2a6771418
Lukas Turcani <lukasturcani93@gmail.com>
2021-04-29 13:13:48 +0100

    Move to a test

--- tests/molecular/molecules/molecule/conftest.py
+++ tests/molecular/molecules/molecule/conftest.py
@@ -347,47 +347,6 @@ def name_db():
     )
 
 
-@pytest.fixture(
-    scope='session',
-)
-def dump_molecular_structures(
-    request,
-    tmp_path,
-    molecule_db,
-    name_db,
-    case_data,
-):
-    """
-    Dump molecular structures.
-
-    This fixture dumps molecules to files and to a MongoDB database
-    so that they can be visually inspected.
-
-    Parameters
-    ----------
-    request : :class:`pytest.FixtureRequest`
-        Holds information about the requesting fixture.
-
-    tmp_path : :class:`pathlib2.Path`
-        A path into which the structure of the molecule is saved.
-
-    molecule_db : :class:`.MoleculeDatabase`
-        A database into which the structure of the molecule  is saved.
-
-    name_db : :class:`.ValueDatabase`
-        A database into which name the name of the molecule is saved.
-
-    Returns
-    -------
-    None : :class:`NoneType`
-
-    """
-
-    case_data.molecule.write(tmp_path / 'molecule.mol')
-    molecule_db.put(case_data.molecule)
-    name_db.put(case_data.molecule, request.node.name)
-
-
 def _get_filename(request):
     """
     Get the filename for a position matrix of a molecule.
---
+++ tests/molecular/molecules/molecule/test_dump_molecular_structures.py
@@ -0,0 +1,36 @@
+def test_dump_molecular_structures(
+    request,
+    tmp_path,
+    molecule_db,
+    name_db,
+    case_data,
+):
+    """
+    Dump molecular structures.
+
+    This test dumps molecules to files and to a MongoDB database
+    so that they can be visually inspected.
+
+    Parameters
+    ----------
+    request : :class:`pytest.FixtureRequest`
+        Holds information about the requesting test.
+
+    tmp_path : :class:`pathlib2.Path`
+        A path into which the structure of the molecule is saved.
+
+    molecule_db : :class:`.MoleculeDatabase`
+        A database into which the structure of the molecule  is saved.
+
+    name_db : :class:`.ValueDatabase`
+        A database into which name the name of the molecule is saved.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    case_data.molecule.write(tmp_path / 'molecule.mol')
+    molecule_db.put(case_data.molecule)
+    name_db.put(case_data.molecule, request.node.name)
