a0c8cc0ce907c5fbbcad537e4401e62e1d217d94
Lukas Turcani <lukasturcani93@gmail.com>
2021-04-29 19:48:43 +0100

    Dump database (#322)
    Related Issues: #277 
    Reviewer: @andrewtarzia 
    
    This commit adds a new test, `test_dump_molecule_structures()`, which 
    is responsible for dumping the molecular structures to files and
    placing them into a MongoDB database. Previously the file dumping
    was done in the `test_get_atoms()` test, but I think having a 
    separate test for this is better, because it keeps better to the single
    responsibility principle. 
    
    The commit also updates the CI to dump the MongoDB database after
    the tests have been run, so that it can be downloaded and viewed
    locally, for example with `stk-vis`. To help viewing with `stk-vis`
    the file `tests/mongo-config.json` has been added. It can be 
    loaded into `stk-vis` and configures it so that the developer does
    not have to manually change the database name or the molecule
    key name.
    
    The commit also adds a new utility script `drop_test_database.py` which
    just drops the databases which running the tests creates. This script
    is meant to be run manually if a developer wants to remove the 
    databases for some reason.
    
    Finally, the commit also removes all uses of the MongoDB action with a 
    MongoDB service container in the CI. This just seems more idiomatic
    to me.

---
+++ tests/drop_test_database.py
@@ -0,0 +1,24 @@
+"""
+A utility for dropping the databases created by running the tests.
+
+This script is not part of the test suite. It is meant to be run
+manually by developers when they feel the need to.
+
+"""
+
+
+import pymongo
+
+
+def main():
+    databases_to_drop = (
+        '_stk_pytest_database',
+        '_stk_test_database_for_testing',
+    )
+    client = pymongo.MongoClient()
+    for database in databases_to_drop:
+        client.drop_database(database)
+
+
+if __name__ == '__main__':
+    main()
