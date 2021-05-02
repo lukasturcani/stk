82edf1159805d8393cb2427a207ddb81ce86742f
Lukas Turcani <lukasturcani93@gmail.com>
2021-05-02 20:18:58 +0100

    Make lists into tuples

--- src/stk/databases/mongo_db/utilities.py
+++ src/stk/databases/mongo_db/utilities.py
@@ -7,7 +7,18 @@ MongoDB Utilities
 
 class HashableDict(dict):
     def __hash__(self):
-        return hash((frozenset(self), frozenset(self.values())))
+        return hash((
+            frozenset(self),
+            frozenset(_to_hashable(self.values())),
+        ))
 
     def __eq__(self, other):
         return super().__eq__(other)
+
+
+def _to_hashable(iterable):
+    for item in iterable:
+        if isinstance(item, list):
+            yield tuple(_to_hashable(subitem) for subitem in item)
+        else:
+            yield item
