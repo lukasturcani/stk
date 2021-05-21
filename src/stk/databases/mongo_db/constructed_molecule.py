f9a4c1e78b1cc0c704bf97193d78763f14362c12
Lukas Turcani <lukasturcani93@gmail.com>
2021-05-21 08:09:56 +0100

    Remove old feature

--- src/stk/databases/mongo_db/constructed_molecule.py
+++ src/stk/databases/mongo_db/constructed_molecule.py
@@ -5,7 +5,6 @@ Constructed Molecule MongoDB
 """
 
 from functools import lru_cache
-import warnings
 
 from stk.serialization import (
     ConstructedMoleculeJsonizer,
@@ -326,7 +325,6 @@ class ConstructedMoleculeMongoDb(ConstructedMoleculeDatabase):
         ),
         jsonizer=ConstructedMoleculeJsonizer(),
         dejsonizer=ConstructedMoleculeDejsonizer(),
-        lru_cache_size='',
         put_lru_cache_size=128,
         get_lru_cache_size=128,
         indices=('InChIKey', ),
@@ -369,17 +367,6 @@ class ConstructedMoleculeMongoDb(ConstructedMoleculeDatabase):
             Used to create :class:`.Molecule` instances from their
             JSON representations.
 
-        lru_cache_size : :class:`int`, optional
-            This argument is deprecated and will be removed in any
-            version of :mod:`stk` released on, or after, 01/01/21.
-            Use the `put_lru_cache_size` and `get_lru_cache_size`
-            arguments instead.
-
-            A RAM-based least recently used cache is used to avoid
-            reading and writing to the database repeatedly. This sets
-            the number of values which fit into the LRU cache. If
-            ``None``, the cache size will be unlimited.
-
         put_lru_cache_size : :class:`int`, optional
             A RAM-based least recently used cache is used to avoid
             writing to the database repeatedly. This sets
@@ -397,17 +384,6 @@ class ConstructedMoleculeMongoDb(ConstructedMoleculeDatabase):
             created, in order to minimize lookup time.
 
         """
-
-        if lru_cache_size != '':
-            warnings.warn(
-                'The lru_cache_size argument is deprecated and will '
-                'be removed in any version of stk released on, or '
-                'after, 01/01/21. Use the put_lru_cache_size and '
-                'get_lru_cache_size arguments instead.',
-                FutureWarning,
-            )
-            put_lru_cache_size = lru_cache_size
-            get_lru_cache_size = lru_cache_size
 
         database = mongo_client[database]
         self._molecules = database[molecule_collection]
