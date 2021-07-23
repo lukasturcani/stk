2276d2722939333a90d45a39e37b3c5cf78cdc3e
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-07-23 09:46:46 +0100

    Improve test to handle overlapping keys.

--- tests/databases/constructed_molecule/test_get_all.py
+++ tests/databases/constructed_molecule/test_get_all.py
@@ -13,11 +13,8 @@ def test_get_all(mongo_client):
     mongo_client.drop_database(database_name)
 
     key_maker = stk.Inchi()
-    jsonizer = stk.ConstructedMoleculeJsonizer(
-        key_makers=(key_maker, )
-    )
-
-    database = stk.ConstructedMoleculeMongoDb(
+    jsonizer = stk.ConstructedMoleculeJsonizer((key_maker, ))
+    database1 = stk.ConstructedMoleculeMongoDb(
         mongo_client=mongo_client,
         database=database_name,
         jsonizer=jsonizer,
@@ -26,13 +23,30 @@ def test_get_all(mongo_client):
         indices=(key_maker.get_key_name(), ),
     )
 
-    molecules = [
+    key_maker = stk.Smiles()
+    jsonizer = stk.ConstructedMoleculeJsonizer((key_maker, ))
+    database2 = stk.ConstructedMoleculeMongoDb(
+        mongo_client=mongo_client,
+        database=database_name,
+        jsonizer=jsonizer,
+        put_lru_cache_size=0,
+        get_lru_cache_size=0,
+        indices=(key_maker.get_key_name(), ),
+    )
+
+    building_blocks = (
+        stk.BuildingBlock('BrCBr', [stk.BromoFactory()]),
+        stk.BuildingBlock('BrCCBr', [stk.BromoFactory()]),
+        stk.BuildingBlock('BrCCCBr', [stk.BromoFactory()]),
+        stk.BuildingBlock('BrCCCCBr', [stk.BromoFactory()]),
+        stk.BuildingBlock('BrCNCBr', [stk.BromoFactory()]),
+        stk.BuildingBlock('BrCCNCBr', [stk.BromoFactory()]),
+    )
+
+    all_molecules = [
         stk.ConstructedMolecule(
             topology_graph=stk.polymer.Linear(
-                building_blocks=(stk.BuildingBlock(
-                    smiles='BrCCCBr',
-                    functional_groups=[stk.BromoFactory()]),
-                ),
+                building_blocks=(building_blocks[0], ),
                 repeating_unit='A',
                 num_repeating_units=3,
             ),
@@ -40,35 +54,84 @@ def test_get_all(mongo_client):
         stk.ConstructedMolecule(
             topology_graph=stk.polymer.Linear(
                 building_blocks=(
-                    stk.BuildingBlock(
-                        smiles='BrCCBr',
-                        functional_groups=[stk.BromoFactory()]
-                    ),
-                    stk.BuildingBlock(
-                        smiles='BrCNCBr',
-                        functional_groups=[stk.BromoFactory()]
-                    ),
+                    building_blocks[0], building_blocks[1]
                 ),
                 repeating_unit='AB',
                 num_repeating_units=2,
             ),
         ),
+        stk.ConstructedMolecule(
+            topology_graph=stk.polymer.Linear(
+                building_blocks=(building_blocks[2], ),
+                repeating_unit='A',
+                num_repeating_units=3,
+            ),
+        ),
+        stk.ConstructedMolecule(
+            topology_graph=stk.polymer.Linear(
+                building_blocks=(
+                    building_blocks[2], building_blocks[3]
+                ),
+                repeating_unit='AB',
+                num_repeating_units=2,
+            ),
+        ),
+        stk.ConstructedMolecule(
+            topology_graph=stk.polymer.Linear(
+                building_blocks=(building_blocks[4], ),
+                repeating_unit='A',
+                num_repeating_units=3,
+            ),
+        ),
     ]
-    molecules_by_key = {
-        key_maker.get_key(molecule): molecule
-        for molecule in molecules
+
+    # Two sets of molecules with two distinct molecules each, and one
+    # shared molecule to test for overlap.
+    molecules1 = (all_molecules[0], all_molecules[1], all_molecules[4])
+    molecules2 = (all_molecules[2], all_molecules[3], all_molecules[4])
+
+    for molecule in molecules1:
+        database1.put(molecule)
+
+    for molecule in molecules2:
+        database2.put(molecule)
+
+    # A new database with both indices.
+    key_makers = (stk.Inchi(), stk.Smiles())
+    jsonizer = stk.ConstructedMoleculeJsonizer(key_makers)
+    database3 = stk.ConstructedMoleculeMongoDb(
+        mongo_client=mongo_client,
+        database=database_name,
+        jsonizer=jsonizer,
+        put_lru_cache_size=0,
+        get_lru_cache_size=0,
+        indices=(i.get_key_name() for i in key_makers),
+    )
+
+    molecules1_by_key = {
+        key_makers[0].get_key(molecule): molecule
+        for molecule in molecules1
+    }
+    molecules2_by_key = {
+        key_makers[1].get_key(molecule): molecule
+        for molecule in molecules2
     }
 
-    for molecule in molecules:
-        database.put(molecule)
-
-    for i, retrieved in enumerate(database.get_all()):
-        key = key_maker.get_key(retrieved)
-        molecule = molecules_by_key[key]
+    for i, retrieved in enumerate(database3.get_all()):
+        try:
+            key = key_makers[0].get_key(retrieved)
+            molecule = molecules1_by_key[key]
+        except KeyError:
+            key = key_makers[1].get_key(retrieved)
+            molecule = molecules2_by_key[key]
         is_equivalent_constructed_molecule(
-            molecule.with_canonical_atom_ordering(),
-            retrieved.with_canonical_atom_ordering(),
+            constructed_molecule1=(
+                molecule.with_canonical_atom_ordering()
+            ),
+            constructed_molecule2=(
+                retrieved.with_canonical_atom_ordering()
+            ),
         )
 
     # Check number of molecules.
-    assert i+1 == len(molecules)
+    assert i+1 == len(all_molecules)
--- tests/databases/molecule/test_get_all.py
+++ tests/databases/molecule/test_get_all.py
@@ -14,8 +14,7 @@ def test_get_all(mongo_client):
 
     key_maker = stk.Inchi()
     jsonizer = stk.MoleculeJsonizer(key_makers=(key_maker, ))
-
-    database = stk.MoleculeMongoDb(
+    database1 = stk.MoleculeMongoDb(
         mongo_client=mongo_client,
         database=database_name,
         jsonizer=jsonizer,
@@ -24,26 +23,68 @@ def test_get_all(mongo_client):
         indices=(key_maker.get_key_name(), ),
     )
 
-    molecules = (
+    key_maker = stk.Smiles()
+    jsonizer = stk.MoleculeJsonizer(key_makers=(key_maker, ))
+    database2 = stk.MoleculeMongoDb(
+        mongo_client=mongo_client,
+        database=database_name,
+        jsonizer=jsonizer,
+        put_lru_cache_size=0,
+        get_lru_cache_size=0,
+        indices=(key_maker.get_key_name(), ),
+    )
+
+    all_molecules = (
         stk.BuildingBlock('CCC').with_canonical_atom_ordering(),
         stk.BuildingBlock('BrCCCBr').with_canonical_atom_ordering(),
         stk.BuildingBlock('NCCN').with_canonical_atom_ordering(),
+        stk.BuildingBlock('CCCCCC').with_canonical_atom_ordering(),
+        stk.BuildingBlock('NCCCCN').with_canonical_atom_ordering(),
     )
-    molecules_by_key = {
-        key_maker.get_key(molecule): molecule
-        for molecule in molecules
+
+    # Two sets of molecules with two distinct molecules each, and one
+    # shared molecule to test for overlap.
+    molecules1 = (all_molecules[0], all_molecules[1], all_molecules[4])
+    molecules2 = (all_molecules[2], all_molecules[3], all_molecules[4])
+
+    for molecule in molecules1:
+        database1.put(molecule)
+
+    for molecule in molecules2:
+        database2.put(molecule)
+
+    # A new database with both indices.
+    key_makers = (stk.Inchi(), stk.Smiles())
+    jsonizer = stk.MoleculeJsonizer(key_makers=key_makers)
+    database3 = stk.MoleculeMongoDb(
+        mongo_client=mongo_client,
+        database=database_name,
+        jsonizer=jsonizer,
+        put_lru_cache_size=0,
+        get_lru_cache_size=0,
+        indices=(i.get_key_name() for i in key_makers),
+    )
+
+    molecules1_by_key = {
+        key_makers[0].get_key(molecule): molecule
+        for molecule in molecules1
+    }
+    molecules2_by_key = {
+        key_makers[1].get_key(molecule): molecule
+        for molecule in molecules2
     }
 
-    for molecule in molecules:
-        database.put(molecule)
-
-    for i, retrieved in enumerate(database.get_all()):
-        key = key_maker.get_key(retrieved)
-        molecule = molecules_by_key[key]
+    for i, retrieved in enumerate(database3.get_all()):
+        try:
+            key = key_makers[0].get_key(retrieved)
+            molecule = molecules1_by_key[key]
+        except KeyError:
+            key = key_makers[1].get_key(retrieved)
+            molecule = molecules2_by_key[key]
         is_equivalent_molecule(
             molecule1=molecule.with_canonical_atom_ordering(),
             molecule2=retrieved.with_canonical_atom_ordering(),
         )
 
     # Check number of molecules.
-    assert i+1 == len(molecules)
+    assert i+1 == len(all_molecules)
