6769c6d60ca62cb677d0fea72d0aaef87f91451e
Lukas Turcani <lukasturcani93@gmail.com>
2021-07-28 12:07:04 +0100

    wip

--- tests/ea/mutation/mutator/fixtures/similar_building_block.py
+++ tests/ea/mutation/mutator/fixtures/similar_building_block.py
@@ -9,29 +9,34 @@ def has_bromo(building_block):
     return fg.__class__ is stk.Bromo
 
 
-bb1 = stk.BuildingBlock('BrCCBr', [stk.BromoFactory()])
-graph1 = stk.polymer.Linear((bb1, ), 'A', 2)
+def _get_case_data_1() -> CaseData:
 
-bb2 = stk.BuildingBlock('BrCNCBr', [stk.BromoFactory()])
-graph2 = stk.polymer.Linear((bb2, ), 'A', 2)
+    bb1 = stk.BuildingBlock('BrCCBr', [stk.BromoFactory()])
+    graph1 = stk.polymer.Linear((bb1, ), 'A', 2)
 
-bb3 = stk.BuildingBlock('BrCNNCCNCBr', [stk.BromoFactory()])
+    bb2 = stk.BuildingBlock('BrCNCBr', [stk.BromoFactory()])
+    graph2 = stk.polymer.Linear((bb2, ), 'A', 2)
+
+    bb3 = stk.BuildingBlock('BrCNNCCNCBr', [stk.BromoFactory()])
+
+    return CaseData(
+        mutator=stk.SimilarBuildingBlock(
+            building_blocks=(bb2, bb3),
+            is_replaceable=has_bromo,
+        ),
+        record=stk.MoleculeRecord(graph1),
+        mutation_record=stk.MutationRecord(
+            molecule_record=stk.MoleculeRecord(graph2),
+            mutator_name='SimilarBuildingBlock',
+        ),
+    )
 
 
 @pytest.fixture(
+    scope='session',
     params=(
-        CaseData(
-            mutator=stk.SimilarBuildingBlock(
-                building_blocks=(bb2, bb3),
-                is_replaceable=has_bromo,
-            ),
-            record=stk.MoleculeRecord(graph1),
-            mutation_record=stk.MutationRecord(
-                molecule_record=stk.MoleculeRecord(graph2),
-                mutator_name='SimilarBuildingBlock',
-            ),
-        ),
+        _get_case_data_1,
     ),
 )
-def similar_building_block(request):
-    return request.param
+def similar_building_block(request) -> CaseData:
+    return request.param()
