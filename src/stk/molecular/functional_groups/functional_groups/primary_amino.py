67e3024b05c1d6052da9be2a8dd58607bb38987b
Lukas Turcani <lukasturcani93@gmail.com>
2021-09-26 16:59:31 +0100

    Primary Amino

--- src/stk/molecular/functional_groups/functional_groups/primary_amino.py
+++ src/stk/molecular/functional_groups/functional_groups/primary_amino.py
@@ -4,7 +4,13 @@ Primary Amino
 
 """
 
+from __future__ import annotations
+
+from typing import Optional
+
+from .utilities import get_atom_map
 from .generic_functional_group import GenericFunctionalGroup
+from ...atoms import N, H, Atom
 
 
 class PrimaryAmino(GenericFunctionalGroup):
@@ -18,106 +24,104 @@ class PrimaryAmino(GenericFunctionalGroup):
 
     def __init__(
         self,
-        nitrogen,
-        hydrogen1,
-        hydrogen2,
-        atom,
-        bonders,
-        deleters,
-        placers=None,
-    ):
+        nitrogen: N,
+        hydrogen1: H,
+        hydrogen2: H,
+        atom: Atom,
+        bonders: tuple[Atom, ...],
+        deleters: tuple[Atom, ...],
+        placers: Optional[tuple[Atom, ...]] = None,
+    ) -> None:
         """
         Initializes a :class:`.PrimaryAmino` instance.
 
-        Parameters
-        ----------
-        nitrogen : :class:`.N`
-            The ``[nitrogen]`` atom.
+        Parameters:
 
-        hydrogen1 : :class:`.H`
-            The ``[hydrogen1]`` atom.
+            nitrogen:
+                The ``[nitrogen]`` atom.
 
-        hydrogen2 : :class:`.H`
-            The ``[hydrogen2]`` atom.
+            hydrogen1:
+                The ``[hydrogen1]`` atom.
 
-        atom : :class:`.Atom`
-            The ``[atom]`` atom.
+            hydrogen2:
+                The ``[hydrogen2]`` atom.
 
-        bonders : :class:`tuple` of :class:`.Atom`
-            The bonder atoms.
+            atom:
+                The ``[atom]`` atom.
 
-        deleters : :class:`tuple` of :class:`.Atom`
-            The deleter atoms.
+            bonders:
+                The bonder atoms.
 
-        placers : :class:`tuple` of :class:`.Atom`, optional
-            The placer atoms. If ``None`` the `bonders` will be used.
+            deleters:
+                The deleter atoms.
+
+            placers:
+                The placer atoms. If ``None`` the `bonders` will be
+                used.
 
         """
 
-        self._nitrogen = nitrogen
-        self._hydrogen1 = hydrogen1
-        self._hydrogen2 = hydrogen2
-        self._atom = atom
-        super().__init__(
+        GenericFunctionalGroup.__init__(
+            self=self,
             atoms=(nitrogen, hydrogen1, hydrogen2, atom),
             bonders=bonders,
             deleters=deleters,
             placers=bonders if placers is None else placers,
         )
+        self._nitrogen = nitrogen
+        self._hydrogen1 = hydrogen1
+        self._hydrogen2 = hydrogen2
+        self._atom = atom
 
-    def get_nitrogen(self):
+    def get_nitrogen(self) -> N:
         """
         Get the ``[nitrogen]`` atom.
 
-        Returns
-        -------
-        :class:`.N`
+        Returns:
+
             The ``[nitrogen]`` atom.
 
         """
 
         return self._nitrogen
 
-    def get_hydrogen1(self):
+    def get_hydrogen1(self) -> H:
         """
         Get the ``[hydrogen1]`` atom.
 
-        Returns
-        -------
-        :class:`.H`
+        Returns:
+
             The ``[hydrogen1]`` atom.
 
         """
 
         return self._hydrogen1
 
-    def get_hydrogen2(self):
+    def get_hydrogen2(self) -> H:
         """
         Get the ``[hydrogen2]`` atom.
 
-        Returns
-        -------
-        :class:`.H`
+        Returns:
+
             The ``[hydrogen2]`` atom.
 
         """
 
         return self._hydrogen2
 
-    def get_atom(self):
+    def get_atom(self) -> Atom:
         """
         Get the ``[atom]`` atom.
 
-        Returns
-        -------
-        :class:`.Atom`
+        Returns:
+
             The ``[atom]`` atom.
 
         """
 
         return self._atom
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (
             f'{self.__class__.__name__}('
             f'{self._nitrogen}, {self._hydrogen1}, {self._hydrogen2}, '
@@ -126,16 +130,53 @@ class PrimaryAmino(GenericFunctionalGroup):
             ')'
         )
 
-    def clone(self):
-        clone = super().clone()
+    def clone(self) -> PrimaryAmino:
+        clone = self._clone()
         clone._nitrogen = self._nitrogen
         clone._hydrogen1 = self._hydrogen1
         clone._hydrogen2 = self._hydrogen2
         clone._atom = self._atom
         return clone
 
-    def with_atoms(self, atom_map):
-        clone = super().with_atoms(atom_map)
+    def with_ids(
+        self,
+        id_map: dict[int, int],
+    ) -> PrimaryAmino:
+        atom_map = get_atom_map(
+            id_map=id_map,
+            atoms=(
+                *self._atoms,
+                *self._placers,
+                *self._core_atoms,
+                *self._bonders,
+                *self._deleters,
+                self._nitrogen,
+                self._hydrogen1,
+                self._hydrogen2,
+                self._atom,
+            ),
+        )
+        clone = self.__class__.__new__(self.__class__)
+        clone._atoms = tuple(
+            atom_map.get(atom.get_id(), atom)
+            for atom in self._atoms
+        )
+        clone._placers = tuple(
+            atom_map.get(atom.get_id(), atom)
+            for atom in self._placers
+        )
+        clone._core_atoms = tuple(
+            atom_map.get(atom.get_id(), atom)
+            for atom in self._core_atoms
+        )
+        clone._bonders = tuple(
+            atom_map.get(atom.get_id(), atom)
+            for atom in self._bonders
+        )
+        clone._deleters = tuple(
+            atom_map.get(atom.get_id(), atom)
+            for atom in self._deleters
+        )
         clone._nitrogen = atom_map.get(
             self._nitrogen.get_id(),
             self._nitrogen,
