2fbd90788ff9271d5c86b9d9561012e92e22e716
Lukas Turcani <lukasturcani93@gmail.com>
2021-09-26 18:21:09 +0100

    wip

--- src/stk/molecular/functional_groups/factories/alcohol_factory.py
+++ src/stk/molecular/functional_groups/factories/alcohol_factory.py
@@ -4,9 +4,12 @@ Alcohol Factory
 
 """
 
+from typing import Optional, Iterable, Literal
+
 from .functional_group_factory import FunctionalGroupFactory
 from .utilities import _get_atom_ids
 from ..functional_groups import Alcohol
+from ...molecule import Molecule
 
 
 class AlcoholFactory(FunctionalGroupFactory):
@@ -16,108 +19,115 @@ class AlcoholFactory(FunctionalGroupFactory):
     Creates functional groups from substructures, which match the
     ``[*][O][H]`` functional group string.
 
-    Examples
-    --------
-    *Creating Functional Groups with the Factory*
+    Examples:
 
-    You want to create a building block which has :class:`.Alcohol`
-    functional groups. You want the oxygen atom in those functional
-    groups to be the bonder atom, and the hydrogen atom to be the
-    deleter atom.
+        *Creating Functional Groups with the Factory*
 
-    .. testcode:: creating-functional-groups-with-the-factory
+        You want to create a building block which has :class:`.Alcohol`
+        functional groups. You want the oxygen atom in those functional
+        groups to be the bonder atom, and the hydrogen atom to be the
+        deleter atom.
 
-        import stk
+        .. testcode:: creating-functional-groups-with-the-factory
 
-        building_block = stk.BuildingBlock(
-            smiles='OCCCO',
-            functional_groups=(stk.AlcoholFactory(), ),
-        )
+            import stk
 
-    .. testcode:: creating-functional-groups-with-the-factory
-        :hide:
+            building_block = stk.BuildingBlock(
+                smiles='OCCCO',
+                functional_groups=(stk.AlcoholFactory(), ),
+            )
 
-        assert all(
-            isinstance(functional_group, stk.Alcohol)
-            for functional_group
-            in building_block.get_functional_groups()
-        )
-        assert building_block.get_num_functional_groups() == 2
+        .. testcode:: creating-functional-groups-with-the-factory
+            :hide:
 
-    *Changing the Bonder and Deleter Atoms*
+            assert all(
+                isinstance(functional_group, stk.Alcohol)
+                for functional_group
+                in building_block.get_functional_groups()
+            )
+            assert building_block.get_num_functional_groups() == 2
 
-    You want to create a building block which has :class:`.Alcohol`
-    functional groups. You want the OH group to be
-    treated as a leaving group. This means the non-hydrogen atom bonded
-    to oxygen is the *bonder* atom and both the oxygen and hydrogen
-    atoms are *deleter* atoms.
+        *Changing the Bonder and Deleter Atoms*
 
-    .. testcode:: changing-the-bonder-and-deleter-atoms
+        You want to create a building block which has :class:`.Alcohol`
+        functional groups. You want the OH group to be
+        treated as a leaving group. This means the non-hydrogen atom
+        bonded to oxygen is the *bonder* atom and both the oxygen and
+        hydrogen atoms are *deleter* atoms.
 
-        import stk
+        .. testcode:: changing-the-bonder-and-deleter-atoms
 
-        alcohol_factory = stk.AlcoholFactory(
-            # The index of the non-hydrogen atom connected to oxygen
-            # is 0 in the functional group string (see docstring).
-            bonders=(0, ),
-            # The indices of the oxygen and hydrogen atoms in the
-            # functional group string (see docstring) are
-            # 1 and 2, respectively.
-            deleters=(1, 2),
-        )
-        building_block = stk.BuildingBlock(
-            smiles='OCCCO',
-            functional_groups=(alcohol_factory, ),
-        )
+            import stk
 
-    .. testcode:: changing-the-bonder-and-deleter-atoms
-        :hide:
+            alcohol_factory = stk.AlcoholFactory(
+                # The index of the non-hydrogen atom connected to
+                # oxygen is 0 in the functional group string (see
+                # docstring).
+                bonders=(0, ),
+                # The indices of the oxygen and hydrogen atoms in the
+                # functional group string (see docstring) are
+                # 1 and 2, respectively.
+                deleters=(1, 2),
+            )
+            building_block = stk.BuildingBlock(
+                smiles='OCCCO',
+                functional_groups=(alcohol_factory, ),
+            )
 
-        fg1, fg2 = building_block.get_functional_groups()
-        assert fg1.get_num_bonders() == 1
-        assert sum(1 for _ in fg1.get_deleters()) == 2
-        assert fg2.get_num_bonders() == 1
-        assert sum(1 for _ in fg2.get_deleters()) == 2
+        .. testcode:: changing-the-bonder-and-deleter-atoms
+            :hide:
 
-        assert all(
-            isinstance(atom, stk.C)
-            for functional_group
-            in building_block.get_functional_groups()
-            for atom
-            in functional_group.get_bonders()
-        )
-        assert all(
-            isinstance(atom, (stk.O, stk.H))
-            for functional_group
-            in building_block.get_functional_groups()
-            for atom
-            in functional_group.get_deleters()
-        )
+            fg1, fg2 = building_block.get_functional_groups()
+            assert fg1.get_num_bonders() == 1
+            assert sum(1 for _ in fg1.get_deleters()) == 2
+            assert fg2.get_num_bonders() == 1
+            assert sum(1 for _ in fg2.get_deleters()) == 2
 
-    See Also
-    --------
-    :class:`.GenericFunctionalGroup`
-        Defines *bonders* and  *deleters*.
+            assert all(
+                isinstance(atom, stk.C)
+                for functional_group
+                in building_block.get_functional_groups()
+                for atom
+                in functional_group.get_bonders()
+            )
+            assert all(
+                isinstance(atom, (stk.O, stk.H))
+                for functional_group
+                in building_block.get_functional_groups()
+                for atom
+                in functional_group.get_deleters()
+            )
+
+    See Also:
+
+        :class:`.GenericFunctionalGroup`
+            Defines *bonders* and  *deleters*.
 
     """
 
-    def __init__(self, bonders=(1, ), deleters=(2, ), placers=None):
+    def __init__(
+        self,
+        bonders: tuple[Literal[0, 1, 2], ...] = (1, ),
+        deleters: tuple[Literal[0, 1, 2], ...] = (2, ),
+        placers: Optional[tuple[Literal[0, 1, 2], ...]] = None,
+    ) -> None:
         """
         Initialize an :class:`.AlcoholFactory` instance.
 
-        Parameters
-        ----------
-        bonders : :class:`tuple` of :class:`int`
-            The indices of atoms in the functional group string, which
-            are *bonder* atoms.
+        Parameters:
 
-        deleters : :class:`tuple` of :class:`int`
-            The indices of atoms in the functional group string, which
-            are *deleter* atoms.
+            bonders:
+                The indices of atoms in the functional group string,
+                which are *bonder* atoms.
 
-        placers : :class:`tuple` of :class:`int`, optional
-            The indices of atoms in the functional group string, which
-            are *placer* atoms. If ``None``, `bonders` will be used.
+            deleters:
+                The indices of atoms in the functional group string,
+                which are *deleter* atoms.
+
+            placers:
+                The indices of atoms in the functional group string,
+                which are *placer* atoms. If ``None``, `bonders` will
+                be used.
 
         """
 
@@ -125,7 +135,11 @@ class AlcoholFactory(FunctionalGroupFactory):
         self._deleters = deleters
         self._placers = bonders if placers is None else placers
 
-    def get_functional_groups(self, molecule):
+    def get_functional_groups(
+        self,
+        molecule: Molecule,
+    ) -> Iterable[Molecule]:
+
         for atom_ids in _get_atom_ids('[*][O][H]', molecule):
             atoms = tuple(molecule.get_atoms(atom_ids))
             yield Alcohol(
