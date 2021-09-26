fbf6ac6c81aa7b5cd79cd8c3c350f3e4e25196f7
Lukas Turcani <lukasturcani93@gmail.com>
2021-09-26 20:56:26 +0100

    wip

--- src/stk/molecular/functional_groups/factories/smarts_functional_group_factory.py
+++ src/stk/molecular/functional_groups/factories/smarts_functional_group_factory.py
@@ -4,90 +4,100 @@ SMARTS Functional Group Factory
 
 """
 
+from typing import Optional, Iterable
+
 from .functional_group_factory import FunctionalGroupFactory
 from .utilities import _get_atom_ids
 from ..functional_groups import GenericFunctionalGroup
+from ...molecule import Molecule
 
 
 class SmartsFunctionalGroupFactory(FunctionalGroupFactory):
     """
     Creates :class:`.GenericFunctionalGroup` instances.
 
-    Examples
-    --------
-    *Using SMARTS to Define Functional Groups*
+    Examples:
 
-    You want to create a building block which has
-    :class:`.GenericFunctionalGroup` functional groups based on the
-    SMARTS string: ``[Br][C]``.
-    You want the ``C`` atom to be the *bonder* atom, and the
-    ``Br`` atom to be the *deleter* atom.
+        *Using SMARTS to Define Functional Groups*
 
-    .. testcode:: using-smarts-to-define-functional-groups
+        You want to create a building block which has
+        :class:`.GenericFunctionalGroup` functional groups based on the
+        SMARTS string: ``[Br][C]``.
+        You want the ``C`` atom to be the *bonder* atom, and the
+        ``Br`` atom to be the *deleter* atom.
 
-        import stk
+        .. testcode:: using-smarts-to-define-functional-groups
 
-        building_block = stk.BuildingBlock(
-            smiles='BrCCCBr',
-            functional_groups=(
-                stk.SmartsFunctionalGroupFactory(
-                    smarts='[Br][C]',
-                    bonders=(1, ),
-                    deleters=(0, ),
+            import stk
+
+            building_block = stk.BuildingBlock(
+                smiles='BrCCCBr',
+                functional_groups=(
+                    stk.SmartsFunctionalGroupFactory(
+                        smarts='[Br][C]',
+                        bonders=(1, ),
+                        deleters=(0, ),
+                    ),
                 ),
-            ),
-        )
+            )
 
-    .. testcode:: using-smarts-to-define-functional-groups
-        :hide:
+        .. testcode:: using-smarts-to-define-functional-groups
+            :hide:
 
-        fg1, fg2 = building_block.get_functional_groups()
-        assert fg1.get_num_bonders() == 1
-        assert sum(1 for _ in fg1.get_deleters()) == 1
-        assert fg2.get_num_bonders() == 1
-        assert sum(1 for _ in fg2.get_deleters()) == 1
+            fg1, fg2 = building_block.get_functional_groups()
+            assert fg1.get_num_bonders() == 1
+            assert sum(1 for _ in fg1.get_deleters()) == 1
+            assert fg2.get_num_bonders() == 1
+            assert sum(1 for _ in fg2.get_deleters()) == 1
 
-        assert all(
-            isinstance(atom, stk.C)
-            for functional_group
-            in building_block.get_functional_groups()
-            for atom
-            in functional_group.get_bonders()
-        )
-        assert all(
-            isinstance(atom, stk.Br)
-            for functional_group
-            in building_block.get_functional_groups()
-            for atom
-            in functional_group.get_deleters()
-        )
+            assert all(
+                isinstance(atom, stk.C)
+                for functional_group
+                in building_block.get_functional_groups()
+                for atom
+                in functional_group.get_bonders()
+            )
+            assert all(
+                isinstance(atom, stk.Br)
+                for functional_group
+                in building_block.get_functional_groups()
+                for atom
+                in functional_group.get_deleters()
+            )
 
-    See Also
-    --------
-    :class:`.GenericFunctionalGroup`
-        Defines *bonders* and  *deleters*.
+    See Also:
+
+        :class:`.GenericFunctionalGroup`
+            Defines *bonders* and  *deleters*.
 
     """
 
-    def __init__(self, smarts, bonders, deleters, placers=None):
+    def __init__(
+        self,
+        smarts: str,
+        bonders: tuple[int, ...],
+        deleters: tuple[int, ...],
+        placers: Optional[tuple[int, ...]] = None,
+    ) -> None:
         """
         Initialize a :class:`.SmartsFunctionalGroupFactory` instance.
 
-        Parameters
-        ----------
-        smarts : :class:`str`
-            The SMARTS defining the functional group.
+        Parameters:
 
-        bonders : :class:`tuple` of :class:`int`
-            The indices of atoms in `smarts`, which are *bonder* atoms.
+            smarts:
+                The SMARTS defining the functional group.
 
-        deleters : :class:`tuple` of :class:`int`
-            The indices of atoms in `smarts`, which are *deleter*
-            atoms.
+            bonders:
+                The indices of atoms in `smarts`, which are *bonder*
+                atoms.
 
-        placers : :class:`tuple` of :class:`int`, optional
-            The indices of atoms in `smarts`, which are *placer* atoms.
-            If ``None``, the *bonder* atoms will be used.
+            deleters:
+                The indices of atoms in `smarts`, which are *deleter*
+                atoms.
+
+            placers:
+                The indices of atoms in `smarts`, which are *placer*
+                atoms. If ``None``, the *bonder* atoms will be used.
 
         """
 
@@ -96,7 +106,11 @@ class SmartsFunctionalGroupFactory(FunctionalGroupFactory):
         self._deleters = deleters
         self._placers = bonders if placers is None else placers
 
-    def get_functional_groups(self, molecule):
+    def get_functional_groups(
+        self,
+        molecule: Molecule,
+    ) -> Iterable[GenericFunctionalGroup]:
+
         for atom_ids in _get_atom_ids(self._smarts, molecule):
             atoms = tuple(molecule.get_atoms(atom_ids))
             yield GenericFunctionalGroup(
