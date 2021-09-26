5de86a42fc64ae1d2cc26d893d9420fd270df655
Lukas Turcani <lukasturcani93@gmail.com>
2021-09-26 20:00:06 +0100

    wip

--- src/stk/molecular/functional_groups/factories/diol_factory.py
+++ src/stk/molecular/functional_groups/factories/diol_factory.py
@@ -4,9 +4,16 @@ Diol Factory
 
 """
 
+from typing import Optional, Iterable, Literal
+
 from .functional_group_factory import FunctionalGroupFactory
 from .utilities import _get_atom_ids
 from ..functional_groups import Diol
+
+from ...molecule import Molecule
+
+
+ValidIndices = tuple[Literal[0, 1, 2, 3, 4, 5], ...]
 
 
 class DiolFactory(FunctionalGroupFactory):
@@ -16,108 +23,109 @@ class DiolFactory(FunctionalGroupFactory):
     Creates functional groups from substructures, which match the
     ``[H][O][#6]~[#6][O][H]`` functional group string.
 
-    Examples
-    --------
-    *Creating Functional Groups with the Factory*
+    Examples:
 
-    You want to create a building block which has :class:`.Diol`
-    functional groups. You want the carbon atoms in those functional
-    groups to be the *bonder* atoms, and the OH groups to be a leaving
-    groups.
+        *Creating Functional Groups with the Factory*
 
-    .. testcode:: creating-functional-groups-with-the-factory
+        You want to create a building block which has :class:`.Diol`
+        functional groups. You want the carbon atoms in those
+        functional groups to be the *bonder* atoms, and the OH groups
+        to be a leaving groups.
 
-        import stk
+        .. testcode:: creating-functional-groups-with-the-factory
 
-        building_block = stk.BuildingBlock(
-            smiles='CCCC(O)C(O)CCCC',
-            functional_groups=(stk.DiolFactory(), ),
-        )
+            import stk
 
-    .. testcode:: creating-functional-groups-with-the-factory
-        :hide:
+            building_block = stk.BuildingBlock(
+                smiles='CCCC(O)C(O)CCCC',
+                functional_groups=(stk.DiolFactory(), ),
+            )
 
-        assert all(
-            isinstance(functional_group, stk.Diol)
-            for functional_group
-            in building_block.get_functional_groups()
-        )
-        assert building_block.get_num_functional_groups() == 1
+        .. testcode:: creating-functional-groups-with-the-factory
+            :hide:
 
-    *Changing the Bonder and Deleter Atoms*
+            assert all(
+                isinstance(functional_group, stk.Diol)
+                for functional_group
+                in building_block.get_functional_groups()
+            )
+            assert building_block.get_num_functional_groups() == 1
 
-    You want to create a building block which has :class:`.Diol`
-    functional groups. You want the oxygen atoms to be the *bonder*
-    atoms and the hydrogen atoms to be the *deleter* atoms.
+        *Changing the Bonder and Deleter Atoms*
 
-    .. testcode:: changing-the-bonder-and-deleter-atoms
+        You want to create a building block which has :class:`.Diol`
+        functional groups. You want the oxygen atoms to be the *bonder*
+        atoms and the hydrogen atoms to be the *deleter* atoms.
 
-        import stk
+        .. testcode:: changing-the-bonder-and-deleter-atoms
 
-        diol_factory = stk.DiolFactory(
-            # The indices of the oxygen atoms in the functional
-            # group string (see docstring) are 1 and 4.
-            bonders=(1, 4),
-            # The indices of the hydrogen atoms in the functional
-            # group string (see docstring) are 0 and 5.
-            deleters=(0, 5),
-        )
-        building_block = stk.BuildingBlock(
-            smiles='CCCC(O)C(O)CCCC',
-            functional_groups=(diol_factory, ),
-        )
+            import stk
 
-    .. testcode:: changing-the-bonder-and-deleter-atoms
-        :hide:
+            diol_factory = stk.DiolFactory(
+                # The indices of the oxygen atoms in the functional
+                # group string (see docstring) are 1 and 4.
+                bonders=(1, 4),
+                # The indices of the hydrogen atoms in the functional
+                # group string (see docstring) are 0 and 5.
+                deleters=(0, 5),
+            )
+            building_block = stk.BuildingBlock(
+                smiles='CCCC(O)C(O)CCCC',
+                functional_groups=(diol_factory, ),
+            )
 
-        fg, = building_block.get_functional_groups()
-        assert fg.get_num_bonders() == 2
-        assert sum(1 for _ in fg.get_deleters()) == 2
+        .. testcode:: changing-the-bonder-and-deleter-atoms
+            :hide:
 
-        assert all(
-            isinstance(atom, stk.O)
-            for functional_group
-            in building_block.get_functional_groups()
-            for atom
-            in functional_group.get_bonders()
-        )
-        assert all(
-            isinstance(atom, stk.H)
-            for functional_group
-            in building_block.get_functional_groups()
-            for atom
-            in functional_group.get_deleters()
-        )
+            fg, = building_block.get_functional_groups()
+            assert fg.get_num_bonders() == 2
+            assert sum(1 for _ in fg.get_deleters()) == 2
 
-    See Also
-    --------
-    :class:`.GenericFunctionalGroup`
-        Defines *bonders* and  *deleters*.
+            assert all(
+                isinstance(atom, stk.O)
+                for functional_group
+                in building_block.get_functional_groups()
+                for atom
+                in functional_group.get_bonders()
+            )
+            assert all(
+                isinstance(atom, stk.H)
+                for functional_group
+                in building_block.get_functional_groups()
+                for atom
+                in functional_group.get_deleters()
+            )
+
+    See Also:
+
+        :class:`.GenericFunctionalGroup`
+            Defines *bonders* and  *deleters*.
 
     """
 
     def __init__(
         self,
-        bonders=(2, 3),
-        deleters=(0, 1, 4, 5),
-        placers=None,
-    ):
+        bonders: ValidIndices = (2, 3),
+        deleters: ValidIndices = (0, 1, 4, 5),
+        placers: Optional[ValidIndices] = None,
+    ) -> None:
         """
         Initialize a :class:`.DiolFactory` instance.
 
-        Parameters
-        ----------
-        bonders : :class:`tuple` of :class:`int`
-            The indices of atoms in the functional group string, which
-            are *bonder* atoms.
+        Parameters:
 
-        deleters : :class:`tuple` of :class:`int`
-            The indices of atoms in the functional group string, which
-            are *deleter* atoms.
+            bonders:
+                The indices of atoms in the functional group string,
+                which are *bonder* atoms.
 
-        placers : :class:`tuple` of :class:`int`, optional
-            The indices of atoms in the functional group string, which
-            are *placer* atoms. If ``None``, `bonders` will be used.
+            deleters:
+                The indices of atoms in the functional group string,
+                which are *deleter* atoms.
+
+            placers:
+                The indices of atoms in the functional group string,
+                which are *placer* atoms. If ``None``, `bonders` will
+                be used.
 
         """
 
@@ -125,7 +133,11 @@ class DiolFactory(FunctionalGroupFactory):
         self._deleters = deleters
         self._placers = bonders if placers is None else placers
 
-    def get_functional_groups(self, molecule):
+    def get_functional_groups(
+        self,
+        molecule: Molecule,
+    ) -> Iterable[Diol]:
+
         ids = _get_atom_ids('[H][O][#6]~[#6][O][H]', molecule)
         for atom_ids in ids:
             atoms = tuple(molecule.get_atoms(atom_ids))
