029dd0171c634ec9d3272a8a084a8976c6013b9c
Lukas Turcani <lukasturcani93@gmail.com>
2021-10-10 13:05:45 +0100

    wip

--- src/stk/molecular/reactions/reactions/reaction/new_atom.py
+++ src/stk/molecular/reactions/reactions/reaction/new_atom.py
@@ -4,54 +4,64 @@ New Atom
 
 """
 
+from __future__ import annotations
+
 import numpy as np
+
+from ....atoms import Atom
 
 
 class NewAtom:
     """
     Represents a new atom added by a :class:`.Reaction`.
 
-    Examples
-    --------
-    *Unpacking the Atom and Position*
+    Examples:
 
-    The class can be elegantly unpacked into the atom and its position.
+        *Unpacking the Atom and Position*
 
-    .. testcode:: unpacking-the-atom-and-position
+        The class can be elegantly unpacked into the atom and its
+        position.
 
-        import stk
-        import numpy as np
+        .. testcode:: unpacking-the-atom-and-position
 
-        new_atoms = (
-            stk.NewAtom(stk.C(-1), np.array([1., 2., 3.])),
-            stk.NewAtom(stk.H(-2), np.array([2., 5., 7.])),
-            stk.NewAtom(stk.F(-3), np.array([8., 2., 3.])),
-        )
-        for atom, position in new_atoms:
-            # Do stuff.
-            print(atom, position)
+            import stk
+            import numpy as np
 
-    .. testoutput:: unpacking-the-atom-and-position
+            new_atoms = (
+                stk.NewAtom(stk.C(-1), np.array([1., 2., 3.])),
+                stk.NewAtom(stk.H(-2), np.array([2., 5., 7.])),
+                stk.NewAtom(stk.F(-3), np.array([8., 2., 3.])),
+            )
+            for atom, position in new_atoms:
+                # Do stuff.
+                print(atom, position)
 
-        C(-1) [1. 2. 3.]
-        H(-2) [2. 5. 7.]
-        F(-3) [8. 2. 3.]
+        .. testoutput:: unpacking-the-atom-and-position
+
+            C(-1) [1. 2. 3.]
+            H(-2) [2. 5. 7.]
+            F(-3) [8. 2. 3.]
 
     """
 
     __slots__ = ['_atom', '_position']
 
-    def __init__(self, atom, position):
+    def __init__(
+        self,
+        atom: Atom,
+        position: np.ndarray,
+    ) -> None:
         """
         Initialize a :class:`.NewAtom` instance.
 
-        Parameters
-        ----------
-        atom : :class:`.Atom`
-            The atom added by the reaction. Must have a negative id.
+        Parameters:
 
-        position : :class:`numpy.ndarray`
-            The position of the new atom.
+            atom:
+                The atom added by the reaction. Must have a negative
+                id.
+
+            position:
+                The position of the new atom.
 
         """
 
@@ -59,40 +69,29 @@ class NewAtom:
         self._position = np.array(position, dtype=np.float64)
         self._position.setflags(write=False)
 
-    def get_atom(self):
+    def get_atom(self) -> Atom:
         """
         Get the new atom.
 
         The new atom will have a negative id, and should be assigned
         a new one by the construction process.
 
-        Returns
-        -------
-        :class:`.Atom`
+        Returns:
+
             The new atom.
 
         """
 
         return self._atom
 
-    def get_position(self):
+    def get_position(self) -> np.ndarray:
         """
         Get the position of the new atom.
 
-        Returns
-        -------
-        :class:`numpy.ndarray`
+        Returns:
+
             The position of the new atom.
 
         """
 
         return self._position
-
-    def __iter__(self):
-        """
-        Iterate over the atom and position.
-
-        """
-
-        yield self._atom
-        yield self._position
