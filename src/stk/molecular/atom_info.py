759bc522204bd1bec6a81bd52e267db8fd5bcafc
Lukas Turcani <lukasturcani93@gmail.com>
2021-10-11 22:29:53 +0100

    Add type hints

--- src/stk/molecular/atom_info.py
+++ src/stk/molecular/atom_info.py
@@ -4,6 +4,16 @@ Atom Info
 
 """
 
+import typing
+
+from .atom import Atom
+from .molecule import Molecule
+
+
+__all__ = (
+    'AtomInfo',
+)
+
 
 class AtomInfo:
     """
@@ -13,37 +23,40 @@ class AtomInfo:
 
     def __init__(
         self,
-        atom,
-        building_block_atom,
-        building_block,
-        building_block_id,
-    ):
+        atom: Atom,
+        building_block_atom: Atom,
+        building_block: typing.Optional[Molecule],
+        building_block_id: typing.Optional[int],
+    ) -> None:
         """
         Initialize an :class:`.AtomInfo` instance.
 
-        Parameters
-        ----------
-        atom : :class:`.Atom`
-            The atom about which information is held.
+        Parameters:
 
-        building_block_atom : :class:`.Atom`
-            The building block atom from which this atom originates.
-            Can be ``None``, if the atom was not part of the building
-            block, but was added by the construction process instead.
+            atom:
+                The atom about which information is held.
 
-        building_block : :class:`.Molecule` or :class:`NoneType`
-            The building block from which the atom originates.
-            Can be ``None``, if the atom was not part of a building
-            block, but was added by the construction process instead.
+            building_block_atom:
+                The building block atom from which this atom
+                originates. Can be ``None``, if the atom was not part
+                of the building block, but was added by the
+                construction process instead.
 
-        building_block_id : :class:`int` or :class:`NoneType`
-            A unique id for each :class:`.Molecule` placed during
-            the construction of the :class:`.ConstructedMolecule`. As a
-            single :class:`.Molecule` can be placed multiple times
-            during construction, the `building_block_id` allows
-            the user to distinguish between each placement. Can be
-            ``None``, if the atom was not part of a building block, but
-            was added by the construction process instead.
+            building_block:
+                The building block from which the atom originates.
+                Can be ``None``, if the atom was not part of a building
+                block, but was added by the construction process
+                instead.
+
+            building_block_id:
+                A unique id for each :class:`.Molecule` placed during
+                the construction of the :class:`.ConstructedMolecule`.
+                As a single :class:`.Molecule` can be placed multiple
+                times during construction, the `building_block_id`
+                allows the user to distinguish between each placement.
+                Can be ``None``, if the atom was not part of a building
+                block, but was added by the construction process
+                instead.
 
         """
 
@@ -52,54 +65,47 @@ class AtomInfo:
         self._building_block = building_block
         self._building_block_id = building_block_id
 
-    def get_atom(self):
+    def get_atom(self) -> Atom:
         """
         Get the atom about which information is held.
 
-        Returns
-        -------
-        :class:`.Atom`
+        Returns:
+
             The atom.
 
         """
 
         return self._atom
 
-    def get_building_block_atom(self):
+    def get_building_block_atom(self) -> typing.Optional[Atom]:
         """
         Get the original atom held by the building block.
 
-        Returns
-        -------
-        :class:`.Atom`
-            The building block atom.
+        Returns:
 
-        None : :class:`NoneType`
-            If the atom was not originally found in a building block,
-            but was added by the construction process instead.
+            The building block atom or ``None`` if the atom was not
+            originally found in a building block, but was added by the
+            construction process instead.
 
         """
 
         return self._building_block_atom
 
-    def get_building_block(self):
+    def get_building_block(self) -> typing.Optional[Molecule]:
         """
         Get the building block from which the atom originates.
 
-        Returns
-        -------
-        :class:`.Molecule`
-            The building block.
+        Returns:
 
-        None : :class:`NoneType`
-            If the atom was not originally found in a building block,
-            but was added by the construction process instead.
+            The building block or ``None`` if the atom was not
+            originally found in a building block, but was added by the
+            construction process instead.
 
         """
 
         return self._building_block
 
-    def get_building_block_id(self):
+    def get_building_block_id(self) -> typing.Optional[int]:
         """
         Get the id of the atom's building block.
 
@@ -109,14 +115,11 @@ class AtomInfo:
         during construction, the building block id allows
         the user to distinguish between each placement.
 
-        Returns
-        -------
-        :class:`int`
-            The id.
+        Returns:
 
-        None : :class:`NoneType`
-            If the atom was not originally found in a building block,
-            but was added by the construction process instead.
+            The id or ``None`` if the atom was not originally found in
+            a building block, but was added by the construction process
+            instead.
 
         """
 
