edf92dd637f0c8c9b1cc4a632f908b99e41e40fb
Lukas Turcani <lukasturcani93@gmail.com>
2021-10-11 22:40:12 +0100

    wip

--- src/stk/molecular/molecular_utilities.py
+++ src/stk/molecular/molecular_utilities.py
@@ -6,8 +6,13 @@ This module defines utilities for molecules.
 
 """
 
+import typing
 
-def sort_bond_atoms_by_id(bond):
+from .bond import Bond
+from .bond_info import BondInfo
+
+
+def sort_bond_atoms_by_id(bond: Bond) -> Bond:
     if bond.get_atom1().get_id() < bond.get_atom2().get_id():
         return bond
     elif bond.get_order() == 9:
@@ -22,12 +27,14 @@ def sort_bond_atoms_by_id(bond):
         )
 
 
-def get_bond_atom_ids(bond):
+def get_bond_atom_ids(bond: Bond) -> typing.Iterable[int]:
     return sorted((
         bond.get_atom1().get_id(),
         bond.get_atom2().get_id(),
     ))
 
 
-def get_bond_info_atom_ids(bond_info):
+def get_bond_info_atom_ids(
+    bond_info: BondInfo,
+) -> typing.Iterable[int]:
     return get_bond_atom_ids(bond_info.get_bond())
