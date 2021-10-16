1f4edee3a5af1a86446272bb7f2a9c383251692d
Lukas Turcani <lukasturcani93@gmail.com>
2021-10-16 17:26:53 +0100

    wip

--- src/stk/molecular/topology_graphs/topology_graph/edge_group.py
+++ src/stk/molecular/topology_graphs/topology_graph/edge_group.py
@@ -5,6 +5,14 @@ Edge Group
 """
 
 
+from collections import abc
+from .edge import Edge
+
+__all__ = (
+    'EdgeGroup',
+)
+
+
 class EdgeGroup:
     """
     Represents a group of edges.
@@ -14,34 +22,36 @@ class EdgeGroup:
 
     """
 
-    def __init__(self, edges):
+    def __init__(
+        self,
+        edges: abc.Iterable[Edge],
+    ) -> None:
         """
         Initialize an :class:`.EdgeGroup` instance.
 
-        Parameters
-        ----------
-        edges : :class:`iterable` of :class:`.Edge`
-            The edges in the group.
+        Parameters:
+
+            edges:
+                The edges in the group.
 
         """
 
         self._edge_ids = tuple(edge.get_id() for edge in edges)
 
-    def get_edge_ids(self):
+    def get_edge_ids(self) -> abc.Iterator[int]:
         """
         Yield the ids of edges in the group.
 
-        Yields
-        ------
-        :class:`int`
+        Yields:
+
             The id of an edge.
 
         """
 
         yield from self._edge_ids
 
-    def __str__(self):
+    def __str__(self) -> str:
         return repr(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.__class__.__name__}(edge_ids={self._edge_ids})'
