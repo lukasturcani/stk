d57702804e103578c5d794c8c5441e80eb62937e
Lukas Turcani <lukasturcani93@gmail.com>
2021-10-22 10:01:13 +0100

    wip

--- src/stk/molecular/placement_result.py
+++ src/stk/molecular/placement_result.py
@@ -4,7 +4,6 @@ Placement Result
 
 """
 
-from typing import NamedTuple
 import numpy as np
 
 
@@ -13,21 +12,35 @@ __all__ = (
 )
 
 
-class PlacementResult(NamedTuple):
+class PlacementResult:
     """
     The result of a building block placement.
 
-    Attributes:
-
-        position_matrix:
-            The position matrix of the building block after it has been
-            placed on the vertex.
-
-        functional_group_edges:
-            Maps the id of a functional group to the id of the edge it
-            is assigned to.
-
     """
 
-    position_matrix: np.ndarray
-    functional_group_edges: dict[int, int]
+    __slots__ = (
+        'position_matrix',
+        'functional_group_edges',
+    )
+
+    def __init__(
+        self,
+        position_matrix: np.ndarray,
+        functional_group_edges: dict[int, int],
+    ) -> None:
+        """
+        Initialize a :class:`.PlacementResult`.
+
+        Parameters:
+
+            position_matrix:
+                The position matrix of the building block after it has
+                been placed on a vertex.
+
+            functional_group_edges:
+                Maps the id of a functional group to the id of the edge
+                it is assigned to.
+        """
+
+        self.position_matrix = position_matrix
+        self.functional_group_edges = functional_group_edges
