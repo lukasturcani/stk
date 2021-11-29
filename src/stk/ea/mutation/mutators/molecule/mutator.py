a4be4fbb67e41661c58495c8bb627e4bf2d43c18
stevenbennett96 <stevenbennettonline@gmail.com>
2021-11-29 18:00:26 +0000

    Fixed edited file

--- src/stk/ea/mutation/mutators/molecule/mutator.py
+++ src/stk/ea/mutation/mutators/molecule/mutator.py
@@ -14,7 +14,7 @@ stk.ea.mutation.mutators.molecule.random_topology_graph\
     Similar Building Block <\
 stk.ea.mutation.mutators.molecule.similar_building_block\
 >
-....Substitute Substructure <\
+    Substitute Substructure <\
 stk.ea.mutation.mutators.molecule.substitute_substructure\
 >
 
