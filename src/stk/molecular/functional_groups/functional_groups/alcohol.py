099f3b4aef85c6ac2bf08ad91b7f67a027698bb0
Lukas Turcani <lukasturcani@mailbox.org>
2022-04-30 18:12:12 +0100

    Add type hint

--- src/stk/molecular/functional_groups/functional_groups/alcohol.py
+++ src/stk/molecular/functional_groups/functional_groups/alcohol.py
@@ -29,7 +29,7 @@ class Alcohol(GenericFunctionalGroup):
         bonders: tuple[Atom, ...],
         deleters: tuple[Atom, ...],
         placers: typing.Optional[tuple[Atom, ...]] = None,
-    ):
+    ) -> None:
         """
         Initialize a :class:`.Alcohol` instance.
 
