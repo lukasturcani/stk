bd0ed36ab5b30f4dd1cd2dacd6653ec443666502
lukasturcani <lukasturcani@users.noreply.github.com>
2022-08-26 15:27:46 +0000

    Format code

--- src/stk/__init__.py
+++ src/stk/__init__.py
@@ -1,6 +1,6 @@
+from ._version import __version__  # noqa
 from .databases import *  # noqa
 from .ea import *  # noqa
 from .molecular import *  # noqa
 from .serialization import *  # noqa
 from .utilities import *  # noqa
-from ._version import __version__  # noqa
