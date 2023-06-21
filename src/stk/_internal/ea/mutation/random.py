33ae366e4d95be1f89976035ad12138b1738a976
Lukas Turcani <lukasturcani93@gmail.com>
2023-06-21 20:30:21 +0100

    wip

--- src/stk/_internal/ea/mutation/random.py
+++ src/stk/_internal/ea/mutation/random.py
@@ -1,157 +1,153 @@
-"""
-Random Mutator
-==============
-
-"""
-
+import typing
+from collections.abc import Iterable
 
 import numpy as np
+
+from stk._internal.ea.mutation.mutator import MoleculeMutator
+from stk._internal.ea.mutation.record import MutationRecord
+
+T = typing.TypeVar("T")
 
 
 class RandomMutator:
     """
     Use some other mutator at random.
 
-    Examples
-    --------
-    *Use One of Several Mutators at Random*
+    Examples:
+        *Use One of Several Mutators at Random*
 
-    .. testcode:: use-one-of-several-mutators-at-random
+        .. testcode:: use-one-of-several-mutators-at-random
 
-        import stk
+            import stk
 
-        random_seed = 12
-        mutator = stk.RandomMutator(
-            mutators=(
-                stk.RandomBuildingBlock(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCBr',
-                            functional_groups=[stk.BromoFactory()],
+            random_seed = 12
+            mutator = stk.RandomMutator(
+                mutators=(
+                    stk.RandomBuildingBlock(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
+                            stk.BuildingBlock(
+                                smiles='BrCCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
-                        stk.BuildingBlock(
-                            smiles='BrCCCBr',
-                            functional_groups=[stk.BromoFactory()],
-                        ),
+                        is_replaceable=lambda building_block: True,
+                        random_seed=random_seed,
                     ),
-                    is_replaceable=lambda building_block: True,
-                    random_seed=random_seed,
-                ),
-                stk.SimilarBuildingBlock(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCCCBr',
-                            functional_groups=[stk.BromoFactory()],
+                    stk.SimilarBuildingBlock(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
+                            stk.BuildingBlock(
+                                smiles='BrCCCCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
-                        stk.BuildingBlock(
-                            smiles='BrCCCCCBr',
-                            functional_groups=[stk.BromoFactory()],
-                        ),
+                        is_replaceable=lambda building_block: True,
+                        random_seed=random_seed,
                     ),
-                    is_replaceable=lambda building_block: True,
-                    random_seed=random_seed,
-                ),
-                stk.RandomBuildingBlock(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCNCBr',
-                            functional_groups=[stk.BromoFactory()],
+                    stk.RandomBuildingBlock(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCNCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
+                            stk.BuildingBlock(
+                                smiles='BrCNCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
-                        stk.BuildingBlock(
-                            smiles='BrCNCBr',
-                            functional_groups=[stk.BromoFactory()],
-                        ),
+                        is_replaceable=lambda building_block: True,
+                        random_seed=random_seed,
                     ),
-                    is_replaceable=lambda building_block: True,
-                    random_seed=random_seed,
                 ),
-            ),
-        )
-        building_block = stk.BuildingBlock(
-            smiles='BrCNNCBr',
-            functional_groups=[stk.BromoFactory()],
-        )
-        record = stk.MoleculeRecord(
-            topology_graph=stk.polymer.Linear(
-                building_blocks=(building_block, ),
-                repeating_unit='A',
-                num_repeating_units=2,
-            ),
-        )
-        # Use one of the component mutators at random.
-        mutation_record1 = mutator.mutate(record)
-        # A different mutator may get selected at random the second,
-        # third, etc, time.
-        mutation_record2 = mutator.mutate(record)
+            )
+            building_block = stk.BuildingBlock(
+                smiles='BrCNNCBr',
+                functional_groups=[stk.BromoFactory()],
+            )
+            record = stk.MoleculeRecord(
+                topology_graph=stk.polymer.Linear(
+                    building_blocks=(building_block, ),
+                    repeating_unit='A',
+                    num_repeating_units=2,
+                ),
+            )
+            # Use one of the component mutators at random.
+            mutation_record1 = mutator.mutate(record)
+            # A different mutator may get selected at random the second,
+            # third, etc, time.
+            mutation_record2 = mutator.mutate(record)
 
-    .. testcode:: use-one-of-several-mutators-at-random
-        :hide:
+        .. testcode:: use-one-of-several-mutators-at-random
+            :hide:
 
-        _smiles = stk.Smiles()
-        assert _smiles.get_key(record.get_molecule()) == 'BrCNNCCNNCBr'
+            _smiles = stk.Smiles()
+            assert _smiles.get_key(record.get_molecule()) == 'BrCNNCCNNCBr'
 
-        _molecule1 = (
-            mutation_record1.get_molecule_record().get_molecule()
-        )
-        assert _smiles.get_key(_molecule1) != 'BrCNNCNNCBr'
+            _molecule1 = (
+                mutation_record1.get_molecule_record().get_molecule()
+            )
+            assert _smiles.get_key(_molecule1) != 'BrCNNCNNCBr'
 
-        _molecule2 = (
-            mutation_record2.get_molecule_record().get_molecule()
-        )
-        assert _smiles.get_key(_molecule2) != 'BrCNNCNNCBr'
-
-
+            _molecule2 = (
+                mutation_record2.get_molecule_record().get_molecule()
+            )
+            assert _smiles.get_key(_molecule2) != 'BrCNNCNNCBr'
     """
 
-    def __init__(self, mutators, weights=None, random_seed=None):
+    def __init__(
+        self,
+        mutators: Iterable[MoleculeMutator[T]],
+        weights: Iterable[float] | None = None,
+        random_seed: int | np.random.Generator | None = None,
+    ) -> None:
         """
-        Initialize a :class:`.RandomMutator` instance.
+        Parameters:
+            mutators (list[MoleculeMutator[T]]):
+                A selection of mutators, each time :meth:`.mutate`
+                is called, one will be selected at random to perform
+                the mutation.
 
-        Parameters
-        ----------
-        mutators : :class:`tuple`
-            Holds instances which have :meth:`.mutate` method. The
-            :meth:`.mutate` method must return an instance of
-            :class:`.MutationRecord`.
+            weights (list[float] | None):
+                For each mutator, the probability that it will be chosen
+                whenever :meth:`.mutate` is called.
+                If ``None`` all `mutators` will have equal chance of being
+                selected.
 
-        weights : :class:`tuple` of :class:`float`, optional
-            For each mutator, the probability that it will be chosen
-            whenever :meth:`.mutate` is called.
-            If ``None`` all `mutators` will have equal chance of being
-            selected.
-
-        random_seed : :class:`int`, optional
-            The random seed to use.
-
+            random_seed:
+                The random seed to use.
         """
+        if weights is not None:
+            weights = tuple(weights)
 
-        self._mutators = mutators
+        if random_seed is None or isinstance(random_seed, int):
+            random_seed = np.random.default_rng(random_seed)
+
+        self._mutators = tuple(mutators)
         self._weights = weights
-        self._generator = np.random.RandomState(random_seed)
+        self._generator = random_seed
 
-    def mutate(self, record):
+    def mutate(self, record: T) -> MutationRecord[T] | None:
         """
         Return a mutant of `record`.
 
-        Parameters
-        ----------
-        record : :class:`.MoleculeRecord`
-            The molecule to be mutated.
+        Parameters:
+            record:
+                The molecule to be mutated.
 
-        Returns
-        -------
-        :class:`.MutationRecord`
-            A record of the mutation. The exact subclass of
-            :class:`.MutationRecord` depends on which mutator was
-            used.
-
-        None : :class:`NoneType`
-            If `record` cannot be mutated.
-
+        Returns:
+            A record of the mutation or ``None``
+            if `record` cannot be mutated.
         """
 
         mutator = self._generator.choice(
-            a=self._mutators,
+            a=self._mutators,  # type: ignore
             p=self._weights,
         )
         return mutator.mutate(record)
--- src/stk/_internal/ea/mutation/record.py
+++ src/stk/_internal/ea/mutation/record.py
@@ -1,60 +1,51 @@
-"""
-Molecule Mutation Record
-========================
+import typing
 
-"""
+T = typing.TypeVar("T")
 
 
-class MutationRecord:
+class MutationRecord(typing.Generic[T]):
     """
     Abstract base class for a record of a mutation operation.
 
-    Notes
-    -----
-    You might notice that the public methods of this abstract base
-    class are implemented. This is just a default implementation, which
-    can be used directly by users and subclasses, but can also be
-    freely replaced during subclass implementation, if need be.
+    Notes:
+
+        You might notice that the public methods of this abstract base
+        class are implemented. This is just a default implementation, which
+        can be used directly by users and subclasses, but can also be
+        freely replaced during subclass implementation, if need be.
 
     """
 
-    def __init__(self, molecule_record, mutator_name):
+    def __init__(self, molecule_record: T, mutator_name: str) -> None:
         """
-        Initialize a :class:`.MutationRecord` instance.
+        Parameters:
+            molecule_record:
+                The molecule produced by the mutation operation.
 
-        Parameters
-        ----------
-        molecule_record : :class:`.MoleculeRecord`
-            The molecule produced by the mutation operation.
-
-        mutator_name : :class:`str`
-            The name of the mutator which carried out the mutator.
+            mutator_name:
+                The name of the mutator which carried out the mutator.
 
         """
 
         self._molecule_record = molecule_record
         self._mutator_name = mutator_name
 
-    def get_molecule_record(self):
+    def get_molecule_record(self) -> T:
         """
-        Get the :class:`.MoleculeRecord` produced by the mutation.
+        Get the molecule record produced by the mutation.
 
-        Returns
-        -------
-        :class:`.MoleculeRecord`
+        Returns:
             The molecule record.
 
         """
 
         return self._molecule_record
 
-    def get_mutator_name(self):
+    def get_mutator_name(self) -> str:
         """
         Get the name of the mutator which carried out the mutation.
 
-        Returns
-        -------
-        :class:`str`
+        Returns:
             The name of the mutator.
 
         """
