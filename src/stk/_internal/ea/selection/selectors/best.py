96096f4c5f49362f7caa1295dd781b41e7ad5d92
Lukas Turcani <lukasturcani93@gmail.com>
2023-06-26 12:43:00 +0100

    Fix docs

--- src/stk/_internal/ea/selection/selectors/best.py
+++ src/stk/_internal/ea/selection/selectors/best.py
@@ -1,167 +1,158 @@
-"""
-Best
-====
-
-"""
-
-import itertools as it
+import itertools
+import typing
+from collections.abc import Callable, Sequence
 
 from stk._internal.key_makers.inchi import Inchi
+from stk._internal.key_makers.molecule import MoleculeKeyMaker
 
 from .selector import Selector
 
+T = typing.TypeVar("T")
 
-class Best(Selector):
+
+class Best(Selector[T]):
     """
     Selects batches of molecules, highest fitness value first.
 
-    Examples
-    --------
-    *Yielding Single Molecule Batches*
+    Examples:
+        *Yielding Single Molecule Batches*
 
-    Yielding molecules one at a time. For example, if molecules need
-    to be selected for mutation or the next generation.
+        Yielding molecules one at a time. For example, if molecules need
+        to be selected for mutation or the next generation.
 
-    .. testcode:: yielding-single-molecule-batches
+        .. testcode:: yielding-single-molecule-batches
 
-        import stk
+            import stk
 
-        # Make the selector.
-        best = stk.Best()
+            # Make the selector.
+            best = stk.Best()
 
-        population = (
-            stk.MoleculeRecord(
-                topology_graph=stk.polymer.Linear(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCBr',
-                            functional_groups=[stk.BromoFactory()],
+            population = (
+                stk.MoleculeRecord(
+                    topology_graph=stk.polymer.Linear(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
+                        repeating_unit='A',
+                        num_repeating_units=2,
                     ),
-                    repeating_unit='A',
-                    num_repeating_units=2,
-                ),
-            ).with_fitness_value(1),
-            stk.MoleculeRecord(
-                topology_graph=stk.polymer.Linear(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCBr',
-                            functional_groups=[stk.BromoFactory()],
+                ).with_fitness_value(1),
+                stk.MoleculeRecord(
+                    topology_graph=stk.polymer.Linear(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
+                        repeating_unit='A',
+                        num_repeating_units=2,
                     ),
-                    repeating_unit='A',
-                    num_repeating_units=2,
-                ),
-            ).with_fitness_value(2)
-        )
+                ).with_fitness_value(2)
+            )
 
-        # Select the molecules.
-        for selected, in best.select(population):
-            # Do stuff with each selected molecule.
-            pass
+            # Select the molecules.
+            for selected, in best.select(population):
+                # Do stuff with each selected molecule.
+                pass
 
-    *Yielding Batches Holding Multiple Molecules*
+        *Yielding Batches Holding Multiple Molecules*
 
-    Yielding multiple molecules at once. For example, if molecules need
-    to be selected for crossover.
+        Yielding multiple molecules at once. For example, if molecules need
+        to be selected for crossover.
 
-    .. testcode:: yielding-batches-holding-multiple-molecules
+        .. testcode:: yielding-batches-holding-multiple-molecules
 
-        import stk
+            import stk
 
-        # Make the selector.
-        best = stk.Best(batch_size=2)
+            # Make the selector.
+            best = stk.Best(batch_size=2)
 
-        population = (
-            stk.MoleculeRecord(
-                topology_graph=stk.polymer.Linear(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCBr',
-                            functional_groups=[stk.BromoFactory()],
+            population = (
+                stk.MoleculeRecord(
+                    topology_graph=stk.polymer.Linear(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
+                        repeating_unit='A',
+                        num_repeating_units=2,
                     ),
-                    repeating_unit='A',
-                    num_repeating_units=2,
-                ),
-            ).with_fitness_value(1),
-            stk.MoleculeRecord(
-                topology_graph=stk.polymer.Linear(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCBr',
-                            functional_groups=[stk.BromoFactory()],
+                ).with_fitness_value(1),
+                stk.MoleculeRecord(
+                    topology_graph=stk.polymer.Linear(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
+                        repeating_unit='A',
+                        num_repeating_units=2,
                     ),
-                    repeating_unit='A',
-                    num_repeating_units=2,
-                ),
-            ).with_fitness_value(2)
-        )
+                ).with_fitness_value(2)
+            )
 
-        # Select the molecules.
-        for selected1, selected2 in best.select(population):
-            # Do stuff with the selected molecules.
-            pass
-
+            # Select the molecules.
+            for selected1, selected2 in best.select(population):
+                # Do stuff with the selected molecules.
+                pass
     """
 
     def __init__(
         self,
-        num_batches=None,
-        batch_size=1,
-        duplicate_molecules=True,
-        duplicate_batches=True,
-        key_maker=Inchi(),
-        fitness_modifier=None,
-    ):
+        num_batches: int | None = None,
+        batch_size: int = 1,
+        duplicate_molecules: bool = True,
+        duplicate_batches: bool = True,
+        key_maker: MoleculeKeyMaker = Inchi(),
+        fitness_modifier: Callable[[Sequence[T]], dict[T, float]]
+        | None = None,
+    ) -> None:
         """
-        Initialize a :class:`.Best` instance.
+        Parameters:
+            num_batches:
+                The number of batches to yield. If ``None`` then yielding
+                will continue forever or until the generator is exhausted,
+                whichever comes first.
 
-        Parameters
-        ----------
-        num_batches : :class:`int`, optional
-            The number of batches to yield. If ``None`` then yielding
-            will continue forever or until the generator is exhausted,
-            whichever comes first.
+            batch_size:
+                The number of molecule records yielded at once.
 
-        batch_size : :class:`int`, optional
-            The number of molecule records yielded at once.
+            duplicate_molecules:
+                If ``True`` the same molecule can be yielded in more than
+                one batch.
 
-        duplicate_molecules : :class:`bool`, optional
-            If ``True`` the same molecule can be yielded in more than
-            one batch.
+            duplicate_batches:
+                If ``True`` the same batch can be yielded more than once.
+                Duplicate batches can occur if the same molecule is found
+                multiple times in a population.
 
-        duplicate_batches : :class:`bool`, optional
-            If ``True`` the same batch can be yielded more than once.
-            Duplicate batches can occur if the same molecule is found
-            multiple times in a population.
+            key_maker:
+                Used to get the keys of molecules. If two molecules have
+                the same key, they are considered duplicates.
 
-        key_maker : :class:`.MoleculeKeyMaker`, optional
-            Used to get the keys of molecules. If two molecules have
-            the same key, they are considered duplicates.
-
-        fitness_modifier : :class:`callable`, optional
-            Takes the `population` on which :meth:`~.Selector.select`
-            is called and returns a :class:`dict`, which maps records
-            in the `population` to the fitness values the
-            :class:`.Selector` should use. If ``None``, the regular
-            fitness values of the records are used.
-
+            fitness_modifier:
+                Takes the `population` on which :meth:`~.Selector.select`
+                is called and returns a :class:`dict`, which maps records
+                in the `population` to the fitness values the
+                :class:`.Selector` should use. If ``None``, the regular
+                fitness values of the records are used.
         """
-
         if fitness_modifier is None:
             fitness_modifier = self._get_fitness_values
+
+        super().__init__(key_maker, fitness_modifier, batch_size)
 
         self._duplicate_molecules = duplicate_molecules
         self._duplicate_batches = duplicate_batches
         self._num_batches = num_batches
         self._batch_size = batch_size
-        super().__init__(
-            key_maker=key_maker,
-            fitness_modifier=fitness_modifier,
-        )
 
     def _select_from_batches(self, batches, yielded_batches):
         batches = sorted(batches, reverse=True)
@@ -178,4 +169,4 @@ class Best(Selector):
                 batches,
             )
 
-        yield from it.islice(batches, self._num_batches)
+        yield from itertools.islice(batches, self._num_batches)
--- src/stk/_internal/ea/selection/selectors/worst.py
+++ src/stk/_internal/ea/selection/selectors/worst.py
@@ -1,108 +1,100 @@
-"""
-Worst
-=====
-
-"""
-
-import itertools as it
+import itertools
+import typing
+from collections.abc import Callable, Sequence
 
 from stk._internal.key_makers.inchi import Inchi
+from stk._internal.key_makers.molecule import MoleculeKeyMaker
 
 from .selector import Selector
 
+T = typing.TypeVar("T")
 
-class Worst(Selector):
+
+class Worst(Selector[T]):
     """
     Selects batches of molecules, lowest fitness value first.
 
-    Examples
-    --------
-    *Yielding Batches Holding Multiple Molecules*
+    Examples:
 
-    Select the worst 5 batches of size 3
+        *Yielding Batches Holding Multiple Molecules*
 
-    .. testcode:: yielding-batches-holding-multiple-molecules
+        Select the worst 5 batches of size 3
 
-        import stk
+        .. testcode:: yielding-batches-holding-multiple-molecules
 
-        worst = stk.Worst(num_batches=5, batch_size=3)
-        population = tuple(
-            stk.MoleculeRecord(
-                topology_graph=stk.polymer.Linear(
-                    building_blocks=(
-                        stk.BuildingBlock(
-                            smiles='BrCCBr',
-                            functional_groups=[stk.BromoFactory()],
+            import stk
+
+            worst = stk.Worst(num_batches=5, batch_size=3)
+            population = tuple(
+                stk.MoleculeRecord(
+                    topology_graph=stk.polymer.Linear(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles='BrCCBr',
+                                functional_groups=[stk.BromoFactory()],
+                            ),
                         ),
+                        repeating_unit='A',
+                        num_repeating_units=2,
                     ),
-                    repeating_unit='A',
-                    num_repeating_units=2,
-                ),
-            ).with_fitness_value(i)
-            for i in range(10)
-        )
-        for batch in worst.select(population):
-            # Do stuff with batch.
-            pass
+                ).with_fitness_value(i)
+                for i in range(10)
+            )
+            for batch in worst.select(population):
+                # Do stuff with batch.
+                pass
 
     """
 
     def __init__(
         self,
-        num_batches=None,
-        batch_size=1,
-        duplicate_molecules=True,
-        duplicate_batches=True,
-        key_maker=Inchi(),
-        fitness_modifier=None,
-    ):
+        num_batches: int | None = None,
+        batch_size: int = 1,
+        duplicate_molecules: bool = True,
+        duplicate_batches: bool = True,
+        key_maker: MoleculeKeyMaker = Inchi(),
+        fitness_modifier: Callable[[Sequence[T]], dict[T, float]]
+        | None = None,
+    ) -> None:
         """
-        Initialize a :class:`.Worst` instance.
+        Parameters:
+            num_batches:
+                The number of batches to yield. If ``None`` then yielding
+                will continue forever or until the generator is exhausted,
+                whichever comes first.
 
-        Parameters
-        ----------
-        num_batches : :class:`int`, optional
-            The number of batches to yield. If ``None`` then yielding
-            will continue forever or until the generator is exhausted,
-            whichever comes first.
+            batch_size:
+                The number of molecules yielded at once.
 
-        batch_size : :class:`int`, optional
-            The number of molecules yielded at once.
+            duplicate_molecules:
+                If ``True`` the same molecule can be yielded in more than
+                one batch.
 
-        duplicate_molecules : :class:`bool`, optional
-            If ``True`` the same molecule can be yielded in more than
-            one batch.
+            duplicate_batches:
+                If ``True`` the same batch can be yielded more than once.
+                Duplicate batches can occur if the same molecule is found
+                multiple times in a population.
 
-        duplicate_batches : :class:`bool`, optional
-            If ``True`` the same batch can be yielded more than once.
-            Duplicate batches can occur if the same molecule is found
-            multiple times in a population.
+            key_maker:
+                Used to get the keys of molecules, which are used to
+                determine if two molecules are duplicates of each
+                other.
 
-        key_maker : :class:`.MoleculeKeyMaker`, optional
-            Used to get the keys of molecules, which are used to
-            determine if two molecules are duplicates of each
-            other.
-
-        fitness_modifier : :class:`callable`, optional
-            Takes the `population` on which :meth:`~.Selector.select`
-            is called and returns a :class:`dict`, which maps records
-            in the `population` to the fitness values the
-            :class:`.Selector` should use. If ``None``, the regular
-            fitness values of the records are used.
-
+            fitness_modifier:
+                Takes the `population` on which :meth:`~.Selector.select`
+                is called and returns a :class:`dict`, which maps records
+                in the `population` to the fitness values the
+                :class:`.Selector` should use. If ``None``, the regular
+                fitness values of the records are used.
         """
-
         if fitness_modifier is None:
             fitness_modifier = self._get_fitness_values
+
+        super().__init__(key_maker, fitness_modifier, batch_size)
 
         self._duplicate_molecules = duplicate_molecules
         self._duplicate_batches = duplicate_batches
         self._num_batches = num_batches
-        self._batch_size = batch_size
-        super().__init__(
-            key_maker=key_maker,
-            fitness_modifier=fitness_modifier,
-        )
 
     def _select_from_batches(self, batches, yielded_batches):
         batches = sorted(batches)
@@ -119,4 +111,4 @@ class Worst(Selector):
                 batches,
             )
 
-        yield from it.islice(batches, self._num_batches)
+        yield from itertools.islice(batches, self._num_batches)
