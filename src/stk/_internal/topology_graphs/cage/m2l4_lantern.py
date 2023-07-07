686c3b6b27b68a548168c6655c530df8d8c1e43d
Andrew Tarzia <andrew.tarzia@gmail.com>
2023-07-07 21:28:41 +0200

    Fix.

--- src/stk/_internal/topology_graphs/cage/m2l4_lantern.py
+++ src/stk/_internal/topology_graphs/cage/m2l4_lantern.py
@@ -111,7 +111,9 @@ class M2L4Lantern(Cage):
         rgb2 = [97, 201, 217]
         points = cage.get_num_atoms()
         colour_list = [
-            [(rgb1[0]-rgb2[0])*i, rgb1[1], rgb1[2]]
+            rgb1[0] + ((rgb2[0]-rgb1[0])/points)*i,
+            rgb1[1] + ((rgb2[1]-rgb1[1])/points)*i,
+            rgb1[2] + ((rgb2[2]-rgb1[2])/points)*i,
             for i in range(points)
         ]
 
