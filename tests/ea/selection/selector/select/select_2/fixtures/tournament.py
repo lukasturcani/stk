6bd70f7b57f9f7214146af35cd5a34b26ac65248
Lukas Turcani <lukasturcani93@gmail.com>
2023-07-14 12:47:22 +0100

    fix styff

--- tests/ea/selection/selector/select/select_2/fixtures/tournament.py
+++ tests/ea/selection/selector/select/select_2/fixtures/tournament.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+from typing import Any
 
 import pytest
 import stk
@@ -6,188 +6,73 @@ import stk
 from ..case_data import CaseData
 
 
-def get_topology_graph(num_repeating_units):
+def get_topology_graph(num_repeating_units: int) -> stk.TopologyGraph:
     return stk.polymer.Linear(
-        building_blocks=(stk.BuildingBlock("BrCCBr", [stk.BromoFactory()]),),
+        building_blocks=[
+            stk.BuildingBlock("BrCCBr", stk.BromoFactory()),
+        ],
         repeating_unit="A",
         num_repeating_units=num_repeating_units,
     )
 
 
 @pytest.fixture(scope="session")
-def tournament_population_1() -> tuple[stk.MolecularRecord, ...]:
-    return (
+def tournament_population_1() -> dict[stk.MoleculeRecord[Any], float]:
+    return {
         stk.MoleculeRecord(
             topology_graph=get_topology_graph(2),
-        ).with_fitness_value(10),
+        ): 10,
         stk.MoleculeRecord(
             topology_graph=get_topology_graph(3),
-        ).with_fitness_value(9),
+        ): 9,
         stk.MoleculeRecord(
             topology_graph=get_topology_graph(4),
-        ).with_fitness_value(2),
+        ): 2,
         stk.MoleculeRecord(
             topology_graph=get_topology_graph(5),
-        ).with_fitness_value(1),
+        ): 1,
         stk.MoleculeRecord(
             topology_graph=get_topology_graph(6),
-        ).with_fitness_value(0),
-    )
+        ): 0,
+    }
 
 
 @pytest.fixture(
     scope="session",
     params=(
-        lambda population: CaseData(
+        lambda population: CaseData.new(
             selector=stk.Tournament(
                 duplicate_molecules=False,
             ),
             population=population,
-            selected=(
-                stk.Batch(
-                    records=(population[0],),
-                    fitness_values={population[0]: 10},
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[1],),
-                    fitness_values={population[1]: 9},
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[2],),
-                    fitness_values={population[2]: 2},
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[3],),
-                    fitness_values={population[3]: 1},
-                    key_maker=stk.Inchi(),
-                ),
-            ),
+            selected=([0], [1], [2], [3]),
         ),
-        lambda population: CaseData(
+        lambda population: CaseData.new(
             selector=stk.Tournament(
                 duplicate_batches=False,
             ),
             population=population,
-            selected=(
-                stk.Batch(
-                    records=(population[0],),
-                    fitness_values={population[0]: 10},
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[1],),
-                    fitness_values={population[1]: 9},
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[2],),
-                    fitness_values={population[2]: 2},
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[3],),
-                    fitness_values={population[3]: 1},
-                    key_maker=stk.Inchi(),
-                ),
-            ),
+            selected=([0], [1], [2], [3]),
         ),
-        lambda population: CaseData(
+        lambda population: CaseData.new(
             selector=stk.Tournament(batch_size=2, duplicate_batches=False),
             population=population,
             selected=(
-                stk.Batch(
-                    records=(population[0], population[1]),
-                    fitness_values={
-                        population[0]: 10,
-                        population[1]: 9,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(
-                        population[0],
-                        population[2],
-                    ),
-                    fitness_values={
-                        population[0]: 10,
-                        population[2]: 2,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[0], population[3]),
-                    fitness_values={
-                        population[0]: 10,
-                        population[3]: 1,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[0], population[4]),
-                    fitness_values={
-                        population[0]: 10,
-                        population[4]: 0,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(
-                        population[1],
-                        population[2],
-                    ),
-                    fitness_values={
-                        population[1]: 9,
-                        population[2]: 2,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[1], population[3]),
-                    fitness_values={
-                        population[1]: 9,
-                        population[3]: 1,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(population[1], population[4]),
-                    fitness_values={
-                        population[1]: 9,
-                        population[4]: 0,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(
-                        population[2],
-                        population[3],
-                    ),
-                    fitness_values={
-                        population[2]: 2,
-                        population[3]: 1,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
-                stk.Batch(
-                    records=(
-                        population[2],
-                        population[4],
-                    ),
-                    fitness_values={
-                        population[2]: 2,
-                        population[4]: 0,
-                    },
-                    key_maker=stk.Inchi(),
-                ),
+                [0, 1],
+                [0, 2],
+                [0, 3],
+                [0, 4],
+                [1, 2],
+                [1, 3],
+                [1, 4],
+                [2, 3],
+                [2, 4],
             ),
         ),
     ),
 )
 def tournament(
-    request,
-    tournament_population_1: tuple[stk.MolecularRecord, ...],
+    request: pytest.FixtureRequest,
+    tournament_population_1: dict[stk.MoleculeRecord[Any], float],
 ) -> CaseData:
     return request.param(tournament_population_1)
