8f2841085c2bee00895b32e66b26c2e60fb8d577
andrewtarzia <andrew.tarzia@gmail.com>
2023-07-23 17:08:11 +0200

    Update alignment.
    Signed-off-by: andrewtarzia <andrew.tarzia@gmail.com>

--- src/stk/_internal/topology_graphs/polymer/ncore/vertices.py
+++ src/stk/_internal/topology_graphs/polymer/ncore/vertices.py
@@ -1,12 +1,14 @@
 """
-Linear Polymer Vertices
-=======================
+NCore Vertices
+==============
 
 """
 
 import logging
+import numpy as np
 
 from stk._internal.topology_graphs.vertex import Vertex
+from stk._internal.utilities.utilities import get_acute_vector
 from ...utilities import _EdgeSorter, _FunctionalGroupSorter
 
 logger = logging.getLogger(__name__)
@@ -19,6 +21,8 @@ class CoreVertex(Vertex):
     """
 
     def place_building_block(self, building_block, edges):
+        # Sets building block in XY plane, then aligns 0th functional
+        # group with the 0th edge.
         assert building_block.get_num_functional_groups() > 2, (
             f"{building_block} needs to have more than 2 functional "
             "groups but has "
@@ -28,46 +32,57 @@ class CoreVertex(Vertex):
             position=self._position,
             atom_ids=building_block.get_placer_ids(),
         )
-        return building_block.get_position_matrix()
-        fg1, fg2 = building_block.get_functional_groups()
-        fg1_position = building_block.get_centroid(
-            atom_ids=fg1.get_placer_ids(),
+        core_centroid = building_block.get_centroid(
+            atom_ids=building_block.get_core_atom_ids(),
         )
-        fg2_position = building_block.get_centroid(
-            atom_ids=fg2.get_placer_ids(),
+        normal = building_block.get_plane_normal(
+            atom_ids=building_block.get_placer_ids(),
         )
-        return building_block.with_rotation_between_vectors(
-            start=fg2_position - fg1_position,
-            target=[-1 if self._flip else 1, 0, 0],
+        normal = get_acute_vector(
+            reference=core_centroid - self._position,
+            vector=normal,
+        )
+        building_block = building_block.with_rotation_between_vectors(
+            start=normal,
+            target=[0, 0, 1],
+            origin=self._position,
+        )
+        (fg,) = building_block.get_functional_groups(0)
+        fg_centroid = building_block.get_centroid(fg.get_placer_ids())
+        edge_position = edges[0].get_position()
+        return building_block.with_rotation_to_minimize_angle(
+            start=fg_centroid - self._position,
+            target=edge_position - self._position,
+            axis=np.array([0, 0, 1], dtype=np.float64),
             origin=self._position,
         ).get_position_matrix()
 
-    def map_functional_groups_to_edges(self, building_block, edges):
-        return {fg_id: edge.get_id() for fg_id, edge in enumerate(edges)}
-
     # def map_functional_groups_to_edges(self, building_block, edges):
-    #     # The idea is to order the functional groups in building_block
-    #     # by their angle with the vector running from the placer
-    #     # centroid to fg0, going in the clockwise direction.
-    #     # The edges are also ordered by their angle with the vector
-    #     # running from the edge centroid to the aligner_edge,
-    #     # going in the clockwise direction.
-    #     #
-    #     # Once the fgs and edges are ordered, zip and assign them.
+    #     return {fg_id: edge.get_id() for fg_id, edge in enumerate(edges)}
 
-    #     fg_sorter = _FunctionalGroupSorter(building_block)
-    #     edge_sorter = _EdgeSorter(
-    #         edges=edges,
-    #         aligner_edge=edges[self._aligner_edge],
-    #         axis=fg_sorter.get_axis(),
-    #     )
-    #     return {
-    #         fg_id: edge.get_id()
-    #         for fg_id, edge in zip(
-    #             fg_sorter.get_items(),
-    #             edge_sorter.get_items(),
-    #         )
-    #     }
+    def map_functional_groups_to_edges(self, building_block, edges):
+        # The idea is to order the functional groups in building_block
+        # by their angle with the vector running from the placer
+        # centroid to fg0, going in the clockwise direction.
+        # The edges are also ordered by their angle with the vector
+        # running from the edge centroid to the aligner_edge,
+        # going in the clockwise direction.
+        #
+        # Once the fgs and edges are ordered, zip and assign them.
+
+        fg_sorter = _FunctionalGroupSorter(building_block)
+        edge_sorter = _EdgeSorter(
+            edges=edges,
+            aligner_edge=edges[0],
+            axis=fg_sorter.get_axis(),
+        )
+        return {
+            fg_id: edge.get_id()
+            for fg_id, edge in zip(
+                fg_sorter.get_items(),
+                edge_sorter.get_items(),
+            )
+        }
 
 
 class TerminalVertex(Vertex):
@@ -98,7 +113,6 @@ class TerminalVertex(Vertex):
         edge_centroid = sum(edge.get_position() for edge in edges) / len(edges)
         return building_block.with_rotation_between_vectors(
             start=(fg_centroid - core_centroid),
-            # _cap_direction is defined by a subclass.
             target=edge_centroid - self._position,
             origin=self._position,
         ).get_position_matrix()
