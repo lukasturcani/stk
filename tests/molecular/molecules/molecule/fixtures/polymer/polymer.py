39a0435406d82ab2fd888ab1634226a0ff082fcd
andrewtarzia <andrew.tarzia@gmail.com>
2023-07-23 20:42:44 +0200

    Write topology graph tests.
    Signed-off-by: andrewtarzia <andrew.tarzia@gmail.com>

--- tests/molecular/molecules/molecule/fixtures/polymer/polymer.py
+++ tests/molecular/molecules/molecule/fixtures/polymer/polymer.py
@@ -3,10 +3,14 @@ from pytest_lazyfixture import lazy_fixture
 
 # Fixtures need to be visible for lazy_fixture() calls.
 from .linear import *  # noqa
+from .ncore import *  # noqa
 
 
 @pytest.fixture(
-    params=(lazy_fixture("polymer_linear"),),
+    params=(
+        lazy_fixture("polymer_linear"),
+        lazy_fixture("polymer_ncore"),
+    ),
 )
 def polymer(request):
     return request.param
